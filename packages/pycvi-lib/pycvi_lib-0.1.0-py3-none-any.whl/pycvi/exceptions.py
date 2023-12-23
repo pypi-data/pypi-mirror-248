34a174b6f8412af4f031a48e4f90184d27b2c459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:57 +0000

    Add package pycvi-lib/0.1.0/pycvi_lib-0.1.0-py3-none-any.whl

---
+++ packages/pycvi-lib/pycvi_lib-0.1.0-py3-none-any.whl/pycvi/exceptions.py
@@ -0,0 +1,41 @@
+"""
+PyCVI exceptions.
+"""
+
+class EmptyClusterError(ValueError):
+    """
+    The clustering algorithm didn't converge.
+
+    As a result of not converging, some clusters are empty resulting
+    in the absence of clustering.
+    """
+    pass
+
+class ShapeError(ValueError):
+    """
+    The given data doesn't have the right shape.
+
+    The acceptable input shapes are: `(N,)` or `(N, d)` or `(N, T, d)`.
+    """
+    pass
+
+class InvalidScoreError(ValueError):
+    """
+    The score given is not among the implemented scores.
+
+    Either because its reduction was not implemented or because its main
+    function was not implemented.
+    """
+    pass
+
+class ScoreError(ValueError):
+    """
+    The score given is invalid, probably because it is `None`.
+    """
+    pass
+
+class InvalidKError(ValueError):
+    """
+    The CVI was called with an incompatible number of clusters.
+    """
+    pass
