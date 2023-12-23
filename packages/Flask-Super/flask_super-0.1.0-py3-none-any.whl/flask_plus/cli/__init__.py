858e0e394410a2f990ee4034d9d8879c91c85249
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:48 +0000

    Add package Flask-Super/0.1.0/flask_super-0.1.0-py3-none-any.whl

---
+++ packages/Flask-Super/flask_super-0.1.0-py3-none-any.whl/flask_plus/cli/__init__.py
@@ -0,0 +1,36 @@
+import click
+
+from flask_plus.registry import lookup, register
+
+__all__ = ["register_commands", "command", "group"]
+
+from flask_plus.scanner import scan_package
+
+
+def command(*args, **kwargs):
+    def decorator(func):
+        module = func.__module__
+        name = func.__name__
+        cmd = click.command(*args, **kwargs)(func)
+        register(cmd, name=name, module=module, tag="cli")
+        return cmd
+
+    return decorator
+
+
+def group(*args, **kwargs):
+    def decorator(func):
+        module = func.__module__
+        name = func.__name__
+        cmd = click.group(*args, **kwargs)(func)
+        register(cmd, name=name, module=module, tags="cli")
+        return cmd
+
+    return decorator
+
+
+def register_commands(app):
+    scan_package("flask_plus.cli.commands")
+
+    for obj in lookup(click.Command):
+        app.cli.add_command(obj)
