a0fe8b94d3c5a40bfdbe642eb28fb178496e7db4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:36 +0000

    Add package Bigsansar/1.9.1/Bigsansar-1.9.1-py3-none-any.whl

---
+++ packages/Bigsansar/Bigsansar-1.9.1-py3-none-any.whl/bigsansar/core/__init__.py
@@ -0,0 +1,29 @@
+import argparse
+from bigsansar.core.init import deploy, initsetup
+
+
+class root:
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('init', required=True, help='Create a django server and related files ')
+        self.parser.add_argument('server', required=False, help=' this is a base in to linux ubuntu. careate a server in to deploy side . ')
+
+        self.args = self.parser.parse_args()
+
+    def execute(self):
+
+        if self.args.init == 'init':
+
+            return initsetup()
+        
+        elif self.args.init.server == 'server':
+            return deploy()
+    
+        else:
+            print('usage: bigsansar --help')
+            exit()
+
+def main():
+   cli = root()
+   cli.execute()
