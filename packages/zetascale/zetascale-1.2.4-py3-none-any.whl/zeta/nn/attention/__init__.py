6505c332c18bb74dd732d43c219cd58260d38f3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:36 +0000

    Add package zetascale/1.2.4/zetascale-1.2.4-py3-none-any.whl

---
+++ packages/zetascale/zetascale-1.2.4-py3-none-any.whl/zeta/nn/attention/__init__.py
@@ -0,0 +1,41 @@
+"""Zeta Halo"""
+
+# attentions
+from zeta.nn.attention.attend import Attend, Intermediates
+from zeta.nn.attention.cross_attn_images import MultiModalCrossAttention
+from zeta.nn.attention.flash_attention import FlashAttention
+
+# from zeta.nn.attention.flash_attention2 import FlashAttentionTwo
+from zeta.nn.attention.local_attention import LocalAttention
+from zeta.nn.attention.local_attention_mha import LocalMHA
+
+# from zeta.nn.attention.mgqa import MGQA
+# from zeta.nn.attention.spatial_linear_attention import SpatialLinearAttention
+from zeta.nn.attention.mixture_attention import (
+    MixtureOfAttention,
+    MixtureOfAutoregressiveAttention,
+)
+from zeta.nn.attention.multi_modal_causal_attention import (
+    MultiModalCausalAttention,
+    SimpleMMCA,
+)
+from zeta.nn.attention.multihead_attention import MultiheadAttention
+from zeta.nn.attention.multiquery_attention import MultiQueryAttention
+from zeta.nn.attention.sparse_attention import SparseAttention
+
+__all__ = [
+    "Attend",
+    "FlashAttention",
+    # "FlashAttentionTwo",
+    "LocalAttention",
+    "LocalMHA",
+    "Intermediates",
+    "MixtureOfAttention",
+    "MixtureOfAutoregressiveAttention",
+    "MultiModalCausalAttention",
+    "SimpleMMCA",
+    "MultiheadAttention",
+    "MultiQueryAttention",
+    "MultiModalCrossAttention",
+    "SparseAttention",
+]
