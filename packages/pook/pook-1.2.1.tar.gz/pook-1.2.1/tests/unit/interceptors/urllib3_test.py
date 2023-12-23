e9f4ac2b50c8391f30c7c65d3ec4871965f28b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:45 +0000

    Add package pook/1.2.1/pook-1.2.1.tar.gz

---
+++ packages/pook/pook-1.2.1.tar.gz/pook-1.2.1/tests/unit/interceptors/urllib3_test.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+import urllib3
+import pook
+import pytest
+import sys
+
+from pathlib import Path
+
+
+binary_file = (
+    Path(__file__).parents[1] / "fixtures" / "nothing.bin"
+).read_bytes()
+
+
+URL = 'https://httpbin.org/foo'
+
+
+@pook.on
+def assert_chunked_response(input_data, expected):
+    (pook.get(URL)
+        .reply(204)
+        .body(input_data, chunked=True))
+
+    http = urllib3.PoolManager()
+    r = http.request('GET', URL)
+
+    assert r.status == 204
+
+    chunks = list(r.read_chunked())
+    chunks = [c.decode() if isinstance(c, bytes) else c for c in chunks]
+    assert chunks == expected
+
+
+def test_chunked_response_list():
+    assert_chunked_response(['a', 'b', 'c'], ['a', 'b', 'c'])
+
+
+def test_chunked_response_str():
+    assert_chunked_response('text', ['text'])
+
+
+def test_chunked_response_byte():
+    assert_chunked_response(b'byteman', ['byteman'])
+
+
+def test_chunked_response_empty():
+    assert_chunked_response('', [])
+
+
+def test_chunked_response_contains_newline():
+    assert_chunked_response('newline\r\n', ['newline\r\n'])
+
+
+def test_activate_disable():
+    original = urllib3.connectionpool.HTTPConnectionPool.urlopen
+
+    interceptor = pook.interceptors.Urllib3Interceptor(pook.MockEngine)
+    interceptor.activate()
+    interceptor.disable()
+
+    assert urllib3.connectionpool.HTTPConnectionPool.urlopen == original
+
+
+@pook.on
+def test_binary_body():
+    (pook.get(URL)
+        .reply(200)
+        .body(binary_file, binary=True))
+
+    http = urllib3.PoolManager()
+    r = http.request('GET', URL)
+
+    assert r.read() == binary_file
+
+
+@pook.on
+def test_binary_body_chunked():
+    (pook.get(URL)
+        .reply(200)
+        .body(binary_file, binary=True, chunked=True))
+
+    http = urllib3.PoolManager()
+    r = http.request('GET', URL)
+
+    assert list(r.read_chunked()) == [binary_file]
+
+
+@pytest.mark.xfail(
+    condition=sys.version_info < (3, 7),
+    reason=(
+        "urllib3 converts https to http with "
+        "port 443 and fails on header parsing"
+    )
+)
+def test_post_with_headers(pook_on):
+    mock = pook.post(URL).header('k', 'v').reply(200).mock
+    http = urllib3.PoolManager(headers={'k': 'v'})
+    resp = http.request('POST', URL)
+    assert resp.status == 200
+    assert len(mock.matches) == 1
---
+++ packages/pook/pook-1.2.1.tar.gz/pook-1.2.1/tests/unit/mock_test.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+from pook.mock import Mock
+from pook.request import Request
+
+
+@pytest.fixture
+def mock():
+    return Mock()
+
+
+def matcher(mock):
+    return mock.matchers[0]
+
+
+def test_mock_url(mock):
+    mock.url("http://google.es")
+    assert str(matcher(mock)) == "http://google.es"
+
+
+@pytest.mark.parametrize(
+    "url, params, req, expected",
+    [
+        ("http://google.es", {}, Request(url="http://google.es"), (True, [])),
+        (
+            "http://google.es",
+            {},
+            Request(url="http://google.es?foo=bar"),
+            (True, []),
+        ),
+        (
+            "http://google.es",
+            {},
+            Request(url="http://google.es?foo=bar&baz=qux"),
+            (True, []),
+        ),
+        (
+            "http://google.es",
+            {},
+            Request(url="http://google.es?baz=qux"),
+            (True, []),
+        ),
+        (
+            "http://google.es",
+            {"foo": "bar"},
+            Request(url="http://google.es"),
+            (False, []),
+        ),
+        (
+            "http://google.es",
+            {"foo": "bar"},
+            Request(url="http://google.es?foo=bar"),
+            (True, []),
+        ),
+        (
+            "http://google.es",
+            {"foo": "bar"},
+            Request(url="http://google.es?foo=bar&baz=qux"),
+            (True, []),
+        ),
+        (
+            "http://google.es",
+            {"foo": "bar"},
+            Request(url="http://google.es?baz=qux"),
+            (False, []),
+        ),
+    ],
+)
+def test_mock_params(url, params, req, expected, mock):
+    mock.url(url)
+    if params:
+        mock.params(params)
+    assert mock.matchers.match(req) == expected
+
+
+def test_new_response(mock):
+    assert mock.reply() != mock.reply(new_response=True, json={})
