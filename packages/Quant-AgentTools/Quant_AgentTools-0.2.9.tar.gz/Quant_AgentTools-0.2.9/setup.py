dd8e744af767c4f6b02ab36d101fbb4c7df44743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:07 +0000

    Add package Quant-AgentTools/0.2.9/Quant_AgentTools-0.2.9.tar.gz

---
+++ packages/Quant-AgentTools/Quant_AgentTools-0.2.9.tar.gz/Quant_AgentTools-0.2.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setup(
+    name="Quant_AgentTools",
+    version="0.2.9",
+    description="Agentic Workflow with Quantized LLMs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["pydantic", "GPT4All"],
+)
