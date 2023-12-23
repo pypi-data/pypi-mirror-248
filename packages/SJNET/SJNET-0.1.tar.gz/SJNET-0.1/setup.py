de103417588fe5b571e0a1d2680ecb134df55292
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:55 +0000

    Add package SJNET/0.1/SJNET-0.1.tar.gz

---
+++ packages/SJNET/SJNET-0.1.tar.gz/SJNET-0.1/setup.py
@@ -0,0 +1,15 @@
+import pathlib
+
+import setuptools
+import pathlib
+import setuptools
+
+setuptools.setup(
+    name="SJNET",
+    version="0.1",
+    description="A neural network Framework",
+    author="Abhijith SJ",
+    author_email="sjabhijith187@gmail.com",
+    classifiers=["Topic :: Scientific/Engineering :: Artificial Intelligence","Development Status :: 5 - Production/Stable","Programming Language :: Python :: 3.10","Programming Language :: Python :: 3.11"],
+
+    )
