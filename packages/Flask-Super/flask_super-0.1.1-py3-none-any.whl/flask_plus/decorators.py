6e0420b2313f6ed67d7fd3d41269fc6c2e47161b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:48 +0000

    Add package Flask-Super/0.1.1/flask_super-0.1.1-py3-none-any.whl

---
+++ packages/Flask-Super/flask_super-0.1.1-py3-none-any.whl/flask_plus/decorators.py
@@ -0,0 +1,12 @@
+from .cli import command, group
+from .registry import register
+
+__all__ = ["register", "command", "group", "service"]
+
+
+def service(obj):
+    return register(obj, tag="service")
+
+
+def singleton(obj):
+    return register(obj, tag="singleton")
