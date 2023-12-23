55d44c22487a32b39fd41f69f900e6d332ad6e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:51 +0000

    Add package scipy-pilutil/0.2.1/scipy_pilutil-0.2.1-py3-none-any.whl

---
+++ packages/scipy-pilutil/scipy_pilutil-0.2.1-py3-none-any.whl/scipy_pilutil/__init__.py
@@ -0,0 +1,11 @@
+from .pilutil import *
+from . import pilutil
+
+__copyright__    = 'Copyright (C) 2023 kairi'
+__version__      = '0.2.1'
+__license__      = 'BSD-3-Clause'
+__author__       = 'kairi'
+__author_email__ = 'kairi.satellite@gmail.com'
+__url__          = 'http://github.com/kairi003/scipy_pilutil'
+
+__all__ = pilutil.__all__
