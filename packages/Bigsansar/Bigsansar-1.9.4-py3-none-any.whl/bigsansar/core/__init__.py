9a4ac2f74e1f13be343d0592981a14bfc93c0f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-23 14:04:52 +0000

    Add package Bigsansar/1.9.4/Bigsansar-1.9.4-py3-none-any.whl

---
+++ packages/Bigsansar/Bigsansar-1.9.4-py3-none-any.whl/bigsansar/core/__init__.py
@@ -0,0 +1,28 @@
+import argparse
+from bigsansar.core.init import deploy, initsetup
+
+
+class root:
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument('init', help='Create a django server and related files ')
+
+        self.args = self.parser.parse_args()
+
+    def execute(self):
+
+        if self.args.init == 'init':
+
+            return initsetup()
+        
+        elif self.args.init == 'init server':
+            return deploy()
+    
+        else:
+            print('usage: bigsansar --help')
+            exit()
+
+def main():
+   cli = root()
+   cli.execute()
