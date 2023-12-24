8291bc15641d27e46e872c046ba8c5ef59de497f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:07 +0000

    Add package edensapdf/1.0/edensapdf-1.0.tar.gz

---
+++ packages/edensapdf/edensapdf-1.0.tar.gz/edensapdf-1.0/edensa/pdf2text.py
@@ -0,0 +1,10 @@
+"""This module provides functions to convert a PDF to a Text"""
+def convertor(path):
+    """Convert the given PDF to text
+
+    Parameters:
+        path (str): The Path to a PDF file.
+    returns:
+        str: The content of the PDF file as text
+    """
+    print("This is my PDF to Text convetor project")
