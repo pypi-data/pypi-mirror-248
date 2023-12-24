698cae9ab7a2177135ef2663265a02a2c99bb5bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:54 +0000

    Add package djangorestframework-util/0.0.1/djangorestframework-util-0.0.1.tar.gz

---
+++ packages/djangorestframework-util/djangorestframework-util-0.0.1.tar.gz/djangorestframework-util-0.0.1/rest_framework_util/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# Email:iamfengdy@126.com
+# DateTime:2023/11/16
+# Tool:PyCharm
+
+"""  """
+__version__ = '0.0.1'
+__history__ = """"""
+__all__ = ['logger']
+
+import logging
+
+logger = logging.getLogger('rest_framework_util')
+__version__ = '0.0.1'
