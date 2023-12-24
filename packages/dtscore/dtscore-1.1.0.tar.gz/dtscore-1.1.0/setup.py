68c7327640d136fff5ae8eb2483b926e24c6cacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:00 +0000

    Add package dtscore/1.1.0/dtscore-1.1.0.tar.gz

---
+++ packages/dtscore/dtscore-1.1.0.tar.gz/dtscore-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='dtscore',
+    version='1.1.0',    
+    description='DTS core',
+    long_description='The constants library for DTS Python routines.',
+    long_description_content_type='text/x-rst',
+    url='',
+    author='D Smith',
+    author_email='',
+    license='BSD 2-clause',
+    packages=['dtscore'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Microsoft :: Windows :: Windows 11',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
