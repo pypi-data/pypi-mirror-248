2d4244deea832142d9e0ca90328e12fab7a2f86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:05:36 +0000

    Add package physicsLab/1.4.0/physicsLab-1.4.0.tar.gz

---
+++ packages/physicsLab/physicsLab-1.4.0.tar.gz/physicsLab-1.4.0/physicsLab/circuit/unionElements/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# 模块化逻辑电路
+from .unionLogic import *
+# 模块化电路引脚连接
+from .wires import *
---
+++ packages/physicsLab/physicsLab-1.4.0.tar.gz/physicsLab-1.4.0/physicsLab/typehint/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import typing
+try:
+    from typing import Self
+except ImportError:
+    from .typing_extensions import Self
+
+from typing import *
