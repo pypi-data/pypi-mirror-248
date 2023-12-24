03a6144bf71348d13d044164777d5997a53c9382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:07 +0000

    Add package pygenetic-code/0.13/pygenetic_code-0.13-py3-none-any.whl

---
+++ packages/pygenetic-code/pygenetic_code-0.13-py3-none-any.whl/pygenetic_code/translations.py
@@ -0,0 +1,30 @@
+"""
+Translation specific methdos
+"""
+
+from .code_to_table import code_to_table, all_possible_codons
+from .dna_and_rna import rna_to_dna
+from .error import InvalidCodonException
+from .genetic_code import three_letters_to_one_letter
+
+
+def translate(codon, translation_table=1, one_letter=False):
+    """
+    Translate a codon given a specific translation table.
+    :param codon: the codon to translate
+    :param translation_table: the translation table to use
+    :param one_letter: return the one letter amino acid abbreviation
+    :return: the amino acid abbreviation
+    """
+
+    if 'U' in codon:
+        codon = rna_to_dna(codon)
+
+    if codon not in all_possible_codons():
+        raise InvalidCodonException(f"{codon} is not a valid codon. It should be one of {all_possible_codons()}")
+
+    code = code_to_table(translation_table)
+    amino_acid = code['codons'][codon]
+    if one_letter:
+        return three_letters_to_one_letter()[amino_acid]
+    return amino_acid
