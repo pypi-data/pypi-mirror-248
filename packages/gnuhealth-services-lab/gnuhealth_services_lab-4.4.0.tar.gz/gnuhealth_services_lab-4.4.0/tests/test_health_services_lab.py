fb80fa6f3d528fb8b742bc19e874042d36d8bffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:46 +0000

    Add package gnuhealth-services-lab/4.4.0/gnuhealth_services_lab-4.4.0.tar.gz

---
+++ packages/gnuhealth-services-lab/gnuhealth_services_lab-4.4.0.tar.gz/gnuhealth_services_lab-4.4.0/tests/test_health_services_lab.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2008-2023 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2023 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class HealthServicesLabTestCase(ModuleTestCase):
+    '''
+    Test Health Services module.
+    '''
+    module = 'health_services_lab'
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        HealthServicesLabTestCase))
+    return suite
