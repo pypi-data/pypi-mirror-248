ec3ec3cc6809d2006ab587db56fb238cc3b29c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:03:28 +0000

    Add package aiotrans/1.2/aiotrans-1.2-py3-none-any.whl

---
+++ packages/aiotrans/aiotrans-1.2-py3-none-any.whl/aiotrans/transport/aiohttp_t.py
@@ -0,0 +1,26 @@
+import html
+
+from .base import Transport, Constructor
+
+try:
+    import aiohttp
+except ImportError:
+    pass
+
+
+class AiohttpTransport(Transport):
+    def __init__(self):
+        self.conn = aiohttp.ClientSession()
+
+    async def send(self, target_lang: str, source_lang: str, text: str) -> str:
+        url = Constructor("translate.google.com") / "m"
+
+        response = await self.conn.get(
+            str(url),
+            params={"tl": target_lang, "sl": source_lang, "q": text},
+        )
+
+        return html.unescape(await response.text(encoding="utf-8"))
+
+    async def close(self):
+        await self.conn.close()
---
+++ packages/aiotrans/aiotrans-1.2-py3-none-any.whl/aiotrans/transport/httpx_t.py
@@ -0,0 +1,26 @@
+import html
+
+from .base import Transport, Constructor
+
+try:
+    import httpx
+except ImportError:
+    pass
+
+
+class HttpxTransport(Transport):
+    def __init__(self):
+        self.conn = httpx.AsyncClient()
+
+    async def send(self, target_lang: str, source_lang: str, text: str) -> str:
+        url = Constructor("translate.google.com") / "m"
+
+        response = await self.conn.get(
+            str(url),
+            params={"tl": target_lang, "sl": source_lang, "q": text},
+        )
+
+        return html.unescape(response.text)
+
+    async def close(self):
+        await self.conn.aclose()
