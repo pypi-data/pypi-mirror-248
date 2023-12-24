cb604fcbff024ee1d899dfeff2c39c3a3a20adb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:05:28 +0000

    Add package automation-utilities/1.5.2/automation_utilities-1.5.2.tar.gz

---
+++ packages/automation-utilities/automation_utilities-1.5.2.tar.gz/automation_utilities-1.5.2/automation_utilities/Intro.py
@@ -0,0 +1,11 @@
+from uuid import getnode
+from art import text2art
+from colorama import Fore
+
+
+def intro(name: str):
+    user_id = getnode()
+    print(f'{Fore.LIGHTYELLOW_EX}{text2art("Hazem3010", "alligator")}')
+    indent = '\t'*15
+    print(f'{Fore.GREEN}{indent}{name}\t{Fore.LIGHTMAGENTA_EX}::\t{Fore.CYAN}Your ID: {user_id}')
+    return user_id
---
+++ packages/automation-utilities/automation_utilities-1.5.2.tar.gz/automation_utilities-1.5.2/setup.py
@@ -0,0 +1,16 @@
+import art
+import bs4
+import names
+import phonenumbers
+import playwright
+import requests
+import setuptools
+from setuptools import setup, find_packages
+
+setup(
+    name='automation_utilities',
+    version='1.5.2',
+    packages=find_packages(),
+)
+
+install_requires = [requests, setuptools, bs4, playwright, names, phonenumbers, art]
