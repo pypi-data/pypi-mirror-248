973d9aee54a77b04c56f8abac505c220d9f5c0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:09 +0000

    Add package staruxian/1.1.1/staruxian-1.1.1.tar.gz

---
+++ packages/staruxian/staruxian-1.1.1.tar.gz/staruxian-1.1.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='staruxian',
+    version='1.1.1',
+    packages=find_packages(),
+    author='Khurshed Ziyovaddinov',
+    author_email='mrx555888@gmail.com',
+    description='Hello I am cosmoboy',
+    install_requires=[
+    ],
+    entry_points={
+        "console_scripts": [
+            "staruxian = staruxian:hello"
+        ],
+    },
+)
