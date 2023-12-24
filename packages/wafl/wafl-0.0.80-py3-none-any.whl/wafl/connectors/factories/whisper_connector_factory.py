6c8924b1ded4659507e70d8d0a595bf678a0ce81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:05:02 +0000

    Add package wafl/0.0.80/wafl-0.0.80-py3-none-any.whl

---
+++ packages/wafl/wafl-0.0.80-py3-none-any.whl/wafl/connectors/factories/whisper_connector_factory.py
@@ -0,0 +1,9 @@
+from wafl.connectors.remote.remote_whisper_connector import RemoteWhisperConnector
+
+
+class WhisperConnectorFactory:
+    @staticmethod
+    def get_connector(config):
+        return RemoteWhisperConnector(
+            config.get_value("listener_model")
+        )
