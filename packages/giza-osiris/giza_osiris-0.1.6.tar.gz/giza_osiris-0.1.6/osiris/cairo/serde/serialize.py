83b4a670b25de983bbf1b4d3ae9bb53437c20c69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:02:54 +0000

    Add package giza-osiris/0.1.6/giza_osiris-0.1.6.tar.gz

---
+++ packages/giza-osiris/giza_osiris-0.1.6.tar.gz/giza_osiris-0.1.6/osiris/cairo/serde/serialize.py
@@ -0,0 +1,35 @@
+from osiris.cairo.serde.data_structures import (
+    FixedPoint,
+    SignedInt,
+    Tensor,
+    UnsignedInt,
+)
+
+
+def serializer(data) -> list[str]:
+    if isinstance(data, bool):
+        return ["1"] if data else ["0"]
+    elif isinstance(data, int):
+        return [str(data)]
+    elif isinstance(data, (list, tuple)):
+        serialized_list = [str(len(data))]
+        for item in data:
+            serialized_list.extend(serializer(item))
+        return serialized_list
+    elif isinstance(data, dict):
+        serialized_dict = [str(len(data))]
+        for key, value in data.items():
+            serialized_dict.extend(serializer(key))
+            serialized_dict.extend(serializer(value))
+        return serialized_dict
+    elif isinstance(data, Tensor):
+        serialized_tensor = serializer(data.shape)
+        serialized_tensor.extend(serializer(data.data))
+        return serialized_tensor
+    elif isinstance(data, (SignedInt, FixedPoint)):
+        return [str(data.mag), str(data.sign)]
+    elif isinstance(data, UnsignedInt):
+        return [str(data.mag)]
+
+    else:
+        raise ValueError("Unsupported data type for serialization")
