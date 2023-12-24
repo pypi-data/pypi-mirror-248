7829558a9bd3a24103a41fa500f6a04b27a7d83f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:03:15 +0000

    Add package jupyprint/0.1.5/jupyprint-0.1.5.tar.gz

---
+++ packages/jupyprint/jupyprint-0.1.5.tar.gz/jupyprint-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name="jupyprint",
+    version="0.1.5",
+    author="Peter Rush",
+    description="A simple python package to print markdown and LaTeX equations from code cells in Jupyter notebooks.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license="MIT",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=[
+    "numpy",
+    "pandas",
+    "ipython"
+    ],
+)
