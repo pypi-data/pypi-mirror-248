5696e5f1073ac04e4119328092fd3e088fad9f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 15:03:40 +0000

    Add package cookiecutter-python/1.11.0/cookiecutter_python-1.11.0-py3-none-any.whl

---
+++ packages/cookiecutter-python/cookiecutter_python-1.11.0-py3-none-any.whl/cookiecutter_python/handle/dialogs/dialog.py
@@ -0,0 +1,18 @@
+from abc import abstractmethod
+
+from software_patterns import SubclassRegistry
+
+
+class Dialog:
+    @abstractmethod
+    def dialog(self, *args, **kwargs):
+        raise NotImplementedError
+
+
+class DialogRegistry(SubclassRegistry[Dialog]):
+    pass
+
+
+class InteractiveDialog(metaclass=DialogRegistry):
+    def dialog(self, *args, **kwargs):
+        raise NotImplementedError
