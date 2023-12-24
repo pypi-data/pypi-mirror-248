9c7987235686d8c26ae5b8c9989d0a7dc864d164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:02:51 +0000

    Add package ccxt-market-ticker/0.0.1/ccxt-market-ticker-0.0.1.tar.gz

---
+++ packages/ccxt-market-ticker/ccxt-market-ticker-0.0.1.tar.gz/ccxt-market-ticker-0.0.1/setup.py
@@ -0,0 +1,43 @@
+"""Setup script for marketticker package."""
+import os.path
+from setuptools import setup
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README_pip.md")) as fid:
+    README = fid.read()
+
+# This call to setup() does all the work
+setup(
+    name="ccxt-market-ticker",
+    version="0.0.1",
+    description="This project uses ccxtpro to stream market data from exchanges. You can "
+                "use this project to stream market data from exchanges to a queue. "
+                "You can select which exchanges and which symbols you want to stream. by "
+                "arguments or by a zookeepernode.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/donnercody/ccxt-market-ticker",
+    author="donnercody",
+    author_email="donnercody86@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    packages=find_packages(include=('marketticker*',)),
+    include_package_data=True,
+    install_requires=[
+        "numpy",
+        "pandas",
+        "ccxt",
+        "pytest",
+        "pika"
+    ],
+    entry_points={"console_scripts": ["marketticker=index:main"]},
+)
