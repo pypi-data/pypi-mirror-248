35f4b2f56185ff9c43ffecfbd718f8a19f55fb70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:02:59 +0000

    Add package trisicell/0.2.3/trisicell-0.2.3-cp39-cp39-manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/trisicell/trisicell-0.2.3-cp39-cp39-manylinux_2_12_i686.manylinux2010_i686.whl/trisicell/__init__.py
@@ -0,0 +1,11 @@
+"""Trisicell Module."""
+
+from trisicell import datasets, io
+from trisicell import logging as logg
+from trisicell import pl, pp, settings, tl, ul
+
+__author__ = ", ".join(["Farid Rashidi"])
+__maintainer__ = ", ".join(["Farid Rashidi"])
+__email__ = ", ".join(["farid.rsh@gmail.com"])
+__version__ = "0.2.3"
+__all__ = (datasets, io, logg, pl, pp, settings, tl, ul)
---
+++ packages/trisicell/trisicell-0.2.3-cp39-cp39-manylinux_2_12_i686.manylinux2010_i686.whl/trisicell/commands/_consensus.py
@@ -0,0 +1,44 @@
+import click
+
+import trisicell as tsc
+
+
+@click.command(
+    short_help="Build consensus tree between two phylogenetic trees (Trisicell-Cons)."
+)
+@click.argument(
+    "first_tree",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+    ),
+)
+@click.argument(
+    "second_tree",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+    ),
+)
+@click.argument(
+    "consensus_path",
+    required=True,
+    type=click.Path(exists=False),
+)
+def consensus(first_tree, second_tree, consensus_path):
+    """Build consensus tree between two phylogenetic trees.
+
+    It writes the conflict-free matrix representing the consensus tree
+    into the `consensus_path` filepath.
+
+    trisicell consensus first_tree.CFMatrix second_tree.CFMatrix
+    """
+
+    tsc.settings.verbosity = "info"
+
+    sc1 = tsc.io.read(first_tree)
+    sc2 = tsc.io.read(second_tree)
+    final_tree = tsc.tl.consensus(sc1, sc2)
+    tsc.io.write(final_tree.graph["data"], consensus_path)
+
+    return None
