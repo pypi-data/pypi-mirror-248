e2365482d5ac142d956de10a26e5a417035daf64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:56 +0000

    Add package minify-html/0.11.5/minify_html-0.11.5.tar.gz

---
+++ packages/minify-html/minify_html-0.11.5.tar.gz/minify_html-0.11.5/minify-html/src/lib.rs
@@ -0,0 +1,59 @@
+use crate::ast::c14n::c14n_serialise_ast;
+pub use crate::cfg::Cfg;
+use crate::minify::content::minify_content;
+use crate::parse::content::parse_content;
+use crate::parse::Code;
+use minify_html_common::spec::tag::ns::Namespace;
+use minify_html_common::spec::tag::EMPTY_SLICE;
+use std::io::Write;
+
+mod ast;
+mod cfg;
+mod entity;
+mod minify;
+mod parse;
+mod tag;
+#[cfg(test)]
+mod tests;
+
+/// Minifies UTF-8 HTML code, represented as an array of bytes.
+///
+/// # Arguments
+///
+/// * `code` - A slice of bytes representing the source code to minify.
+/// * `cfg` - Configuration object to adjust minification approach.
+///
+/// # Examples
+///
+/// ```
+/// use minify_html::{Cfg, minify};
+///
+/// let mut code: &[u8] = b"<p>  Hello, world!  </p>";
+/// let mut cfg = Cfg::new();
+/// cfg.keep_comments = true;
+/// let minified = minify(&code, &cfg);
+/// assert_eq!(minified, b"<p>Hello, world!".to_vec());
+/// ```
+pub fn minify(src: &[u8], cfg: &Cfg) -> Vec<u8> {
+  let mut code = Code::new(src);
+  let parsed = parse_content(&mut code, Namespace::Html, EMPTY_SLICE, EMPTY_SLICE);
+  let mut out = Vec::with_capacity(src.len());
+  minify_content(
+    cfg,
+    &mut out,
+    Namespace::Html,
+    false,
+    EMPTY_SLICE,
+    parsed.children,
+  );
+  out
+}
+
+pub fn canonicalise<T: Write>(out: &mut T, src: &[u8]) -> std::io::Result<()> {
+  let mut code = Code::new(src);
+  let parsed = parse_content(&mut code, Namespace::Html, EMPTY_SLICE, EMPTY_SLICE);
+  for c in parsed.children {
+    c14n_serialise_ast(out, &c)?;
+  }
+  Ok(())
+}
