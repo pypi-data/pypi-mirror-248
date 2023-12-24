9a613b0706dee1050017d24a322481e8d77eb2b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:43 +0000

    Add package gnuhealth-crypto/4.4.0/gnuhealth_crypto-4.4.0.tar.gz

---
+++ packages/gnuhealth-crypto/gnuhealth_crypto-4.4.0.tar.gz/gnuhealth_crypto-4.4.0/tests/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2008-2023 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2023 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .test_health_crypto import suite
