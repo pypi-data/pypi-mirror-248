7e13a370074ca4757b3669debf3177bfcf2e0486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:03:18 +0000

    Add package mkdocs-minecraft/0.1.6/mkdocs_minecraft-0.1.6-py3-none-any.whl

---
+++ packages/mkdocs-minecraft/mkdocs_minecraft-0.1.6-py3-none-any.whl/mkdocs_minecraft/config.py
@@ -0,0 +1,11 @@
+from mkdocs.config.base import Config
+from mkdocs.config.config_options import Deprecated, Type
+
+
+# Minecraft plugin configuration
+class MinecraftConfig(Config):
+    enabled = Type(bool, default=True)
+
+    images_dir = Type(str, default="assets/images/minecraft")
+    background_path = Type(str, default="")
+    items_path = Type(str, default="")
