588cf2a1f1e5f766cae002d07fa3c26b46a26234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:08 +0000

    Add package gaspra/0.1.0a2/gaspra-0.1.0a2-py3-none-any.whl

---
+++ packages/gaspra/gaspra-0.1.0a2-py3-none-any.whl/gaspra/changesets.py
@@ -0,0 +1,220 @@
+from __future__ import annotations
+from collections.abc import Iterable
+from dataclasses import dataclass
+import os
+
+from gaspra.common import DATA_DIR
+from gaspra.suffix_automaton import build, find_lcs
+
+
+@dataclass
+class CopyFragment:
+    insert: str
+    length: int
+
+
+@dataclass
+class ChangeFragment:
+    insert: str
+    delete: str
+    length: int
+
+
+@dataclass
+class ConflictFragment:
+    version1: str
+    version2: str
+    length: int
+
+
+@dataclass
+class ChangesetLeaf:
+    original: str
+    modified: str
+
+    original_slice: slice
+    modified_slice: slice
+
+    def changes(self):
+        yield self
+
+    def _fragments(self, _: str):
+        # Construction of the tree creates "empty" changesets.
+        # Omit those from the output stream.
+        if self.modified or self.original:
+            yield ChangeFragment(
+                insert=self.modified,
+                delete=self.original,
+                length=len(
+                    self.original,
+                ),
+            )
+
+    def fragments(self, _: str) -> Iterable[str | tuple[str, str]]:
+        # Construction of the tree creates "empty" changesets.
+        # Omit those from the output stream.
+        if self.modified or self.original:
+            yield (self.modified, self.original)
+
+    def apply_forward(self, _: str):
+        yield self.modified
+
+    def apply_reverse(self, _: str):
+        yield self.original
+
+    def show(self, __original__: str, __modified__: str) -> str:
+        result = ""
+
+        if self.original:
+            result += f"[red strike]{escape(self.original)}[/]"
+            # result += f"[delete]{escape(self.original_str)}[/delete]"
+        if self.modified:
+            result += f"[green]{escape(self.modified)}[/]"
+        # result += f"[insert]{escape(self.modified_str)}[/insert]"
+
+        return result
+
+    def __str__(self):
+        result = ""
+        if self.original:
+            result += f"original: {self.original}\n"
+        if self.modified:
+            result += f"modified: {self.modified}\n"
+        return result
+
+
+@dataclass
+class Changeset:
+    common_original: slice
+    common_modified: slice
+
+    prefix: Changeset | ChangesetLeaf
+    suffix: Changeset | ChangesetLeaf
+
+    def changes(self):
+        yield from self.prefix.changes()
+        yield self
+        yield from self.suffix.changes()
+
+    def _fragments(self, original):
+        yield from self.prefix._fragments(original)
+        copy = original[self.common_original]
+        yield CopyFragment(insert=copy, length=len(copy))
+        yield from self.suffix._fragments(original)
+
+    def fragments(self, original) -> Iterable[str | tuple[str, str]]:
+        yield from self.prefix.fragments(original)
+        yield original[self.common_original]
+        yield from self.suffix.fragments(original)
+
+    def apply_forward(self, original: str):
+        yield from self.prefix.apply_forward(original)
+        yield original[self.common_original]
+        yield from self.suffix.apply_forward(original)
+
+    def apply_reverse(self, modified: str):
+        yield from self.prefix.apply_reverse(modified)
+        yield modified[self.common_modified]
+        yield from self.suffix.apply_reverse(modified)
+
+    def show(self, original: str, modified: str):
+        return (
+            self.prefix.show(original, modified)
+            + escape(original[self.common_original])
+            + self.suffix.show(original, modified)
+        )
+
+    def __str__(self):
+        s_original = f"{self.common_original.start}:{self.common_original.stop}"
+        s_modified = f"{self.common_modified.start}:{self.common_modified.stop}"
+        return f"original[{s_original}]/modified[{s_modified}]\n"
+
+
+def escape(s):
+    return s.replace("[", r"\[")
+    # return s
+
+
+def find_changeset(
+    original: str,
+    modified: str,
+    original_slice: slice = slice(0, None),
+    modified_slice: slice = slice(0, None),
+) -> Changeset | ChangesetLeaf:
+    automaton_original = build(original[original_slice])
+
+    common_offset_original, common_offset_modified, common_length = find_lcs(
+        automaton_original, modified[modified_slice]
+    )
+
+    if common_length == 0:
+        changeset = ChangesetLeaf(
+            original=original[original_slice],
+            modified=modified[modified_slice],
+            original_slice=original_slice,
+            modified_slice=modified_slice,
+        )
+    else:
+        common_original = slice(
+            original_slice.start + common_offset_original,
+            original_slice.start + common_offset_original + common_length,
+        )
+        common_modified = slice(
+            modified_slice.start + common_offset_modified,
+            modified_slice.start + common_offset_modified + common_length,
+        )
+
+        prefix = find_changeset(
+            original,
+            modified,
+            slice(original_slice.start, common_original.start),
+            slice(modified_slice.start, common_modified.start),
+        )
+        suffix = find_changeset(
+            original,
+            modified,
+            slice(common_original.stop, original_slice.stop),
+            slice(common_modified.stop, modified_slice.stop),
+        )
+        changeset = Changeset(
+            prefix=prefix,
+            suffix=suffix,
+            common_original=common_original,
+            common_modified=common_modified,
+        )
+
+    return changeset
+
+
+def apply_forward(changeset, original: str):
+    patched_original = "".join(changeset.apply_forward(original))
+    return patched_original
+
+
+def apply_reverse(changeset, modified: str):
+    reverse_patched_modified = "".join(changeset.apply_reverse(modified))
+    return reverse_patched_modified
+
+
+if __name__ == "__main__":
+    from rich.console import Console
+
+    console = Console(highlight=False)
+
+    with open(os.path.join(DATA_DIR, "file1")) as file1, open(
+        os.path.join(DATA_DIR, "file2")
+    ) as file2:
+        original = file1.read()
+        modified = file2.read()
+
+    changeset = find_changeset(original, modified)
+
+    markup = changeset.show(original, modified)
+
+    patched_original = apply_forward(changeset, original)
+    reverse_patched_modified = apply_reverse(changeset, modified)
+
+    assert patched_original == modified
+    assert reverse_patched_modified == original
+
+    console.print(markup)
---
+++ packages/gaspra/gaspra-0.1.0a2-py3-none-any.whl/gaspra/merge.py
@@ -0,0 +1,316 @@
+from dataclasses import replace
+from os.path import commonprefix
+
+from gaspra.changesets import (
+    ChangeFragment,
+    ConflictFragment,
+    CopyFragment,
+    find_changeset,
+)
+
+OutputType = CopyFragment | ChangeFragment | ConflictFragment
+InputType = CopyFragment | ChangeFragment
+
+
+def merge(parent: str, branch0: str, branch1: str):
+    changeset0 = find_changeset(parent, branch0)
+    changeset1 = find_changeset(parent, branch1)
+
+    fragments0 = list(reversed(list(changeset0._fragments(parent))))
+    fragments1 = list(reversed(list(changeset1._fragments(parent))))
+
+    merged = _merge(fragments0, fragments1)
+
+    return accumulate_result(merged)
+
+
+def accumulate_result(output):
+    conflict_free_accumulation = ""
+    nothing_has_been_output = True
+    for fragment in output:
+        if isinstance(fragment, ConflictFragment):
+            # Flush the conflict free string we've been accumulating.
+            if conflict_free_accumulation:
+                yield conflict_free_accumulation
+                conflict_free_accumulation = ""
+
+            yield (fragment.version1, fragment.version2)
+            nothing_has_been_output = False
+        else:
+            conflict_free_accumulation += fragment.insert
+
+    # Flush the conflict free string we've been accumulating and
+    # return empty string if no output yet.
+
+    if conflict_free_accumulation or nothing_has_been_output:
+        yield conflict_free_accumulation or ""
+
+
+def _merge(fragments0: list[InputType], fragments1):
+    """Process changes in fragments0 and fragments1 until
+    they are empty.  Note, this function modifies the passed lists"""
+
+    while fragments0 and fragments1:
+        fragment0 = fragments0.pop()
+        fragment1 = fragments1.pop()
+
+        output, tail0, tail1 = process_fragments(fragment0, fragment1)
+
+        if output:
+            yield output
+
+        # If the fragments weren't fully processed, push their tails
+        # back onto their respective stacks.
+
+        if tail0:
+            fragments0.append(tail0)
+
+        if tail1:
+            fragments1.append(tail1)
+
+    if fragments0 or fragments1:
+        remaining = fragments0 or fragments1
+        yield from reversed(remaining)
+
+
+def process_fragments(fragment0, fragment1):
+    if isinstance(fragment0, CopyFragment) and isinstance(fragment1, CopyFragment):
+        output, tail0, tail1 = copy_copy(fragment0, fragment1)
+
+    elif isinstance(fragment0, ChangeFragment) and isinstance(
+        fragment1, ChangeFragment
+    ):
+        output, tail0, tail1 = change_change(fragment0, fragment1)
+
+    elif isinstance(fragment0, CopyFragment) and isinstance(fragment1, ChangeFragment):
+        output, tail0, tail1 = copy_change(fragment0, fragment1)
+
+    elif isinstance(fragment0, ChangeFragment) and isinstance(fragment1, CopyFragment):
+        output, tail1, tail0 = copy_change(fragment1, fragment0)
+
+    else:
+        raise RuntimeError(f"Unexpected types: {type(fragment0)} or {type(fragment1)}")
+
+    return output, tail0, tail1
+
+
+def copy_copy(fragment0: CopyFragment, fragment1: CopyFragment):
+    if fragment0.length < fragment1.length:
+        shorter, longer = fragment0, fragment1
+    else:
+        shorter, longer = fragment1, fragment0
+
+    long_queue_tail = None
+    if shorter.length != longer.length:
+        *_, long_queue_tail = split_copy_fragment(longer, shorter.length)
+
+    tail0 = tail1 = None
+
+    if fragment0 == longer:
+        tail0 = long_queue_tail
+    else:
+        tail1 = long_queue_tail
+
+    return shorter, tail0, tail1
+
+
+def copy_change(copy_fragment, change_fragment):
+    output = copy_tail = change_tail = None
+
+    smaller_length = min(copy_fragment.length, change_fragment.length)
+    if change_fragment.length == smaller_length:
+        output = change_fragment
+
+        # Anything left over?
+        if copy_fragment.length > smaller_length:
+            *_, copy_tail = split_copy_fragment(copy_fragment, smaller_length)
+
+    else:  # Change is longer than copy implies a conflict.
+        head0, head1 = split_change_fragment(
+            change_fragment, len(change_fragment.insert), smaller_length
+        )
+        if head0:
+            output = ConflictFragment(
+                head0.insert, copy_fragment.insert, smaller_length
+            )
+
+        if head1:
+            change_tail = head1
+
+    return output, copy_tail, change_tail
+
+
+def change_change(fragment0: ChangeFragment, fragment1: ChangeFragment):
+    """Handle two change blocks appearing at the same location"""
+
+    if has_composable_changes(fragment0, fragment1):
+        return compose_changes(fragment0, fragment1)
+
+    # Determine how much is common between the two changesets
+    insert_length, delete_length = common_prefix_lengths(fragment0, fragment1)
+
+    # Exactly the same changeset can simply be passed along.
+    if are_identical_changes(fragment0, fragment1, insert_length, delete_length):
+        return fragment0, None, None
+
+    if has_factorable_common_prefix(fragment0, fragment1, insert_length, delete_length):
+        return factor_common_prefix(fragment0, fragment1, insert_length, delete_length)
+
+    return ordinary_conflict(fragment0, fragment1)
+
+
+def has_composable_changes(fragment0, fragment1):
+    # This is an edge case that showed up in testing. If one change is a
+    # pure insertion (no deletion) and the other is a pure deletion (no
+    # insertion), they can be combined into a single conflict-free
+    # change.  It needs to be pushed back onto the input list to be
+    # processed properly One input sequence was [insert x/delete
+    # nothing][s] The other input sequence was [insert nothing/delete
+    # s]. These are transformed to the sequences 1) [s] and 2) [insert
+    # x/delete s] by pushing [insert x/delete s] back onto the input
+    # queue which has the affect of inserting 's' at position where 's'
+    # was.  See compose_changes() for how the changes are composed.
+
+    return (fragment0.length == 0 and fragment1.insert == "") or (
+        fragment0.insert == "" and fragment1.length == 0
+    )
+
+
+def compose_changes(fragment0, fragment1):
+    if fragment0.length == 0 and fragment1.insert == "":
+        tail1 = ChangeFragment(
+            fragment0.insert,
+            fragment1.delete,
+            fragment1.length,
+        )
+        return None, None, tail1
+    elif fragment0.insert == "" and fragment1.length == 0:
+        tail0 = ChangeFragment(
+            fragment1.insert,
+            fragment0.delete,
+            fragment0.length,
+        )
+        return None, tail0, None
+    else:
+        raise ValueError("Changes are not composable")
+
+
+def are_identical_changes(
+    fragment0,
+    fragment1,
+    insert_length,
+    delete_length,
+):
+    return (len(fragment0.insert) == len(fragment1.insert) == insert_length) and (
+        fragment0.length == fragment1.length == delete_length
+    )
+
+
+def has_factorable_common_prefix(
+    fragment0,
+    fragment1,
+    insert_length,
+    delete_length,
+):
+    """Check for the case where the two changesets have a non-empty
+    common prefix. If it isn't non-empty, there's nothing to do.  To
+    split/factor the fragment, the part of the insertion that's factored
+    must be a proper substring of both insertions.  Otherwise, the
+    conflict can't be detected in the tail.  This elif used to be more
+    restrictive: just insert_length > 0 and delete_length > 0:
+    """
+    return (
+        (insert_length > 0 or delete_length > 0)
+        and insert_length < len(fragment0.insert)
+        and insert_length < len(fragment1.insert)
+    )
+
+
+def factor_common_prefix(
+    fragment0: ChangeFragment,
+    fragment1: ChangeFragment,
+    insert_length,
+    delete_length,
+):
+    output, tail0 = split_change_fragment(
+        fragment0,
+        insert_length,
+        delete_length,
+    )
+    *_, tail1 = split_change_fragment(
+        fragment1,
+        insert_length,
+        delete_length,
+    )
+    return output, tail0, tail1
+
+
+def ordinary_conflict(fragment0, fragment1):
+    output = None
+
+    length = min(fragment0.length, fragment1.length)
+    head0, tail0 = split_change_fragment(
+        fragment0,
+        len(fragment0.insert),
+        length,
+    )
+    head1, tail1 = split_change_fragment(
+        fragment1,
+        len(fragment1.insert),
+        length,
+    )
+
+    if head0 and head1:
+        output = ConflictFragment(head0.insert, head1.insert, length)
+
+    return output, tail0, tail1
+
+
+def split_copy_fragment(fragment: CopyFragment, length: int):
+    head = tail = None
+    if length > 0:
+        head = replace(
+            fragment,
+            insert=fragment.insert[:length],
+            length=length,
+        )
+    if length < fragment.length:
+        tail = replace(
+            fragment,
+            insert=fragment.insert[length:],
+            length=fragment.length - length,
+        )
+    return head, tail
+
+
+def split_change_fragment(
+    fragment: ChangeFragment,
+    insert_length,
+    length: int,
+):
+    head = tail = None
+    if length > 0 or insert_length > 0:
+        head = replace(
+            fragment,
+            insert=fragment.insert[:insert_length],
+            delete=fragment.delete[:length],
+            length=length,
+        )
+    if length < fragment.length or insert_length < len(fragment.insert):
+        tail = replace(
+            fragment,
+            insert=fragment.insert[insert_length:],
+            delete=fragment.delete[length:],
+            length=fragment.length - length,
+        )
+    return head, tail
+
+
+def common_prefix_lengths(
+    change1: ChangeFragment,
+    change2: ChangeFragment,
+):
+    insert_length = len(commonprefix((change1.insert, change2.insert)))
+    delete_length = len(commonprefix((change1.delete, change2.delete)))
+
+    return insert_length, delete_length
