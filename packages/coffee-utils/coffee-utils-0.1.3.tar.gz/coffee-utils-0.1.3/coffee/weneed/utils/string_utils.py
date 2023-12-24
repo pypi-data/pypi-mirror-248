a9ff0f21547720da0226d6052b4ed06bed90f138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 15:03:28 +0000

    Add package coffee-utils/0.1.3/coffee-utils-0.1.3.tar.gz

---
+++ packages/coffee-utils/coffee-utils-0.1.3.tar.gz/coffee-utils-0.1.3/coffee/weneed/utils/string_utils.py
@@ -0,0 +1,10 @@
+import string
+
+class StringUtils:
+
+    @staticmethod
+    def get_ascii_chars():
+        """
+        Returns a set of ASCII characters.
+        """
+        return set(string.ascii_letters + string.digits + string.punctuation + string.whitespace)
