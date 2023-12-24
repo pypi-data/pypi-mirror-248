bba069afa2138822d98f8cce0501471dea876fb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:46 +0000

    Add package bensapdf2/2.0/bensapdf2-2.0.tar.gz

---
+++ packages/bensapdf2/bensapdf2-2.0.tar.gz/bensapdf2-2.0/bensapdf/pdf2text.py
@@ -0,0 +1,13 @@
+"""
+This Module provides functions to convert a PDF to Text.
+"""
+
+def convert(path):
+    """ Convert the given PDF to text.
+    Parameters:
+    path(str): The path to a PDF file.
+    
+    Returns:
+    str: The content of the PDF file as text.
+    """
+    print("This is my first test project of pdf")
