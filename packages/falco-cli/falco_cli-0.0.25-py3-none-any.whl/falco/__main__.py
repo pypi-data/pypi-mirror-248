7eb17d3340edb4749d76268634a7a9e15b654edb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:20 +0000

    Add package falco-cli/0.0.25/falco_cli-0.0.25-py3-none-any.whl

---
+++ packages/falco-cli/falco_cli-0.0.25-py3-none-any.whl/falco/__main__.py
@@ -0,0 +1,39 @@
+import cappa
+from falco.commands import Htmx
+from falco.commands import HtmxExtension
+from falco.commands import InstallCrudUtils
+from falco.commands import MakeSuperUser
+from falco.commands import ModelCRUD
+from falco.commands import RmMigrations
+from falco.commands import StartProject
+from falco.commands import SyncDotenv
+from falco.commands import Work
+
+
+@cappa.command(
+    help="Initialize a new django project using the falco project template.",
+    description="""This is a wrapper around the django-admin startproject command using my custom project template at
+    https://github.com/Tobi-De/falco. This cli also includes some additional commands to make setting up
+    a new project faster.
+    """,
+)
+class Falco:
+    subcommand: cappa.Subcommands[
+        StartProject
+        | ModelCRUD
+        | InstallCrudUtils
+        | Htmx
+        | HtmxExtension
+        | Work
+        | SyncDotenv
+        | RmMigrations
+        | MakeSuperUser
+    ]
+
+
+def main():
+    cappa.invoke(Falco)
+
+
+if __name__ == "__main__":
+    main()
