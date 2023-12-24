173c616a70e1a0a32e708e626a7a2208d0311453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:03:29 +0000

    Add package pycad-medic/0.0.7/pycad-medic-0.0.7.tar.gz

---
+++ packages/pycad-medic/pycad-medic-0.0.7.tar.gz/pycad-medic-0.0.7/pycad/visualization/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023 PYCAD
+# This file is part of the PYCAD library and is released under the MIT License:
+# https://github.com/amine0110/pycad/blob/main/LICENSE
+
+
+from .stl_visualizer import STLVisualizer
+from .nifti_visualizer import NIFTIVisualizer
