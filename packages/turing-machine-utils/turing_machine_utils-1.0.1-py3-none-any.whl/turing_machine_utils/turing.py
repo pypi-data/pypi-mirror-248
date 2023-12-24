c731c6c618f5df276703f65e659cfb780bdfe9c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:05:18 +0000

    Add package turing-machine-utils/1.0.1/turing_machine_utils-1.0.1-py3-none-any.whl

---
+++ packages/turing-machine-utils/turing_machine_utils-1.0.1-py3-none-any.whl/turing_machine_utils/turing.py
@@ -0,0 +1,36 @@
+def create_turing_machine(tape, states, transition_rules):
+    # Initialize the tape head position
+    head_position = 0
+
+    # Run the Turing machine
+    current_state = states[0]  # Start from the initial state
+
+    while current_state not in ['accept', 'discard']:
+        current_symbol = tape[head_position]
+
+        if (current_state, current_symbol) in transition_rules:
+            new_state, new_symbol, move_direction = transition_rules[(current_state, current_symbol)]
+
+            # Update tape
+            tape[head_position] = new_symbol
+
+            # Move tape head
+            if move_direction == 'R':
+                head_position += 1
+            elif move_direction == 'L':
+                head_position -= 1
+
+            # Update current state
+            current_state = new_state
+        else:
+            print("Undefined transition for state: {}, symbol: {}".format(current_state, current_symbol))
+            return 0  # Return 0 for an undefined transition
+
+    if current_state == 'accept':
+        print("Turing machine accepted the tape.")
+        return 1
+    elif current_state == 'discard':
+        print("Turing machine discarded the tape.")
+        return -1
+
+    print("Final tape content:", ''.join(tape))
