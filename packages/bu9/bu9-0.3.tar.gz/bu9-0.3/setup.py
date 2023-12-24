62f6534b9b57ecb4b3943c647b424d6d698147ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:09 +0000

    Add package bu9/0.3/bu9-0.3.tar.gz

---
+++ packages/bu9/bu9-0.3.tar.gz/bu9-0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='bu9',
+    version='0.3',
+    py_modules=['bu9'],
+    install_requires=[
+        'colored==1.4.4',
+        'colorama==0.4.6',
+        'ipaddress==1.0.23',
+        'requests==2.26.0',
+        'beautifulsoup4==4.10.0',
+        'tqdm==4.62.3',
+        'termcolor==1.1.0',
+        'retry==0.9.2',
+        'bs4==0.0.1',
+        'ipcalc',
+        'emoji',
+        'dnspython',
+        'nslookup',
+        'six',
+        
+        
+
+        # Add other dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'bu9 = bu9:main'
+        ]
+    },
+)
