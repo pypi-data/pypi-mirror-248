1db87acbe1ec6b418b7ac1d73a8dc4aa5b44cd34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:10 +0000

    Add package PFDiscord/1.0.4/PFDiscord-1.0.4.tar.gz

---
+++ packages/PFDiscord/PFDiscord-1.0.4.tar.gz/PFDiscord-1.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='PFDiscord',
+    version='1.0.4',
+    packages=find_packages(),
+    license='AGPL-3.0',
+    author='Digital Heretic',
+    author_email='agiheretic@proton.me',
+    description='OpenAI GPT, driven by Discord',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Digital-Heresy/PFDiscord',
+    install_requires=[
+        'openai==0.28.0',
+        'PyYAML>=6.0',
+        'httpx>=0.24.1',
+        'beautifulsoup4>=4.12.2',
+        'typing>=3.7.4.3',
+        'validators>=0.21.2',
+        'discord.py>=2.3',
+        'tiktoken>=0.4'
+    ],
+    python_requires='>=3.10',
+)
