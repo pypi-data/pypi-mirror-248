cc15b2e9f115ef88ac5a574892a09d0ddb66f402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:11 +0000

    Add package spaceone-cost-analysis/2.0.dev31/spaceone-cost-analysis-2.0.dev31.tar.gz

---
+++ packages/spaceone-cost-analysis/spaceone-cost-analysis-2.0.dev31.tar.gz/spaceone-cost-analysis-2.0.dev31/spaceone/cost_analysis/model/cost_query_set_model.py
@@ -0,0 +1,38 @@
+from mongoengine import *
+
+from spaceone.core.model.mongo_model import MongoModel
+
+
+class CostQuerySet(MongoModel):
+    cost_query_set_id = StringField(max_length=40, generate_id='query', unique=True)
+    name = StringField(max_length=255)
+    options = DictField(default={})
+    tags = DictField(default={})
+    user_id = StringField(max_length=40)
+    data_source_id = StringField(max_length=40)
+    domain_id = StringField(max_length=40)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    meta = {
+        'updatable_fields': [
+            'name',
+            'options',
+            'tags'
+        ],
+        'minimal_fields': [
+            'cost_query_set_id',
+            'name',
+            'user_id',
+            'data_source_id'
+        ],
+        'ordering': [
+            'name'
+        ],
+        'indexes': [
+            'name',
+            'user_id',
+            'data_source_id',
+            'domain_id'
+        ]
+    }
