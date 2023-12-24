d41084cd88cd50d98b4ba4a68ab3f5421e82ad68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:46 +0000

    Add package gnuhealth-reporting/4.4.0/gnuhealth_reporting-4.4.0.tar.gz

---
+++ packages/gnuhealth-reporting/gnuhealth_reporting-4.4.0.tar.gz/gnuhealth_reporting-4.4.0/doc/index.rst
@@ -0,0 +1,9 @@
+.. SPDX-FileCopyrightText: 2008-2023 Luis Falcón <falcon@gnuhealth.org>
+.. SPDX-FileCopyrightText: 2011-2023 GNU Solidario <health@gnusolidario.org>
+..
+.. SPDX-License-Identifier: CC-BY-SA-4.0
+
+GNU Health Reporting Module
+###########################
+
+This module adds several statistical reports and charts.
---
+++ packages/gnuhealth-reporting/gnuhealth_reporting-4.4.0.tar.gz/gnuhealth_reporting-4.4.0/tests/test_health_reporting.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2008-2023 Luis Falcón <falcon@gnuhealth.org>
+# SPDX-FileCopyrightText: 2011-2023 GNU Solidario <health@gnusolidario.org>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import unittest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class HealthReportingTestCase(ModuleTestCase):
+    '''
+    Test Health Reporting module.
+    '''
+    module = 'health_reporting'
+
+
+def suite():
+    suite = trytond.tests.test_tryton.suite()
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        HealthReportingTestCase))
+    return suite
