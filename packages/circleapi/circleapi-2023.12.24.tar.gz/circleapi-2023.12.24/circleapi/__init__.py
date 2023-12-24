44d9c4e35fbb3b2e4a6a1c8932892c693af74220
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:52 +0000

    Add package circleapi/2023.12.24/circleapi-2023.12.24.tar.gz

---
+++ packages/circleapi/circleapi-2023.12.24.tar.gz/circleapi-2023.12.24/circleapi/__init__.py
@@ -0,0 +1,12 @@
+from .token import UserToken, GuestToken
+from .async_token import AsyncGuestToken, AsyncUserToken
+from .logger import logger, setup_queue_logging
+from .api import ApiV2, ExternalApi
+from .utils import RateLimit, RequestThread, AsyncRateLimit
+from .async_api import AsyncApiV2, AsyncExternalApi
+from .models import (
+    BeatmapExtended, BeatmapUserScore, BeatmapUserScores,
+    BeatmapScores, Beatmaps, BeatmapAttributes,
+    Score, BeatmapsetExtended, User, ScoreScope, Ruleset, UserExtended,
+    BaseStruct
+)
