23beb9d410c168e6266766871ca90cd2e89a1313
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:09 +0000

    Add package storage-hub/0.0.9/storage-hub-0.0.9.tar.gz

---
+++ packages/storage-hub/storage-hub-0.0.9.tar.gz/storage-hub-0.0.9/setup.py
@@ -0,0 +1,35 @@
+"""
+Sample setup.py file
+"""
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "Readme.md"), encoding="utf-8") as fh:
+    long_description = "\\n" + fh.read()
+
+setup(
+    name="storage-hub",
+    version='0.0.9',
+    author="Vitaliy Zakharkiv",
+    author_email="vzaharkiv28@mail.com",
+    description="soon",
+    url="https://github.com/VitailOG/storage-hub",
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(where='storage_hub'),
+    package_dir={'': 'storage_hub'},
+    install_requires=[],
+    license="MIT",
+    keywords=['pypi', 'cicd', 'python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows"
+    ]
+)
