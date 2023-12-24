0050f8db1403565b9cc6572a0c3c490d523c65ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:02 +0000

    Add package hellya/3.0/hellya-3.0.tar.gz

---
+++ packages/hellya/hellya-3.0.tar.gz/hellya-3.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+  with open('README.md', 'r', encoding='utf-8') as f:
+    return f.read()
+
+
+setup(
+  name='hellya',
+  version='3.0',
+  author='soviet_workshop',
+  author_email='soviet.workshop23@gmail.com',
+  description='Библиотека для программирования рисунков.',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  packages=find_packages(),
+  install_requires=[
+    'requests>=2.25.1',
+    'pillow>=10.1.0',
+    'opencv-python>=4.8.1.78',
+    'numpy>=1.26.2'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='soviet hellya pixel workshop',
+  python_requires='>=3.9',
+  url = "https://github.com/Soviet-WorkShop/hellya_lite"
+)
