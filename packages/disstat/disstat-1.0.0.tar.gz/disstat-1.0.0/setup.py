dc999c84f0cd3b16b8d57a753e378b2872220c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:03 +0000

    Add package disstat/1.0.0/disstat-1.0.0.tar.gz

---
+++ packages/disstat/disstat-1.0.0.tar.gz/disstat-1.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='disstat',
+    version='1.0.0',
+    description='A Python wrapper for the DisStat API.',
+    author='Your Name',
+    author_email='TheUntraceable@The-Untraceable.xz',
+    url='https://github.com/TheUntraceable/disstat',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
