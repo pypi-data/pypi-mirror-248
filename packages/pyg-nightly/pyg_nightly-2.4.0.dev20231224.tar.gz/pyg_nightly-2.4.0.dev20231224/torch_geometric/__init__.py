1048d128f84c7ed9757abd2113598b6ba5d5fca7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:10 +0000

    Add package pyg-nightly/2.4.0.dev20231224/pyg_nightly-2.4.0.dev20231224.tar.gz

---
+++ packages/pyg-nightly/pyg_nightly-2.4.0.dev20231224.tar.gz/pyg_nightly-2.4.0.dev20231224/torch_geometric/__init__.py
@@ -0,0 +1,42 @@
+from .edge_index import EdgeIndex
+
+import torch_geometric.utils
+import torch_geometric.data
+import torch_geometric.sampler
+import torch_geometric.loader
+import torch_geometric.transforms
+import torch_geometric.datasets
+import torch_geometric.nn
+import torch_geometric.explain
+import torch_geometric.profile
+
+from .seed import seed_everything
+from .home import get_home_dir, set_home_dir
+from .compile import compile
+from .isinstance import is_torch_instance
+from .debug import is_debug_enabled, debug, set_debug
+from .experimental import (is_experimental_mode_enabled, experimental_mode,
+                           set_experimental_mode)
+from .lazy_loader import LazyLoader
+
+contrib = LazyLoader('contrib', globals(), 'torch_geometric.contrib')
+graphgym = LazyLoader('graphgym', globals(), 'torch_geometric.graphgym')
+
+__version__ = '2.4.0.dev20231224'
+
+__all__ = [
+    'EdgeIndex',
+    'seed_everything',
+    'get_home_dir',
+    'set_home_dir',
+    'compile',
+    'is_torch_instance',
+    'is_debug_enabled',
+    'debug',
+    'set_debug',
+    'is_experimental_mode_enabled',
+    'experimental_mode',
+    'set_experimental_mode',
+    'torch_geometric',
+    '__version__',
+]
