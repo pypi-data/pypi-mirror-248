8a01e0f82c9696280540c91041fbbe444c23c0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:03:06 +0000

    Add package voicesynth/0.0.2/voicesynth-0.0.2.tar.gz

---
+++ packages/voicesynth/voicesynth-0.0.2.tar.gz/voicesynth-0.0.2/voicesynth/__init__.py
@@ -0,0 +1,13 @@
+from voicesynth.voicesynth import (
+    Synthesizer, Model, show_available_models
+)
+from voicesynth.utils.config import (
+    log_enable, version
+)
+
+
+__all__ = ["Synthesizer", "Model", "show_available_models"]
+
+
+
+
