6421737fe9c63e71166eba644ac392ceb2f77d51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:03:30 +0000

    Add package im2dhist/1.0.0/im2dhist-1.0.0.tar.gz

---
+++ packages/im2dhist/im2dhist-1.0.0.tar.gz/im2dhist-1.0.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(parent_dir, "README.md"), encoding="utf-8") as readme:
+    readme_description = "\n" + readme.read()
+
+
+setup(
+    name="im2dhist",
+    version="1.0.0",
+    author="mamdasn s",
+    author_email="<mamdassn@gmail.com>",
+    url="https://github.com/Mamdasn/im2dhist",
+    description="Calculates two dimensional histogram of a given image.",
+    long_description=readme_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    package_dir={"": "src"},
+    py_modules=["im2dhist"],
+    install_requires=[
+        "numpy",
+        "numba",
+    ],
+    keywords=[
+        "python",
+        "histogram",
+        "imhist",
+        "2dhist",
+        "hist2d",
+        "two dimensional histogram",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+)
