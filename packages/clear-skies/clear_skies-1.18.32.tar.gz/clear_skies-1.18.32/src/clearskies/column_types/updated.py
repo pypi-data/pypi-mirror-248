9bf3bf157982ae38ff02f7eee503cc78649d684c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:02:58 +0000

    Add package clear-skies/1.18.32/clear_skies-1.18.32.tar.gz

---
+++ packages/clear-skies/clear_skies-1.18.32.tar.gz/clear_skies-1.18.32/src/clearskies/column_types/updated.py
@@ -0,0 +1,14 @@
+from .datetime import DateTime
+
+
+class Updated(DateTime):
+    def __init__(self, di, now):
+        super().__init__(di)
+        self.now = now
+
+    @property
+    def is_writeable(self):
+        return False
+
+    def pre_save(self, data, model):
+        return {**data, self.name: self.now}
