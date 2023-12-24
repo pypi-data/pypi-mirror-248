82cbe303190b3f22f7d81344aeeaf1747ddecef1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:03:08 +0000

    Add package alexisomghw2/0.0.1/alexisomghw2-0.0.1.tar.gz

---
+++ packages/alexisomghw2/alexisomghw2-0.0.1.tar.gz/alexisomghw2-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+  name='alexisomghw2',
+  version='0.0.1',
+  author='AlexisOMG',
+  author_email='alesha-gramazeka@yandex.ru',
+  description='ДЗ по Python ',
+  packages=find_packages(include=['hw_2']),
+  python_requires='>=3.8'
+)
