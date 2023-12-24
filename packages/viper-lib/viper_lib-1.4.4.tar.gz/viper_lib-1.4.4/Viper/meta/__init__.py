89c95a513911f9665a03d62b73f7e273e9b79dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:49 +0000

    Add package viper-lib/1.4.4/viper_lib-1.4.4.tar.gz

---
+++ packages/viper-lib/viper_lib-1.4.4.tar.gz/viper_lib-1.4.4/Viper/meta/__init__.py
@@ -0,0 +1,7 @@
+"""
+This package defines new useful tools for function/classes definitions and operations.
+This includes new python-like decorators, classes, etc.
+"""
+
+from .decorators import *
+from .iterable import *
