d441c8c9ac31d4eef9775175b4cd23ebd916efba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:20 +0000

    Add package rlvoice-1/1.3.4/rlvoice_1-1.3.4-py3-none-any.whl

---
+++ packages/rlvoice-1/rlvoice_1-1.3.4-py3-none-any.whl/rlvoice/drivers/nsss.py
@@ -0,0 +1,161 @@
+#coding:utf-8
+from AppKit import NSSpeechSynthesizer
+from PyObjCTools import AppHelper
+from ..voice import Voice
+from objc import super
+import objc
+from Foundation import NSObject, NSTimer, NSURL, NSDefaultRunLoopMode, NSRunLoop
+from PyObjCTools.AppHelper import PyObjCAppHelperRunLoopStopper
+
+def buildDriver(proxy):
+    return NSSpeechDriver.alloc().initWithProxy(proxy)
+
+class RunLoopStopper(PyObjCAppHelperRunLoopStopper):
+
+    def init(self):
+        self = super(RunLoopStopper, self).init()
+        self.shouldStop = False
+        return self
+
+    def stop(self):
+        self.shouldStop = True
+        # this should go away when/if runEventLoop uses
+        # runLoop iteration
+        # if NSApp() is not None:
+        #     NSApp().terminate_(self)
+
+class NSSpeechDriver(NSObject):
+    @objc.python_method
+    def initWithProxy(self, proxy):
+        self = super(NSSpeechDriver, self).init()
+        if self:
+            self._proxy = proxy
+            self._tts = NSSpeechSynthesizer.alloc().initWithVoice_(None)
+            self._tts.setDelegate_(self)
+            # default rate
+            self._tts.setRate_(200)
+            self._completed = True
+        return self
+
+    def destroy(self):
+        self._tts.setDelegate_(None)
+        del self._tts
+
+    def onPumpFirst_(self, timer):
+        self._proxy.setBusy(False)
+
+    def startLoop(self):
+        NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
+            0.0, self, 'onPumpFirst:', None, False)
+        runLoop = NSRunLoop.currentRunLoop()
+        stopper = RunLoopStopper.alloc().init()
+        PyObjCAppHelperRunLoopStopper.addRunLoopStopper_toRunLoop_(stopper, runLoop)
+        try:
+
+            while stopper.shouldRun():
+                nextfire = runLoop.limitDateForMode_(NSDefaultRunLoopMode)
+                if not stopper.shouldRun():
+                    break
+                if not runLoop.runMode_beforeDate_(NSDefaultRunLoopMode, nextfire):
+                    stopper.stop()
+
+        finally:
+            PyObjCAppHelperRunLoopStopper.removeRunLoopStopperFromRunLoop_(runLoop)
+
+    def endLoop(self):
+        AppHelper.stopEventLoop()
+
+    def iterate(self):
+        self._proxy.setBusy(False)
+        yield
+
+    @objc.python_method
+    def say(self, text):
+        import time
+        #self._proxy.setBusy(True)
+        #self._completed = True
+        #self._proxy.notify('started-utterance')
+        #print('debug:nsss:say start', time.time())
+        self._tts.startSpeakingString_(text)
+        # add this delay and call to didFinishSpeaking_ to prevent unfinished dead locks
+        time.sleep(0.1)
+        cnt = 0
+        # needed so script doesn't end w/o talking
+        while self._tts.isSpeaking():
+            time.sleep(0.1)
+            cnt+=1
+            #if cnt>100:
+            #    print('debug:nsss:say start more than 10seconds. stucked?',cnt)
+            #    break
+        #self.speechSynthesizer_didFinishSpeaking_(self._tts, True)
+        #print('debug:nsss:say end', time.time())
+        self._proxy.setBusy(False)
+
+
+    def stop(self):
+        if self._proxy.isBusy():
+            self._completed = False
+        self._tts.stopSpeaking()
+
+    @objc.python_method
+    def _toVoice(self, attr):
+
+        return Voice(attr.get('VoiceIdentifier'), attr.get('VoiceName'),
+                     [attr.get('VoiceLanguage')], attr.get('VoiceGender'),
+                     attr.get('VoiceAge'))
+
+    @objc.python_method
+    def getProperty(self, name):
+        if name == 'voices':
+            return [self._toVoice(NSSpeechSynthesizer.attributesForVoice_(v))
+                    for v in list(NSSpeechSynthesizer.availableVoices())]
+        elif name == 'voice':
+            return self._tts.voice()
+        elif name == 'rate':
+            return self._tts.rate()
+        elif name == 'volume':
+            return self._tts.volume()
+        elif name == "pitch":
+            print("Pitch adjustment not supported when using NSSS")
+        else:
+            raise KeyError('unknown property %s' % name)
+
+    @objc.python_method
+    def setProperty(self, name, value):
+        if name == 'voice':
+            # vol/rate gets reset, so store and restore it
+            vol = self._tts.volume()
+            rate = self._tts.rate()
+            self._tts.setVoice_(value)
+            self._tts.setRate_(rate)
+            self._tts.setVolume_(vol)
+        elif name == 'rate':
+            self._tts.setRate_(value)
+        elif name == 'volume':
+            self._tts.setVolume_(value)
+        elif name == 'pitch':
+            print("Pitch adjustment not supported when using NSSS")
+        else:
+            raise KeyError('unknown property %s' % name)
+
+    @objc.python_method
+    def save_to_file(self, text, filename):
+        url = NSURL.fileURLWithPath_(filename)
+        import time
+        self._tts.startSpeakingString_toURL_(text, url)
+        time.sleep(max(1, len(text) * 0.01))
+        # needed so script doesn't end w/o talking
+        while self._tts.isSpeaking():
+            time.sleep(0.1)
+
+    def speechSynthesizer_didFinishSpeaking_(self, tts, success):
+        if not self._completed:
+            success = False
+        else:
+            success = True
+        self._proxy.notify('finished-utterance', completed=success)
+        self._proxy.setBusy(False)
+
+    def speechSynthesizer_willSpeakWord_ofString_(self, tts, rng, text):
+        self._proxy.notify('started-word', location=rng.location,
+                           length=rng.length)
