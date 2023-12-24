77300ffd486903f41fd40c66e1861f999698b12e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:52 +0000

    Add package just-testsimhash-pybind/0.0.2/just-testsimhash-pybind-0.0.2.tar.gz

---
+++ packages/just-testsimhash-pybind/just-testsimhash-pybind-0.0.2.tar.gz/just-testsimhash-pybind-0.0.2/setup.py
@@ -0,0 +1,44 @@
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+
+ext_modules = [
+    Pybind11Extension(
+        name="simhash._simhash",
+        sources = ['simhash/simhash.cpp',
+                    'simhash/simhash-cpp/src/permutation.cpp',
+                    'simhash/simhash-cpp/src/simhash.cpp'],
+        extra_compile_args=['-O3', '-std=c++14'],
+        include_dirs=["simhash/simhash-cpp/include"]
+    ),
+]
+
+
+setup(
+    name="just-testsimhash-pybind",
+    version="0.0.2",
+    description="Near-Duplicate Detection with Simhash",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+        "Programming Language :: C++",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: C++",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    setup_requires=['pybind11'],
+    python_requires='>=3.6',
+    packages=["simhash"],
+    package_dir={"simhash": "simhash"},
+    ext_modules=ext_modules,
+)
