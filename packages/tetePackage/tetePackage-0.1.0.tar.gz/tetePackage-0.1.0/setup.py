19c6dc5145afb137bc8ce9a8ccc5468d824ef08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 15:03:44 +0000

    Add package tetePackage/0.1.0/tetePackage-0.1.0.tar.gz

---
+++ packages/tetePackage/tetePackage-0.1.0.tar.gz/tetePackage-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = 'tetePackage',
+    version = "0.1.0",
+    description= "THIS IS MY TEST PACKAGE",
+    packages=find_packages(),
+    author = 'TETE',
+    
+)
