7e788cea8159e6a10302d27277ea18d581a90c5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:02:55 +0000

    Add package trisicell/0.2.3/trisicell-0.2.3-cp37-cp37m-manylinux_2_12_i686.manylinux2010_i686.whl

---
+++ packages/trisicell/trisicell-0.2.3-cp37-cp37m-manylinux_2_12_i686.manylinux2010_i686.whl/trisicell/commands/_trees.py
@@ -0,0 +1,47 @@
+import os
+
+import click
+
+import trisicell as tsc
+from trisicell.pl._trees import _newick_info2_mutation_list
+
+
+@click.command(short_help="Convert conflict-free to newick file.")
+@click.argument(
+    "cf_file",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+    ),
+)
+def cf2newick(cf_file):
+    """CF2Newick."""
+
+    outfile = os.path.splitext(cf_file)[0]
+    cf = tsc.io.read(cf_file)
+    tree = tsc.ul.to_tree(cf)
+    newick, info2, mutations = _newick_info2_mutation_list(tree)
+    with open(f"{outfile}.newick", "w") as fout:
+        fout.write(newick + "\n")
+    info2.to_csv(f"{outfile}.info2", index=None)
+
+    return None
+
+
+@click.command(short_help="Convert conflict-free to clonal tree.")
+@click.argument(
+    "cf_file",
+    required=True,
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, readable=True, resolve_path=True
+    ),
+)
+def cf2tree(cf_file):
+    """CF2Tree."""
+
+    outfile = os.path.splitext(cf_file)[0]
+    cf = tsc.io.read(cf_file)
+    tree = tsc.ul.to_tree(cf)
+    tsc.pl.clonal_tree(tree, output_file=f"{outfile}.png")
+
+    return None
