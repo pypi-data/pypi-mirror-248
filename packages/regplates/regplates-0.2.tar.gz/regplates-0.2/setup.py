1a1c3907d329c897ffecc7868b77bad144a83ecc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:03:14 +0000

    Add package regplates/0.2/regplates-0.2.tar.gz

---
+++ packages/regplates/regplates-0.2.tar.gz/regplates-0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='regplates',
+    version='0.2',
+    packages=find_packages(),
+    install_requires=[
+        'pygame',
+        'pygame_gui',
+        'tkinter',
+        'itertools',
+        'pickle',
+        'pandas'
+    ],
+    entry_points={
+        'console_scripts': [
+            'xxxxxxxxxx = main',
+        ],
+    },
+)
