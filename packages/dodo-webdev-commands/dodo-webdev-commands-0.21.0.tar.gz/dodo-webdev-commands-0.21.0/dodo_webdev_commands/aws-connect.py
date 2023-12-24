b2c9c15634207c508862a8f984577e4c21d8998d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:17 +0000

    Add package dodo-webdev-commands/0.21.0/dodo-webdev-commands-0.21.0.tar.gz

---
+++ packages/dodo-webdev-commands/dodo-webdev-commands-0.21.0.tar.gz/dodo-webdev-commands-0.21.0/dodo_webdev_commands/aws-connect.py
@@ -0,0 +1,39 @@
+from argparse import ArgumentParser
+
+from dodo_commands import Dodo
+
+
+def _args():
+    parser = ArgumentParser(description="")
+    choices = Dodo.get_config("/AWS/profiles", {}).keys()
+    parser.add_argument("profile", choices=choices)
+
+    # Use the parser to create the command arguments
+    args = Dodo.parse_args(parser, config_args=[])
+
+    return args
+
+
+# Use safe=False if the script makes changes other than through Dodo.run
+if Dodo.is_main(__name__, safe=True):
+    args = _args()
+    region = Dodo.get_config("/AWS/region", "eu-central-1")
+    profiles = Dodo.get_config("/AWS/profiles", {})
+    profile = profiles[args.profile]
+
+    Dodo.run(["aws", "configure", "sso", f"--profile={profile}"])
+    Dodo.run(["aws", "sts", "get-caller-identity", f"--profile={profile}"])
+
+    print("Connecting to tailscale")
+    Dodo.run(["sudo", "tailscale", "up", "--accept-dns", "--accept-routes"])
+    Dodo.run(
+        [
+            "aws",
+            "eks",
+            "update-kubeconfig",
+            f"--region={region}",
+            f"--name={profile}-eks",
+            f"--alias={profile}",
+        ],
+        variable_map=dict(AWS_PROFILE=profile),
+    )
---
+++ packages/dodo-webdev-commands/dodo-webdev-commands-0.21.0.tar.gz/dodo-webdev-commands-0.21.0/dodo_webdev_commands/kubectl-cp.py
@@ -0,0 +1,24 @@
+from argparse import ArgumentParser
+
+from dodo_commands import Dodo
+from dodo_commands.extra.dodo_docker_commands.decorators.kubectl import (
+    Decorator as KubeCtlDecorator,
+)
+
+
+def _args():
+    parser = ArgumentParser(description="")
+    parser.add_argument("filename")
+
+    # Use the parser to create the command arguments
+    args = Dodo.parse_args(parser, config_args=[])
+
+    return args
+
+
+# Use safe=False if the script makes changes other than through Dodo.run
+if Dodo.is_main(__name__, safe=True):
+    args = _args()
+    container_name = KubeCtlDecorator.get_full_container_name(Dodo.get_config)
+
+    Dodo.run(["kubectl", "cp", args.filename, f"{container_name}:/app"])
