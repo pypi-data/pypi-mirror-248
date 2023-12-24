ce8003ecf47418b6ee82a28704df6eb83a9913e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 15:03:28 +0000

    Add package fuzzy-couscous/3.1.2/fuzzy_couscous-3.1.2.tar.gz

---
+++ packages/fuzzy-couscous/fuzzy_couscous-3.1.2.tar.gz/fuzzy_couscous-3.1.2/README.md
@@ -0,0 +1,98 @@
+# fuzzy-couscous
+
+> [!WARNING]
+> Please note that this project, fuzzy-couscous, will no longer receive further updates. Instead, check out its cooler and older brother, 
+> falco at: [https://github.com/tobi-de/falco](https://github.com/tobi-de/falco).
+
+<img align="right" width="170" height="170" src="https://res.cloudinary.com/dgugjkmqg/image/upload/v1672335414/Dream_TradingCard_bw46ec.png">
+
+[![CD](https://github.com/Tobi-De/fuzzy-couscous/actions/workflows/deploy.yml/badge.svg)](https://github.com/Tobi-De/fuzzy-couscous/actions/workflows/deploy.yml)
+[![CI](https://github.com/Tobi-De/fuzzy-couscous/actions/workflows/test.yml/badge.svg)](https://github.com/Tobi-De/fuzzy-couscous/actions/workflows/test.yml)
+[![pypi](https://badge.fury.io/py/fuzzy-couscous.svg)](https://pypi.org/project/fuzzy-couscous/)
+[![Docs: Mkdocs](https://img.shields.io/badge/mkdocs-docs-blue.svg)](https://tobi-de.github.io/fuzzy-couscous)
+[![Ruff](https://img.shields.io/endpoint?url=https://raw.githubusercontent.com/astral-sh/ruff/main/assets/badge/v2.json)](https://github.com/astral-sh/ruff)
+[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/Tobi-De/fuzzy-couscous/blob/main/LICENSE)
+
+✨📚✨ [Read the full documentation](https://tobi-de.github.io/fuzzy-couscous)
+
+A cli tool based on [django's startproject --template](https://docs.djangoproject.com/en/stable/ref/django-admin/#startproject) to bootstrap
+your django projects with a modern stack. The project template is heavily inspired by [cookiecutter-django](https://github.com/cookiecutter/cookiecutter-django) but is meant to be a simpler version.
+The cli also comes with [additional commands](https://tobi-de.github.io/fuzzy-couscous/usage/#cuzzy) to hopefully improve your django development experience.
+
+![showcase gif](https://raw.githubusercontent.com/Tobi-De/fuzzy-couscous/main/docs/assets/cuzzy_demo.gif)
+
+## Features
+
+- Django 4+
+- Python 3.10+
+- Frontend: [htmx](https://htmx.org/) with [editor support](https://oluwatobi.dev/blog/posts/htmx-support-in-pycharm/) using [web-types](https://github.com/JetBrains/web-types#web-types)
+- Template fragment with [django-template-partials](https://github.com/carltongibson/django-template-partials)
+- Secure production settings, https only.
+- Settings using [django-environ](https://github.com/joke2k/django-environ)
+- Login / signup via [django-allauth](https://github.com/pennersr/django-allauth)
+- Login using email instead of username
+- Automatically reload your browser in development via [django-browser-reload](https://github.com/adamchainz/django-browser-reload)
+- Better development experience with [django-fastdev](https://github.com/boxed/django-fastdev)
+- [Amazon SES](https://aws.amazon.com/ses/?nc1=h_ls) for production email via [Anymail](https://github.com/anymail/django-anymail)
+- [Docker](https://www.docker.com/) ready for production
+- Optional production cache settings using the `CACHE_URL` or `REDIS_URL` environment variables.
+- `captain-definition` for deploying to [caprover](https://caprover.com/)
+- [Sentry](https://sentry.io/welcome/) for performance/error monitoring
+- Serve static files with [Whitenoise](https://whitenoise.evans.io/en/latest/)
+- Default integration with [pre-commit](https://github.com/pre-commit/pre-commit) for identifying simple issues before submission to code review
+- Integrated task runner with [poethepoet](https://github.com/nat-n/poethepoet)
+- Dependency management using [poetry](https://github.com/python-poetry/poetry) (can be replaced by [virtualenv](https://github.com/pypa/virtualenv) using the [`remove-poetry` command](https://tobi-de.github.io/fuzzy-couscous/usage/#cuzzy-remove-poetry))
+
+## Templates
+
+I use github branches to create variations of the base template.
+
+- [main](https://github.com/Tobi-De/fuzzy-couscous): The base template
+- [tailwind](https://github.com/Tobi-De/fuzzy-couscous/tree/tailwind): The base template + [tailwindcss](https://github.com/timonweb/pytailwindcss)  via [pytailwindcss](https://github.com/timonweb/pytailwindcss)
+- [bootstrap](https://github.com/Tobi-De/fuzzy-couscous/tree/bootstrap): The base template + [bootstrap5](https://getbootstrap.com/) via [django-bootstrap5](https://github.com/zostera/django-bootstrap5)
+
+> **Note**: If some of my decisions about the project template don't make sense to you, read [this section](https://tobi-de.github.io/fuzzy-couscous/project/) of the documentation.
+
+## Quickstart
+
+Install the latest version of the package
+
+```shell
+pip install fuzzy-couscous --upgrade
+```
+
+Initialize a new project
+
+```shell
+cuzzy make project_name
+```
+
+## Development
+
+Poetry is required (not really, you can set up the environment however you want and install the requirements
+manually) to set up a virtualenv, install it then run the following:
+
+```sh
+pre-commit install --install-hooks
+```
+
+Tests can then be run quickly in that environment:
+
+```sh
+pytest
+```
+
+## Feedback
+
+If you have any feedback, please reach out to me at tobidegnon@proton.me or [open a discussion](https://github.com/Tobi-De/fuzzy-couscous/discussions/new).
+
+## Contributors
+
+<!-- ALL-CONTRIBUTORS-LIST:START - Do not remove or modify this section -->
+<!-- prettier-ignore-start -->
+<!-- markdownlint-disable -->
+
+<!-- markdownlint-restore -->
+<!-- prettier-ignore-end -->
+
+<!-- ALL-CONTRIBUTORS-LIST:END -->
---
+++ packages/fuzzy-couscous/fuzzy_couscous-3.1.2.tar.gz/fuzzy_couscous-3.1.2/fuzzy_couscous/commands/remove_poetry/__init__.py
@@ -0,0 +1,169 @@
+from __future__ import annotations
+
+from copy import deepcopy
+from pathlib import Path
+
+import typer
+from dict_deep import deep_del
+from dict_deep import deep_get
+from dict_deep import deep_set
+from rich import print as rich_print
+from rich.progress import Progress
+from rich.progress import SpinnerColumn
+from rich.progress import TextColumn
+
+from . import config_converters
+from ...utils import get_current_dir_as_project_name
+from ...utils import RICH_COMMAND_MARKER
+from ...utils import RICH_COMMAND_MARKER_END
+from ...utils import RICH_ERROR_MARKER
+from ...utils import RICH_INFO_MARKER
+from ...utils import RICH_SUCCESS_MARKER
+from ...utils import write_toml
+from .utils import get_updated_poe_tasks
+from .utils import is_valid_poetry_project
+from .virtualenv_ import compile_requirements
+from .virtualenv_ import install_dependencies
+from .virtualenv_ import new_virtualenv
+
+__all__ = ["remove_poetry"]
+
+
+def remove_poetry(
+    create_virtualenv: bool = typer.Option(
+        False,
+        "-c",
+        "--create-virtualenv",
+        is_flag=True,
+        help="Create an environment using virtualenv",
+    ),
+    pyproject_file: Path = typer.Argument(Path("pyproject.toml"), hidden=True),
+    project_name: str = typer.Argument(
+        "", callback=get_current_dir_as_project_name, hidden=True
+    ),
+) -> None:
+    """
+    Run this command to remove poetry as a dependency from your project, it updates your pyproject.toml file
+    to use HATCH as the build-system and can optionally create a virtual environment using virtualenv.
+    """
+
+    old_config, error_message = is_valid_poetry_project(pyproject_file)
+
+    if error_message:
+        rich_print(f"{RICH_ERROR_MARKER} {error_message}")
+        raise typer.Abort()
+
+    new_config = deepcopy(old_config)
+
+    build_system = config_converters.convert_build_backend()
+    new_config.update({"build-system": build_system})
+
+    # project basic metadata, name, description, etc.
+    project_details = config_converters.convert_project_details(
+        new_config, default_project_name=project_name
+    )
+    new_config.update({"project": project_details})
+
+    project_urls = config_converters.convert_project_urls(new_config)
+    deep_set(new_config, "project.urls", project_urls)
+
+    project_scripts = config_converters.convert_project_scripts(new_config)
+    deep_set(new_config, "project.scripts", project_scripts)
+
+    python_requirement = config_converters.convert_python_requirement(new_config)
+    deep_set(new_config, "project.requires-python", python_requirement)
+
+    authors = config_converters.convert_authors(new_config)
+    deep_set(new_config, "project.authors", authors)
+
+    project_dependencies = config_converters.convert_project_dependencies(new_config)
+    deep_set(new_config, "project.dependencies", project_dependencies)
+
+    optional_dependencies = config_converters.convert_optional_dependencies(new_config)
+    deep_set(new_config, "project.optional-dependencies", optional_dependencies)
+
+    # remove poetry config
+    deep_del(new_config, "tool.poetry")
+
+    # remove poetry.lock if is exists
+    Path("poetry.lock").unlink(missing_ok=True)
+
+    msg = (
+        f"{RICH_SUCCESS_MARKER} Poetry has been removed as a dependency from your project, "
+        f"check the updated pyproject.toml to see what has changed."
+    )
+
+    write_toml(pyproject_file, new_config)
+
+    rich_print(msg)
+
+    if not create_virtualenv:
+        return
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Creating virtualenv... :boom:", total=None)
+        new_virtualenv()
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(
+            description="Compiling requirements.txt... :boom:", total=None
+        )
+        dev_group = deep_get(new_config, "project.optional-dependencies.dev")
+        groups = ["dev"] if dev_group else []
+        compile_requirements(groups=groups)
+
+    with Progress(
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        transient=True,
+    ) as progress:
+        progress.add_task(description="Installing dependencies... :boom:", total=None)
+        install_dependencies()
+
+    msg = (
+        f"{RICH_INFO_MARKER} A new environment has been created using virtualenv, "
+        f"you activate it with the command {RICH_COMMAND_MARKER}source venv/bin/activate"
+    )
+    msg += (
+        f"\n{RICH_INFO_MARKER} To install your dependencies you need to generate a "
+        f"requirements.txt file with \n"
+        f"{RICH_COMMAND_MARKER}pip-compile -o requirements.txt pyproject.toml --resolver=backtracking"
+    )
+
+    at_least_one_group_defined = bool(
+        deep_get(new_config, "project.optional-dependencies")
+    )
+
+    if at_least_one_group_defined:
+        msg += (
+            f"\n{RICH_INFO_MARKER} Your project defines optional dependencies, to generate a requirements.txt file "
+            f"that includes the dependencies of a group, add a "
+            f"{RICH_COMMAND_MARKER}--extra group_name{RICH_COMMAND_MARKER_END} option to the pip-compile command"
+        )
+
+    msg += f"\n{RICH_INFO_MARKER} For the first run, we have already compiled and installed the dependencies for you."
+
+    if poe_tasks := get_updated_poe_tasks(new_config):
+        deep_set(new_config, "tool.poe.tasks", poe_tasks)
+        msg += (
+            f"\n{RICH_INFO_MARKER} poethepoet was found in your pyproject.toml file, a task to generate the "
+            f"requirements.txt file was added, run it with {RICH_COMMAND_MARKER} poe d"
+        )
+
+    write_toml(pyproject_file, new_config)
+
+    # a note on using pip-sync
+    msg += (
+        f"\n{RICH_INFO_MARKER} Run {RICH_COMMAND_MARKER}pip-sync{RICH_COMMAND_MARKER_END} to install the dependencies, "
+        "more infos on pip-tools docs https://pip-tools.readthedocs.io/en/latest/"
+    )
+
+    rich_print(msg)
