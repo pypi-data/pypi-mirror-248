edd70e26853c9bb9702d99f2f7bb2b21ce3615a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:05:19 +0000

    Add package EasyObj/0.2.3/EasyObj-0.2.3.tar.gz

---
+++ packages/EasyObj/EasyObj-0.2.3.tar.gz/EasyObj-0.2.3/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+VERSION = '0.2.3'
+
+with open("README.md", "r", encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='EasyObj',
+    version=VERSION,
+    description='EasyObj lets you manipulate objects as easily as in JavaScript.',
+    packages=["EasyObj"],
+    zip_safe=False,
+    url="https://github.com/Howardzhangdqs/EasyObj",
+    python_requires=">=3.8",
+    author_email="zjh@shanghaiit.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+    ),
+)
