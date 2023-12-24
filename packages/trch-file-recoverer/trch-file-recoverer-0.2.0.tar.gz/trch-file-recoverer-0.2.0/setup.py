8e523ab9c0f578323d606fec6363c6f55c0c928f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:05:35 +0000

    Add package trch-file-recoverer/0.2.0/trch-file-recoverer-0.2.0.tar.gz

---
+++ packages/trch-file-recoverer/trch-file-recoverer-0.2.0.tar.gz/trch-file-recoverer-0.2.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='trch-file-recoverer',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'recover-files=file_recoverer.file_recoverer:main',
+        ],
+    },
+    # NEW ADDITIONS BELOW
+    long_description=long_description,  # Set the long description to the README contents
+    long_description_content_type='text/markdown',  # Specify the content type as Markdown
+)
