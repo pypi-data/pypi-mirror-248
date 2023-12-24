494ffcbf9029b9e5e972fca779c7f8bfc9789943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:03:09 +0000

    Add package SpecialPurpose/0.3/SpecialPurpose-0.3.tar.gz

---
+++ packages/SpecialPurpose/SpecialPurpose-0.3.tar.gz/SpecialPurpose-0.3/README.md
@@ -0,0 +1,3 @@
+This package is designed to support MonieCrypt (Cryptography tool).
+Cyptography Class / JU
+semester of 2023-2023
---
+++ packages/SpecialPurpose/SpecialPurpose-0.3.tar.gz/SpecialPurpose-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SpecialPurpose",
+    version="0.3",
+    author="Sara , Sm6r",
+    long_description="A special package for MonieCrypt (Crypto tool) designed for a Cryptography assignment /JU"
+    )
+
