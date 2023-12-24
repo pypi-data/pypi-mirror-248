cca73b27889b8ae22a901d7d72bfe6aa3436e9bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:03:09 +0000

    Add package ntt-command-service/1.0.3/ntt-command-service-1.0.3.tar.gz

---
+++ packages/ntt-command-service/ntt-command-service-1.0.3.tar.gz/ntt-command-service-1.0.3/ntt_command_service/CommandService.py
@@ -0,0 +1,34 @@
+from typing import *
+from .ICommand import ICommand
+from .ICommandService import ICommandService
+from .Exceptions import *
+from ntt_signal import *
+
+
+class CommandService(Signal, ICommandService):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self._iCommands: List[ICommand] = []
+
+    def AddCommand(self, iCommand: ICommand) -> None:
+        if iCommand.CanExecute():
+            iCommand.Execute()
+            self._iCommands.append(iCommand)
+            self.Emit()
+
+    def Undo(self) -> None:
+        if self.CanUndo():
+            self._iCommands[-1].Undo()
+            self._iCommands.pop()
+            self.Emit()
+        else:
+            raise CommandServiceEmptyError("Command Service has no command")
+
+    def CanUndo(self) -> bool:
+        return len(self._iCommands) != 0
+
+    def Clear(self) -> None:
+        if self.CanUndo():
+            self.Emit()
+        self._iCommands = []
