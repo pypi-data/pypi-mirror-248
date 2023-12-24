080740602a4bb35303de9753a13eeedba686d4cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 04:03:27 +0000

    Add package alexisomggeneratelatextable/0.0.2/alexisomggeneratelatextable-0.0.2.tar.gz

---
+++ packages/alexisomggeneratelatextable/alexisomggeneratelatextable-0.0.2.tar.gz/alexisomggeneratelatextable-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+  name='alexisomggeneratelatextable',
+  version='0.0.2',
+  author='AlexisOMG',
+  author_email='alesha-gramazeka@yandex.ru',
+  description='ДЗ по Python ',
+  packages=find_packages(include=['alexisomggeneratelatextable']),
+  python_requires='>=3.8',
+  project_urls={
+      'GitHub': 'https://github.com/AlexisOMG/mhs-python'
+  }
+)
