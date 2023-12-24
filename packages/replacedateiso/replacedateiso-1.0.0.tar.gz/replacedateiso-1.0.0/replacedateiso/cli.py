889cd05a9c63d4beef3bc874f524eb4f5a4bb033
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:03:27 +0000

    Add package replacedateiso/1.0.0/replacedateiso-1.0.0.tar.gz

---
+++ packages/replacedateiso/replacedateiso-1.0.0.tar.gz/replacedateiso-1.0.0/replacedateiso/cli.py
@@ -0,0 +1,30 @@
+"""CLI interface for replacedateiso project.
+
+Be creative! do whatever you want!
+
+- Install click or typer and create a CLI app
+- Use builtin argparse
+- Start a web application
+- Import things from your .base module
+"""
+from pathlib import Path
+import re, os
+
+def main():  # pragma: no cover
+    """
+    The main function executes on commands:
+    `python -m replacedateiso` and `$ replacedateiso `.
+
+    This is your program's entry point.
+    """
+
+    for file in Path.cwd().iterdir():
+    match = re.search('\d{2}.\d{2}.\d{4}', file.name)
+    if match != None:
+        dateparam = match.group().split(".")
+        day = dateparam[0]
+        month = dateparam[1]
+        year = dateparam[2]
+        newname = file.name.replace(match.group(), year + "-" + month + "-" + day)
+        print("Found Date:" + file.name + " [ " + match.group() + " ] -> " + newname)
+        os.rename(file, file.parent / newname)
