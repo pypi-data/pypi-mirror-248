f423d8b126dab6a320899d537deb0369726d6144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 15:03:46 +0000

    Add package mockafka-py/0.1.45/mockafka_py-0.1.45-py3-none-any.whl

---
+++ packages/mockafka-py/mockafka_py-0.1.45-py3-none-any.whl/mockafka/__init__.py
@@ -0,0 +1,16 @@
+from .admin_client import FakeAdminClientImpl
+from .producer import FakeProducer
+from .conumser import FakeConsumer
+from .message import Message
+from .decorators import produce, bulk_produce, setup_kafka, consume
+
+__all__ = [
+    "FakeProducer",
+    "FakeConsumer",
+    "FakeAdminClientImpl",
+    "Message",
+    "produce",
+    "bulk_produce",
+    "setup_kafka",
+    "consume"
+]
