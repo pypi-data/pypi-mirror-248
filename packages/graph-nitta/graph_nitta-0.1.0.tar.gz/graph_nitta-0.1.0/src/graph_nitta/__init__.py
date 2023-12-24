75e3d3311532077b67d4284623e12877984fdac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:04:09 +0000

    Add package graph-nitta/0.1.0/graph_nitta-0.1.0.tar.gz

---
+++ packages/graph-nitta/graph_nitta-0.1.0.tar.gz/graph_nitta-0.1.0/src/graph_nitta/__init__.py
@@ -0,0 +1,8 @@
+from .axes.config import AxConfig as AxConfig
+from .axes.config import LegendLoc as LegendLoc
+from .axes.config import Scale as Scale
+from .axes.config import SpineConfig as SpineConfig
+from .axes.resolver import apply_ax_config as apply_ax_config
+from .axes.resolver import apply_x_config as apply_x_config
+from .axes.resolver import apply_y_config as apply_y_config
+from .graph import make_graph as make_graph
