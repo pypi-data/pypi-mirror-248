f052b03595f797a995663a592db9032bfd2b720b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:03:21 +0000

    Add package wilmaandelmo/0.0.15/wilmaandelmo-0.0.15.tar.gz

---
+++ packages/wilmaandelmo/wilmaandelmo-0.0.15.tar.gz/wilmaandelmo-0.0.15/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="wilmaandelmo",
+    version="0.0.15",
+    author="Julian Marx",
+    description="For L",
+    packages=setuptools.find_packages(),
+    license = "MIT",
+    keywords = ["christmas"]
+)
