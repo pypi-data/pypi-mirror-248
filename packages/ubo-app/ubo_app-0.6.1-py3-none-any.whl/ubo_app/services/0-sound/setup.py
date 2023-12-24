3d777d7e01d21803ceae4d5ba12c390f499be5cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:02:59 +0000

    Add package ubo-app/0.6.1/ubo_app-0.6.1-py3-none-any.whl

---
+++ packages/ubo-app/ubo_app-0.6.1-py3-none-any.whl/ubo_app/services/0-sound/setup.py
@@ -0,0 +1,20 @@
+# ruff: noqa: D100, D101, D102, D103, D104, D107, N999
+from __future__ import annotations
+
+from ubo_app.store import dispatch
+from ubo_app.store.status_icons import (
+    StatusIconsRegisterAction,
+    StatusIconsRegisterActionPayload,
+)
+
+
+def init_service() -> None:
+    dispatch(
+        StatusIconsRegisterAction(
+            payload=StatusIconsRegisterActionPayload(
+                icon='mic_off',
+                priority=-2,
+                id='sound_mic_status',
+            ),
+        ),
+    )
