354b10ee5d6ad48f05ba7766f9171e48c640b393
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 03:05:35 +0000

    Add package obscure_stats/0.1.8/obscure_stats-0.1.8-py3-none-any.whl

---
+++ packages/obscure_stats/obscure_stats-0.1.8-py3-none-any.whl/obscure_stats/variation/__init__.py
@@ -0,0 +1,21 @@
+"""Variation module."""
+
+from .variation import (
+    avdev,
+    b_index,
+    gibbs_m1,
+    gibbs_m2,
+    mod_vr,
+    negative_extropy,
+    range_vr,
+)
+
+__all__ = [
+    "avdev",
+    "b_index",
+    "gibbs_m1",
+    "gibbs_m2",
+    "mod_vr",
+    "negative_extropy",
+    "range_vr",
+]
