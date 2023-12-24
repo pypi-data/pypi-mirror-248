28b34612819177397a9afdfff0a6c0b52f26d5e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-24 14:03:07 +0000

    Add package mytestlib-hitsuji/1.0.0/mytestlib-hitsuji-1.0.0.tar.gz

---
+++ packages/mytestlib-hitsuji/mytestlib-hitsuji-1.0.0.tar.gz/mytestlib-hitsuji-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='mytestlib-hitsuji',
+      version='1.0.0',
+      url='https://www.yahoo.co.jp/',
+      author='hitsuji',
+      author_email='takeuchi_masat0@yahoo.co.jp',
+      description='My Test Library.',
+      packages=find_packages()
+      )
