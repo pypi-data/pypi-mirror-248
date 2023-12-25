71774fe2285fa20f161225323677dacbeeae5bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:32 +0000

    Add package endpointsplitter/0.0.1/endpointsplitter-0.0.1.tar.gz

---
+++ packages/endpointsplitter/endpointsplitter-0.0.1.tar.gz/endpointsplitter-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic URL splitter tool'
+LONG_DESCRIPTION = 'A basic tool for Endpoint Splitting and output the data in a file.'
+
+# Setting up
+setup(
+    name="endpointsplitter",
+    version=VERSION,
+    author="Thejas hari",
+    author_email="<thejaskala308@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'url', 'url-splitter', 'endpoint splitter'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
