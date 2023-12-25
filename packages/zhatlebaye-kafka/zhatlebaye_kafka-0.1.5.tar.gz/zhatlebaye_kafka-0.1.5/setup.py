9913bb0015dc94eae4b20515f38f9facf42f4c8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 04:03:02 +0000

    Add package zhatlebaye-kafka/0.1.5/zhatlebaye_kafka-0.1.5.tar.gz

---
+++ packages/zhatlebaye-kafka/zhatlebaye_kafka-0.1.5.tar.gz/zhatlebaye_kafka-0.1.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='zhatlebaye_kafka',
+    version='0.1.5',
+    packages=find_packages(),
+    description='An extension to the aiokafka library with enhanced producer and consumer functionalities',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Yerlan Yesmoldin',
+    author_email='e_esmoldin@kbtu.kz',
+    url='https://gitlab.com/di-halyk-academy-zhatlebaye/zhatlebaye-kafka',
+    install_requires=[
+        'aiokafka',
+        'loguru',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.10',
+)
---
+++ packages/zhatlebaye-kafka/zhatlebaye_kafka-0.1.5.tar.gz/zhatlebaye_kafka-0.1.5/zhatlebaye_kafka/producer.py
@@ -0,0 +1,77 @@
+import asyncio
+import json
+
+from aiokafka import AIOKafkaProducer
+from loguru import logger
+
+
+class KafkaProducer:
+    def __init__(self, bootstrap_servers: str):
+        self.bootstrap_servers = bootstrap_servers
+        self._producer = None
+
+    async def __aenter__(self):
+        try:
+            await self.connect()
+        except Exception as e:
+            logger.error('Error while starting Kafka producer', error=e)
+        return self
+
+    async def __aexit__(self, exc_type, exc_value, traceback):
+        try:
+            await self.disconnect()
+        except Exception as e:
+            logger.error('Error while stopping Kafka producer', error=e)
+
+    async def connect(self):
+        if self._producer is None:
+            self._producer = AIOKafkaProducer(bootstrap_servers=self.bootstrap_servers)
+            await self._producer.start()
+
+            logger.info('Kafka producer started')
+
+    async def disconnect(self):
+        if self._producer is not None:
+            await self._producer.stop()
+
+    async def send_message(self, topic, message, callback=None):
+        try:
+            serialized_message = self._serialize_message(message)
+            if callback:
+                result = await self._producer.send(topic, serialized_message)
+                await callback(result)
+            else:
+                await self._producer.send_and_wait(topic, serialized_message)
+        except Exception as e:
+            logger.error('Error while sending message to Kafka', error=e, payload=message)
+
+    def send_message_sync(self, topic, message):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.send_message(topic, message))
+
+    @staticmethod
+    def _serialize_message(message):
+        return json.dumps(message).encode()
+
+
+class KafkaProducerSingleton:
+    _producer = None
+
+    @classmethod
+    async def connect(cls, bootstrap_servers: str):
+        if cls._producer is None:
+            cls._producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+            await cls._producer.connect()
+
+    @classmethod
+    async def disconnect(cls):
+        if cls._producer is not None:
+            await cls._producer.disconnect()
+
+    @classmethod
+    async def send_message(cls, topic, message, callback=None):
+        await cls._producer.send_message(topic, message, callback)
+
+    @classmethod
+    def send_message_sync(cls, topic, message):
+        cls._producer.send_message_sync(topic, message)
