a6d116ce2107ea52868489f3cdfebba5c308c453
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:31 +0000

    Add package PyFormCode/1.1.2/PyFormCode-1.1.2.tar.gz

---
+++ packages/PyFormCode/PyFormCode-1.1.2.tar.gz/PyFormCode-1.1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyFormCode',
+    version='1.1.2',
+    description='PyForm',
+    author='dwashere',
+    author_email='den.langeto45@yandex.ru',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
