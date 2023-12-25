e3cc4c0c3c11cd644621a01f858da0766c119a9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:03:34 +0000

    Add package hello-tiida/0.2/hello_tiida-0.2.tar.gz

---
+++ packages/hello-tiida/hello_tiida-0.2.tar.gz/hello_tiida-0.2/README.md
@@ -0,0 +1,14 @@
+# hello_tiida
+
+
+## インストール方法
+
+```bash
+pip install hello-tiida
+```
+
+## how to use
+``` python
+from hello_tiida.main import *
+hello_tiida()
+```
---
+++ packages/hello-tiida/hello_tiida-0.2.tar.gz/hello_tiida-0.2/hello_tiida/main.py
@@ -0,0 +1,13 @@
+
+class tiida(object):
+    def __init__(self):
+        self.name = 'tiida'
+        self.height = 178
+        self.weight = 77
+
+    def hello_tiida(self):
+        print(f"hello {self.name}")
+        return f"hello {self.name}"
+
+def hello_tiida():
+    print('hello tiida')
