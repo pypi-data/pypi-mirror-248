5eb1440615f893c707fd9f220d4725e63b5d452c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:35 +0000

    Add package falco-cli/0.0.29/falco_cli-0.0.29.tar.gz

---
+++ packages/falco-cli/falco_cli-0.0.29.tar.gz/falco_cli-0.0.29/demo/demo/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+from django.urls import reverse_lazy
+from django.views.generic.base import RedirectView
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url=reverse_lazy("products:product_list"))),
+    path("products/", include("products.urls", namespace="products")),
+    # path("orders/", include("orders.urls", namespace="orders")),
+]
