16553bd5a0cd90973bf420399d890e55d168d9d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 15:03:29 +0000

    Add package capsphere/0.3.62rc0/capsphere-0.3.62rc0-py3-none-any.whl

---
+++ packages/capsphere/capsphere-0.3.62rc0-py3-none-any.whl/capsphere/service/test/test_data.py
@@ -0,0 +1,10 @@
+LOAN_PAYMENT_DICT = {
+    "loan_amount_paid": 23406.72,
+    "loan_payment_ref_no": "LPAY_27920236513CF1234567",
+    "loan_paid_uid": "PX1051801b4e0311c123",
+    "loan_id": 235,
+    "loan_payment_type": "fpx",
+    "manual_payment_proof_file": None,
+    "user_name": "Gerry",
+    "user_id": "1813"
+}
