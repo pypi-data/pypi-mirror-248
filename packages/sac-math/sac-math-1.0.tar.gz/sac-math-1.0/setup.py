53782e7bc957be4509945650a3b14a547e618a57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:44 +0000

    Add package sac-math/1.0/sac-math-1.0.tar.gz

---
+++ packages/sac-math/sac-math-1.0.tar.gz/sac-math-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sac-math',
+      version='1.0',
+      description='sample python package',
+      packages=['sac-math'],
+      author_email='sac_erande@yahoo.co.in',
+      zip_safe=False)
