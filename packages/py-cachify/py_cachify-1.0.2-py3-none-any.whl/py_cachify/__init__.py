feaab0cb863873c6fc2b79f3946faf9f1889cb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:35 +0000

    Add package py-cachify/1.0.2/py_cachify-1.0.2-py3-none-any.whl

---
+++ packages/py-cachify/py_cachify-1.0.2-py3-none-any.whl/py_cachify/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from .backend.exceptions import CachifyInitError
+from .backend.lib import init_cachify
+from .cached import cached
+from .exceptions import CachifyLockError
+from .lock import async_lock, lock, once
+
+
+__all__ = ['CachifyLockError', 'CachifyInitError', 'init_cachify', 'cached', 'async_lock', 'lock', 'once']
