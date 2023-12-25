4141f08f35db4a408630d763dc9235fa307f7d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:32 +0000

    Add package directory-splitter/1.0.2/directory-splitter-1.0.2.tar.gz

---
+++ packages/directory-splitter/directory-splitter-1.0.2.tar.gz/directory-splitter-1.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='directory-splitter',
+    version='1.0.2',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'argparse',
+       
+    ],
+    entry_points={
+        'console_scripts': [
+            'directory-splitter = tester1:print_url',
+        ],
+    },
+    
+    description='A script to split and extract unique endpoints from a list of URLs.',
+    license='MIT',
+    keywords='directory-splitter url-parser',
+    
+)
