42c1bf5497c4b8a7d3a9e1c35a7c67afff9a3091
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 15:02:56 +0000

    Add package iscasmodel/0.8/iscasmodel-0.8.tar.gz

---
+++ packages/iscasmodel/iscasmodel-0.8.tar.gz/iscasmodel-0.8/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name="iscasmodel",
+    version="0.8",
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # 如果你用requests替代urllib，需要添加这个依赖
+        'accelerate'
+    ],
+    author="ls",
+    author_email="your.email@example.com",
+    description="A library to send accuracy data",
+    long_description="A longer description of your library",
+    url="https://github.com/yourusername/mylibrary",
+)
