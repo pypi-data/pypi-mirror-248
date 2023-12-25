a50b824135dccfcc0acd0a9c544676392cc08aaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:06 +0000

    Add package test-matstat/1/test_matstat-1.tar.gz

---
+++ packages/test-matstat/test_matstat-1.tar.gz/test_matstat-1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='test_matstat',
+      version='1',
+      description='none',
+      packages=['test_matstat'],
+      author_email='fisefag888@vkr1.com',
+      zip_safe=False)
