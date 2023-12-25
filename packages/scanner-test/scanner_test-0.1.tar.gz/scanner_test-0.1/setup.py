e763b8bcd529be7c54fb0739acb13c3073d3743a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:32 +0000

    Add package scanner-test/0.1/scanner_test-0.1.tar.gz

---
+++ packages/scanner-test/scanner_test-0.1.tar.gz/scanner_test-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scanner_test",
+    version="0.1",
+    author="lolb",
+    description="HuggingFace integration",
+    license='MIT'
+)
