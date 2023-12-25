74bf16a8805690991db7b0e849d30cec771ddd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 14:03:46 +0000

    Add package Faab/0.1.9/Faab-0.1.9.tar.gz

---
+++ packages/Faab/Faab-0.1.9.tar.gz/Faab-0.1.9/Faab/FaabFunction.py
@@ -0,0 +1,371 @@
+import json
+from functools import wraps
+
+from flasgger import swag_from
+from flask import request, g, send_file
+from sqlalchemy import and_
+from sqlalchemy.orm import class_mapper
+from flask_sqlalchemy import SQLAlchemy
+
+from Faab.FaabJWT import login_required
+from Faab.extensions import db
+import pandas as pd
+import io
+
+
+# ......
+
+class AutoUrl:
+    def __init__(self, add_url_list):
+        for i in add_url_list:
+            AutoDB(i["model"], i["bp"], i["url_prefix"])
+
+
+class AutoDB:
+    model = {}
+    bp = object
+    url_name = ""
+
+    def __init__(self, model, bp, url_name):
+
+        self.model = model
+        self.bp = bp
+        self.url_name = url_name
+        self.bp.add_url_rule('/' + url_name + '/get', endpoint=bp.name + url_name + 'get',
+                             view_func=self.get,
+                             methods=['GET'])
+        self.bp.add_url_rule('/' + url_name + '/get_one', endpoint=bp.name + url_name + 'get_one',
+                             view_func=self.get_one,
+                             methods=['GET'])
+        self.bp.add_url_rule('/' + url_name + '/post', endpoint=bp.name + url_name + 'post',
+                             view_func=self.post,
+                             methods=['POST'])
+        self.bp.add_url_rule('/' + url_name + '/delete/<int:one_or_list>/<int:true_del_or_false_del>',
+                             endpoint=bp.name + url_name + 'delete', view_func=self.delete,
+                             methods=['POST'])
+        self.bp.add_url_rule('/' + url_name + '/put', endpoint=bp.name + url_name + 'put',
+                             view_func=self.put,
+                             methods=['POST'])
+        self.bp.add_url_rule('/' + url_name + '/export', endpoint=bp.name + url_name + 'export',
+                             view_func=self.export,
+                             methods=['POST'])
+
+    def list_to_return(self, get_list):
+        """
+            FuncName:列表转返回值
+            Parameter：查询出的结果
+            Return：Http返回值
+        """
+        result = []
+        for item in get_list:
+            data = {}
+            for col in class_mapper(self.model).mapped_table.c:
+                value = str(getattr(item, col.name))
+                if value != 'None':
+                    data[col.name] = value
+                else:
+                    continue
+            result.append(data)
+        return result
+
+    def one_to_return(self, info):
+        """
+            FuncName:单个数据转返回值
+            Parameter：查询出的结果
+            Return：Http返回值
+        """
+        data = {}
+        if info:
+            for col in class_mapper(self.model).mapped_table.c:
+                value = str(getattr(info, col.name))
+                if value != 'None':
+                    data[col.name] = value
+                else:
+                    continue
+            return data
+        else:
+            return {}
+
+    # noinspection ALL
+    def check_request_delete(func):
+        # noinspection PyTypeChecker
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            params = request.json
+            for key, value in params.items():
+                exists = self.check_parameter_exists(key)
+                if not exists:
+                    return {'error': '参数错误', 'code': 0}
+            # noinspection PyCallingNonCallable
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    # noinspection ALL
+    def check_request_export(func):
+        # noinspection PyTypeChecker
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            form = request.json
+            need_export = form.get('need_export')
+            condition = form.get('condition')
+            for key, value in condition.items():
+                if key == "_Own" or key == "_Price":
+                    continue
+                exists = self.check_parameter_exists(key)
+                if not exists:
+                    return {'error': 'a参数错误', 'code': 11}
+            for key, value in need_export.items():
+                exists = self.check_parameter_exists(key)
+                if not exists:
+                    return {'error': 'b参数错误', 'code': 10}
+            # noinspection PyCallingNonCallable
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    # noinspection ALL
+    def check_request_turn(func):
+        # noinspection PyTypeChecker
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            form = request.json
+            need_update = form.get('need_update')
+            condition = form.get('condition')
+            for key, value in condition.items():
+                if key == "_Own":
+                    continue
+                exists = self.check_parameter_exists(key)
+                if not exists:
+                    return {'error': 'a参数错误', 'code': 11}
+            for key, value in need_update.items():
+                exists = self.check_parameter_exists(key)
+                if not exists:
+                    return {'error': 'b参数错误', 'code': 10}
+            # noinspection PyCallingNonCallable
+            return func(self, *args, **kwargs)
+
+        return wrapper
+
+    def check_parameter_exists(self, parameter):
+        mapper = class_mapper(self.model)
+        return hasattr(mapper.column_attrs, parameter)
+
+    @swag_from('swag_config/get.yml')
+    def get(self):
+        params = dict(request.args)
+        _Not_Filter = False
+        if '_Not_Filter' in params:
+            _Not_Filter = json.loads(params.pop('_Not_Filter'))
+        if '_Own' in params:
+            name = params.get('_Own')
+            params[name] = g.username
+            params.pop('_Own')
+        if '_Pagination' not in params:
+            if '_Desc' not in params:
+                query = self.model.query.filter_by(is_delete=0)
+            else:
+                query = self.model.query.filter_by(is_delete=0).order_by(self.model.id.desc())
+                params.pop('_Desc')
+
+            filters = []
+            if '_Search' in params:
+                key = params.pop('_Search')
+                value = params.pop('_Search_value')
+                filters.append(getattr(self.model, key).like('%' + value + '%'))
+
+            for key, value in params.items():
+                filters.append(getattr(self.model, key) == value)
+
+            if _Not_Filter != False:
+                filters.append(getattr(self.model, _Not_Filter['key']) != _Not_Filter['value'])
+
+            if filters:
+                query = query.filter(and_(*filters))
+
+            lists = query.all()
+            return self.list_to_return(lists)
+        else:
+            params.pop('_Pagination')
+            page = int(params.pop('page'))
+            per_page = int(params.pop('per_page'))
+            if '_Desc' not in params:
+                query = self.model.query.filter_by(is_delete=0)
+            else:
+                query = self.model.query.filter_by(is_delete=0).order_by(self.model.id.desc())
+                params.pop('_Desc')
+            filters = []
+            if '_Search' in params:
+                key = params.pop('_Search')
+                value = params.pop('_Search_value')
+                filters.append(getattr(self.model, key).like('%' + value + '%'))
+
+            for key, value in params.items():
+                filters.append(getattr(self.model, key) == value)
+
+            if _Not_Filter != False:
+                filters.append(getattr(self.model, _Not_Filter['key']) != _Not_Filter['value'])
+
+            if filters:
+                query = query.filter(and_(*filters))
+            lists = query.paginate(page=page, per_page=per_page, error_out=False)
+            items = lists.items
+            has_next = lists.has_next
+            has_prev = lists.has_prev
+            total = lists.total
+            pages = lists.pages
+            return {'items': self.list_to_return(items), 'has_next': has_next, 'has_prev': has_prev, 'total': total,
+                    'pages': pages}
+
+    @swag_from('swag_config/get_one.yml')
+    def get_one(self):
+        params = dict(request.args)
+        if '_Own' in params:
+            name = params.get('_Own')
+            params[name] = g.username
+            params.pop('_Own')
+        filters = []
+        query = self.model.query.filter_by(is_delete=0)
+        for key, value in params.items():
+            filters.append(getattr(self.model, key) == value)
+
+        if filters:
+            query = query.filter(and_(*filters))
+        item = query.first()
+
+        return self.one_to_return(item)
+
+    @swag_from('swag_config/post.yml')
+    def post(self):
+        sets = request.json
+        new_item = self.model()
+        for key, value in sets.items():
+            setattr(new_item, key, value)
+        try:
+            db.session.add(new_item)
+            db.session.commit()
+            return {'id': new_item.id, 'code': 1}
+        except Exception as e:
+            print(e)
+            return {'error': e, 'code': -1}
+
+    # noinspection PyArgumentList
+    @swag_from('swag_config/delete.yml')
+    @check_request_delete
+    def delete(self, one_or_list=1, true_del_or_false_del=0):
+        params = request.json
+        query = self.model.query.filter_by(is_delete=0)
+        filters = []
+        for key, value in params.items():
+            filters.append(getattr(self.model, key) == value)
+
+        if filters:
+            query = query.filter(and_(*filters))
+
+        if len(query.all()) > 0:
+            if one_or_list == 1:
+                if true_del_or_false_del == 0:
+                    info = query.first()
+                    info.is_delete = 1
+                else:
+                    query.delete()
+            else:
+                if true_del_or_false_del == 0:
+                    query = query.all()
+                    for i in query:
+                        i.is_delete = 1
+                else:
+                    query.delete(synchronize_session=False)
+            try:
+                db.session.commit()
+                return {'code': 1, 'message': '已成功删除'}
+            except Exception as e:
+                return {'error': e, 'code': -1}
+        else:
+            return {'error': '未查询到数据', 'code': 0}
+
+    # noinspection PyArgumentList
+    @swag_from('swag_config/put.yml')
+    @check_request_turn
+    def put(self):
+        form = request.json
+        query = self.model.query.filter_by(is_delete=0)
+        filters = []
+        need_update = form.get('need_update')
+        condition = form.get('condition')
+        if '_Own' in condition:
+            name = condition.get('_Own')
+            condition[name] = g.username
+            condition.pop('_Own')
+        for key, value in condition.items():
+            filters.append(getattr(self.model, key) == value)
+        if filters:
+            query = query.filter(and_(*filters))
+        if len(query.all()) > 0:
+            for i in query:
+                for key, value in need_update.items():
+                    setattr(i, key, value)
+            try:
+                db.session.commit()
+                return {'code': 1, 'message': '已成功更新', 'num': len(query.all())}
+            except Exception as e:
+                return {'error': e, 'code': -1}
+        else:
+            return {'error': '未查询到匹配数据', 'code': 0}
+
+    # noinspection PyArgumentList
+    @swag_from('swag_config/export.yml')
+    @check_request_export
+    def export(self):
+        form = request.json
+        query = self.model.query.filter_by(is_delete=0)
+        filters = []
+        need_export = form.get('need_export')
+        condition = form.get('condition')
+        _Price = []
+        if "_Price" in form:
+            # 金额化处理
+            _Price = form.get('_Price').split(',')
+        if '_Own' in condition:
+            name = condition.get('_Own')
+            condition[name] = g.username
+            condition.pop('_Own')
+        for key, value in condition.items():
+            filters.append(getattr(self.model, key) == value)
+        if filters:
+            query = query.filter(and_(*filters))
+        db_list = query.all()
+        if len(db_list) > 0:
+            try:
+                return export_to_excel(db_list, need_export, _Price)
+            except Exception as e:
+                return {'error': e, 'code': -1}
+        else:
+            return {'error': '未查询到匹配数据', 'code': 0}
+
+
+def export_to_excel(db_list, need_export, _Price):
+    # 将查询结果转换为DataFrame
+    out_data = []
+    i = 1
+    for obj in db_list:
+        data = {}
+        data.update({'序号': i})
+        for key, value in need_export.items():
+            if key in _Price:
+                data.update({value: str('{:.2f}'.format(getattr(obj, key)/100))})
+            else:
+                data.update({value: str(getattr(obj, key))})
+        out_data.append(data)
+        i += 1
+    df = pd.DataFrame(out_data)
+
+    # 将数据表导出为XLSX文件
+    output = io.BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    writer._save()
+    output.seek(0)
+    # 返回XLSX文件供下载
+    resu = send_file(output, download_name='exported_data.xlsx', as_attachment=True)
+    return resu
---
+++ packages/Faab/Faab-0.1.9.tar.gz/Faab-0.1.9/README.md
@@ -0,0 +1,145 @@
+
+
+<div align="center"><a href="https://github.com/OliverMao/FlaskAutoApiBuilder" ><img width="300" src="https://z1.ax1x.com/2023/11/14/piYkvUU.png" alt="piYkvUU.png" border="0" /></a>
+<h1> 📦 Faab - Flask Auto API Builder</h1><div><a href="https://github.com/OliverMao/FlaskAutoApiBuilder" ><img  src="https://img.shields.io/badge/license-GPL3.0-blue.svg" alt="license" border="0" /></a>
+  <img  src="https://img.shields.io/github/stars/OliverMao/FlaskAutoApiBuilder.svg" alt="stars" border="0" />
+  <img  src="https://img.shields.io/github/forks/OliverMao/FlaskAutoApiBuilder.svg" alt="forks" border="0" />
+  <img  src="https://img.shields.io/badge/version-0.1.6-686480r.svg" alt="forks" border="0" />
+</div></div>
+
+
+
+
+
+
+
+## 项目简介
+
+"Flask Auto API Builder" 是一个基于 Flask 框架的自动 API 构建工具。
+
+本工具可以根据给定的数据模型或数据库模式自动生成 API 端点，包括创建、读取、更新和删除（CRUD）操作。它们可以减少开发人员手动编写和配置 API 端点的工作量，提高开发效率。同时可以生成Swagger文档，便于API调试。此外，Faab还提供了常见的Web工具，如用户鉴权、上传下载等功能，进一步增强了开发者的体验。
+
+## 功能特性
+
+- 用户身份验证：通过用户名和密码进行用户身份验证，确保只有授权用户可以访问受保护的功能。
+- 创建资源：允许用户创建新的资源，例如创建新的文章、任务或事件等。
+- 读取资源：提供对已存在资源的读取访问权限，例如获取文章的详细信息或查看任务列表。
+- 更新资源：允许用户对现有资源进行更新，例如编辑文章内容或修改任务状态。
+- 删除资源：允许用户删除不再需要的资源，例如删除文章、任务或事件等。
+- 搜索功能：提供基于关键字的搜索功能，使用户能够快速找到所需的资源。
+- 分页和排序：支持对资源列表进行分页和排序，以便用户可以方便地浏览和管理大量数据。
+- 文件上传和下载：允许用户上传文件到服务器，并提供下载已上传的文件的功能。
+- 数据验证：对用户输入的数据进行验证，确保数据的完整性和一致性。
+- 错误处理：提供友好的错误提示和处理机制，使用户能够轻松识别和解决问题。
+- 访问控制：基于用户角色和权限，限制用户对敏感资源和功能的访问权限。
+- 日志记录：记录关键操作和事件，以便进行故障排查和审计。
+- API 文档生成：自动生成 API 文档，使开发人员和用户能够了解可用的 API 端点和参数。
+
+## 技术栈与依赖项
+
+- Flask
+- SQLAlchemy
+- flasgger
+- Flask-JWT
+- Flask-CORS
+- Redis
+- PyMySQL
+
+## 快速开始
+
+0. 创建app.py
+1. 创建factory.py、blueprints（根据开发项目自定）
+	
+	> 可直接使用Faab提供的demo进行开发
+	
+2. 安装Faab：
+    ```
+    pip install Faab
+    ```
+3. 项目中引入
+    ```python
+    from Faab import Faab
+    
+    from Faab.FaabJWT import jwt_authentication
+    from blueprints.test import test_bp
+    from blueprints.test.model import Users, TabNavMenu
+    import factory as fac 
+    ```
+4. 配置数据库连接
+    ```python
+    class DBConfig(object):
+        # DB及Flask基础配置
+        user = 'faab'
+        host = 'localhost'
+        password = 'faab'
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://%s:%s@%s:3306/%s' % (user, password, host, 'faab')
+        SQLALCHEMY_BINDS = {
+            'test': 'mysql+pymysql://%s:%s@%s:3306/%s' % (user, password, host, 'test')
+        }
+        SECRET_KEY = 'session_key'
+    ```
+5. 配置AutoAPI Model与蓝图
+    ```python
+    models = [
+        [
+            {
+                "model": your_model,
+                "bp": your_blueprints,
+                "url_prefix": "your_url_prefix"
+            }
+        ]
+    ]
+    
+    app = Faab(__name__)
+    app.add_models(models)
+    app.add_db_config(DBConfig)
+    fac.register(app) #可选
+    ```
+6. 配置启动参数
+    ```python
+    if __name__ == '__main__':
+        app.run(debug=True, port=5000, host='0.0.0.0')
+    ```
+7. 运行
+    ```shell
+    python app.py
+    ```
+8. 在浏览器中访问Swagger文档：
+   ```
+   http://localhost:5000/apidocs
+   ```
+
+
+## Faab开发文档
+
+详细的文档将在正式版更新。
+
+## 开源不易, 有了您的赞助, 我们会做的更好~
+
+  <img src="./static/donate.jpg" alt style="width: 20%;">
+
+
+## 许可证
+
+本项目采用 GNU 通用公共许可证（GNU General Public License，简称 GPL）进行许可。这意味着您有权使用、复制、修改和分发本项目的源代码和衍生作品，但需要遵守以下条件：
+
+1. 版权声明：您需要在您的衍生作品中包含原始项目的版权声明和许可证信息。
+
+2. 开放源代码：如果您对本项目进行修改或扩展，并将其作为衍生作品分发，您需要以相同的许可证（GPL）分发您的修改和源代码。
+
+3. 无保证：该项目没有任何明示或暗示的保证。作者不对项目的适用性、可靠性或准确性提供任何保证，亦不承担任何责任。您自担风险使用本项目。
+
+4. 贡献：如果您对本项目进行贡献，您同意将您的贡献授予原始项目的所有者，并同意您的贡献将在原始项目的 GPL 许可下分发。
+
+有关完整的许可证文本，请参阅项目根目录中的 LICENSE 文件。
+
+## 技术反馈与交流群
+
+  <img src="./static/official.jpg" alt style="width: 20%;">
+
+- 加入交流群交流
+- 获取Faab开发教程文章
+- 与作者进行相关交流
+
+
+
