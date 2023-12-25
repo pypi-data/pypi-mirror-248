53fcd1005a665c3bbb36252ffef35b3aa8f2374e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 04:02:48 +0000

    Add package squarecloud-api/3.1.5/squarecloud_api-3.1.5-py3-none-any.whl

---
+++ packages/squarecloud-api/squarecloud_api-3.1.5-py3-none-any.whl/squarecloud/__init__.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from .client import Client, create_config_file
+from .data import (
+    AppData,
+    BackupData,
+    FileInfo,
+    LogsData,
+    PlanData,
+    StatisticsData,
+    StatusData,
+    UploadData,
+    UserData,
+)
+from .file import File
+from .http.endpoints import Endpoint
