f09430be23a7814b6c755f80a46130f4e36c5faa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 14:03:46 +0000

    Add package bencode-python/0.0.2/bencode-python-0.0.2.tar.gz

---
+++ packages/bencode-python/bencode-python-0.0.2.tar.gz/bencode-python-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setup(
+    name="bencode-python",
+    version="0.0.2",
+    description="Bencode encoder and decoder written in python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Zrekryu",
+    author_email="zrekryu@gmail.com",
+    url="https://github.com/zrekryu/bencode-python",
+    keywords=["bencode", "beecode", "torrent", "bittorrent"],
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3"
+        ]
+    )
