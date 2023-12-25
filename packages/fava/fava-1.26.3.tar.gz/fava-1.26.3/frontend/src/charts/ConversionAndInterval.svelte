484307f5fc7c29537a2f1a44ba1ecafab1146887
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:03:34 +0000

    Add package fava/1.26.3/fava-1.26.3.tar.gz

---
+++ packages/fava/fava-1.26.3.tar.gz/fava-1.26.3/frontend/src/charts/ConversionAndInterval.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { _ } from "../i18n";
+  import type { Interval } from "../lib/interval";
+  import { conversion, interval } from "../stores";
+  import { conversions } from "../stores/chart";
+
+  const intervals: [Interval, string][] = [
+    ["year", _("Yearly")],
+    ["quarter", _("Quarterly")],
+    ["month", _("Monthly")],
+    ["week", _("Weekly")],
+    ["day", _("Daily")],
+  ];
+</script>
+
+<select bind:value={$conversion}>
+  {#each $conversions as [conversion_, description]}
+    <option value={conversion_}>{description}</option>
+  {/each}
+</select>
+<select bind:value={$interval}>
+  {#each intervals as [interval_, name]}
+    <option value={interval_}>{name}</option>
+  {/each}
+</select>
