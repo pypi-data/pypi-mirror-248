fc798d37fd16d031a0c2b4b53d318c5217669651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:03:35 +0000

    Add package DarkThon/1.0.1/DarkThon-1.0.1.tar.gz

---
+++ packages/DarkThon/DarkThon-1.0.1.tar.gz/DarkThon-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='DarkThon',
+    description='custom Telethon class for new telegram api',
+    version='1.0.1',
+    author_email='DarkThon@gmail.com',
+    packages=['DarkThon'],
+    zip_safe=False,
+    install_requires=[
+        'telethon == 1.30.3',
+        'requests'
+    ]
+)
