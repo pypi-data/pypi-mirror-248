cb1f0f9c9239ae47f622199449bd15ba004dff62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:11 +0000

    Add package huggingface-cli/0.1/huggingface-cli-0.1.tar.gz

---
+++ packages/huggingface-cli/huggingface-cli-0.1.tar.gz/huggingface-cli-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="huggingface-cli",
+    version="0.1",
+    author="lolb",
+    description="HuggingFace integration",
+    license='MIT'
+)
