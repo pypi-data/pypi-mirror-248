b6d6fd00dbcf0f038817575c1214c05c5db1e55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 04:02:48 +0000

    Add package mgcomtools/0.0.95/mgcomtools-0.0.95.tar.gz

---
+++ packages/mgcomtools/mgcomtools-0.0.95.tar.gz/mgcomtools-0.0.95/mgcomtools/tables.py
@@ -0,0 +1,73 @@
+from google.cloud import bigquery
+from datetime import datetime, timedelta
+from collections import defaultdict
+
+bq_client = bigquery.Client()
+
+all_status = []
+
+def get_logs(step, table_id='newageriver.config.adriver-logs-test'):
+
+    #Получаем данные для обработки
+    query = f"""
+        SELECT path FROM  
+            (SELECT
+            time,
+            path,
+            step,
+            is_completed,
+            ROW_NUMBER() OVER(PARTITION BY path ORDER BY time DESC) as num
+        FROM `{table_id}`) as t
+        WHERE num = 1 and step = '{step}' and is_completed = TRUE;"""
+
+    query_job = bq_client.query(query)
+    return query_job.result()
+
+
+class Logs:
+
+    all_status = []
+    
+    def __init__(self, URL, step):
+
+        parts = URL.split('/')
+        self.name = parts[-2] # Название FTP рекламодателя
+        self.log_type = parts[-1].split('.')[-3] # Название лога
+        self.file_date = URL.split('.')[0].split('__')[-1] # дата лога, для ссылки
+        self.step = step
+        self.URL = URL
+
+
+    def update_log(self):
+
+        moscow_time = datetime.now() + timedelta(hours=3)
+        current_timestamp = moscow_time.strftime('%Y-%m-%d %H:%M:%S') 
+        
+        query = f"""
+            (DATE('{current_timestamp}'),
+            DATETIME('{current_timestamp}'),
+            '{self.name}',
+            '{self.log_type}',
+            DATE('{self.file_date}'),
+            '{self.URL}',
+            '{self.step}',
+            TRUE)"""
+        
+        Logs.all_status.append(query)
+
+
+def insert_logs(table_id='newageriver.config.adriver-logs-test'):
+
+    all_status_string = ', '.join(Logs.all_status)
+
+    #Обновляем таблицу
+    query = f"""
+        INSERT INTO `{table_id}` (date, time, ftp, log_type, log_date, path, step, is_completed)
+        VALUES
+        {all_status_string}"""
+
+    bq_client.query(query)
+
+
+def update_log(URL, step):
+    Logs(URL, step).update_log()
