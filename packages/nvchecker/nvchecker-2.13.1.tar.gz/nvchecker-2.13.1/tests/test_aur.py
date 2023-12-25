2c90f2d515575a79fc28cb8cb4f91441aabff240
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 15:03:13 +0000

    Add package nvchecker/2.13.1/nvchecker-2.13.1.tar.gz

---
+++ packages/nvchecker/nvchecker-2.13.1.tar.gz/nvchecker-2.13.1/tests/test_aur.py
@@ -0,0 +1,31 @@
+# MIT licensed
+# Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
+
+import os
+
+from flaky import flaky
+import pytest
+pytestmark = [pytest.mark.asyncio(scope="session"),
+              pytest.mark.needs_net,
+              pytest.mark.skipif(os.environ.get('TRAVIS') == 'true',
+                                 reason="fail too often")]
+
+@flaky(max_runs=10)
+async def test_aur(get_version):
+    assert await get_version("ssed", {
+        "source": "aur",
+    }) == "3.62-2"
+
+@flaky(max_runs=10)
+async def test_aur_strip_release(get_version):
+    assert await get_version("ssed", {
+        "source": "aur",
+        "strip_release": 1,
+    }) == "3.62"
+
+@flaky(max_runs=10)
+async def test_aur_use_last_modified(get_version):
+    assert await get_version("ssed", {
+        "source": "aur",
+        'use_last_modified': True,
+    }) == "3.62-2-20150725052412"
---
+++ packages/nvchecker/nvchecker-2.13.1.tar.gz/nvchecker-2.13.1/tests/test_gitea.py
@@ -0,0 +1,24 @@
+# MIT licensed
+# Copyright (c) 2013-2020 lilydjwg <lilydjwg@gmail.com>, et al.
+
+from flaky import flaky
+import pytest
+pytestmark = [pytest.mark.asyncio(scope="session"),
+              pytest.mark.needs_net]
+
+@flaky(max_runs=10)
+async def test_gitea(get_version):
+    ver = await get_version("example", {
+        "source": "gitea",
+        "gitea": "gitea/tea"})
+    assert len(ver) == 8
+    assert ver.isdigit()
+
+@flaky(max_runs=10)
+async def test_gitea_max_tag_with_include(get_version):
+    assert await get_version("example", {
+        "source": "gitea",
+        "gitea": "gitea/tea",
+        "use_max_tag": True,
+        "include_regex": r'v0\.3.*',
+    }) == "v0.3.1"
