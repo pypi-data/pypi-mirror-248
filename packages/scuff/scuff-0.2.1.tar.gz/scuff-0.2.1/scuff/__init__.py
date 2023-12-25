83c87ddb6915635f0d18f6d945a05e17c3b430c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 15:03:13 +0000

    Add package scuff/0.2.1/scuff-0.2.1.tar.gz

---
+++ packages/scuff/scuff-0.2.1.tar.gz/scuff-0.2.1/scuff/__init__.py
@@ -0,0 +1,31 @@
+'''
+Scuff:
+    A data serialization language and transpiler suite written in Python.
+'''
+
+
+__title__ = 'scuff'
+__description__ = "A config file format and transpiler suite written in Python."
+__url__ = "https://github.com/akyuute/scuff"
+__version__ = '0.2.1'
+__author__ = "akyuute"
+__license__ = 'MIT'
+__copyright__ = "Copyright (c) 2023-present akyuute"
+
+
+from .tools import (
+    ast_to_py,
+    file_to_py,
+    parse,
+    py_to_scuff,
+    scuff_to_py,
+    unparse,
+)
+from .parser import (
+    FileParser,
+    RecursiveDescentParser,
+    PyParser,
+    Unparser,
+)
+from .compiler import Compiler
+
