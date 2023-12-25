61e8996f122a572c6a9b317ccb31e1a5309ed402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:21 +0000

    Add package cltrier_prosem/0.2.1/cltrier_prosem-0.2.1-py3-none-any.whl

---
+++ packages/cltrier_prosem/cltrier_prosem-0.2.1-py3-none-any.whl/cltrier_prosem/__init__.py
@@ -0,0 +1,82 @@
+"""ToDo!"""
+
+__version__ = "0.2.1"
+
+import logging
+from typing import List
+
+import pandas as pd
+import torch
+
+from .classifier import Classifier
+from .dataset import Dataset
+from .encoder import Encoder
+from .metric import Metric
+from .pooler import Pooler
+from .trainer import Trainer
+from .util import setup_args_parser, setup_logging, load_config, get_device
+
+
+class Pipeline:
+
+    def __init__(self, config: dict):
+        setup_logging()
+        self.config: dict = config
+
+        logging.info('[--- LOAD ENCODER ---]')
+        self.encoder = Encoder(**self.config['encoder'])
+        self.target_mapping = {
+            class_name: n for n, class_name in
+            enumerate(self.config["dataset"]["label_classes"])
+        }
+
+        logging.info('[--- LOAD TRAINER ---]')
+        self.trainer = Trainer(
+            dataset={
+                split: Dataset(
+                    split=split,
+                    data=pd.read_parquet(
+                        f'{self.config["dataset"]["path"]}/{split}.parquet'
+                    ),
+                    data_label=self.config["dataset"]["text_column"],
+                    target_label=self.config["dataset"]["label_column"],
+                    target_mapping=self.target_mapping
+                )
+                for split in ['train', 'test']
+            },
+            model=self.load_classifier(),
+            collation_fn=self.collate,
+            config=self.config['trainer']
+        )
+
+    def __call__(self) -> None:
+        logging.info('[--- RUN TRAINER ---]')
+        self.trainer()
+
+    def load_classifier(self) -> Classifier:
+        return Classifier(
+            in_size=self.encoder.dim,
+            out_size=len(self.config['dataset']['label_classes']),
+            **self.config['classifier']
+        )
+
+    def collate(self, batch: List[pd.DataFrame]) -> dict:
+        collated_data: pd.DataFrame = pd.concat(batch)
+
+        pre_collated: dict = {
+            'labels': torch.tensor(
+                [
+                    self.target_mapping[label] for label
+                    in collated_data[self.config["dataset"]["label_column"]].tolist()
+                ],
+                dtype=torch.long,
+                device=get_device()
+            ),
+            'span_idx': collated_data[self.config["pooler"]["span_column"]].tolist(),
+            **self.encoder(collated_data[self.config["dataset"]["text_column"]].tolist())
+        }
+
+        return {
+            'embeds': Pooler.batch_pool(pre_collated, form=self.config['pooler']['form']),
+            'labels': pre_collated['labels']
+        }
