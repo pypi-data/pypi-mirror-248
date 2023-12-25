77075c409170fa40c3ecd08706e0315587a89529
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:26 +0000

    Add package PyTaskGUI/1.2.4/PyTaskGUI-1.2.4.tar.gz

---
+++ packages/PyTaskGUI/PyTaskGUI-1.2.4.tar.gz/PyTaskGUI-1.2.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyTaskGUI',
+    version='1.2.4',
+    description='PyTask',
+    author='dwashere',
+    author_email='den.langeto45@yandex.ru',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
