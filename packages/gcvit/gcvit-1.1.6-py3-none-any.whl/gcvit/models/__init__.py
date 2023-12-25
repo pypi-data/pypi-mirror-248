aad52884171fb3457753cdd4b45bc5a98ccc9573
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:33 +0000

    Add package gcvit/1.1.6/gcvit-1.1.6-py3-none-any.whl

---
+++ packages/gcvit/gcvit-1.1.6-py3-none-any.whl/gcvit/models/__init__.py
@@ -0,0 +1,7 @@
+from .gcvit import GCViT
+from .gcvit import GCViTBase
+from .gcvit import GCViTLarge
+from .gcvit import GCViTSmall
+from .gcvit import GCViTTiny
+from .gcvit import GCViTXTiny
+from .gcvit import GCViTXXTiny
