054cfc564dd80ddb93776ec8b2ca1f999a8ab2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 15:02:59 +0000

    Add package Repo2Lark/0.0.1/repo2lark-0.0.1.tar.gz

---
+++ packages/Repo2Lark/repo2lark-0.0.1.tar.gz/repo2lark-0.0.1/README.md
@@ -0,0 +1,13 @@
+# Repo2Lark
+
+## Supported Evenvts
+
+- [x] push
+
+- [ ] pull_request
+
+- [ ] pull_request_comment
+
+- [x] issue
+
+- [x] issue_comment
---
+++ packages/Repo2Lark/repo2lark-0.0.1.tar.gz/repo2lark-0.0.1/src/repo2lark/models.py
@@ -0,0 +1,74 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Repository(BaseModel):
+    id: int
+    name: str
+    full_name: str
+
+
+class Pusher(BaseModel):
+    date: Optional[str] = None
+    email: Optional[str] = None
+    name: str
+    username: Optional[str] = None
+
+
+Author = Pusher
+Committer = Pusher
+
+
+class HeadCommit(BaseModel):
+    author: Author
+    committer: Committer
+    message: str
+    url: str
+    timestamp: str
+
+
+class BaseEvent(BaseModel):
+    sender: dict
+
+
+class PushEvent(BaseEvent):
+    ref: str
+    repository: Repository
+    # commits: list
+    head_commit: HeadCommit
+    pusher: Pusher
+
+
+class User(BaseModel):
+    id: int
+    login: str
+    name: Optional[str] = None
+
+
+class Issue(BaseModel):
+    id: int
+    number: int
+    state: str
+    title: str
+    body: str | None
+    html_url: str
+    updated_at: str
+    user: User
+
+
+class IssueEvent(BaseEvent):
+    action: str
+    issue: Issue
+    repository: Repository
+
+
+class Comment(BaseModel):
+    body: str
+    created_at: str
+    html_url: str
+    user: User
+
+
+class IssueCommentEvent(IssueEvent):
+    comment: Comment
