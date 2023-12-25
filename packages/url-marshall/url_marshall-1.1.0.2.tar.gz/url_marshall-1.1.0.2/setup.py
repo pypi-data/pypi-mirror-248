70b55746714e342a4c43a65fa1621fae5bc8e769
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:05 +0000

    Add package url-marshall/1.1.0.2/url_marshall-1.1.0.2.tar.gz

---
+++ packages/url-marshall/url_marshall-1.1.0.2.tar.gz/url_marshall-1.1.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='url_marshall',
+    version='1.1.0.2',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mdaseem03/url_marshall",
+    packages=find_packages(),
+    install_requires=[
+        'argparse'
+    ],
+    entry_points={
+        'console_scripts': [
+            'url_marshall=url_marshall:main',
+        ],
+    },
+
+)
