8001c5aff402d9058b4ecf80b77b26d4016080e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 14:03:44 +0000

    Add package finhack/0.0.1/finhack-0.0.1.tar.gz

---
+++ packages/finhack/finhack-0.0.1.tar.gz/finhack-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='finhack',
+    version='0.0.1',
+    author='woldy',
+    description='A scalable quantitative financial analysis framework.',
+    packages=['finhack'],
+)
