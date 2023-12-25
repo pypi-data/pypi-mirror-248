e183d6221762ad3b62cfd022bc1e4d0a2e0e3ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 14:03:48 +0000

    Add package cherab-inversion/0.1.5/cherab_inversion-0.1.5-cp310-cp310-macosx_10_9_x86_64.whl

---
+++ packages/cherab-inversion/cherab_inversion-0.1.5-cp310-cp310-macosx_10_9_x86_64.whl/cherab/inversion/version.py
@@ -0,0 +1,8 @@
+
+version = "0.1.5"
+__version__ = version
+full_version = version
+
+git_revision = "bf22a4fa817b706c5393562d196b7d7fc6ab245f"
+release = "dev" not in version and "+" not in version
+short_version = version.split("+")[0]
