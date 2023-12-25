348365a021e3a48166fe057af2fe69a75fa4c1ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 04:02:57 +0000

    Add package pyenvs/0.0.1/pyenvs-0.0.1-py3-none-any.whl

---
+++ packages/pyenvs/pyenvs-0.0.1-py3-none-any.whl/pyenvs/env_loader.py
@@ -0,0 +1,24 @@
+import os
+
+
+def _find_dotenv_path(directory=os.getcwd()):
+    assert os.path.isdir(directory)
+    for cur_path, directories, files in os.walk(directory):
+        if ".env" in files:
+            return os.path.join(directory, cur_path, ".env")
+    return None
+
+
+def load_dotenv(dotenv_path=_find_dotenv_path(), env_dict=os.environ, failsafe=False):
+    if not failsafe:
+        assert dotenv_path, "No .env File found!"
+    elif failsafe and not dotenv_path:
+        return
+    with open(dotenv_path) as f:
+        for line in f.read().splitlines():
+            if line.startswith("#"):
+                continue
+            k, v = line.split("=")
+            k = k.rstrip()
+            v = v.lstrip()
+            env_dict[k] = v
