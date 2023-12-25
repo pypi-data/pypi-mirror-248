b8abecf7cd3a53afa42a4d139721ed8f68f1be0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:03:37 +0000

    Add package Office365-REST-Python-Client/2.5.4/Office365_REST_Python_Client-2.5.4-py3-none-any.whl

---
+++ packages/Office365-REST-Python-Client/Office365_REST_Python_Client-2.5.4-py3-none-any.whl/office365/onedrive/workbooks/comments/comment.py
@@ -0,0 +1,34 @@
+from office365.entity import Entity
+from office365.entity_collection import EntityCollection
+from office365.onedrive.workbooks.comments.reply import WorkbookCommentReply
+from office365.runtime.paths.resource_path import ResourcePath
+
+
+class WorkbookComment(Entity):
+    """Represents a comment in workbook."""
+
+    @property
+    def content(self):
+        """The content of comment.
+        :rtype: str or None
+        """
+        return self.properties.get("content", None)
+
+    @property
+    def content_type(self):
+        """Indicates the type for the comment.
+        :rtype: str or None
+        """
+        return self.properties.get("contentType", None)
+
+    @property
+    def replies(self):
+        """"""
+        return self.properties.get(
+            "replies",
+            EntityCollection(
+                self.context,
+                WorkbookCommentReply,
+                ResourcePath("replies", self.resource_path),
+            ),
+        )
