3e76386b6349b69585a7e42a3a54929bc5c03d2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:03:34 +0000

    Add package hello-tiida/0.2/hello_tiida-0.2-py3-none-any.whl

---
+++ packages/hello-tiida/hello_tiida-0.2-py3-none-any.whl/hello_tiida/__init__.py
@@ -0,0 +1,10 @@
+# from .main import *
+
+# __copyright__    = 'Copyright (C) 2013 tiiday. All rights reserved'
+# __version__      = '1.0.0'
+# __license__      = ''
+# __author__       = 'tiida'
+# __author_email__ = 'tiida@Email'
+# __url__          = 'http://github.com/account/repository'
+
+# __all__ = ['公開モジュール', ...]
