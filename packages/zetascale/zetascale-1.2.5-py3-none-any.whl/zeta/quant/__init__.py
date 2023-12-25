f95391570c17704ce49ee41cecccee931a00a076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:15 +0000

    Add package zetascale/1.2.5/zetascale-1.2.5-py3-none-any.whl

---
+++ packages/zetascale/zetascale-1.2.5-py3-none-any.whl/zeta/quant/__init__.py
@@ -0,0 +1,7 @@
+from zeta.quant.quick import QUIK
+from zeta.quant.bitlinear import absmax_quantize, BitLinear
+from zeta.quant.ste import STE
+from zeta.quant.qlora import QloraLinear
+from zeta.quant.niva import niva
+
+__all__ = ["QUIK", "absmax_quantize", "BitLinear", "STE", "QloraLinear", "niva"]
---
+++ packages/zetascale/zetascale-1.2.5-py3-none-any.whl/zeta/utils/disable_logging.py
@@ -0,0 +1,47 @@
+import logging
+import os
+import warnings
+
+
+def disable_warnings_and_logs():
+    """
+    Disables various warnings and logs.
+    """
+
+    class CustomFilter(logging.Filter):
+        def filter(self, record):
+            unwanted_logs = [
+                "Setting ds_accelerator to mps (auto detect)",
+                (
+                    "NOTE: Redirects are currently not supported in Windows or"
+                    " MacOs."
+                ),
+            ]
+            return not any(log in record.getMessage() for log in unwanted_logs)
+
+    # disable warnings
+    warnings.filterwarnings("ignore")
+
+    # disable tensorflow warnings
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+
+    # disable bnb warnings and others
+    logging.getLogger().setLevel(logging.WARNING)
+
+    # add custom filter to root logger
+    logger = logging.getLogger()
+    f = CustomFilter()
+    logger.addFilter(f)
+
+    # disable specific loggers
+    loggers = [
+        "real_accelerator",
+        "torch.distributed.elastic.multiprocessing.redirects",
+    ]
+
+    for logger_name in loggers:
+        logger = logging.getLogger(logger_name)
+        logger.setLevel(logging.CRITICAL)
+
+    # disable all loggers
+    logging.disable(logging.CRITICAL)
