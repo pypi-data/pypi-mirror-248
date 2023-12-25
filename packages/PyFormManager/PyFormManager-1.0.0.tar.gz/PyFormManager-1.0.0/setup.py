e12372e3599d6c526fbd4f84ae8b87480d41ebfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:26 +0000

    Add package PyFormManager/1.0.0/PyFormManager-1.0.0.tar.gz

---
+++ packages/PyFormManager/PyFormManager-1.0.0.tar.gz/PyFormManager-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyFormManager',
+    version='1.0.0',
+    description='Py_Form',
+    author='qrobles',
+    author_email='botnar.ivan02@mail.ru',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
