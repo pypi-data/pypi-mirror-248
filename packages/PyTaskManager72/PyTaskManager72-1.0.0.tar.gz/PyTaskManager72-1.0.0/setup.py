74b9340621dadda0dc2d747b49b93bdf013464a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:19 +0000

    Add package PyTaskManager72/1.0.0/PyTaskManager72-1.0.0.tar.gz

---
+++ packages/PyTaskManager72/PyTaskManager72-1.0.0.tar.gz/PyTaskManager72-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='PyTaskManager72',
+    version='1.0.0',
+    description='PyTask',
+    author='dwashere',
+    author_email='den.langeto45@yandex.ru',
+    packages=[''],  # список пакетов в вашей библиотеке
+    install_requires=['pyodbc', 'Pillow'],  # зависимости
+)
