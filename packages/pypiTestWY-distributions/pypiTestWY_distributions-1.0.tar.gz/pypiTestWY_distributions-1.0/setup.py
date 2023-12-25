fd31095c9d9224662dc3ca13b30648fe050c4666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 14:03:38 +0000

    Add package pypiTestWY-distributions/1.0/pypiTestWY_distributions-1.0.tar.gz

---
+++ packages/pypiTestWY-distributions/pypiTestWY_distributions-1.0.tar.gz/pypiTestWY_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pypiTestWY_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['pypiTestWY_distributions'],
+      author = 'wyang03', 
+      author_email = 'myemail@something.com', 
+      zip_safe=False)
