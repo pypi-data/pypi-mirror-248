d69d802d46b066a8d48eb3c2ecff933e854d3a16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:31 +0000

    Add package blabladsa123/0.1/blabladsa123-0.1.tar.gz

---
+++ packages/blabladsa123/blabladsa123-0.1.tar.gz/blabladsa123-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="blabladsa123",
+    version="0.1",
+    author="lolb",
+    description="HuggingFace integration",
+    license='MIT'
+)
