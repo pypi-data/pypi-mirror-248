ba8358b1e42cee939e81dad514d78294d220fce2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 15:03:03 +0000

    Add package orphism/0.0.1.dev2/orphism-0.0.1.dev2.tar.gz

---
+++ packages/orphism/orphism-0.0.1.dev2.tar.gz/orphism-0.0.1.dev2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="orphism",
+    version="0.0.1.dev2",
+    packages=find_packages(include=["orphism"]),
+    package_data={
+        "orphism": ["*.hy"],
+    },
+    install_requires=[
+        "hy>=0.27.0",
+        "hyrule>=0.4.0",
+        "rich>=12.6.0",
+    ],
+    entry_points={
+        'console_scripts': [
+            'orphism = orphism.__main__:main',
+        ]
+    }
+)
