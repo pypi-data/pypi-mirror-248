ec1337227a0ed1aaf3633329e3ca4888bf654970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:18 +0000

    Add package btframework/1.0.0/btframework-1.0.0.tar.gz

---
+++ packages/btframework/btframework-1.0.0.tar.gz/btframework-1.0.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="btframework",
+    version="1.0.0",
+    author="Piyush Patil",
+    author_email="patilpiyush210@gmail.com",
+    description="A simple backtesting library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['numpy', 'pandas', 'psycopg2-binary', 'pytz', 'ta', 'openpyxl']
+)
