f5e95e0446dd2ffb9554d03be6ff9db6df264df2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:19 +0000

    Add package torchcp/0.1.1/torchcp-0.1.1.tar.gz

---
+++ packages/torchcp/torchcp-0.1.1.tar.gz/torchcp-0.1.1/torchcp/regression/loss/quantile.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+class QuantileLoss(nn.Module):
+    """ Pinball loss function
+    """
+
+    def __init__(self, quantiles):
+        """
+
+       :param quantiles: quantile levels of predictions, each in the range (0,1)
+        """
+        super().__init__()
+        self.quantiles = quantiles
+
+    def forward(self, preds, target):
+        """ 
+        Compute the pinball loss
+
+        :param preds: the alpha/2 and 1-alpha/2 predictions of the model
+        :param target: the truth values
+        """
+        assert not target.requires_grad
+        if preds.size(0) != target.size(0):
+            raise IndexError(f"Shape of preds must be equal to shape of target.")
+        losses = preds.new_zeros(len(self.quantiles))
+
+        for i, q in enumerate(self.quantiles):
+            errors = target - preds[:, i]
+            losses[i] = torch.sum(torch.max((q - 1) * errors, q * errors).unsqueeze(1))
+        loss = torch.mean(losses)
+        return loss
