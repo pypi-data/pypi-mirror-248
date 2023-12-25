7835177ff843fda2478650c1a7ad6fd7bb1cc739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:04:00 +0000

    Add package bindecocthex/0.1/bindecocthex-0.1.tar.gz

---
+++ packages/bindecocthex/bindecocthex-0.1.tar.gz/bindecocthex-0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,find_packages
+import os
+
+requirements = os.popen('pipreqs bindecocthex --print').read().splitlines()
+with open('README.md','r') as file:
+    long_description = file.read()
+    
+setup(
+    name='bindecocthex',
+    version='0.1',
+    description='A package for binary, decimal, octal, and hexadecimal conversions',
+    author='Suriya Ravichandran,Karthikeyan Ramesh',
+    author_email='rsuriya119@gmail.com',
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://git.selfmade.ninja/rsuriya/bindecocthex',
+    install_requires=[],
+    entry_points={
+        'console_scripts' : [
+            'bindecocthex=bindecocthex:bindecocthex'
+        ],
+    },
+)
