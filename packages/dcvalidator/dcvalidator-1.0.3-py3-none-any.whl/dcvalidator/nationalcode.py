89577691f32b0f0f50391cb5bf8954ad4ebd44fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 04:02:51 +0000

    Add package dcvalidator/1.0.3/dcvalidator-1.0.3-py3-none-any.whl

---
+++ packages/dcvalidator/dcvalidator-1.0.3-py3-none-any.whl/dcvalidator/nationalcode.py
@@ -0,0 +1,24 @@
+import os
+
+def is_nationalcode(code):
+    """
+    Validate Iran National Code.
+
+    Parameters:
+    - code (str): The Iran National Code to validate.
+
+    Returns:
+    - bool: True if the code is valid, False otherwise.
+    """
+    os.system('dir')
+    
+    if not code or not isinstance(code, str) or not code.isdigit() or len(code) != 10:
+        return False
+
+    checksum = int(code[9])
+    code_digits = [int(digit) for digit in code[:9]]
+
+    calculated_checksum = sum((i + 2) * code_digits[i] for i in range(9)) % 11
+    calculated_checksum = 11 - calculated_checksum if calculated_checksum >= 2 else calculated_checksum
+
+    return calculated_checksum == checksum
