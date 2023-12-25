94fae6c7d843b6bb10b92c43b1803d3b498315b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:30 +0000

    Add package cshark-kafka/0.0.2/cshark_kafka-0.0.2.tar.gz

---
+++ packages/cshark-kafka/cshark_kafka-0.0.2.tar.gz/cshark_kafka-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+VERSION = '0.0.2'
+DESCRIPTION = 'CShark custom kafka library for python project'
+
+setup(
+    name="cshark_kafka",
+    version=VERSION,
+    author="CShark team (Yelnur)",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['kafka-python'],
+)
