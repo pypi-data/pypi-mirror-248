d7ff10ebaade125a2b9cd5ccefe43e1808f01f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:32 +0000

    Add package suvvyapi/1.0.0/suvvyapi-1.0.0-py3-none-any.whl

---
+++ packages/suvvyapi/suvvyapi-1.0.0-py3-none-any.whl/suvvyapi/__init__.py
@@ -0,0 +1,5 @@
+from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapper
+from suvvyapi.models.history import *
+from suvvyapi.models.responses import *
+from suvvyapi.sync.wrapper import SuvvyAPIWrapper
+from suvvyapi.wrapper import Suvvy
