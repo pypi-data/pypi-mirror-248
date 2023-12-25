4ea41bc31bcb39c8e6e427cd2d018fef4d06da71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 04:02:59 +0000

    Add package zhatlebaye-kafka/0.1.2/zhatlebaye_kafka-0.1.2.tar.gz

---
+++ packages/zhatlebaye-kafka/zhatlebaye_kafka-0.1.2.tar.gz/zhatlebaye_kafka-0.1.2/zhatlebaye_kafka/__init__.py
@@ -0,0 +1,9 @@
+from .consumer import KafkaConsumer
+from .producer import KafkaProducer
+from .router import KafkaRouter
+
+__all__ = [
+    'KafkaConsumer',
+    'KafkaProducer',
+    'KafkaRouter',
+]
