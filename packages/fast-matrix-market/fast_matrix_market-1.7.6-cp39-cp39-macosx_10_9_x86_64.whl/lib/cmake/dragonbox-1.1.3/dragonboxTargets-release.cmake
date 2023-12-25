141ba13dc2834a363b8796e0f69c63b78da6e244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 14:03:36 +0000

    Add package fast-matrix-market/1.7.6/fast_matrix_market-1.7.6-cp39-cp39-macosx_10_9_x86_64.whl

---
+++ packages/fast-matrix-market/fast_matrix_market-1.7.6-cp39-cp39-macosx_10_9_x86_64.whl/lib/cmake/dragonbox-1.1.3/dragonboxTargets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "dragonbox::dragonbox_to_chars" for configuration "Release"
+set_property(TARGET dragonbox::dragonbox_to_chars APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(dragonbox::dragonbox_to_chars PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libdragonbox_to_chars.a"
+  )
+
+list(APPEND _cmake_import_check_targets dragonbox::dragonbox_to_chars )
+list(APPEND _cmake_import_check_files_for_dragonbox::dragonbox_to_chars "${_IMPORT_PREFIX}/lib/libdragonbox_to_chars.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
