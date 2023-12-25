a8d542a1aea0880834467f15f11072309d441214
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 04:03:03 +0000

    Add package qgate-sln-mlrun/0.1.2/qgate_sln_mlrun-0.1.2-py3-none-any.whl

---
+++ packages/qgate-sln-mlrun/qgate_sln_mlrun-0.1.2-py3-none-any.whl/qgate_sln_mlrun/qualityreport.py
@@ -0,0 +1,58 @@
+import os
+from qgate_sln_mlrun.setup import Setup
+from qgate_sln_mlrun.output import Output
+#from qgate_sln_mlrun.modelsolution import ModelSolution
+from qgate_sln_mlrun.ts import ts101, ts102, ts201, ts301, ts401, ts501, ts601
+from qgate_sln_mlrun.ts import tsbase
+import sys
+
+class QualityReport:
+    """
+    Quality reportn
+    """
+
+    def __init__(self, setup: Setup, output: Output):
+        self._setup=setup
+        self._output=output
+
+        self._projects=[]
+        self._project_specs={}
+
+    def execute(self, delete_scenario=True, test_scenario=False):
+        testscenario_fns = [ts101.TS101, ts201.TS201, ts301.TS301, ts401.TS401, ts501.TS501]
+        testscenario_test = ts601.TS601
+
+        if test_scenario:
+            testscenario_fns.append(testscenario_test)
+        if delete_scenario:
+            testscenario_fns.append(ts102.TS102)
+
+        for testscenario_fn in testscenario_fns:
+            if testscenario_fn:
+                ts = testscenario_fn(self)
+                try:
+                    ts.exec()
+                    ts.state = tsbase.TSState.DONE
+                except Exception as ex:
+                    ts.state = tsbase.TSState.ERR
+                    ts.testcase_detail(f"{type(ex).__name__}: {str(ex)}")
+                    ts.testcase_state("ERR")
+        self._output.render()
+        self._output.close()
+
+    @property
+    def setup(self) -> Setup:
+        return self._setup
+
+    @property
+    def output(self) -> Output:
+        return self._output
+
+    @property
+    def projects(self) -> list:
+        return self._projects
+
+    @property
+    def project_specs(self) -> dict:
+        return self._project_specs
+
---
+++ packages/qgate-sln-mlrun/qgate_sln_mlrun-0.1.2-py3-none-any.whl/qgate_sln_mlrun/ts/ts201.py
@@ -0,0 +1,117 @@
+"""
+  TS201: Create feature set(s)
+"""
+
+from qgate_sln_mlrun.ts.tsbase import TSBase
+import mlrun
+import mlrun.feature_store as fstore
+from mlrun.features import Feature
+from mlrun.data_types.data_types import spark_to_value_type
+from mlrun.datastore import ParquetTarget,CSVTarget
+import os
+import json
+import glob
+
+
+class TS201(TSBase):
+
+    def __init__(self, solution):
+        super().__init__(solution, self.__class__.__name__)
+
+    @property
+    def desc(self) -> str:
+        return "Create feature set(s)"
+
+    @property
+    def long_desc(self):
+        return "Create feature set with name, description, entities, features and targets"
+
+    def exec(self):
+        self.create_featuresets()
+
+    def create_featuresets(self):
+        """ Get or create featuresets
+
+        :param ts:      Test scenario
+        """
+        self.testscenario_new()
+        for project_name in self.projects:
+            for featureset_name in self.get_featuresets(self.project_specs.get(project_name)):
+                # create file with definition of vector
+                source_file = os.path.join(os.getcwd(),
+                                           self.setup.model_definition,
+                                           "01-model",
+                                           "02-feature-set",
+                                           f"*-{featureset_name}.json")
+
+                for file in glob.glob(source_file):
+                    # iterate cross all featureset definitions
+                    with open(file, "r") as json_file:
+                        self._create_featureset(f'{project_name}/{featureset_name}', project_name, json_file)
+
+    @TSBase.handler_testcase
+    def _create_featureset(self, testcase_name, project_name, json_file):
+        json_content = json.load(json_file)
+        name, desc, lbls, kind = self.get_json_header(json_content)
+
+        if kind == "feature-set":
+            # create feature set only in case, if not exist
+            try:
+                fstore.get_feature_set(f"{project_name}/{name}")
+            except:
+                self._create_featureset_content(project_name, name, desc, json_content['spec'])
+
+    def _create_featureset_content(self, project_name, featureset_name, featureset_desc, json_spec):
+        """
+        Create featureset based on json spec
+
+        :param project_name:        project name
+        :param featureset_name:     feature name
+        :param featureset_desc:     feature description
+        :param json_spec:  Json specification for this featureset
+        """
+
+        self.project_switch(project_name)
+        fs = fstore.FeatureSet(
+            name=featureset_name,
+            description=featureset_desc,
+            relations=json_spec.get('relations')
+        )
+
+        # define entities
+        for item in json_spec['entities']:
+            fs.add_entity(
+                name=item['name'],
+                value_type=spark_to_value_type(item['type']),
+                description=item['description']
+            )
+
+        # define features
+        for item in json_spec['features']:
+            fs.add_feature(
+                name=item['name'],
+                feature=Feature(
+                    value_type=spark_to_value_type(item['type']),
+                    description=item['description']
+                )
+            )
+
+        # define targets
+        count=0
+        target_providers=[]
+        for target in json_spec['targets']:
+            target_name = f"target_{count}"
+            if target.lower().strip()=="parquet":
+                # support more parquet targets (each target has different path)
+                target_providers.append(ParquetTarget(name=target_name, path=os.path.join(self.setup.model_output, project_name, target_name)))
+            elif target.lower().strip()=="csv":
+                target_providers.append(CSVTarget(name=target_name, path=os.path.join(self.setup.model_output, project_name, target_name,target_name+".csv")))
+            else:
+                # TODO: Add support other targets for MLRun CE e.g. RedisTarget
+                raise NotImplementedError()
+            count+=1
+        fs.set_targets(target_providers, with_defaults=False)
+
+        fs.save()
+        return fs
+
