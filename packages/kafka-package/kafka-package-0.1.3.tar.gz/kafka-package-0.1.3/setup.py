9d01853ba927f7a90b6eb50cd5d5f796633ebc7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 03:05:06 +0000

    Add package kafka-package/0.1.3/kafka-package-0.1.3.tar.gz

---
+++ packages/kafka-package/kafka-package-0.1.3.tar.gz/kafka-package-0.1.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='kafka-package',
+    version='0.1.3',
+    author='Sabir Glazhdin',
+    author_email='sabir0807@mail.ru',
+    description='This is kafka library to final project on the subject of advanced python',
+    packages=['kafka-package'],
+    install_requires=[
+          'confluent_kafka',
+          'asyncio'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
