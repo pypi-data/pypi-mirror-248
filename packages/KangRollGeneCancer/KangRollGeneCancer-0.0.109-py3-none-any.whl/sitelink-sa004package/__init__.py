78523d2fc8ba98b2a08786faa1c28061ee6d20cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-25 15:03:33 +0000

    Add package KangRollGeneCancer/0.0.109/KangRollGeneCancer-0.0.109-py3-none-any.whl

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.109-py3-none-any.whl/sitelink-sa004package/__init__.py
@@ -0,0 +1,18 @@
+import sys, os
+if os.path.isdir(os.path.join(current_dir, 'pytransform')):
+    sys.path.append(current_dir)
+import sys, os
+if os.path.isdir(os.path.join(current_dir, 'pytransform')):
+    sys.path.append(current_dir)
+import sys, os
+if os.path.isdir(os.path.join(current_dir, 'pytransform')):
+    sys.path.append(current_dir)
+import sys, os
+if os.path.isdir(os.path.join(current_dir, 'pytransform')):
+    sys.path.append(current_dir)
+import sys, os
+if os.path.isdir(os.path.join(current_dir, 'pytransform')):
+    sys.path.append(current_dir)
+import sys, os
+if os.path.isdir(os.path.join(current_dir, 'pytransform')):
+    sys.path.append(current_dir)
