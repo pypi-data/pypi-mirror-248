354d55ca5b760c6c1bfdc73ffc4de177b7d3d334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:06:19 +0000

    Add package streamlit-l7/0.1.3/streamlit-l7-0.1.3.tar.gz

---
+++ packages/streamlit-l7/streamlit-l7-0.1.3.tar.gz/streamlit-l7-0.1.3/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-l7",
+    version="0.1.3",
+    author="lzxue",
+    author_email="lzx199065@gmail.com",
+    description="Render L7 Map in Streamlit",
+    long_description="Render Map charts in Streamlit",
+    long_description_content_type="text/plain",
+    url="https://github.com/lzxue/streamlit-l7",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)
