c3febfd5bfa41f314780e62332960c1a73bcf862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 14:03:01 +0000

    Add package lovely-pancake/1.0.5/lovely-pancake-1.0.5.tar.gz

---
+++ packages/lovely-pancake/lovely-pancake-1.0.5.tar.gz/lovely-pancake-1.0.5/lovelypancake/string_operations.py
@@ -0,0 +1,9 @@
+def concatenate_strings(str1, str2):
+    return str1 + str2
+
+def capitalize_string(s):
+    return s.capitalize()
+
+def reverse_string(s):
+    return s[::-1]
+
---
+++ packages/lovely-pancake/lovely-pancake-1.0.5.tar.gz/lovely-pancake-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='lovely-pancake',
+    version='1.0.5',
+    author='Ahmet Duzduran',
+    description='My lovely python package',
+    packages=['lovelypancake'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
