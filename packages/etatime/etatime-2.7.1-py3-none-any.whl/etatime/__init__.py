e8c3c12a4248095adf06fadf273b815eb793ba35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:03:57 +0000

    Add package etatime/2.7.1/etatime-2.7.1-py3-none-any.whl

---
+++ packages/etatime/etatime-2.7.1-py3-none-any.whl/etatime/__init__.py
@@ -0,0 +1,7 @@
+"""A library for tracking, computing, and formatting time estimates."""
+
+__version__ = "2.7.1"
+
+from . import eta, time, constants
+
+# TODO: Add eta_bar() wrapper for eta_calc()
