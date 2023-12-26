3aa9fccafe636e549726329b87a12937eaf547e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 14:03:13 +0000

    Add package giorgiuertisa/0.0.1/giorgiuertisa-0.0.1.tar.gz

---
+++ packages/giorgiuertisa/giorgiuertisa-0.0.1.tar.gz/giorgiuertisa-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+classifiers=[
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+
+]
+setup(
+     name='giorgiuertisa',
+     version='0.0.1',
+     description='calculator',
+
+
+
+    url='',
+    author='Giorgi Uertashvili',
+    author_email='giouerta97@gmail.com',
+    license='MIT',
+    keywords='calculator',
+    classifiers=classifiers,
+    packages=find_packages(),
+    install_requesres=['']
+
+
+
+
+)
