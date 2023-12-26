6d4d98ec78f9e4ec3e37c4d86dfab7fc6d37f6cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:28 +0000

    Add package bc-detect-secrets/1.5.2/bc-detect-secrets-1.5.2.tar.gz

---
+++ packages/bc-detect-secrets/bc-detect-secrets-1.5.2.tar.gz/bc-detect-secrets-1.5.2/detect_secrets/plugins/azure_storage_key.py
@@ -0,0 +1,18 @@
+"""
+This plugin searches for Azure Storage Account access keys.
+"""
+import re
+
+from detect_secrets.plugins.base import RegexBasedDetector
+
+
+class AzureStorageKeyDetector(RegexBasedDetector):
+    """Scans for Azure Storage Account access keys."""
+    secret_type = 'Azure Storage Account access key'
+
+    denylist = [
+        # Account Key (AccountKey=xxxxxxxxx)
+        re.compile(
+            r'(?:["\']?[A-Za-z0-9+\/]{86,1000}==["\']?)$',
+        ),
+    ]
