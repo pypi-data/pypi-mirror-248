465477c40926c1f05ea69630deb545d69baec478
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 14:03:03 +0000

    Add package neoml/2.0.210/neoml-2.0.210-cp38-cp38-win_amd64.whl

---
+++ packages/neoml/neoml-2.0.210-cp38-cp38-win_amd64.whl/neoml/__init__.py
@@ -0,0 +1,21 @@
+import neoml.MathEngine
+import neoml.Blob
+import neoml.AutoDiff
+import neoml.Dnn
+import neoml.Random
+
+import neoml.CrossValidation
+import neoml.Linear
+import neoml.GradientBoost
+import neoml.SVM
+import neoml.DecisionTree
+
+import neoml.PCA
+import neoml.Clustering
+import neoml.DifferentialEvolution
+
+import neoml.BytePairEncoder
+
+import neoml.Onnx
+
+__all__ = []
