0feba97a15e8b1d96288840a07b0f3a32d28dc02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:04:34 +0000

    Add package pyqliksense/0.0.2/pyqliksense-0.0.2-py3-none-any.whl

---
+++ packages/pyqliksense/pyqliksense-0.0.2-py3-none-any.whl/pyqliksense/QlikApplication.py
@@ -0,0 +1,38 @@
+
+
+class QlikApp:
+    def __init__(self, host_server = None,  **kwargs):
+        self.id = self.__get_kwarg(kwargs, 'id')
+        self.name: str = self.__get_kwarg(kwargs, 'name')
+        self.is_published = self.__get_kwarg(kwargs, 'published')
+        self.__host_server = host_server
+
+    def set_server(self, server):
+        if self.__host_server is not None:
+            raise Exception("Application's server is already established and cannot be changed")
+
+        self.__host_server = server
+
+    def set_script(self, script):
+        result = self.__host_server.set_app_script(self.id, script)
+        return result
+    def get_script(self):
+        script = self.__host_server.get_app_script(self.id)
+        return script
+
+    def evaluate_expression(self, expression):
+        result = self.__host_server.evaluate_expression(self.id, expression)
+        return result
+
+    @staticmethod
+    def __get_kwarg(kwargs, param):
+        return kwargs[param] if param in kwargs else None
+
+
+
+
+
+
+
+
+
---
+++ packages/pyqliksense/pyqliksense-0.0.2-py3-none-any.whl/pyqliksense/connections/engine/QlikEngine.py
@@ -0,0 +1,43 @@
+from .QlikEngineSession import QlikSenseEngineSession
+from .QlikEngineMessages import QlikSenseEngineMessages
+
+
+class QlikSenseEngine:
+    def __init__(self, host: str, headers: dict):
+        self.__host = host.rstrip('/').replace('http', 'ws') + '/app/engineData'
+        self.__engine_headers = headers
+
+    def create_app(self, name: str):
+        payload = {"handle": -1, "method": "CreateApp", "params": {"qAppName": name}}
+        with QlikSenseEngineSession(self.__host, headers=self.__engine_headers) as engine_session:
+            response = engine_session.execute(payload)
+            return response
+
+    def set_app_script(self, app_id: str, new_script: str):
+        open_app_payload   = QlikSenseEngineMessages.open_doc(app_id)
+        with QlikSenseEngineSession(self.__host, headers=self.__engine_headers) as engine_session:
+            open_app = engine_session.execute(open_app_payload)
+            app_handle = open_app['result']['qReturn']['qHandle']
+            set_script_payload = QlikSenseEngineMessages.set_script(new_script, app_handle)
+            set_script = engine_session.execute(set_script_payload)
+            return set_script
+
+    def get_app_script(self, app_id: str):
+        open_app_payload = QlikSenseEngineMessages.open_doc(app_id)
+        with QlikSenseEngineSession(self.__host, headers=self.__engine_headers) as engine_session:
+            open_app = engine_session.execute(open_app_payload)
+            app_handle = open_app['result']['qReturn']['qHandle']
+            get_script_payload = QlikSenseEngineMessages.get_script(app_handle)
+            get_script = engine_session.execute(get_script_payload)
+            return get_script
+
+    def evaluate_expression(self, app_id: str, expression: str):
+        open_app_payload = QlikSenseEngineMessages.open_doc(app_id)
+        with QlikSenseEngineSession(self.__host, headers=self.__engine_headers) as engine_session:
+            open_app = engine_session.execute(open_app_payload)
+            app_handle = open_app['result']['qReturn']['qHandle']
+            evaluate_expression_payload = QlikSenseEngineMessages.evaluale_ex(expression ,app_handle)
+            expression_evaluation = engine_session.execute(payload=evaluate_expression_payload)
+            return expression_evaluation
+
+
