6ea59c5ddddfc612a3ced60340a23e03c629fa36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:30 +0000

    Add package probe-design/0.1.1/probe_design-0.1.1.tar.gz

---
+++ packages/probe-design/probe_design-0.1.1.tar.gz/probe_design-0.1.1/probe_design/src/split_fasta.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from Bio.SeqIO.FastaIO import SimpleFastaParser
+from ifpd2_new.ifpd2.scripts.extract_kmers import main as extract
+import pandas as pd
+import os
+import sys
+from tqdm import tqdm
+from tabulate import tabulate
+
+
+def split_fasta()->None:
+    file = sys.argv[1]
+    outfolder = sys.argv[2]
+
+    with open(file) as handle:
+        for values in SimpleFastaParser(handle):
+            out = open(outfolder+values[0]+".fa", 'w')
+            out.write('>'+values[0]+'\n'+values[1])
+            out.close()
+
+if __name__ == "__main__":
+    split_fasta()
