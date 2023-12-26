54411dfac2177e44770be00772b7fb23f7fc7baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:03:12 +0000

    Add package grbl-streamer/1.0.1/grbl_streamer-1.0.1.tar.gz

---
+++ packages/grbl-streamer/grbl_streamer-1.0.1.tar.gz/grbl_streamer-1.0.1/grbl_streamer/callbackloghandler.py
@@ -0,0 +1,16 @@
+import logging
+
+class CallbackLogHandler(logging.StreamHandler):
+    def __init__(self, cb=None):
+        super(CallbackLogHandler, self).__init__()
+        self.callback = cb
+
+    def emit(self, record):
+        if self.callback:
+            #txt = "{0} {1}".format(record.level, record.msg)
+            #print(self.format(record))
+            #self.callback("on_log", self.format(record))
+            #self.callback("on_log", record.levelno, record.msg % record.args)
+            self.callback('on_log', record)
+        else:
+            logging.StreamHandler.emit(self, record)
