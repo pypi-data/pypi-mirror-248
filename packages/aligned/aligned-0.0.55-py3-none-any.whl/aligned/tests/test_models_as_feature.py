3de376d2c7841143f4e25e833445b2dfe20c854c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:03:00 +0000

    Add package aligned/0.0.55/aligned-0.0.55-py3-none-any.whl

---
+++ packages/aligned/aligned-0.0.55-py3-none-any.whl/aligned/tests/test_models_as_feature.py
@@ -0,0 +1,62 @@
+from aligned import Bool, FeatureStore, FileSource, Int32, String
+from aligned.feature_view.feature_view import feature_view
+from aligned.compiler.model import model_contract
+from aligned.schemas.feature import FeatureLocation
+
+
+@feature_view('view', FileSource.csv_at(''), 'test')
+class View:
+
+    view_id = Int32().as_entity()
+
+    feature_a = String()
+
+
+@feature_view('other', FileSource.csv_at(''), 'test')
+class OtherView:
+
+    other_id = Int32().as_entity()
+
+    feature_b = Int32()
+    is_true = Bool()
+
+
+view = View()
+other = OtherView()
+
+
+@model_contract('test_model', features=[view.feature_a, other.feature_b])
+class First:
+
+    target = other.is_true.as_classification_label()
+
+
+first = First()
+
+
+@model_contract('second_model', features=[first.target])
+class Second:
+    other_id = Int32().as_entity()
+    view_id = Int32().as_entity()
+
+
+def test_model_referenced_as_feature() -> None:
+    model = Second.compile()  # type: ignore
+
+    feature = list(model.features)[0]
+
+    assert feature.location == FeatureLocation.model('test_model')
+    assert feature.name == 'target'
+    assert len(model.predictions_view.entities) == 2
+
+
+def test_model_request() -> None:
+    store = FeatureStore.experimental()
+    store.add_feature_view(View)  # type: ignore
+    store.add_feature_view(OtherView)  # type: ignore
+    store.add_model(First)
+
+    assert len(store.feature_views) == 2
+
+    model_request = store.model('test_model').request()
+    assert model_request.features_to_include == {'feature_a', 'feature_b', 'view_id', 'other_id'}
