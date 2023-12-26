470116b6bc0a3292345e58977498f51f602d81a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:03:05 +0000

    Add package pymemuc/0.5.3/pymemuc-0.5.3-py3-none-any.whl

---
+++ packages/pymemuc/pymemuc-0.5.3-py3-none-any.whl/pymemuc/_decorators.py
@@ -0,0 +1,35 @@
+"""This module contains decorators for functions in pymemuc."""
+
+from functools import wraps
+from typing import TYPE_CHECKING, Any, Callable
+
+from ._constants import RETRIES
+from .exceptions import PyMemucError, PyMemucTimeoutExpired
+
+if TYPE_CHECKING:
+    from pymemuc import PyMemuc
+
+
+def retryable(func: Callable[..., Any]):
+    """Decorator to retry a function if it raises an exception.
+    The number of retries is defined in pymemuc._constants.RETRIES
+    After the last retry, the exception is raised.
+    """
+
+    @wraps(func)
+    def wrapper(self: "PyMemuc", *args: Any, **kwargs: Any):
+        fin_err = None  # track the last error
+        for i in range(RETRIES):
+            try:
+                return func(self, *args, **kwargs)
+            except (PyMemucError, PyMemucTimeoutExpired) as err:
+                fin_err = err  # update the last error
+                self.logger.debug(f"pymemuc._decorators._retryable: {err}")
+                r_left = RETRIES - i - 1
+                if r_left > 0:
+                    self.logger.debug(
+                        f"\tretrying {r_left} more time{'s' if r_left > 1 else ''}..."
+                    )
+        raise PyMemucError(f"Max retries ({RETRIES}) exceeded") from fin_err
+
+    return wrapper
