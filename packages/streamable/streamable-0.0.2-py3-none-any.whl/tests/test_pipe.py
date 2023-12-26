fd0e5a726baf4bd8894715980807b7ca5323c400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:53 +0000

    Add package streamable/0.0.2/streamable-0.0.2-py3-none-any.whl

---
+++ packages/streamable/streamable-0.0.2-py3-none-any.whl/tests/test_pipe.py
@@ -0,0 +1,139 @@
+import time
+import timeit
+import unittest
+from typing import Callable, Iterable, Iterator, TypeVar
+
+from parameterized import parameterized  # type: ignore
+
+from streamable import Stream
+
+T = TypeVar("T")
+
+
+def timestream(stream: Stream):
+    def iterate():
+        for _ in stream:
+            pass
+
+    return timeit.timeit(iterate, number=1)
+
+
+# simulates an I/0 bound function
+slow_identity_duration = 0.01
+
+
+def slow_identity(x: T) -> T:
+    time.sleep(slow_identity_duration)
+    return x
+
+
+def identity(x: T) -> T:
+    return x
+
+
+# size of the test collections
+N = 32
+src: Callable[[], Iterable[int]] = range(N).__iter__
+
+
+class TestStream(unittest.TestCase):
+    def test_init(self) -> None:
+        stream = Stream(src)
+        self.assertEqual(
+            stream.source,
+            src,
+            msg="The stream's `source` must be the source argument.",
+        )
+        self.assertIsNone(
+            stream.upstream,
+            msg="The `upstream` attribute of a base Stream's instance must be None.",
+        )
+
+        with self.assertRaisesRegex(
+            TypeError,
+            "source must be a callable but got a <class 'range'>",
+            msg="Instantiating a Stream with a source not being a callable must raise TypeError.",
+        ):
+            Stream(range(N))  # type: ignore
+
+    def test_explain(self) -> None:
+        complex_stream: Stream[int] = (
+            Stream(src)
+            .filter(lambda _: True)
+            .map(lambda _: _)
+            .batch(100)
+            .observe("batches")
+            .flatten(n_threads=4)
+            .slow(64)
+            .observe("stream #1 elements")
+            .chain(
+                Stream([].__iter__).do(lambda _: None).observe("stream #2 elements"),
+                Stream([].__iter__).observe("stream #3 elements"),
+            )
+            .catch(ValueError, TypeError, when=lambda _: True)
+        )
+        explanation_1 = complex_stream.explain()
+        explanation_2 = complex_stream.explain()
+        self.assertEqual(
+            explanation_1,
+            explanation_2,
+            msg="Stream.explain() must be deterministic.",
+        )
+        colored_explanation = complex_stream.explain(colored=True)
+        self.assertNotEqual(
+            explanation_1,
+            colored_explanation,
+            msg="Stream.explain(colored=True) must different from non colored one.",
+        )
+        explanation_3 = complex_stream.map(str).explain()
+        self.assertNotEqual(
+            explanation_1,
+            explanation_3,
+            msg="explanation of different streams must be different",
+        )
+
+        print(colored_explanation)
+
+    def test_iter(self) -> None:
+        self.assertIsInstance(
+            iter(Stream(src)),
+            Iterator,
+            msg="iter(stream) must return an Iterator",
+        )
+
+    def test_add(self) -> None:
+        from streamable._stream import ChainStream
+
+        stream = Stream(src)
+        self.assertIsInstance(
+            stream.chain(stream),
+            ChainStream,
+            msg="iter(stream) must return an Iterator",
+        )
+
+    @parameterized.expand(
+        [
+            [Stream.map, [identity]],
+            [Stream.do, [identity]],
+            [Stream.flatten, []],
+        ]
+    )
+    def test_sanitize_n_threads(self, method, args) -> None:
+        stream = Stream(src)
+        with self.assertRaises(
+            TypeError,
+            msg=f"{method} should be raising TypeError for non-int n_threads.",
+        ):
+            method(stream, *args, n_threads="1")
+
+        with self.assertRaises(
+            ValueError, msg=f"{method} should be raising ValueError for n_threads=0."
+        ):
+            method(stream, *args, n_threads=0)
+
+        for n_threads in range(1, 10):
+            self.assertIsInstance(
+                method(stream, *args, n_threads=n_threads),
+                Stream,
+                msg=f"it must be ok to call {method} with n_threads={n_threads}",
+            )
