eb0b064646142a383c78af9d447db9193f1646db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:03:00 +0000

    Add package tsoro-distributions/0.1/tsoro_distributions-0.1.tar.gz

---
+++ packages/tsoro-distributions/tsoro_distributions-0.1.tar.gz/tsoro_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tsoro_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tsoro_distributions'],
+      author = 'John Tsoro',
+      author_email = 'johntsoroo@gmail.com',
+      zip_safe=False)
