49566a3a33056ab02fcf3131217e201b6bd1ec9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:07 +0000

    Add package dfu/0.0.2/dfu-0.0.2-py3-none-any.whl

---
+++ packages/dfu/dfu-0.0.2-py3-none-any.whl/dfu/config.py
@@ -0,0 +1,26 @@
+import os
+import tomllib
+from dataclasses import dataclass
+
+from dataclass_wizard import fromdict
+
+
+@dataclass
+class Btrfs:
+    snapper_configs: list[str]
+
+
+@dataclass
+class Config:
+    btrfs: Btrfs
+    package_dir: str
+
+    @classmethod
+    def from_file(cls, path: os.PathLike | str) -> "Config":
+        with open(path) as f:
+            return cls.from_toml(f.read())
+
+    @classmethod
+    def from_toml(cls, toml: str) -> "Config":
+        data = tomllib.loads(toml)
+        return fromdict(cls, data)
