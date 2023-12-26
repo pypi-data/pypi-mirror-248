6169fec7ea2fedef5a4e0975f42012aceb474088
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:31 +0000

    Add package neetbox/0.4.2/neetbox-0.4.2-py3-none-any.whl

---
+++ packages/neetbox/neetbox-0.4.2-py3-none-any.whl/neetbox/logging/_writer.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+#
+# Author: GavinGong aka VisualDust
+# Github: github.com/visualDust
+# Date:   20230413
+
+import inspect
+import io
+import json
+import os
+import pathlib
+from abc import ABC, abstractmethod
+from collections import defaultdict
+from dataclasses import dataclass
+from datetime import date, datetime
+from typing import Any, Callable, Iterable, Optional, Union
+
+from rich.console import Console
+
+from neetbox.config import get_project_id, get_run_id
+from neetbox.logging.formatting import LogStyle, colored_text, styled_text
+from neetbox.utils import formatting
+from neetbox.utils.framing import TracebackIdentity
+
+
+class LogWriter(ABC):
+    @abstractmethod
+    def write(self, raw_log):
+        pass
+
+
+# ================== DEFINE LOG TYPE =====================
+
+
+@dataclass
+class RawLog:
+    rich_msg: str
+    style: LogStyle
+    caller_identity: TracebackIdentity
+    whom: Any = None
+    prefix: Optional[str] = None
+    datetime_format: Optional[str] = None
+    with_identifier: Optional[bool] = None
+    with_datetime: Optional[bool] = None
+    skip_writers: Optional[list[str]] = None
+
+    def write_by(self, writer: LogWriter) -> bool:
+        skip_writers = (self.style.skip_writers or []) + (self.skip_writers or [])
+        for swr in skip_writers:
+            if isinstance(writer, RawLog.name2writerType[swr]):
+                return False  # skip this writer, do not write
+        writer.write(self)
+        return False
+
+    @property
+    def json(self) -> dict:
+        default_style = self.style
+        # prefix
+        prefix = self.prefix or default_style.prefix
+        # composing datetime
+        with_datetime = (
+            default_style.with_datetime if self.with_datetime is None else self.with_datetime
+        )
+        timestamp = ""
+        if with_datetime:
+            datetime_fmt = self.datetime_format or default_style.datetime_format
+            timestamp = datetime.now().strftime(datetime_fmt)
+
+        # composing identifier
+        whom = ""
+        with_identifier = (
+            default_style.with_identifier if self.with_identifier is None else self.with_identifier
+        )
+        if with_identifier:
+            identity_seq = self.caller_identity.as_str_sequence(lineno=False)
+            if len(identity_seq) > 1:
+                identity_seq = identity_seq[1:]
+            if len(identity_seq) > self.style.trace_level:
+                identity_seq = identity_seq[: self.style.trace_level - 1]
+            whom = (
+                str(self.whom)
+                if self.whom is not None
+                else default_style.split_char_identity.join(identity_seq)
+            )  # check identity
+        return {"series": prefix, "timestamp": timestamp, "whom": whom, "message": self.rich_msg}
+
+    def __repr__(self) -> str:
+        return json.dumps(self.json, default=str)
+
+
+# ================== CONSOLE LOG WRITER =====================
+
+
+class DefaultDictThatReturnsKeyOnMissing(defaultdict):
+    def __missing__(self, key):
+        return key
+
+
+_console_prefix_2_colored_text = DefaultDictThatReturnsKeyOnMissing(
+    str,
+    {
+        "ok": colored_text("[ok]", "green"),
+        "debug": colored_text("[debug]", "cyan"),
+        "info": colored_text("[info]", "white"),
+        "warning": colored_text("[warning]", "yellow"),
+        "mention": colored_text("[mention]", "yellow"),
+        "error": colored_text("[error]", "red"),
+    },
+)
+
+
+class __ConsoleLogWriter(LogWriter):
+    _console = Console()
+
+    def write(self, raw_log: RawLog):
+        _msg_dict = raw_log.json
+        _style = raw_log.style
+        _prefix = _msg_dict["series"]
+        _prefix = _console_prefix_2_colored_text[_prefix] + " " if _prefix else _prefix
+        rich_msg = str(
+            _prefix
+            + _msg_dict["timestamp"]
+            + _style.split_char_cmd * min(len(_msg_dict["timestamp"]), 1)
+            + styled_text(_msg_dict["whom"], style=_style)
+            + _style.split_char_cmd * min(len(_msg_dict["whom"]), 1)
+            + _msg_dict["message"]
+        )
+        self.__class__._console.print(rich_msg)
+
+
+# console writer singleton
+consoleLogWriter = __ConsoleLogWriter()
+
+
+# ================== FILE LOG WRITER =====================
+
+
+class FileLogWriter(LogWriter):
+    # class level static pool
+    PATH_2_FILE_WRITER = {}
+
+    # instance level non-static things
+    file_writer = None  # assign in __init__
+
+    def __new__(cls, path, *args, **kwargs):
+        # per file, per writer.
+        file_abs_path = os.path.abspath(path)
+        if os.path.isdir(file_abs_path):
+            raise Exception("Target path is not a file.")
+        filename = formatting.legal_file_name_of(os.path.basename(path))
+        dirname = os.path.dirname(path) if len(os.path.dirname(path)) != 0 else "."
+        if not os.path.exists(dirname):
+            raise Exception(f"Could not find dictionary {dirname}")
+        real_path = os.path.join(dirname, filename)
+        if file_abs_path not in FileLogWriter.PATH_2_FILE_WRITER:
+            newWriter = LogWriter.__new__(cls)
+            newWriter.file_writer = open(real_path, "a", encoding="utf-8", buffering=1)
+            FileLogWriter.PATH_2_FILE_WRITER[file_abs_path] = newWriter
+
+        return FileLogWriter.PATH_2_FILE_WRITER[file_abs_path]
+
+    def __init__(self, path) -> None:
+        self.file_writer = open(path, "a", encoding="utf-8", buffering=1)
+
+    def write(self, raw_log: RawLog):
+        _msg_dict = raw_log.json
+        _style = raw_log.style
+        _series_text = f"[{_msg_dict['series']}]" if _msg_dict["series"] else ""
+        text_msg = _style.split_char_txt.join(
+            [_msg_dict["timestamp"], _series_text, _msg_dict["whom"], _msg_dict["message"]]
+        )
+        self.file_writer.write(text_msg + "\n")
+
+
+# ================== WS LOG WRITER =====================
+
+
+class _WebSocketLogWriter(LogWriter):
+    # class level statics
+    connection = None  # connection should be assigned by neetbox._daemon.client._client to avoid recursive import
+
+    def write(self, raw_log: RawLog):
+        json_data = raw_log.json
+        payload = {"whom": json_data["whom"], "message": json_data["message"]}
+        if _WebSocketLogWriter.connection:
+            _WebSocketLogWriter.connection.ws_send(
+                event_type="log",
+                series=json_data["series"],
+                payload=payload,
+                timestamp=json_data["timestamp"],
+            )
+
+
+def _assign_connection_to_WebSocketLogWriter(conn):
+    _WebSocketLogWriter.connection = conn
+
+
+webSocketLogWriter = _WebSocketLogWriter()
+
+
+# ================== JSON LOG WRITER =====================
+
+
+class JsonLogWriter(FileLogWriter):
+    def write(self, raw_log: RawLog):
+        # todo convert to json and write to file
+        pass
+
+
+# ================== POST INIT TYPE REF =====================
+
+RawLog.name2writerType = {
+    "stdout": __ConsoleLogWriter,
+    "file": FileLogWriter,
+    "ws": _WebSocketLogWriter,
+    "json": JsonLogWriter,
+}
---
+++ packages/neetbox/neetbox-0.4.2-py3-none-any.whl/neetbox/utils/massive.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+#
+# Author: GavinGong aka VisualDust
+# Github: github.com/visualDust
+# Date:   20231216
+
+import collections
+import functools
+import os
+import socket
+import struct
+from concurrent.futures import ThreadPoolExecutor
+
+_ThreadPoolExecutor = ThreadPoolExecutor()
+
+
+def is_loopback(host):
+    loopback_checker = {
+        socket.AF_INET: lambda x: struct.unpack("!I", socket.inet_aton(x))[0] >> (32 - 8) == 127,
+        socket.AF_INET6: lambda x: x == "::1",
+    }
+    for family in (socket.AF_INET, socket.AF_INET6):
+        try:
+            r = socket.getaddrinfo(host, None, family, socket.SOCK_STREAM)
+        except socket.gaierror:
+            return False
+        for family, _, _, _, sockaddr in r:
+            if not loopback_checker[family](sockaddr[0]):
+                return False
+    return True
+
+
+def nonblocking(func):
+    @functools.wraps(func)
+    def _use_thread_pool(*args, **kwargs):
+        try:
+            return _ThreadPoolExecutor.submit(func, *args, **kwargs)  # type: ignore
+        except Exception as e:
+            raise e
+
+    return _use_thread_pool
+
+
+def update_dict_recursively(self: dict, the_other: dict):
+    for _k, _v in the_other.items():
+        if type(_v) is dict:  # currently resolving a dict child
+            if _k in self:  # dfs merge
+                update_dict_recursively(self=self[_k], the_other=the_other[_k])
+            else:
+                self[_k] = the_other[_k]
+        else:  # not a dict, overwriting
+            self[_k] = the_other[_k]
+
+
+def get_user_config_directory():
+    """Returns a platform-specific root directory for user config settings.
+    On Windows, prefer %LOCALAPPDATA%, then %APPDATA%, since we can expect the AppData directories to be ACLed to be visible only to the user and admin users (https://stackoverflow.com/a/7617601/1179226). If neither is set, return None instead of falling back to something that may be world-readable.
+    """
+    if os.name == "nt":
+        appdata = os.getenv("LOCALAPPDATA")
+        if appdata:
+            return appdata
+        appdata = os.getenv("APPDATA")
+        if appdata:
+            return appdata
+        return None
+    # On non-windows, use XDG_CONFIG_HOME if set, else default to ~/.config.
+    xdg_config_home = os.getenv("XDG_CONFIG_HOME")
+    if xdg_config_home:
+        return xdg_config_home
+    return os.path.join(os.path.expanduser("~"), ".config")
+
+
+def check_read_toml(path) -> bool:
+    import toml
+
+    if not os.path.isfile(path):  # but config file not exist
+        return False
+    try:
+        return toml.load(path)  # try load as toml
+    except Exception as e:
+        return False
+
+
+def describe_object(obj, length_limit=None) -> str:
+    if hasattr(obj, "__name__"):
+        description = obj.__name__
+    elif hasattr(obj, "__class__"):
+        description = f"{obj.__class__.__name__} object"
+    else:
+        description = str(obj)
+    if length_limit and len(description) > length_limit:
+        description = description[:length_limit] + "..."
+    return description
