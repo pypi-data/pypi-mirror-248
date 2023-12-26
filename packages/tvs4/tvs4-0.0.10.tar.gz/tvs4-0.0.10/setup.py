d3b873e35ed0366bf114bdfe53cdf9d21cacbabf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:26 +0000

    Add package tvs4/0.0.10/tvs4-0.0.10.tar.gz

---
+++ packages/tvs4/tvs4-0.0.10.tar.gz/tvs4-0.0.10/setup.py
@@ -0,0 +1,16 @@
+from io import open
+from setuptools import setup
+
+version = '0.0.10'
+
+setup(
+    name = 'tvs4',
+    version=version,
+
+    author='Lohi',
+    author_email='9628782183oleg@gmail.com',
+
+    license='Free' ,
+    
+    packages= ['tvs4']
+)
