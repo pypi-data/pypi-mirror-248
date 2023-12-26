75e72da442cf1f6baf7f1ef5dafb5cfd59fda35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:06:46 +0000

    Add package spaceone-monitoring/2.0.dev37/spaceone_monitoring-2.0.dev37-py3-none-any.whl

---
+++ packages/spaceone-monitoring/spaceone_monitoring-2.0.dev37-py3-none-any.whl/spaceone/monitoring/manager/identity_manager.py
@@ -0,0 +1,46 @@
+import logging
+
+from spaceone.core import config
+from spaceone.core.connector.space_connector import SpaceConnector
+from spaceone.core.manager import BaseManager
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class IdentityManager(BaseManager):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.identity_connector: SpaceConnector = self.locator.get_connector(
+            "SpaceConnector", service="identity"
+        )
+
+    def get_domain(self, domain_id):
+        return self.identity_connector.dispatch("Domain.get", {"domain_id": domain_id})
+
+    def get_domain_by_system_token(self, domain_id):
+        system_token = config.get_global("TOKEN")
+        return self.identity_connector.dispatch(
+            "Domain.get",
+            {"domain_id": domain_id},
+            token=system_token,
+            x_domain_id=domain_id,
+        )
+
+    def get_user(self, user_id):
+        return self.identity_connector.dispatch("User.get", {"user_id": user_id})
+
+    def get_project(self, project_id: str) -> dict:
+        return self.identity_connector.dispatch(
+            "Project.get", {"project_id": project_id}
+        )
+
+    def check_workspace(self, workspace_id: str, domain_id: str) -> dict:
+        return self.identity_connector.dispatch(
+            "Workspace.check", {"workspace_id": workspace_id, "domain_id": domain_id}
+        )
+
+    def get_service_account(self, service_account_id, domain_id):
+        return self.identity_connector.dispatch(
+            "ServiceAccount.get",
+            {"service_account_id": service_account_id, "domain_id": domain_id},
+        )
