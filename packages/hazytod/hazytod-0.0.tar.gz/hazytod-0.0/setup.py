ce0c6093d284565d05cdd1818bb7932e041d2af2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:02:45 +0000

    Add package hazytod/0.0/hazytod-0.0.tar.gz

---
+++ packages/hazytod/hazytod-0.0.tar.gz/hazytod-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name='hazytod',
+      version='0.0',
+      description='tod',
+      packages=['hazytod'],
+      author_email='karpenko.milorad@gmail.com',
+      zip_safe=False)
