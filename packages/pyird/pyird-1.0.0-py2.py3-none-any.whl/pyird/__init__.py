dc171bd02ed88bb6a213ef887a592c225b5997a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:04:57 +0000

    Add package pyird/1.0.0/pyird-1.0.0-py2.py3-none-any.whl

---
+++ packages/pyird/pyird-1.0.0-py2.py3-none-any.whl/pyird/__init__.py
@@ -0,0 +1,11 @@
+__all__ = ['fitsset', 'irdstream']
+
+__version__ = '1.0.0'
+
+#from . import fitsset
+#from . import irdstream
+__uri__ = ''
+__author__ = 'Yui Kasagi, Hajime Kawahara, Kento Masuda'
+__email__ = 'divrot@gmail.com'
+__license__ = ''
+__description__ = 'Python IRD'
