14a280d513133ddc501598845354006e3a284bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:02:55 +0000

    Add package Flask-Status/1.0.1/Flask-Status-1.0.1.tar.gz

---
+++ packages/Flask-Status/Flask-Status-1.0.1.tar.gz/Flask-Status-1.0.1/flask_status/main.py
@@ -0,0 +1,77 @@
+from types import FunctionType
+from typing import Any, Dict, Callable
+from flask import Flask, jsonify
+
+
+class FlaskStatus:
+    """Flask status - Add status ping route to flask application
+    ```
+    FlaskStatus(app)
+    ```
+    """
+
+    def __init__(
+        self,
+        app: Flask | None = None,
+        url: str = "/api/status",
+        message: str | Dict[str, str] = {"status": "OK"},
+        authenticator: Callable[[Callable], Callable] = None,
+    ):
+        """Initialize flask-status"""
+        if app is not None:
+            self.init_app(app, url, message, authenticator)
+
+    def init_app(
+        self,
+        app: Flask,
+        url: str = "/api/status",
+        message: str | Dict[str, str] = {"status": "OK"},
+        authenticator: Callable[[Callable], Callable] = None,
+    ) -> None:
+        """Add flask app to flask-status"""
+        if type(url) is not str:
+            raise TypeError("Status Ping URL must be a string")
+        if not url.startswith("/"):
+            url = "/" + url
+        self.status_ping_url = url
+
+        if type(message) is str:
+            self.status_message = {"message": message}
+        elif type(message) is dict:
+            self.status_message = message
+        else:
+            raise TypeError("Status Ping message must be a string or dictionary")
+
+        if authenticator is not None:
+            if type(authenticator) is not FunctionType:
+                raise TypeError(
+                    "Status Ping authenticator must be a decorator function"
+                )
+
+            @app.route(self.status_ping_url, methods=["GET"])
+            @authenticator
+            def status_ping():
+                """Authenticated status ping endpoint"""
+                return jsonify(self.status_message), 200
+
+            return
+
+        @app.route(self.status_ping_url, methods=["GET"])
+        def status_ping():
+            """Status ping endpoint"""
+            return jsonify(self.status_message), 200
+
+    def add_field(self, key: str, value: Any) -> None:
+        """Add more field data to payload"""
+        if type(key) is not str:
+            raise TypeError("Status Ping field key must be a string")
+        if type(value) is FunctionType:
+            self.status_message[key] = value()
+        else:
+            try:
+                import json
+
+                json.dumps(value)
+            except TypeError:
+                raise TypeError("Status Ping field value is not JSON serializable")
+            self.status_message[key] = value
