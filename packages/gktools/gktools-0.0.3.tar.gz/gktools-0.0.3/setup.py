37f42e3f00771129281b50eb2e5d212513d37c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:17 +0000

    Add package gktools/0.0.3/gktools-0.0.3.tar.gz

---
+++ packages/gktools/gktools-0.0.3.tar.gz/gktools-0.0.3/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.3'
+DESCRIPTION = 'gktools is an assiting package for doing basic math operations'
+LONG_DESCRIPTION = 'A package that allows to do basic math operations. This package is built as part of a tutorial for packaging python module'
+
+# Setting up
+setup(
+    name="gktools",
+    version=VERSION,
+    author="Gokul Kumar Jayaram",
+    author_email="<gokulkumar.jayaram@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'gktools', 'packaging' , 'baisc math'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
