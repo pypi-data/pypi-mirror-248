0b0394741b97143622a798aae50011e28471fc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:40 +0000

    Add package tazocheishvili/0.0.1/tazocheishvili-0.0.1.tar.gz

---
+++ packages/tazocheishvili/tazocheishvili-0.0.1.tar.gz/tazocheishvili-0.0.1/__init__.py
@@ -0,0 +1,9 @@
+
+def add_two_nums(num1,num2):
+    return num1+num2
+def substract_two_nums(num1,num2):
+    return num1-num2
+def multiple_two_nums(num1,num2):
+    return num1*num2
+def divide_two_nums(num1,num2):
+    return num1/num2
---
+++ packages/tazocheishvili/tazocheishvili-0.0.1.tar.gz/tazocheishvili-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup,find_packages
+classifiers=[
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+
+]
+setup(
+     name='tazocheishvili',
+     version='0.0.1',
+     description='calculator',
+     long_description=open('README.txt').read()+'\n\n'+open('CHANGELOG.txt').read(),
+    url='',
+    author='Tazo Cheishvili',
+    author_email='tazo.cheishvili.1@btu.edu.ge',
+    license='MIT',
+    keywords='calculator',
+    classifiers=classifiers,
+    packages=find_packages(),
+    install_requires=['']
+
+
+
+
+)
