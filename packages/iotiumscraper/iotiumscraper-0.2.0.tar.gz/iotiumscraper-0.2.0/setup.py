04ff370c9f779e46a7f3bbd96df47aafb25a6172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:04:00 +0000

    Add package iotiumscraper/0.2.0/iotiumscraper-0.2.0.tar.gz

---
+++ packages/iotiumscraper/iotiumscraper-0.2.0.tar.gz/iotiumscraper-0.2.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='iotiumscraper',
+    packages=find_packages(),
+    version='0.2.0',
+    description='scrape data from kastle and push to postgres',
+    author='Raja',
+)
