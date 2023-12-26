b002fc3621e59e42cc4280be031c82c8efb96cad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 14:03:28 +0000

    Add package lovely-pancake/1.0.3/lovely-pancake-1.0.3.tar.gz

---
+++ packages/lovely-pancake/lovely-pancake-1.0.3.tar.gz/lovely-pancake-1.0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='lovely-pancake',
+    version='1.0.3',
+    author='Ahmet Duzduran',
+    description='My lovely python package',
+    packages=['lovelypancake'],
+)
