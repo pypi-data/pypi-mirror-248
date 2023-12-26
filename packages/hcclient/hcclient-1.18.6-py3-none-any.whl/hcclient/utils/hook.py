71b50731965ab02ce5d29bd743314cd070ba3c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:03:35 +0000

    Add package hcclient/1.18.6/hcclient-1.18.6-py3-none-any.whl

---
+++ packages/hcclient/hcclient-1.18.6-py3-none-any.whl/hcclient/utils/hook.py
@@ -0,0 +1,31 @@
+# Author:    AnnikaV9
+# License:   Unlicense
+
+import os
+import sys
+import importlib.util
+
+from hcclient.client.client import Client
+
+
+def load_hooks(client: Client) -> Client:
+    """
+    Loads hooks from the default hooks directory and returns the modified client
+    """
+    hook_dir = os.path.join(os.getenv("APPDATA"), "hcclient", "hooks") if os.name == "nt" else os.path.join(os.getenv("HOME"), ".config", "hcclient", "hooks")
+    if not os.path.isdir(hook_dir):
+        return client
+
+    for hook in os.listdir(hook_dir):
+        if hook.endswith(".py"):
+            try:
+                hook_path = os.path.join(hook_dir, hook)
+                spec = importlib.util.spec_from_file_location(hook, hook_path)
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                client = module.hook(client)
+
+            except Exception as e:
+                sys.exit(f"{sys.argv[0]}: error: Unable to load hook '{hook}': {e}")
+
+    return client
