3fd5976320fe43bbefc7b3f538600cbf75a8b7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:08 +0000

    Add package etatime/2.8.0/etatime-2.8.0-py3-none-any.whl

---
+++ packages/etatime/etatime-2.8.0-py3-none-any.whl/etatime/__init__.py
@@ -0,0 +1,11 @@
+"""A library for tracking, computing, and formatting time estimates."""
+
+__version__ = "2.8.0"
+
+from . import eta, time, completion, constants, validate
+
+# TODO: Update raises docstrings for all validate calls
+# TODO: Finish adding docstrings and function annotations
+# TODO: Add doctest strings
+# TODO: Add comprehensive unit tests
+# TODO: Investigate if inlining instance attributes helps with speed
