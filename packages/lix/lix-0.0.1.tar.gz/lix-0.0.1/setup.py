a5ceb864ee4e2a7f588bbb5973dbff21367ee167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:04:38 +0000

    Add package lix/0.0.1/lix-0.0.1.tar.gz

---
+++ packages/lix/lix-0.0.1.tar.gz/lix-0.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="lix",
+    version="0.0.1",
+    description="...",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://www.google.com",
+    author="...",
+    license="MIT",
+    packages=find_packages(),
+    include_package_data=True,
+)
