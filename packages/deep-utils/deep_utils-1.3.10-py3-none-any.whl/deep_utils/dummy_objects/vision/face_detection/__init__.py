125b71e7fc1f78d3844b025f614bc13e6f74a2c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:22 +0000

    Add package deep-utils/1.3.10/deep_utils-1.3.10-py3-none-any.whl

---
+++ packages/deep-utils/deep_utils-1.3.10-py3-none-any.whl/deep_utils/dummy_objects/vision/face_detection/__init__.py
@@ -0,0 +1,35 @@
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# class RetinaFaceTorchFaceDetector(metaclass=DummyObject):
+#     _backend = ["torch", "cv2"]
+#
+#     def __init__(self, *args, **kwargs):
+#         requires_backends(self, self._backend)
+#
+#
+# class SSDCV2CaffeFaceDetector(metaclass=DummyObject):
+#     _backend = ["cv2"]
+#
+#     def __init__(self, *args, **kwargs):
+#         requires_backends(self, self._backend)
+#
+#
+# class UltralightTFFaceDetector(metaclass=DummyObject):
+#     _backend = ["tensorflow", "cv2"]
+#
+#     def __init__(self, *args, **kwargs):
+#         requires_backends(self, self._backend)
+#
+#
+# class UltralightTorchFaceDetector(metaclass=DummyObject):
+#     _backend = ["torch", "cv2"]
+#
+#     def __init__(self, *args, **kwargs):
+#         requires_backends(self, self._backend)
