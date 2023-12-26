c8b2e12f0ecb1c43dfd6e374949533564f45c2a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:57 +0000

    Add package SegmentCar/0.1.0/SegmentCar-0.1.0.tar.gz

---
+++ packages/SegmentCar/SegmentCar-0.1.0.tar.gz/SegmentCar-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+
+
+
+setup(
+    name='SegmentCar',
+    version='0.1.0',
+    package_data={'carbgremover': ['pretrained_models/*', 'images/*']},
+    packages=find_packages(),
+    install_requires=['numpy', 'tensorflow', 'ultralytics', 'segment_anything','tqdm','requests'],
+    long_description=open('README.md').read(),
+    long_description_content_type='markdown',
+
+)
