dc43a24dde79682959c6ce6c4a878d236d0a60f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 14:03:30 +0000

    Add package halftones/0.1.0/halftones-0.1.0.tar.gz

---
+++ packages/halftones/halftones-0.1.0.tar.gz/halftones-0.1.0/README.md
@@ -0,0 +1,16 @@
+# Halftones Package
+
+Python package for creating halftone images.
+
+## Installation
+
+`pip install halftones`
+
+## Usage
+
+Import the package and use the functions as required.
+
+```python
+import halftones_pkg
+# Use package functions here
+```
---
+++ packages/halftones/halftones-0.1.0.tar.gz/halftones-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='halftones',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Python package for processing and creating halftone images',
+    author='Erkin Ötleş',
+    author_email='hi@eotles.com',
+    url='https://github.com/eotles/halftones_pkg',
+)
