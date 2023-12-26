049777d3ab09d9e426613c9547a0efb9a307a501
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:03:57 +0000

    Add package python-listmonk/0.0.0/python_listmonk-0.0.0-py3-none-any.whl

---
+++ packages/python-listmonk/python_listmonk-0.0.0-py3-none-any.whl/listmonk/api/__init__.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+
+# import apis into api package
+from listmonk.api.admin_api import AdminApi
+from listmonk.api.bounces_api import BouncesApi
+from listmonk.api.campaigns_api import CampaignsApi
+from listmonk.api.import_api import ImportApi
+from listmonk.api.lists_api import ListsApi
+from listmonk.api.logs_api import LogsApi
+from listmonk.api.maintenance_api import MaintenanceApi
+from listmonk.api.media_api import MediaApi
+from listmonk.api.miscellaneous_api import MiscellaneousApi
+from listmonk.api.public_api import PublicApi
+from listmonk.api.settings_api import SettingsApi
+from listmonk.api.subscribers_api import SubscribersApi
+from listmonk.api.templates_api import TemplatesApi
+from listmonk.api.transactional_api import TransactionalApi
+
