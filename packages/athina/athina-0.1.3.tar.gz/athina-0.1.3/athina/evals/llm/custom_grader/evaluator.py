345990924b17b34a52ce2759e3d6bfe1419cdc3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:04:37 +0000

    Add package athina/0.1.3/athina-0.1.3.tar.gz

---
+++ packages/athina/athina-0.1.3.tar.gz/athina-0.1.3/athina/evals/llm/custom_grader/evaluator.py
@@ -0,0 +1,42 @@
+from typing import List
+from ..llm_evaluator import LlmEvaluator
+from ..eval_type import AthinaEvalTypeId
+
+
+class CustomGrader(LlmEvaluator):
+    """
+    This evaluator checks if the response is correct according to a provided `grading_criteria`.
+    """
+
+    _examples = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if "grading_criteria" not in kwargs:
+            raise Exception("grading_criteria is required for CustomLlmEvaluator")
+        if "examples" in kwargs:
+            self._examples = kwargs["examples"]
+
+    @property
+    def name(self):
+        return AthinaEvalTypeId.CUSTOM.value
+
+    @property
+    def metric_id(self) -> str:
+        return None
+
+    @property
+    def display_name(self):
+        return "Custom"
+
+    @property
+    def default_model(self):
+        return "gpt-4-1106-preview"
+
+    @property
+    def required_args(self):
+        return ["response"]
+
+    @property
+    def examples(self):
+        return self._examples
