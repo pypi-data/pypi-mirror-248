65582a94e095de00cb83cf6dd7b832c65aa2114b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 14:02:58 +0000

    Add package unegui-models/0.1.5/unegui_models-0.1.5.tar.gz

---
+++ packages/unegui-models/unegui_models-0.1.5.tar.gz/unegui_models-0.1.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='unegui_models',
+    version='0.1.5',
+    packages=find_packages(),
+    include_package_data=True,
+    description='A library for shared SQLAlchemy models',
+    install_requires=[
+        'sqlalchemy', 
+        'python-dotenv',
+        'psycopg2'
+        # Add other dependencies required by your models here
+    ],
+    python_requires='>=3.6',
+    # Add other necessary package metadata
+)
