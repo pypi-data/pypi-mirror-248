e2cc29c3151c09359b6199155b1d30bb601ece7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:05 +0000

    Add package leaven/1.1.0b33/leaven-1.1.0b33.tar.gz

---
+++ packages/leaven/leaven-1.1.0b33.tar.gz/leaven-1.1.0b33/leaven/__init__.py
@@ -0,0 +1,7 @@
+from leaven.src.lean_manager import *
+get_elan()
+get_lean()
+get_mathlib_datasets()
+from leaven.src.lean_server import *
+from leaven.src.proof_search_agent import *
+from leaven.src.lean_file_analyse import *
---
+++ packages/leaven/leaven-1.1.0b33.tar.gz/leaven-1.1.0b33/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_namespace_packages, setup
+
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setup(name='leaven',
+      version='1.1.0b33',
+      license='MIT',
+      description='A package bridging Python and Lean verification for proof search using language models',
+      long_description=long_description,
+      author='Huajian Xin',
+      author_email='xinhuajian2000@gmail.com',
+      url='https://github.com/xinhjBrant/leaven',
+      download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+      install_requires=[
+        "networkx", "pathlib", "datetime", "pytz", "tqdm"
+      ],
+      packages=find_namespace_packages(exclude=['leaven.__pycache__', 'leaven.src.__pycache__']),
+      include_package_data=True,
+      platforms=["all"],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.9',
+          'Topic :: Software Development :: Libraries'
+      ],
+      long_description_content_type="text/markdown",
+      python_requires='>=3.9',
+      )
