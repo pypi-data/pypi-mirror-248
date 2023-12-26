6845ef111a32a2e279f3e0a8a9d02077b8487d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:23 +0000

    Add package flamme/0.0.8a0/flamme-0.0.8a0-py3-none-any.whl

---
+++ packages/flamme/flamme-0.0.8a0-py3-none-any.whl/flamme/analyzer/__init__.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+__all__ = [
+    "BaseAnalyzer",
+    "ColumnContinuousAnalyzer",
+    "ColumnDiscreteAnalyzer",
+    "ColumnSubsetAnalyzer",
+    "ColumnTemporalContinuousAnalyzer",
+    "ColumnTemporalDiscreteAnalyzer",
+    "ColumnTemporalNullValueAnalyzer",
+    "DataTypeAnalyzer",
+    "DuplicatedRowAnalyzer",
+    "FilteredAnalyzer",
+    "GlobalTemporalNullValueAnalyzer",
+    "MappingAnalyzer",
+    "MarkdownAnalyzer",
+    "NullValueAnalyzer",
+    "TemporalNullValueAnalyzer",
+    "is_analyzer_config",
+    "setup_analyzer",
+]
+
+from flamme.analyzer.base import BaseAnalyzer, is_analyzer_config, setup_analyzer
+from flamme.analyzer.column import ColumnSubsetAnalyzer
+from flamme.analyzer.continuous import (
+    ColumnContinuousAnalyzer,
+    ColumnTemporalContinuousAnalyzer,
+)
+from flamme.analyzer.discrete import (
+    ColumnDiscreteAnalyzer,
+    ColumnTemporalDiscreteAnalyzer,
+)
+from flamme.analyzer.dtype import DataTypeAnalyzer
+from flamme.analyzer.duplicate import DuplicatedRowAnalyzer
+from flamme.analyzer.filter import FilteredAnalyzer
+from flamme.analyzer.mapping import MappingAnalyzer
+from flamme.analyzer.markdown import MarkdownAnalyzer
+from flamme.analyzer.null import NullValueAnalyzer, TemporalNullValueAnalyzer
+from flamme.analyzer.null_temp_col import ColumnTemporalNullValueAnalyzer
+from flamme.analyzer.null_temp_global import GlobalTemporalNullValueAnalyzer
