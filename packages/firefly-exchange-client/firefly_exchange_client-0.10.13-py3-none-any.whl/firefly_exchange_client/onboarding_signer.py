2e6cad62c0061d1dcb6b678aa7ca88e00b0791a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:35 +0000

    Add package firefly-exchange-client/0.10.13/firefly_exchange_client-0.10.13-py3-none-any.whl

---
+++ packages/firefly-exchange-client/firefly_exchange_client-0.10.13-py3-none-any.whl/firefly_exchange_client/onboarding_signer.py
@@ -0,0 +1,20 @@
+from web3 import Web3
+from .interfaces import *
+from .signer import Signer
+
+class OnboardingSigner(Signer):
+    def __init__(self):
+        super().__init__()
+
+    def create_signature(self, msg, private_key):
+        """
+            Signs the message.
+            Inputs:
+                - msg: the message to be signed
+                - private_key: the signer's private key
+            Returns:
+                - str: signed msg hash
+        """
+        hash = Web3.keccak(text=msg).hex()
+        return self.sign_hash(hash, private_key)
+
---
+++ packages/firefly-exchange-client/firefly_exchange_client-0.10.13-py3-none-any.whl/firefly_exchange_client/signer.py
@@ -0,0 +1,38 @@
+from web3 import Web3
+import eth_account
+
+class Signer:
+    def __init__(self):
+        pass
+        
+
+    def get_eip712_hash(self, domain_hash, struct_hash):
+        """
+            Returns the EIP712 hash.
+            Inputs:
+                - domain_hash: chain domain hash
+                - struct_hash: struct hash of information to be signed
+        """
+        return Web3.solidity_keccak(
+        [
+            'bytes2',
+            'bytes32',
+            'bytes32'
+        ],
+        [
+            '0x1901',
+            domain_hash,
+            struct_hash
+        ]
+    ).hex()
+
+
+    def sign_hash(self, hash, private_key, append=''):
+        """
+            Signs the hash and returns the signature. 
+        """
+        result = eth_account.account.Account.sign_message(
+            eth_account.messages.encode_defunct(hexstr=hash),
+            private_key
+        )
+        return result['signature'].hex() + append
