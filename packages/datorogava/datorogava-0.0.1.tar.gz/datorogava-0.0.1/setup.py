ebc20d8ff686ea2ba227b487fe71f9830b39cd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:11 +0000

    Add package datorogava/0.0.1/datorogava-0.0.1.tar.gz

---
+++ packages/datorogava/datorogava-0.0.1.tar.gz/datorogava-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Operating System :: Microsoft :: Windows :: Windows 10',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+]
+setup(
+    name='datorogava',
+    version='0.0.1',
+    classifiers=classifiers,
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[""]
+)
