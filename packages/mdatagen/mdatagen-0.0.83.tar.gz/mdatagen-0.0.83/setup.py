11cd4abae0e9cc9f71e8762ccd9482d42a559377
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:02:56 +0000

    Add package mdatagen/0.0.83/mdatagen-0.0.83.tar.gz

---
+++ packages/mdatagen/mdatagen-0.0.83.tar.gz/mdatagen-0.0.83/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mdatagen',
+    version='0.0.83',
+    keywords=['machine learning', 'preprocessing data'],
+    license='MIT',
+    author='Arthur Dantas Mangussi',
+    author_email='mangussiarthur@gmail.com',
+    url='https://github.com/ArthurMangussi/pymdatagen',
+    
+    packages=find_packages(),
+    description='mdatagen: A Python library to Generate Artifical Missing Data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+
+    install_requires=[
+        'python >= 3.11',
+        'numpy >= 1.25.0',
+        'pandas >= 2.0.3',
+        'scikit-learn == 1.3.0',
+    ],
+)
+
+
