1a446d9ed883ba4af92bfdebe58387de7279d500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:27 +0000

    Add package jsonschema-specifications/2023.12.1/jsonschema_specifications-2023.12.1.tar.gz

---
+++ packages/jsonschema-specifications/jsonschema_specifications-2023.12.1.tar.gz/jsonschema_specifications-2023.12.1/noxfile.py
@@ -0,0 +1,137 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import os
+
+import nox
+
+ROOT = Path(__file__).parent
+PYPROJECT = ROOT / "pyproject.toml"
+DOCS = ROOT / "docs"
+PACKAGE = ROOT / "jsonschema_specifications"
+
+
+nox.options.sessions = []
+
+
+def session(default=True, **kwargs):  # noqa: D103
+    def _session(fn):
+        if default:
+            nox.options.sessions.append(kwargs.get("name", fn.__name__))
+        return nox.session(**kwargs)(fn)
+
+    return _session
+
+
+@session(python=["3.8", "3.9", "3.10", "3.11", "3.12", "pypy3"])
+def tests(session):
+    """
+    Run the test suite with a corresponding Python version.
+    """
+    session.install("pytest", ROOT)
+    env = dict(os.environ, PYTHONWARNDEFAULTENCODING="1")
+    session.run("pytest", "--verbosity=3", "--pythonwarnings=error", env=env)
+
+
+@session()
+def audit(session):
+    """
+    Audit dependencies for vulnerabilities.
+    """
+    session.install("pip-audit", ROOT)
+    session.run("python", "-m", "pip_audit")
+
+
+@session(tags=["build"])
+def build(session):
+    """
+    Build a distribution suitable for PyPI and check its validity.
+    """
+    session.install("build", "twine")
+    with TemporaryDirectory() as tmpdir:
+        session.run("python", "-m", "build", ROOT, "--outdir", tmpdir)
+        session.run("twine", "check", "--strict", tmpdir + "/*")
+
+
+@session(tags=["style"])
+def style(session):
+    """
+    Check Python code style.
+    """
+    session.install("ruff")
+    session.run("ruff", "check", ROOT)
+
+
+@session()
+def typing(session):
+    """
+    Check static typing.
+    """
+    session.install("mypy", "pytest", ROOT)
+    session.run("python", "-m", "mypy", PACKAGE)
+
+
+@session(tags=["docs"])
+@nox.parametrize(
+    "builder",
+    [
+        nox.param(name, id=name)
+        for name in [
+            "dirhtml",
+            "doctest",
+            "linkcheck",
+            "man",
+            "spelling",
+        ]
+    ],
+)
+def docs(session, builder):
+    """
+    Build the documentation using a specific Sphinx builder.
+    """
+    session.install("-r", DOCS / "requirements.txt")
+    with TemporaryDirectory() as tmpdir_str:
+        tmpdir = Path(tmpdir_str)
+        argv = ["-n", "-T", "-W"]
+        if builder != "spelling":
+            argv += ["-q"]
+        posargs = session.posargs or [tmpdir / builder]
+        session.run(
+            "python",
+            "-m",
+            "sphinx",
+            "-b",
+            builder,
+            DOCS,
+            *argv,
+            *posargs,
+        )
+
+
+@session(tags=["docs", "style"], name="docs(style)")
+def docs_style(session):
+    """
+    Check the documentation style.
+    """
+    session.install(
+        "doc8",
+        "pygments",
+        "pygments-github-lexers",
+    )
+    session.run("python", "-m", "doc8", "--config", PYPROJECT, DOCS)
+
+
+@session(default=False)
+def requirements(session):
+    """
+    Update the project's pinned requirements. Commit the result.
+    """
+    session.install("pip-tools")
+    for each in [DOCS / "requirements.in"]:
+        session.run(
+            "pip-compile",
+            "--resolver",
+            "backtracking",
+            "--strip-extras",
+            "-U",
+            each.relative_to(ROOT),
+        )
