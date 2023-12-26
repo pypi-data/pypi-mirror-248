96a42b51821c82933051f3dfb45fc3c3a88502eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 04:02:45 +0000

    Add package CarSegPro/0.0.2/CarSegPro-0.0.2.tar.gz

---
+++ packages/CarSegPro/CarSegPro-0.0.2.tar.gz/CarSegPro-0.0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+import os
+
+def get_long_description():
+    """Read long description from README"""
+    with open("README.md", encoding="utf-8") as f:
+        long_description = f.read()
+    return long_description
+
+
+setup(
+    name='CarSegPro',
+    version='0.0.2',
+    package_data={'carbgremover': ['pretrained_models/*', 'images/*']},
+    url="https://github.com/NechbaMohammed/CarSegPro",
+    author=['Tawfiq Adnane','Mohammed Nechba'],
+    author_email='tawfiqaadnane@gmail.com',
+    maintainer_email='mohammednechba@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy', 'tensorflow', 'ultralytics', 'segment_anything','tqdm','requests'],
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+
+)
