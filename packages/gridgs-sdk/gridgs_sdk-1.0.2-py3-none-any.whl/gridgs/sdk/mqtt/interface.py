46be36612cd1c78f6db22b2b3bdec469964766b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:04:59 +0000

    Add package gridgs-sdk/1.0.2/gridgs_sdk-1.0.2-py3-none-any.whl

---
+++ packages/gridgs-sdk/gridgs_sdk-1.0.2-py3-none-any.whl/gridgs/sdk/mqtt/interface.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+from typing import Callable
+
+from paho.mqtt.client import MQTTMessageInfo
+
+from gridgs.sdk.entity.frame import Frame
+from gridgs.sdk.entity.session import Session
+
+
+class Connector(ABC):
+    @abstractmethod
+    def connect(self) -> None:
+        pass
+
+    @abstractmethod
+    def disconnect(self) -> None:
+        pass
+
+
+class Subscriber(ABC):
+    @abstractmethod
+    def subscribe(self, session: Session, on_downlink: Callable[[Frame], None]) -> None:
+        pass
+
+
+class Sender(ABC):
+    @abstractmethod
+    def send(self, session: Session, raw_data: bytes) -> MQTTMessageInfo:
+        pass
