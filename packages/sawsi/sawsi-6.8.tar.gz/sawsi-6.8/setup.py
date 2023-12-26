8e93afe74e4493e293f76669c5f07fcbc7eb9f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 15:03:07 +0000

    Add package sawsi/6.8/sawsi-6.8.tar.gz

---
+++ packages/sawsi/sawsi-6.8.tar.gz/sawsi-6.8/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sawsi",
+    version="6.8",
+    packages=find_packages(),
+    install_requires=[
+        'requests==2.31.0'
+    ],
+)
