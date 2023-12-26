65a163851d23eecdd28ccd729c7b839ed2847b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 03:05:53 +0000

    Add package streamable/0.0.2/streamable-0.0.2.tar.gz

---
+++ packages/streamable/streamable-0.0.2.tar.gz/streamable-0.0.2/streamable/_execution/_concurrency.py
@@ -0,0 +1,152 @@
+import time
+from concurrent.futures import Future, ThreadPoolExecutor
+from dataclasses import dataclass
+from queue import Queue
+from typing import Callable, Iterable, Iterator, Optional, Set, TypeVar, Union
+
+from streamable import _util
+from streamable._execution._core import IteratorWrapper
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+@dataclass
+class _ExceptionContainer(Exception):
+    exception: Exception
+
+
+class _Skip:
+    pass
+
+
+class ThreadedMappingIteratorWrapper(IteratorWrapper[R]):
+    def __init__(
+        self,
+        iterator: Iterator[T],
+        func: Callable[[T], Union[R, _Skip]],
+        n_workers: int,
+    ):
+        super().__init__(iter(ThreadedMappingIterable(iterator, func, n_workers)))
+
+    def __next__(self) -> R:
+        while True:
+            elem = next(self.iterator)
+            if isinstance(elem, _ExceptionContainer):
+                raise elem.exception
+            if elem != ThreadedFlatteningIteratorWrapper._SKIP:
+                return elem
+
+
+BUFFER_SIZE_FACTOR = 16
+
+
+class ThreadedMappingIterable(Iterable[Union[R, _ExceptionContainer]]):
+    def __init__(self, iterator: Iterator[T], func: Callable[[T], R], n_workers: int):
+        self.iterator = iterator
+        self.func = func
+        self.n_workers = n_workers
+        self.max_queue_size = n_workers * BUFFER_SIZE_FACTOR
+
+    def __iter__(self) -> Iterator[Union[R, _ExceptionContainer]]:
+        futures: "Queue[Future]" = Queue()
+        iterator_exhausted = False
+        n_yields = 0
+        n_iterated_elems = 0
+        with ThreadPoolExecutor(max_workers=self.n_workers) as executor:
+            while True:
+                try:
+                    while (
+                        not iterator_exhausted
+                        and executor._work_queue.qsize() < self.max_queue_size
+                        and n_iterated_elems - n_yields < self.max_queue_size
+                    ):
+                        try:
+                            elem = next(self.iterator)
+                            n_iterated_elems += 1
+                            futures.put(executor.submit(self.func, elem))
+                        except StopIteration:
+                            iterator_exhausted = True
+                    while True:
+                        if n_yields < n_iterated_elems:
+                            n_yields += 1
+                            yield futures.get().result()
+                        if iterator_exhausted and n_iterated_elems == n_yields:
+                            return
+                        if (
+                            not iterator_exhausted
+                            and executor._work_queue.qsize() < self.max_queue_size // 2
+                        ):
+                            break
+                except Exception as e:
+                    yield _ExceptionContainer(e)
+
+
+class ThreadedFlatteningIteratorWrapper(ThreadedMappingIteratorWrapper[T]):
+    _SKIP: _Skip = _Skip()
+    _INIT_RETRY_BACKFOFF = 0.0005
+
+    class IteratorIteratorNextsShuffler(Iterator[Callable[[], Union[T, _Skip]]]):
+        def __init__(self, iterator_iterator: Iterator[Iterable[T]], pool_size: int):
+            self.iterator_iterator = iterator_iterator
+            self.iterator_iterator_exhausted = False
+            self.pool_size = pool_size
+            self.iterators_pool: Set[Iterator[T]] = set()
+            self.iterators_being_iterated: Set[Iterator[T]] = set()
+
+        def __next__(self) -> Callable[[], Union[T, _Skip]]:
+            backoff = ThreadedFlatteningIteratorWrapper._INIT_RETRY_BACKFOFF
+            while True:
+                while (
+                    not self.iterator_iterator_exhausted
+                    and len(self.iterators_pool) < self.pool_size
+                ):
+                    try:
+                        elem = next(self.iterator_iterator)
+                        _util.ducktype_assert_iterable(elem)
+                        self.iterators_pool.add(iter(elem))
+                    except StopIteration:
+                        self.iterator_iterator_exhausted = True
+
+                try:
+                    next_iterator_elem = self.iterators_pool.pop()
+                    self.iterators_being_iterated.add(next_iterator_elem)
+                    backoff = ThreadedFlatteningIteratorWrapper._INIT_RETRY_BACKFOFF
+                except KeyError:  # KeyError: 'pop from an empty set'
+                    if (
+                        self.iterator_iterator_exhausted
+                        and len(self.iterators_being_iterated) == 0
+                    ):
+                        raise StopIteration()
+                    time.sleep(backoff)
+                    backoff *= 2
+                    continue
+
+                def f() -> Union[T, _Skip]:
+                    exhausted = False
+                    to_be_raised: Optional[Exception] = None
+                    try:
+                        elem = next(next_iterator_elem)
+                    except StopIteration:
+                        exhausted = True
+                    except Exception as e:
+                        to_be_raised = e
+                    self.iterators_being_iterated.remove(next_iterator_elem)
+                    if exhausted:
+                        return ThreadedFlatteningIteratorWrapper._SKIP
+                    else:
+                        self.iterators_pool.add(next_iterator_elem)
+                    if to_be_raised is not None:
+                        raise to_be_raised
+                    return elem
+
+                return f
+
+    def __init__(self, iterator: Iterator[Iterable[T]], n_workers: int):
+        super().__init__(
+            ThreadedFlatteningIteratorWrapper.IteratorIteratorNextsShuffler(
+                iterator, pool_size=n_workers * BUFFER_SIZE_FACTOR
+            ),
+            func=lambda f: f(),
+            n_workers=n_workers,
+        )
---
+++ packages/streamable/streamable-0.0.2.tar.gz/streamable-0.0.2/streamable/_util.py
@@ -0,0 +1,71 @@
+import logging
+from typing import Any, Callable, Iterable, Iterator, Type, TypeVar, Union
+
+from typing_extensions import TypeGuard
+
+LOGGER = logging.getLogger("streamable")
+LOGGER.propagate = False
+handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+LOGGER.addHandler(handler)
+LOGGER.setLevel(logging.INFO)
+
+
+T = TypeVar("T")
+R = TypeVar("R")
+
+
+def sidify(func: Callable[[T], Any]) -> Callable[[T], T]:
+    def wrap(arg):
+        func(arg)
+        return arg
+
+    return wrap
+
+
+def map_exception(
+    func: Callable[[T], R], source: Type[Exception], target: Type[Exception]
+) -> Callable[[T], R]:
+    def wrap(arg):
+        try:
+            return func(arg)
+        except source as e:
+            raise target() from e
+
+    return wrap
+
+
+def iterate(it: Union[Iterator[T], Iterable[T]]) -> None:
+    for _ in it:
+        pass
+
+
+def identity(obj: T) -> T:
+    return obj
+
+
+def ducktype_assert_iterable(expected_iterator: Any) -> TypeGuard[Iterable]:
+    """
+    Raises:
+        TypeError: If the expected_iterator does not implement __iter__ and __next__ methods.
+    """
+    try:
+        expected_iterator.__iter__
+    except AttributeError:
+        raise TypeError(
+            f"Provided object is not an iterator because it does not implement the __iter__ methods."
+        )
+    return True
+
+
+def colorize_in_red(s: str) -> str:
+    return f"\033[91m{s}\033[0m"
+
+
+def colorize_in_grey(s: str) -> str:
+    return f"\033[90m{s}\033[0m"
+
+
+def bold(s: str) -> str:
+    return f"\033[1m{s}\033[0m"
