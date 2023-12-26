53afcf5a745571ff44ae39d576748c59792626e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-26 14:02:59 +0000

    Add package samaker/0.0.2/samaker-0.0.2.tar.gz

---
+++ packages/samaker/samaker-0.0.2.tar.gz/samaker-0.0.2/samaker/__init__.py
@@ -0,0 +1,4 @@
+from emoji import emojize
+__version__ = "1.0.1"
+__description__ = "Quickly Arrange,Quickly Test!"
+__image__ = emojize(fr"""{__description__}:rocket::rocket::rocket:version:{__version__}""")
