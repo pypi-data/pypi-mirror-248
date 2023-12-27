96e2606eee6047bbfea09bd5681946fcd54ebd75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:47 +0000

    Add package django-csvimport/3.2/django-csvimport-3.2.tar.gz

---
+++ packages/django-csvimport/django-csvimport-3.2.tar.gz/django-csvimport-3.2/setup.py
@@ -0,0 +1,55 @@
+import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = "3.2"
+
+with open("README.rst", "r") as fp:
+    csvimport_description = fp.read() + "\n"
+for fname in ("HISTORY.txt",):
+    with open(os.path.join("docs", fname), "r") as fp:
+        csvimport_description += fp.read() + "\n"
+
+setup(
+    name="django-csvimport",
+    version=version,
+    description="Import CSV files to django models",
+    long_description_content_type="text/x-rst",
+    long_description=csvimport_description,
+    # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Development Status :: 5 - Production/Stable",
+        "Framework :: Django",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    keywords="CVS import django fixture",
+    author="Ed Crewe",
+    author_email="edmundcrewe@gmail.com",
+    url="https://github.com/edcrewe/django-csvimport",
+    license="Apache",
+    packages=["csvimport"],
+    include_package_data=True,
+    namespace_packages=["csvimport"],
+    # this line always breaks install?
+    # package_data = {'csvimport': ['*.csv', '*.rst']},
+    zip_safe=False,
+    install_requires=["django>=2.2.5", "chardet", "dateparser"],
+    entry_points="""
+      # -*- Entry points: -*-
+      """,
+)
