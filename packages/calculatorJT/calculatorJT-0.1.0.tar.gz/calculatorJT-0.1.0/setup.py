3ed83aa92e2e84a789d81c11a335b4ae60b93c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:29 +0000

    Add package calculatorJT/0.1.0/calculatorJT-0.1.0.tar.gz

---
+++ packages/calculatorJT/calculatorJT-0.1.0.tar.gz/calculatorJT-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='calculatorJT',
+    version='0.1.0',
+    author='JT',
+    author_email='trunce123@gmail.com',
+    description='eik nx',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
