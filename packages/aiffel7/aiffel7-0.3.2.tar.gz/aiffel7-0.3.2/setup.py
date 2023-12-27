ac830b26e1db7f156a70dc6034647c0cea63a58e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:29 +0000

    Add package aiffel7/0.3.2/aiffel7-0.3.2.tar.gz

---
+++ packages/aiffel7/aiffel7-0.3.2.tar.gz/aiffel7-0.3.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='aiffel7',
+    version='0.3.2',
+    packages=find_packages(),
+    description='A simple Python library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='aiffel7_그루',
+    author_email='your.email@example.com',
+    url='https://github.com/dudnjsckrgo/aiffel7_library',
+    license='LICENSE',
+    install_requires=[
+        # 필요한 의존성 패키지 나열
+        'beautifulsoup4',  # This is the package name for bs4
+        'requests',
+        'matplotlib'
+    ],
+    classifiers=[
+        # PyPI 분류자
+        # 참조: https://pypi.org/classifiers/
+    ],
+)
