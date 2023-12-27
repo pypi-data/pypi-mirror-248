8d550d81c9888da5c133168a10ccfef2e9fddb57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:30 +0000

    Add package taowa-web3/1.0.0/taowa_web3-1.0.0-py3-none-any.whl

---
+++ packages/taowa-web3/taowa_web3-1.0.0-py3-none-any.whl/taowa-web3/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import pkgutil,types,importlib
+from .装饰器 import *
+
+
+def 自动装饰器_应用(包名):
+    包 = importlib.import_module(包名)
+    for 加载器, 模块名, 是包 in pkgutil.walk_packages(包.__path__, 包.__name__ + '.'):
+        if not 是包:
+            模块 = importlib.import_module(模块名)
+            for 属性名 in dir(模块):
+                属性 = getattr(模块, 属性名)
+                if isinstance(属性, types.FunctionType):
+                    装饰后的函数 = 异常捕获()(属性)
+                    setattr(模块, 属性名, 装饰后的函数)
+
+自动装饰器_应用('taowa-web3')
+
+
+
+from .以太坊 import *
+
+__version__ = '1.0.0'
