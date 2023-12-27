a84d1d215426188699baa3f7f4bfac39c9d41b8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:43 +0000

    Add package gym-open-poker/0.0.2/gym-open-poker-0.0.2.tar.gz

---
+++ packages/gym-open-poker/gym-open-poker-0.0.2.tar.gz/gym-open-poker-0.0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="gym-open-poker",
+    packages = ["gym-open-poker"],
+    description='gym-open-poker',
+    version="0.0.2",
+    author='Min-Hsueh Chiu',
+    author_email='peter810601@gmail.com',
+    url="https://github.com/minhsueh/gym-open-poker",
+    install_requires=["gym==0.26.0", "pygame==2.1.0"],
+    python_requires=">=3.9"
+)
