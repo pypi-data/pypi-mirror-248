e835f42ed0ef3f943068ab5a6f57bc051ea56114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 16:03:44 +0000

    Add package ShippingContainerNumberValidate/1.0.7/ShippingContainerNumberValidate-1.0.7.tar.gz

---
+++ packages/ShippingContainerNumberValidate/ShippingContainerNumberValidate-1.0.7.tar.gz/ShippingContainerNumberValidate-1.0.7/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+classifiers = [
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+]
+
+setup(
+    name="ShippingContainerNumberValidate",
+    version="1.0.7",
+    description="Helps you validate a shipping container number as per ISO 6346 standard",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="",
+    author="Anuj Panchal",
+    author_email="anujpanchal57@gmail.com",
+    license="MIT",
+    classifiers=classifiers,
+    keywords="Shipping, Logistics, Container Number, Shipping Container, Exports, Imports, Validate Shipping Container Number",
+    packages=find_packages(),
+)
