80e3146920c63f385e0e140f0157026514961f44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:31 +0000

    Add package scale-nucleus/0.16.13/scale_nucleus-0.16.13-py3-none-any.whl

---
+++ packages/scale-nucleus/scale_nucleus-0.16.13-py3-none-any.whl/nucleus/validate/data_transfer_objects/scenario_test.py
@@ -0,0 +1,26 @@
+from typing import List
+
+try:
+    # NOTE: we always use pydantic v1 but have to do these shenanigans to support both v1 and v2
+    from pydantic.v1 import validator
+except ImportError:
+    from pydantic import validator
+
+from nucleus.pydantic_base import ImmutableModel
+
+
+class EvalFunctionListEntry(ImmutableModel):
+    id: str
+    eval_func_arguments: dict
+
+
+class CreateScenarioTestRequest(ImmutableModel):
+    name: str
+    slice_id: str
+    evaluation_functions: List[EvalFunctionListEntry]
+
+    @validator("slice_id")
+    def startswith_slice_indicator(cls, v):  # pylint: disable=no-self-argument
+        if not v.startswith("slc_"):
+            raise ValueError(f"Expected field to start with 'slc_', got '{v}'")
+        return v
