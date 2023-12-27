5f0bc40f2d080ca7fe46becfea0d670de8389665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:05:45 +0000

    Add package roleft/1.0.10/roleft-1.0.10.tar.gz

---
+++ packages/roleft/roleft-1.0.10.tar.gz/roleft-1.0.10/roleft/__init__.py
@@ -0,0 +1,12 @@
+# import KeyValue
+
+from roleft.Enumerable.RoleftList import xList
+
+
+def add_one_other(number: int | float):
+    return number + 1
+
+# KV = KeyValue.KeyValue
+
+hehe = xList[str]()
+
