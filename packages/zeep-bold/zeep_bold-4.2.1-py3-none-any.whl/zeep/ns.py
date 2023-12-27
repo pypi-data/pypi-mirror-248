52f9ddfde000b5eef713b2aace40679ff08f0cf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 14:03:31 +0000

    Add package zeep-bold/4.2.1/zeep_bold-4.2.1-py3-none-any.whl

---
+++ packages/zeep-bold/zeep_bold-4.2.1-py3-none-any.whl/zeep/ns.py
@@ -0,0 +1,25 @@
+
+SOAP_11 = "https://schemas.xmlsoap.org/wsdl/soap/"
+SOAP_12 = "https://schemas.xmlsoap.org/wsdl/soap12/"
+SOAP_ENV_11 = "https://schemas.xmlsoap.org/soap/envelope/"
+SOAP_ENV_12 = "https://www.w3.org/2003/05/soap-envelope"
+
+XSI = "https://www.w3.org/2001/XMLSchema-instance"
+XSD = "https://www.w3.org/2001/XMLSchema"
+
+WSDL = "https://schemas.xmlsoap.org/wsdl/"
+HTTP = "https://schemas.xmlsoap.org/wsdl/http/"
+MIME = "https://schemas.xmlsoap.org/wsdl/mime/"
+
+WSA = "https://www.w3.org/2005/08/addressing"
+
+
+DS = "https://www.w3.org/2000/09/xmldsig#"
+WSSE = (
+    "https://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+)
+WSU = (
+    "https://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+)
+
+NAMESPACE_TO_PREFIX = {XSD: "xsd"}
