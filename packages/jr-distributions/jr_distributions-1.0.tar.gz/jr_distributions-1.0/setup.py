dae87c3dd885885da461432667529f6fc246b1fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 05:03:22 +0000

    Add package jr-distributions/1.0/jr_distributions-1.0.tar.gz

---
+++ packages/jr-distributions/jr_distributions-1.0.tar.gz/jr_distributions-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = 'jr_distributions',
+      version = '1.0',
+      description = 'JR Gaussian distributions',
+      packages = ['distributions'],
+      author = 'Joel Ruetas',
+      author_email = 'contact@joelruetas.com',
+      zip_safe = False)
+
