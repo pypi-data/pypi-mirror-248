91b596284d59671e57c84bc7373a625f9fc7cd0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:05:08 +0000

    Add package CryptoMathTrade-test/0.1/CryptoMathTrade-test-0.1.tar.gz

---
+++ packages/CryptoMathTrade-test/CryptoMathTrade-test-0.1.tar.gz/CryptoMathTrade-test-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='CryptoMathTrade-test',
+    version='0.1',
+    author='Trader_Xylioner',
+    author_email='treider.xylioner@gmail.com',
+    description='tools for trades',
+    packages=['CryptoMathTrade'],
+    install_requires=[],
+)
