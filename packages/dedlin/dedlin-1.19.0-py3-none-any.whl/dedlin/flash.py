51b228dc5e7c36129adba9b241397bf778f7fb1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:29 +0000

    Add package dedlin/1.19.0/dedlin-1.19.0-py3-none-any.whl

---
+++ packages/dedlin/dedlin-1.19.0-py3-none-any.whl/dedlin/flash.py
@@ -0,0 +1,11 @@
+"""
+Flashy stuff
+"""
+from art import tprint
+
+
+def title_screen(user_is_blind: bool) -> None:
+    """Flashy title screen"""
+    if not user_is_blind:
+        print("\033[H\033[J", end="")
+        tprint("dedlin", font="small", chr_ignore=True)
