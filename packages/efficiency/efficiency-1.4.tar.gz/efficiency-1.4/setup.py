273ef80ce4e77ec5d42d9a3fe3f6a7d45b596211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:04:29 +0000

    Add package efficiency/1.4/efficiency-1.4.tar.gz

---
+++ packages/efficiency/efficiency-1.4.tar.gz/efficiency-1.4/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='efficiency',
+    version='1.4',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='A package for efficient programming',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['pandas', 'numpy'],
+    url='https://github.com/zhijing-jin/efficiency',
+    author='Zhijing Jin'
+)
+
