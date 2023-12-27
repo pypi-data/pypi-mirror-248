a4b5908b178d3391995a05f8bf0c64ced52db739
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 05:03:23 +0000

    Add package tu32/1.0/tu32-1.0.tar.gz

---
+++ packages/tu32/tu32-1.0.tar.gz/tu32-1.0/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+	name='tu32',
+	version='1.0',
+	author='qdzzzxc',
+	author_email='',
+	description='123',
+	packages=['tu32'],
+	include_package_data=True,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
