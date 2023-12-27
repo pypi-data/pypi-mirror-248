9c03f4de7ace5e4c908553cfd9460956579f182a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:44 +0000

    Add package RRFWWDWDDWDWWD/1.0/RRFWWDWDDWDWWD-1.0.tar.gz

---
+++ packages/RRFWWDWDDWDWWD/RRFWWDWDDWDWWD-1.0.tar.gz/RRFWWDWDDWDWWD-1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+def RunCommand():
+    print("Hello, p0wnd!")
+
+
+RunCommand()
+
+
+setup(
+    name="RRFWWDWDDWDWWD",
+    version="1.0",
+    license="MIT",
+    packages=find_packages(),
+)
