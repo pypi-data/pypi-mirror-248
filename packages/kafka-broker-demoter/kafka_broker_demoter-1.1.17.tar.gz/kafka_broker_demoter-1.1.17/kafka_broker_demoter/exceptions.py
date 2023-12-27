466101bc04f155cbd874f93bd047bd7df889506c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:32 +0000

    Add package kafka-broker-demoter/1.1.17/kafka_broker_demoter-1.1.17.tar.gz

---
+++ packages/kafka-broker-demoter/kafka_broker_demoter-1.1.17.tar.gz/kafka_broker_demoter-1.1.17/kafka_broker_demoter/exceptions.py
@@ -0,0 +1,18 @@
+class PreferredLeaderMismatchCurrentLeader(Exception):
+    pass
+
+
+class BrokerStatusError(Exception):
+    pass
+
+
+class TriggerLeaderElectionError(Exception):
+    pass
+
+
+class ProduceRecordError(Exception):
+    pass
+
+
+class ChangeReplicaAssignmentError(Exception):
+    pass
