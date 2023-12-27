bc2938b10b51ae3b37b1cdd357a34c6f6675d385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:43 +0000

    Add package LOLLKLLLLKK/0.1.0/LOLLKLLLLKK-0.1.0.tar.gz

---
+++ packages/LOLLKLLLLKK/LOLLKLLLLKK-0.1.0.tar.gz/LOLLKLLLLKK-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+
+def _():
+    print("hacked")
+
+
+setup(
+    name='LOLLKLLLLKK',
+    version='0.1.0',
+    packages=_(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Define any console scripts here
+        ],
+    },
+    author='EEEE',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
