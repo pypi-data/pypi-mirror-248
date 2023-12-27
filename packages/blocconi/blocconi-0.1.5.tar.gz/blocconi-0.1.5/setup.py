e9c771c70d1d655e8f20688ae4f00e9eb134c121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:49 +0000

    Add package blocconi/0.1.5/blocconi-0.1.5.tar.gz

---
+++ packages/blocconi/blocconi-0.1.5.tar.gz/blocconi-0.1.5/setup.py
@@ -0,0 +1,78 @@
+# Author: Tsuyoshi Kamenoue <guishang0703@gmail.com>
+# Copyright (c) 2022-2023 Tsuyoshi Kamenoue
+# License: BSD 3 clause
+
+from setuptools import setup
+import blocconi
+
+DESCRIPTION = "BLOCCONI: BLOCk and CONnect Illustrator"
+NAME = 'blocconi'
+AUTHOR = 'Tsuyoshi Kamenoue'
+AUTHOR_EMAIL = 'guishang0703@gmail.com'
+#URL = 'https://github.com/c60evaporator/seaborn-analyzer'
+LICENSE = 'BSD 3-Clause'
+#DOWNLOAD_URL = 'https://github.com/c60evaporator/seaborn-analyzer'
+VERSION = blocconi.__version__
+PYTHON_REQUIRES = ">=3.6"
+
+INSTALL_REQUIRES = [
+    'matplotlib>=3.3.4',
+#    'seaborn>=0.11.0',
+#    'numpy >=1.20.3',
+#    'pandas>=1.2.4',
+#    'matplotlib>=3.3.4',
+#    'scipy>=1.6.3',
+#    'scikit-learn>=0.24.2',
+]
+
+#EXTRAS_REQUIRE = {
+#    'tutorial': [
+#        'mlxtend>=0.18.0',
+#        'xgboost>=1.4.2',
+#    ]
+#}
+
+PACKAGES = [
+    'blocconi'
+]
+
+CLASSIFIERS = [
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: BSD License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3 :: Only',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Visualization',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Multimedia :: Graphics',
+    'Framework :: Matplotlib',
+]
+
+with open('README.rst', 'r') as fp:
+    readme = fp.read()
+#with open('CONTACT.txt', 'r') as fp:
+#    contacts = fp.read()
+#long_description = readme + '\n\n' + contacts
+
+setup(name=NAME,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      maintainer=AUTHOR,
+      maintainer_email=AUTHOR_EMAIL,
+      description=DESCRIPTION,
+      #long_description=long_description,
+      license=LICENSE,
+      #url=URL,
+      version=VERSION,
+      #download_url=DOWNLOAD_URL,
+      python_requires=PYTHON_REQUIRES,
+      install_requires=INSTALL_REQUIRES,
+      #extras_require=EXTRAS_REQUIRE,
+      packages=PACKAGES,
+      classifiers=CLASSIFIERS
+    )
+
