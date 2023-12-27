c490456e5edf40f87a2d35dc2e17b1a8bed66005
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:05:35 +0000

    Add package roleft/1.0.12/roleft-1.0.12.tar.gz

---
+++ packages/roleft/roleft-1.0.12.tar.gz/roleft-1.0.12/roleft/__init__.py
@@ -0,0 +1,27 @@
+from roleft.Enumerable.RoleftList import xList
+from roleft.Enumerable.RoleftDict import xDict
+
+from roleft.Entities.RoleftDateTime import xDateTime
+from roleft.Entities.RoleftKeyValue import KeyValue
+from roleft.Entities.RoleftMpn import MpnBase, MpnAliProduct
+from roleft.Entities.RoleftMysql import QueryObject4Mysql
+
+import roleft.Utilities.AU
+import roleft.Utilities.CU
+import roleft.Utilities.HU
+import roleft.Utilities.ListU
+
+# from roleft.Entities.RoleftKeyValue import KeyValue
+
+# from .RoleftKeyValue import KeyValue
+# from .RoleftMpn import MpnBase, MpnAliProduct, EatDictable
+# from .RoleftMysql import QueryObject4Mysql
+
+
+# def add_one_other(number: int | float):
+#     return number + 1
+
+# KV = KeyValue.KeyValue
+
+# hehe = xList[str]()
+
