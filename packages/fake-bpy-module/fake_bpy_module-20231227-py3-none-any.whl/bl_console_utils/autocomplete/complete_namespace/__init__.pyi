314289975574b359b0012b4a539f13e32a72322d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:26 +0000

    Add package fake-bpy-module/20231227/fake_bpy_module-20231227-py3-none-any.whl

---
+++ packages/fake-bpy-module/fake_bpy_module-20231227-py3-none-any.whl/bl_console_utils/autocomplete/complete_namespace/__init__.pyi
@@ -0,0 +1,44 @@
+import sys
+import typing
+
+GenericType = typing.TypeVar("GenericType")
+
+
+def complete(word, namespace, private):
+    ''' 
+
+    '''
+
+    ...
+
+
+def complete_indices(word, namespace, obj, base):
+    ''' 
+
+    '''
+
+    ...
+
+
+def complete_names(word, namespace):
+    ''' 
+
+    '''
+
+    ...
+
+
+def is_dict(obj):
+    ''' 
+
+    '''
+
+    ...
+
+
+def is_struct_seq(obj):
+    ''' 
+
+    '''
+
+    ...
