d1017c5cf5947e95e7ff817a8fdafc502739cb1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:30 +0000

    Add package APIRetrys/0.0.3/APIRetrys-0.0.3.tar.gz

---
+++ packages/APIRetrys/APIRetrys-0.0.3.tar.gz/APIRetrys-0.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.3'
+DESCRIPTION = 'To handle API request retries due to timeouts'
+LONG_DESCRIPTION = 'APIRetry offers seamless integration to automatically handle API request retries in Python projects'
+
+# Setting up
+setup(
+    name="APIRetrys",
+    version=VERSION,
+    author="Ryosoraa (Rio Dwi Saputra)",
+    author_email="<riodwi12174@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['logging'],
+    keywords=['python', 'request', 'retry', 'api', 'timeout', 'auto'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
