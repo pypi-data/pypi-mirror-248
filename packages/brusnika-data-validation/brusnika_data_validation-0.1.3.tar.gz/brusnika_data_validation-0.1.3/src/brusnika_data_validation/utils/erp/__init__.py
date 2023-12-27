33532c26fd5fa6e0224d2ddc53bd8224c0003381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:05:14 +0000

    Add package brusnika-data-validation/0.1.3/brusnika_data_validation-0.1.3.tar.gz

---
+++ packages/brusnika-data-validation/brusnika_data_validation-0.1.3.tar.gz/brusnika_data_validation-0.1.3/src/brusnika_data_validation/utils/erp/__init__.py
@@ -0,0 +1,6 @@
+from . import parameters, indicators, versioning, processes, bluebook
+from .parameters import URL_ERP_PARAMETERS
+from .indicators import URL_ERP_INDICATORS
+from .versioning import URL_ERP_VERSIONING
+from .processes import URL_ERP_PROCESSES
+from .bluebook import URL_ERP_BLUEBOOK
