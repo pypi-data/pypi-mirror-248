6bf63f2d98e6c3e101a2fcfbbcca3c23a87b8a54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 14:03:45 +0000

    Add package newg-dl/0.2.6/newg_dl-0.2.6.tar.gz

---
+++ packages/newg-dl/newg_dl-0.2.6.tar.gz/newg_dl-0.2.6/newgrounds_dl/newgrounds_dl.py
@@ -0,0 +1,87 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def main():
+    print("newgrounds_dl not working in console.")
+
+
+class ngdl:
+    # Get song data
+    def get_data(id: int):
+        url = f'https://www.newgrounds.com/audio/listen/{id}/json'
+        
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  # Проверка наличия ошибок при запросе
+            if response.status_code != 200: 
+                print("blyat pizdec suka")
+                raise("blyat pizdec suka")
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            # Ищем элементы на странице для получения информации
+            title_element = soup.find('h2', class_='rated-e')
+            author_container = soup.find('h4', class_='smaller')
+            artist = author_container.find('a').text.strip() if author_container else 'N/A'
+            rating_element = soup.find('span', class_='pod-head-rating')
+
+            # Idiotism
+            dd_genre_container = soup.find('dl', class_="sidestats flex-1")
+            genre_container = dd_genre_container.find('dd') if dd_genre_container else 'N/A'
+            genre_element = genre_container.find('a').text.strip() if genre_container else 'N/A'
+            
+            title = title_element.text.strip() if title_element else 'N/A'
+            genre = genre_element if genre_element else 'N/A'
+            rating = rating_element.text.strip() if rating_element else 'E'
+
+            song_info = {
+                'title': title,
+                'artist': artist,
+                'genre': genre,
+                'rating': rating,
+            }
+
+            return song_info
+        except requests.exceptions.RequestException as e:
+            print(f"Error during request: {e}")
+            raise("sukaaaa pizdaaaaa")
+        
+    def download_music(id: int, output_folder="./"):
+        song_info = get_data(id=id)
+        song_name_old = song_info['title']
+        song_name = song_name_old.replace(" ", "-")
+
+        # Формирование URL для скачивания
+        base_url = 'https://audio.ngfiles.com'
+        folder_id = str(id)[:4] + '000'
+        audio_url = f'{base_url}/{folder_id}/{id}_{song_name}.mp3'
+
+        try:
+            # Загрузка музыки
+            audio_data = requests.get(audio_url).content
+            output_path = os.path.join(output_folder, f"{id}_{song_name}.mp3")
+
+            with open(output_path, 'wb') as audio_file:
+                audio_file.write(audio_data)
+
+            print(f"Music downloaded: {output_path}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error during request: {e}")
+
+if __name__ == "__main__":
+    main()
+
+# Замените SONG_ID на реальный идентификатор песни
+# song_id = 1154735
+# audio_url = download_music(song_id)
+
+# if audio_url:
+#     print(f"Audio URL: {audio_url}")
+
+#     # Загрузка музыки
+#     audio_data = requests.get(audio_url).content
+#     with open("downloaded_music.mp3", 'wb') as audio_file:
+#         audio_file.write(audio_data)
+#     print("Music downloaded.")
+# else:
+#     print("Failed to retrieve audio information.")
