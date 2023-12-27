9a77b8895c18e79046f705629db87ded021ed149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 16:03:43 +0000

    Add package lemon-rag/0.1.6/lemon_rag-0.1.6.tar.gz

---
+++ packages/lemon-rag/lemon_rag-0.1.6.tar.gz/lemon_rag-0.1.6/lemon_rag/tools/debug.py
@@ -0,0 +1,4 @@
+# 这个文件可以写一些debug的内容
+
+import importlib
+print(dir(importlib))
---
+++ packages/lemon-rag/lemon_rag-0.1.6.tar.gz/lemon_rag-0.1.6/lemon_rag/tools/debug_in_runtime.py
@@ -0,0 +1,18 @@
+import requests
+
+from lemon_rag.configs.local_dev_config import config
+
+import code
+code.compile_command()
+def run():
+    code = open("debug.py", "r").read()
+    res = requests.post(
+        f"https://lemon.lemonstudio.tech:8443/{config.lemon.app_uuid}test/{config.lemon.tenant_uuid}/restful/v1/debug_code",
+        json={"code": "code"}
+    )
+    res.raise_for_status()
+
+    print("\n".join(res.json().get("output")))
+
+if __name__ == '__main__':
+    run()
