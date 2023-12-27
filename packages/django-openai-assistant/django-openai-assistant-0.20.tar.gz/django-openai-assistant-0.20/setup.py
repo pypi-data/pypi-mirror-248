e45960ea477dfab3578e489cad48b79efd837121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:05:22 +0000

    Add package django-openai-assistant/0.20/django-openai-assistant-0.20.tar.gz

---
+++ packages/django-openai-assistant/django-openai-assistant-0.20.tar.gz/django-openai-assistant-0.20/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-openai-assistant',
+    version='0.20',
+    license='MIT',
+    author="Jean-Luc Vanhulst",
+    author_email='jl@valor.vc',
+    packages=['django_openai_assistant'],
+    package_dir={'': 'src'},
+    url='https://github.com/jlvanhulst/django-openai',
+    keywords='django celery openai assistants',
+    install_requires=[
+         'openai','markdown'
+      ],
+
+)
