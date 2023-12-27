23f669a4308325007fd2fd29a8e647e523eaf5e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:37 +0000

    Add package OeSNN-AD/1.0.0/OeSNN-AD-1.0.0.tar.gz

---
+++ packages/OeSNN-AD/OeSNN-AD-1.0.0.tar.gz/OeSNN-AD-1.0.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '1.0.0'
+DESCRIPTION = 'OeSNN anomaly detector implementation for Python.'
+LONG_DESCRIPTION = 'Online evolutionary Spiking Neural Network anomaly detector implementation for Python.'
+
+# Setting up
+setup(
+    name="OeSNN-AD",
+    version=VERSION,
+    author="Mariusz Paluch",
+    author_email="mariuszpaluch001@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['numpy'],
+    keywords=['python', 'data stream', 'spiking neural network', 'SNN', 'anomalies', 'anomaly detection'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
