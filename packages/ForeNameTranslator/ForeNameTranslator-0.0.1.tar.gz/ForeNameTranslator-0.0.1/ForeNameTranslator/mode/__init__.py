8e11d7126e1cc1eec36ad7e0ec3c655f5dae0680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:45 +0000

    Add package ForeNameTranslator/0.0.1/ForeNameTranslator-0.0.1.tar.gz

---
+++ packages/ForeNameTranslator/ForeNameTranslator-0.0.1.tar.gz/ForeNameTranslator-0.0.1/ForeNameTranslator/mode/__init__.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+THIS FILE IS PART OF NETWORK FOR MWAFU LIBRARY LOVE BOOK STORE BY MATT BELFAST BROWN
+__init__.py - The core part of the Foreign Name Translator.
+
+Author: Matt Belfast Brown
+Creat Date: 2023-12-25
+Version Date: 2023-12-27
+Version: 0.0.1
+
+THIS PROGRAM IS FREE FOR EVERYONE,IS LICENSED UNDER GPL-3.0
+YOU SHOULD HAVE RECEIVED A COPY OF GPL-3.0 LICENSE.
+
+Copyright (C) 2023 Matt Belfast Brown
+Copyright (C) 2023 Love Book Store Volunteer Association of NWAFU
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with this program.  If not,
+see <http://www.gnu.org/licenses/>.
+"""
+
+__title__ = "ForeNameTranslator.mode"
+__version__ = "0.0.1"
+__author__ = "Matt Belfast Brown"
+__license__ = "GPL-3.0"
+__copyright__ = "Copyright (c) 2023 Matt Belfast Brown \nCopyright (C) 2023 Love Book Store Volunteer Association of NWAFU"
+__all__ = ["mode_TRA_Api", "mode_FNM_Tra"]
