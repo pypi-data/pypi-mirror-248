d436bd207b917cf3a5881b0d70fb1a2ff6ca8bd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 16:03:29 +0000

    Add package seunggabi-core-python/1.5.0/seunggabi_core_python-1.5.0.tar.gz

---
+++ packages/seunggabi-core-python/seunggabi_core_python-1.5.0.tar.gz/seunggabi_core_python-1.5.0/seunggabi_core_python/util/json_util.py
@@ -0,0 +1,22 @@
+import json
+from typing import Union
+
+
+def pretty(obj: Union[dict, str]) -> str:
+    try:
+        if isinstance(obj, str):
+            obj = to_dict(obj)
+
+        return json.dumps(obj, ensure_ascii=False, sort_keys=True, indent=4)
+    except:
+        return obj
+
+
+def to_dict(s: str) -> dict:
+    try:
+        return json.loads(s)
+    except:
+        try:
+            return json.loads(json.dumps(eval(s)))
+        except:
+            return s
