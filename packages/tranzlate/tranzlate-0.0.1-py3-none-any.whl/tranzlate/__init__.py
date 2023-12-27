d4a64938e4cbdc4c04abf14e09d3aa2fd95fc3eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:03:35 +0000

    Add package tranzlate/0.0.1/tranzlate-0.0.1-py3-none-any.whl

---
+++ packages/tranzlate/tranzlate-0.0.1-py3-none-any.whl/tranzlate/__init__.py
@@ -0,0 +1,13 @@
+"""
+#### tranzlate
+
+Simple python package for multi-lingual translation of text, files, markup and BeautifulSoup objects.
+
+@Author: Daniel T. Afolayan (ti-oluwa.github.io)
+"""
+
+from .translator import Translator
+
+__all__ = ["Translator"]
+__version__ = "0.0.1"
+__author__ = "Daniel T. Afolayan"
