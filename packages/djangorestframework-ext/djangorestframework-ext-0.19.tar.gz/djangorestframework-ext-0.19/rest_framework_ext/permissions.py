86a1c674b7e99babcbd9cd6c13006b1a825f8c9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 16:03:42 +0000

    Add package djangorestframework-ext/0.19/djangorestframework-ext-0.19.tar.gz

---
+++ packages/djangorestframework-ext/djangorestframework-ext-0.19.tar.gz/djangorestframework-ext-0.19/rest_framework_ext/permissions.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions
+
+
+class DjangoModelPermissions(permissions.DjangoModelPermissions):
+    def __init__(self):
+        self.perms_map['GET'] = ['%(app_label)s.view_%(model_name)s']
+        self.perms_map['OPTIONS'] = ['%(app_label)s.view_%(model_name)s']
+        self.perms_map['HEAD'] = ['%(app_label)s.view_%(model_name)s']
+
+
+class IsCurrentUser(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
+
+
+class IsSuperuser(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_superuser)
+
+
+class ExportPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if view.action == 'export':
+            model = view.get_queryset().model
+            kwargs = {
+                'app_label': model._meta.app_label,
+                'model_name': model._meta.model_name
+            }
+
+            return request.user.has_perm('%(app_label)s.export_%(model_name)s' % kwargs)
+        else:
+            return False
