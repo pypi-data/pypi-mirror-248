10568d29811a32e937f69a077a0a4ba061798a63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:30 +0000

    Add package taowa-web3/1.0.0/taowa-web3-1.0.0.tar.gz

---
+++ packages/taowa-web3/taowa-web3-1.0.0.tar.gz/taowa-web3-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name='taowa-web3',
+    version='1.0.0',
+    description='',
+    long_description_content_type='text/markdown',
+    author='陈炳强',
+    author_email='99396686@qq.com',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    # 如果有依赖包，可以在此处添加
+    install_requires=[
+        'web3',
+    ],
+)
