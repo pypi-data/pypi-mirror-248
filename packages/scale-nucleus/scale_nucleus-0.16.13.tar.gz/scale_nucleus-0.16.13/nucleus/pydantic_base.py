fd53dc6166df2a1bc06ff53c79cb6b0a3d3c360d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:31 +0000

    Add package scale-nucleus/0.16.13/scale_nucleus-0.16.13.tar.gz

---
+++ packages/scale-nucleus/scale_nucleus-0.16.13.tar.gz/scale_nucleus-0.16.13/nucleus/pydantic_base.py
@@ -0,0 +1,36 @@
+"""
+NOTE:
+We started using pydantic during v1 and are kind of stuck with it now unless we write a compatibility layers.
+As a library we want to support v1 and v2 such that we're not causing downstream problems for our users.
+This means we have to do some import shenanigans to support both v1 and v2.
+"""
+try:
+    # NOTE: we always use pydantic v1 but have to do these shenanigans to support both v1 and v2
+    from pydantic.v1 import BaseModel  # pylint: disable=no-name-in-module
+except ImportError:
+    from pydantic import BaseModel
+
+
+class ImmutableModel(BaseModel):
+    class Config:
+        allow_mutation = False
+
+
+class DictCompatibleModel(BaseModel):
+    """Backwards compatible wrapper where we transform dictionaries into Pydantic Models
+
+    Allows us to access model.key with model["key"].
+    """
+
+    def __getitem__(self, key):
+        return getattr(self, key)
+
+
+class DictCompatibleImmutableModel(ImmutableModel):
+    """Backwards compatible wrapper where we transform dictionaries into Pydantic Models
+
+    Allows us to access model.key with model["key"].
+    """
+
+    def __getitem__(self, key):
+        return getattr(self, key)
---
+++ packages/scale-nucleus/scale_nucleus-0.16.13.tar.gz/scale_nucleus-0.16.13/nucleus/validate/data_transfer_objects/scenario_test_evaluations.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+try:
+    # NOTE: we always use pydantic v1 but have to do these shenanigans to support both v1 and v2
+    from pydantic.v1 import root_validator, validator
+except ImportError:
+    from pydantic import root_validator, validator
+
+from nucleus.pydantic_base import ImmutableModel
+
+
+class EvaluationResult(ImmutableModel):
+    track_ref_id: Optional[str] = None
+    item_ref_id: Optional[str] = None
+    scene_ref_id: Optional[str] = None
+    score: float = 0
+    weight: float = 1
+
+    @root_validator()
+    def is_item_or_scene_provided(
+        cls, values
+    ):  # pylint: disable=no-self-argument
+        ref_ids = [
+            values.get("track_ref_id", None),
+            values.get("item_ref_id", None),
+            values.get("scene_ref_id", None),
+        ]
+        if len([ref_id for ref_id in ref_ids if ref_id is not None]) != 1:
+            raise ValueError(
+                "Must provide exactly one of track_ref_id, item_ref_id, or scene_ref_id"
+            )
+        return values
+
+    @validator("weight")
+    def is_normalized(cls, v):  # pylint: disable=no-self-argument
+        if 0 <= v <= 1:
+            return v
+        raise ValueError(
+            f"Expected evaluation weights to be normalized between 0 and 1, but got: {v}"
+        )
