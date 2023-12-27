c9bc6d670c462c348c5759a61af6b2abca9dd41d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:03:58 +0000

    Add package KangRollGeneCancer/0.0.122/KangRollGeneCancer-0.0.122.tar.gz

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.122.tar.gz/KangRollGeneCancer-0.0.122/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='KangRollGeneCancer',
+    version='0.0.122',
+    packages=find_packages(),
+    package_data={
+        'sa005package': [
+        # 'kangroll_basic_fn001_mln',
+        'kangroll_basic_fn001_mln_release.py',
+        'pytransform/*',
+        'pytransform/_pytransform.dylib',
+        'pytransform/_pytransform.dylib'],
+    },
+    description='fn001 Python release edtion with KangTest',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='xiaowen',
+    author_email='xiaowenseekjob@gmail.com',
+    url='https://github.com/yourusername/standalone001pyarmor',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
