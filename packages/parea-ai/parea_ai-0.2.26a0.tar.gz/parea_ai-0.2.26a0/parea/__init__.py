6745396ed077dbb93e19bfe74989ab06b1b5f1f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:04:29 +0000

    Add package parea-ai/0.2.26a0/parea_ai-0.2.26a0.tar.gz

---
+++ packages/parea-ai/parea_ai-0.2.26a0.tar.gz/parea_ai-0.2.26a0/parea/__init__.py
@@ -0,0 +1,34 @@
+# type: ignore[attr-defined]
+# flake8: noqa
+
+"""
+    Parea API SDK
+
+    The Parea SDK allows you to interact with Parea from your product or service.
+    To install the official [Python SDK](https://pypi.org/project/parea/),
+    run the following command:  ```bash pip install parea ```.
+"""
+import sys
+from importlib import metadata as importlib_metadata
+
+from parea.benchmark import run_benchmark
+from parea.cache import InMemoryCache, RedisCache
+from parea.client import Parea, init
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
+
+
+def main():
+    args = sys.argv[1:]
+    if args[0] == "benchmark":
+        run_benchmark(args[1:])
+    else:
+        print(f"Unknown command: '{args[0]}'")
