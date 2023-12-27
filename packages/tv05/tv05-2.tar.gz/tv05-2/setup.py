f0a965cc96fe7e8f2a6c2933e79fca2580943233
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:05:14 +0000

    Add package tv05/2/tv05-2.tar.gz

---
+++ packages/tv05/tv05-2.tar.gz/tv05-2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='tv05',
+      version='2',
+      description='none',
+      packages=['tv05'],
+      author_email='antoha123_3@mail.ru',
+      zip_safe=False)
---
+++ packages/tv05/tv05-2.tar.gz/tv05-2/tv05/__init__.py
@@ -0,0 +1,113 @@
+import pyperclip as pc
+def all():
+    s = 'zapw, tapw, xi0, xi, xy, fin_pok'
+    return pc.copy(s)
+def zapw():
+    s = '''x = np.array(...)
+std = 3.4
+alpha = 0.01
+m0 = 1.29
+m1 = 1.17
+n = len(x)
+Z = n**0.5 * (x.mean() - m0) / std
+za = norm(0, 1).isf(alpha / 2)
+A = za
+P = 2 * min(norm(0, 1).cdf(Z), norm(0, 1).sf(Z))
+W = 2 - (norm(0, 1).cdf(za + n**0.5 * (m1 - m0) / std) + norm(0, 1).cdf(za - n**0.5 * (m1 - m0) / std))
+Z, A, P, W
+    '''
+    return pc.copy(s)
+    
+def tapw():
+    s = '''x = np.array(...)
+std = x.std(ddof = 1)
+alpha = 0.05
+m0 = 1.1
+m1 = 0.91
+n = len(x)
+T = n**0.5 * (x.mean() - m0) / std
+ta = t(n - 1).isf(alpha / 2)
+A = ta
+P = 2 * min(t(n - 1).cdf(T), t(n - 1).sf(T))
+W = 1 - (nct(n - 1, n**0.5 * (m1 - m0) / std).cdf(ta) - nct(n - 1, n**0.5 * (m1 - m0) / std).cdf(-ta))
+T, A, P, W
+    '''
+    return pc.copy(s)
+
+def xi0():
+    s = '''x = np.array(...)
+m = 1.18
+alpha = 0.02
+std = 1.14
+std1 = 1.24
+n = len(x)
+X0 = sum((x - m)**2) / std**2
+chia = chi2(n).isf(alpha / 2)
+chi1_a = chi2(n).isf(1 - alpha / 2)
+A = chi1_a
+B = chia
+P = 2 * min(chi2(n).cdf(X0), chi2(n).sf(X0))
+b = chi2(n).cdf(std**2 / std1**2 * chia) - chi2(n).cdf(std**2 / std1**2 * chi1_a)
+X0, A, B, P, b
+    '''
+    return pc.copy(s)
+    
+def xi():
+    s = '''x = np.array(...)
+alpha = 0.02
+std = 1.14
+std1 = 1.24
+n = len(x)
+X = (n - 1) * x.std(ddof = 1)**2 / std**2
+chia = chi2(n - 1).isf(alpha / 2)
+chi1_a = chi2(n - 1).isf(1 - alpha / 2)
+A = chi1_a
+B = chia
+P = 2 * min(chi2(n - 1).cdf(X), chi2(n - 1).sf(X))
+b = chi2(n - 1).cdf(std**2 / std1**2 * chia) - chi2(n - 1).cdf(std**2 / std1**2 * chi1_a)
+X, A, B, P, b
+    '''
+    return pc.copy(s)
+    
+def xy():
+    s = '''x = np.array(...)
+y = np.array(...)
+alpha = 0.02
+std1 = 0.7
+std2 = 1.4
+n = len(x)
+m = len(y)
+mx_my = 0.1
+Z = (x.mean() - y.mean()) / (std1**2 / n + std2**2 / m)**0.5
+za = norm(0, 1).isf(alpha)
+A = za
+P = norm(0, 1).sf(Z)
+W = 1 - norm(0, 1).cdf(za - (n * m)**0.5 / (m * std1**2 + n * std2**2)**0.5 * mx_my)
+Z, P, A, W
+    '''
+    return pc.copy(s)
+
+def fin_pok():
+    s = '''A = np.array([1,2,3])
+B = np.array([1,2,3])
+C = np.array([1,2,3])
+ABC = np.concatenate([A, B, C])
+alpha = 0.01
+n = np.array([len(A), len(B), len(C)])
+k = 3
+N = len(ABC)
+g = ABC.mean()
+m = np.array([A.mean(), B.mean(), C.mean()])
+var = np.array([A.var(), B.var(), C.var()])
+d2 = sum((m - g)**2 * n) / N
+std2_ = sum(var * n) / N
+SSE = N * std2_
+MSE = SSE / (N - k)
+SSTR = N * d2
+MSTR = SSTR / (k - 1)
+F = MSTR / MSE
+P = f(k - 1, N - k).sf(F)
+d2, std2_, F, P
+    '''
+    return pc.copy(s)
+    
