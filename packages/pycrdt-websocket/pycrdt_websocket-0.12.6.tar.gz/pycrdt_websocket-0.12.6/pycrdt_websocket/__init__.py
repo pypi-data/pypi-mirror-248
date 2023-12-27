346bbf3dae750d3745b175f84c6ab8ae8cc02acc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:04:02 +0000

    Add package pycrdt-websocket/0.12.6/pycrdt_websocket-0.12.6.tar.gz

---
+++ packages/pycrdt-websocket/pycrdt_websocket-0.12.6.tar.gz/pycrdt_websocket-0.12.6/pycrdt_websocket/__init__.py
@@ -0,0 +1,6 @@
+from .asgi_server import ASGIServer  # noqa
+from .websocket_provider import WebsocketProvider  # noqa
+from .websocket_server import WebsocketServer, YRoom  # noqa
+from .yutils import YMessageType  # noqa
+
+__version__ = "0.12.6"
