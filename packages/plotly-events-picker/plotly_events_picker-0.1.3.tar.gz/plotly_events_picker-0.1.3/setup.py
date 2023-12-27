50eaf26b287c955f84643ceb04b7584371493e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:37 +0000

    Add package plotly-events-picker/0.1.3/plotly_events_picker-0.1.3.tar.gz

---
+++ packages/plotly-events-picker/plotly_events_picker-0.1.3.tar.gz/plotly_events_picker-0.1.3/setup.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="plotly_events_picker",
+    version="0.1.3",
+    author="harrit",
+    author_email="xyz@streamlit.io",
+    description=(
+        "Streamlit component "
+        "click"
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    url="https://github.com/",
+    license="MIT License",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.7",
+    install_requires=["streamlit>=1.2", "jinja2"],
+    keywords="python streamlit",
+)
