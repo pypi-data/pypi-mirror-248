2b67c06cfd9476e155d4927131dd3b35a4fcd2c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:45 +0000

    Add package craigslistscraper/1.1.1/craigslistscraper-1.1.1-py3-none-any.whl

---
+++ packages/craigslistscraper/craigslistscraper-1.1.1-py3-none-any.whl/craigslistscraper/__init__.py
@@ -0,0 +1,11 @@
+from .ad import Ad
+from .ad import AdParser
+from .ad import fetch_ad
+
+from .search import Search
+from .search import SearchParser
+from .search import fetch_search 
+
+__version__ = "1.1.1"
+
+
