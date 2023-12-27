f1842b1a3f59f43d36e7ba0a959af2aa29cfba1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 14:03:47 +0000

    Add package xingyun/0.0.1/xingyun-0.0.1.tar.gz

---
+++ packages/xingyun/xingyun-0.0.1.tar.gz/xingyun-0.0.1/README.md
@@ -0,0 +1,7 @@
+# XingYun
+
+XingYun is a library that helps backup code, data and log to cloud and manage them. Tastes better if consumed together with [EgorovSystem](https://github.com/FFTYYY/EgorovSystem/tree/main).
+
+## Installation
+
+`pip install xingyun`
---
+++ packages/xingyun/xingyun-0.0.1.tar.gz/xingyun-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", encoding="utf-8") as f:
+    readme = f.read()
+
+with open("LICENSE", encoding="utf-8") as f:
+    license = f.read()
+
+with open("requirements.txt", encoding="utf-8") as f:
+    reqs = f.read()
+
+pkgs = [p for p in find_packages() if p.startswith("YTools")]
+print(pkgs)
+
+setup(
+    name="xingyun",
+    version="0.0.1",
+    url="http://github.com/FFTYYY/XingYun",
+    description="",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    author = "Yongyi Yang",
+	author_email = "yongyi@umich.edu",
+    python_requires = ">=3.10",
+    packages = pkgs,
+    install_requires = reqs.strip().split("\n"),
+
+    include_package_data = True , 
+)
