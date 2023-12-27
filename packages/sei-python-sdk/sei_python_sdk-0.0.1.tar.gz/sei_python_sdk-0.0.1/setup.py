afe4a5fd5fa74ca2752e2f87acc2b2fa3fabf941
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:53 +0000

    Add package sei-python-sdk/0.0.1/sei_python_sdk-0.0.1.tar.gz

---
+++ packages/sei-python-sdk/sei_python_sdk-0.0.1.tar.gz/sei_python_sdk-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='sei_python_sdk', #package name
+      version='0.0.1',
+      description='Python Package By jordin',
+      author='jordin',
+      license='MIT',
+      zip_safe=False
+      )
