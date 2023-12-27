a7890a1e4360cf1f06ba953e00cc83f8d97573c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 14:03:47 +0000

    Add package matplotilib/0.3.0/matplotilib-0.3.0-py3-none-any.whl

---
+++ packages/matplotilib/matplotilib-0.3.0-py3-none-any.whl/matplotilib/hello.py
@@ -0,0 +1,21 @@
+import nbformat
+from IPython.display import display, HTML, FileLink
+
+def hello():
+    print("Hello from matplotilib!")
+    # Load the notebook content
+    notebook_path = 'notebook/CNN.ipynb'
+    with open(notebook_path, 'r', encoding='utf-8') as notebook_file:
+        notebook_content = nbformat.read(notebook_file, as_version=4)
+
+    # Display a download link
+    display(HTML(f'<a href="{notebook_path}" download></a>'))
+
+def CNN():
+    # Load the notebook content
+    notebook_path = 'notebook/CNN.ipynb'
+    with open(notebook_path, 'r', encoding='utf-8') as notebook_file:
+        notebook_content = nbformat.read(notebook_file, as_version=4)
+
+    # Display a download link
+    display(HTML(f'<a href="{notebook_path}" download></a>'))
