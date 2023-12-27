13a7ec22ad334451b1b2b4caeb061e8a2e129013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:33 +0000

    Add package zerohertzLib/0.7.6/zerohertzLib-0.7.6.tar.gz

---
+++ packages/zerohertzLib/zerohertzLib-0.7.6.tar.gz/zerohertzLib-0.7.6/test/test_algorithm.py
@@ -0,0 +1,58 @@
+import zerohertzLib as zz
+
+
+# FROM: https://www.acmicpc.net/problem/1260
+def test_dfs_1():
+    assert zz.algorithm.dfs([[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]], 1) == [
+        1,
+        2,
+        4,
+        3,
+    ]
+
+
+def test_dfs_2():
+    assert zz.algorithm.dfs([[], [2, 3], [1, 5], [1, 4], [3, 5], [2, 4]], 3) == [
+        3,
+        1,
+        2,
+        5,
+        4,
+    ]
+
+
+# FROM: https://www.acmicpc.net/problem/1260
+def test_bfs_1():
+    assert zz.algorithm.bfs([[], [2, 3, 4], [1, 4], [1, 4], [1, 2, 3]], 1) == [
+        1,
+        2,
+        3,
+        4,
+    ]
+
+
+def test_bfs_2():
+    assert zz.algorithm.bfs([[], [2, 3], [1, 5], [1, 4], [3, 5], [2, 4]], 3) == [
+        3,
+        1,
+        4,
+        2,
+        5,
+    ]
+
+
+def test_soe():
+    assert zz.algorithm.soe(10) == [2, 3, 5, 7]
+
+
+def test_fft():
+    assert zz.algorithm.fft([1, 0, 0, 0]) == [(1 + 0j), (1 + 0j), (1 + 0j), (1 + 0j)]
+
+
+def test_fft_inv():
+    assert zz.algorithm.fft([1 + 0j, 1 + 0j, 1 + 0j, 1 + 0j], True) == [
+        (4 + 0j),
+        0j,
+        0j,
+        0j,
+    ]
