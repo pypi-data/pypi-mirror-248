e2af7f2ee7f7d66aa7df96b11b19613dd1fb0c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:33 +0000

    Add package hoisaai/0.0.0/hoisaai-0.0.0.tar.gz

---
+++ packages/hoisaai/hoisaai-0.0.0.tar.gz/hoisaai-0.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import (
+    setup,
+    find_packages,
+)
+
+setup(
+    name="hoisaai",
+    version="0.0.0",
+    author="Mike Mo Shun Cheng",
+    author_email="mike@hoisaai.com",
+    description="An open source artificial general intelligence",
+    packages=find_packages(),
+    license="AGPL3",
+    python_requires=">=3.11",
+)
