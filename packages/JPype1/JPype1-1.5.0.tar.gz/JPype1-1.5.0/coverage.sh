6a3c12593b9819bb58d80e18df53cb35b10e1275
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:05:40 +0000

    Add package JPype1/1.5.0/JPype1-1.5.0.tar.gz

---
+++ packages/JPype1/JPype1-1.5.0.tar.gz/JPype1-1.5.0/coverage.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Script for testing coverage locally
+PYTHON=python
+PIP="python -m pip"
+./resolve.sh
+$PYTHON setup.py test_java
+$PIP install gcovr pytest-cov jedi
+$PYTHON setup.py --enable-coverage --enable-build-jar build_ext --inplace
+$PYTHON -m pytest -rsx -v test/jpypetest \
+	--cov-report=xml:coverage_py.xml \
+	--cov-report=html:build/coverage/python \
+	--cov=jpype \
+	--classpath="build/classes" --jacoco --checkjni
+java -jar lib/org.jacoco.cli-0.8.5-nodeps.jar report build/coverage/jacoco.exec \
+	--classfiles build/classes/ \
+	--html build/coverage/java \
+	--sourcefiles native/java
+mkdir build/coverage/cpp
+gcovr -r . --html-details -o build/coverage/cpp/jpype.html --exclude-unreachable-branches --exclude-throw-branches
