76c29925779114a355f2cda5b28d43a5278329d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:05:41 +0000

    Add package polyants/0.2a0/polyants-0.2a0-py3-none-any.whl

---
+++ packages/polyants/polyants-0.2a0-py3-none-any.whl/polyants/__init__.py
@@ -0,0 +1,12 @@
+""" This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+Created on Dec 22, 2023
+
+@author: pymancer@gmail.com (polyanalitika.ru)
+"""
+
+from polyants.adapters import dict_to_enumdef
+
+__version__ = '0.2a0'
