f16f84d3a8a295602eaf33a2e5debcb100a78559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:56 +0000

    Add package hello-world-rrrmib/0.0.1/hello-world-rrrmib-0.0.1.tar.gz

---
+++ packages/hello-world-rrrmib/hello-world-rrrmib-0.0.1.tar.gz/hello-world-rrrmib-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="hello-world-rrrmib",
+	version="0.0.1",
+	author="rrrmib",
+	author_email="rrrmib@gmail.com",
+	description="A hello-world example package",
+	packages=find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+)
