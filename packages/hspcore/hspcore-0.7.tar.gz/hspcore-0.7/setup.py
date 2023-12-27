0d7d3ecf351c7b429b30de339ab09861f3610f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:04:18 +0000

    Add package hspcore/0.7/hspcore-0.7.tar.gz

---
+++ packages/hspcore/hspcore-0.7.tar.gz/hspcore-0.7/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+with open("README.md", "r",encoding="utf-8",errors='ignore') as description:
+    long_description = description.read()
+
+setup(
+  name = 'hspcore',
+  version = '0.7',
+  license='MIT',
+  description = 'Hansen Solubility Parameters (HSP) core functions',
+  long_description_content_type='text/markdown',
+  long_description=long_description,
+  author = 'Alejandro Gutierrez',
+  author_email = 'a.gutierrez@g-npd.com',
+  url = 'https://github.com/Gnpd/hspcore', 
+  download_url = 'https://github.com/Gnpd/hspcore/archive/refs/tags/v_0.6.tar.gz',
+  keywords = ['HSP', 'Solubility'],
+  install_requires=["scipy"],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.11',
+  ],
+  python_requires='>=3.6',
+)
