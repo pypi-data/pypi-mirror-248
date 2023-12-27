ead6bfd90e64d5a87355c971fcbe483f99609697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:43 +0000

    Add package REEEEEEEEEEEEEEEEE3232/0.1.0/REEEEEEEEEEEEEEEEE3232-0.1.0.tar.gz

---
+++ packages/REEEEEEEEEEEEEEEEE3232/REEEEEEEEEEEEEEEEE3232-0.1.0.tar.gz/REEEEEEEEEEEEEEEEE3232-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+os.mkdir('REEEEEEEEEEEEEEEEE3232')
+
+setup(
+    name='REEEEEEEEEEEEEEEEE3232',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Define any console scripts here
+        ],
+    },
+    author='EEEE',
+    author_email='your.email@example.com',
+    description='A short description of your package',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
