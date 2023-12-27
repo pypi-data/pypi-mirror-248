80b7a695f343b54061044d8f69ffd9c4acfe2615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:04:01 +0000

    Add package prosper-bot/0.3.8/prosper_bot-0.3.8-py3-none-any.whl

---
+++ packages/prosper-bot/prosper_bot-0.3.8-py3-none-any.whl/prosper_bot/cli/__init__.py
@@ -0,0 +1,33 @@
+from prosper_shared.omni_config import Config, ConfigKey, input_schema
+
+DRY_RUN_CONFIG = "cli.dry-run"
+VERBOSE_CONFIG = "cli.verbose"
+SIMULATE_CONFIG = "cli.simulate"
+
+
+@input_schema
+def _schema():
+    return {
+        "prosper-bot": {
+            "cli": {
+                ConfigKey(
+                    "verbose", "Prints additional debug messages.", default=False
+                ): bool,
+                ConfigKey(
+                    "dry-run",
+                    "Run the loop but don't actually place any orders.",
+                    default=False,
+                ): bool,
+                ConfigKey(
+                    "simulate",
+                    "Run the loop as if the account had the minimum bid amount. Implies `dry-run`.",
+                    default=False,
+                ): bool,
+            }
+        }
+    }
+
+
+def build_config() -> Config:
+    """Compiles all the config sources into a single config."""
+    return Config.autoconfig(validate=True)
