2959a8ac6b25e974d1201e67f4eb4dd4a7aecf9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:04:51 +0000

    Add package iotiumscraper/0.2.4/iotiumscraper-0.2.4.tar.gz

---
+++ packages/iotiumscraper/iotiumscraper-0.2.4.tar.gz/iotiumscraper-0.2.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='iotiumscraper',
+    packages=find_packages(),
+    version='0.2.4',
+    description='scrape data from kastle and push to postgres',
+    author='Raja',
+    install_requires=[
+        'selenium ',
+        'chromedriver_py',
+        'psycopg2'
+    ]
+)
