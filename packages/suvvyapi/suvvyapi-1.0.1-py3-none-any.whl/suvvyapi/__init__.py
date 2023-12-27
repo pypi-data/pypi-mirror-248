311387ac9a10f80dee9443164518c341f8995ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:04:27 +0000

    Add package suvvyapi/1.0.1/suvvyapi-1.0.1-py3-none-any.whl

---
+++ packages/suvvyapi/suvvyapi-1.0.1-py3-none-any.whl/suvvyapi/__init__.py
@@ -0,0 +1,5 @@
+from suvvyapi.asynchronous.wrapper import AsyncSuvvyAPIWrapperv  # noqa: F401
+from suvvyapi.models.history import *  # noqa: F403
+from suvvyapi.models.responses import *  # noqa: F403
+from suvvyapi.sync.wrapper import SuvvyAPIWrapper  # noqa: F401
+from suvvyapi.wrapper import Suvvy  # noqa: F401
