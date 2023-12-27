d765edcca010ac29f2e0dd8c48d4496ce4c607cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:04:18 +0000

    Add package hero-selection/0.0.2/hero-selection-0.0.2.tar.gz

---
+++ packages/hero-selection/hero-selection-0.0.2.tar.gz/hero-selection-0.0.2/hero_selection_component/__init__.py
@@ -0,0 +1,20 @@
+import os
+import streamlit.components.v1 as components
+
+_RELEASE = True
+
+if not _RELEASE:
+    _hero_selection_component = components.declare_component(
+        "hero_selection_component",
+        url="http://localhost:3001", 
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _hero_selection_component = components.declare_component("hero_selection_component", path=build_dir)
+
+def hero_selection_slider(heroData=None, styles=None, key=None):
+  
+    component_value = _hero_selection_component(heroData=heroData, styles=styles, key=key, default=0)
+
+    return component_value
