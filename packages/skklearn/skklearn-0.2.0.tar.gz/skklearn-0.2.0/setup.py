e61b14ac0b675d93404c4e94259b4947137b795b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 14:03:49 +0000

    Add package skklearn/0.2.0/skklearn-0.2.0.tar.gz

---
+++ packages/skklearn/skklearn-0.2.0.tar.gz/skklearn-0.2.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='skklearn',
+    version='0.2.0',
+    packages=find_packages(),
+    package_data={'tnsorflow': ['notebook/*.ipynb']},
+    install_requires=[
+        'nbformat',  # Add nbformat as a dependency
+        # List your other dependencies here if any
+    ],
+    entry_points={
+        'console_scripts': [
+            'tnsorflow=tnsorflow.hello:hello',
+        ],
+    },
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A simple package that prints "Hello from tnsorflow!"',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/tnsorflow',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
