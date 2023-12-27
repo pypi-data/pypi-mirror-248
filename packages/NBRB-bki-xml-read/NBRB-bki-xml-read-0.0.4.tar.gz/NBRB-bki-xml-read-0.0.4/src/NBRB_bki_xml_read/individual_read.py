a8036296e57780d236ee70d87584db63e440cdcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:32 +0000

    Add package NBRB-bki-xml-read/0.0.4/NBRB-bki-xml-read-0.0.4.tar.gz

---
+++ packages/NBRB-bki-xml-read/NBRB-bki-xml-read-0.0.4.tar.gz/NBRB-bki-xml-read-0.0.4/src/NBRB_bki_xml_read/individual_read.py
@@ -0,0 +1,100 @@
+'''
+Functions that allows to read information for 
+individual client.
+'''
+from NBRB_bki_xml_read.common_tools import (
+    dict_reading_decorator, 
+    sub_dict_reader
+)
+
+
+@dict_reading_decorator
+def read_client(client_dict, keys_separator="/"):
+    '''
+    Read bki information starting from the "client" field.
+    
+    Arguments
+    -----------
+        client_dict :    (dict) dictionary which can be created
+                         from infromation between <client> tag
+                         of bki.xml
+        keys_separator : (str) describe symbols that will separate keys
+                         of input dictionaries in the result dictionary.
+    Returns
+    ----------
+        (dict) flat dictionary that contains 
+        infromation between <clint> tag from bki.xml. 
+    '''
+
+    res = {
+        # here are relatively simple fields that can be read just as
+        # as dictionaries throw sub_dict_reader
+        **{
+            "client" + keys_separator + key:val
+            for field_name in [
+                "titul",
+                "registrationplace",
+                "range",
+                "scoring",
+                "RequestNumber7Days",
+                "RequestNumber30Days"
+            ]
+            for key, val in sub_dict_reader(
+                client_dict, field_name,
+                keys_separator = keys_separator
+            ).items()
+        },
+        # there are values that can't be encoded with
+        # sub_dict_reader so we just take them.
+        # we use it in three cases.
+        # - Final value is under key;
+        # - Some complex type that cannot be properly 
+        #   encoded with sub_dict_reader;
+        # - We didn't understand yet that this field can
+        #   be encoded with sub_dict_reader ;).
+        **{
+            "client" + keys_separator + field_name : client_dict[field_name]
+            for field_name in [
+                "owner",
+                "contracttype",
+                "ContractList",
+                "requestnumber",
+                "TotalDebtSum"
+            ]
+            if field_name in client_dict
+        }
+    }
+
+    return res
+
+
+@dict_reading_decorator
+def read_result(result_dict, keys_separator="/"):
+    '''
+    Read the response from bki starting from the "result" field.
+
+    Arguments
+    -----------
+        result_dict :    (dict) dictionary which can be created
+                         from infromation between <result> tag
+                         of bki.xml
+        keys_separator : (str) describe symbols that will separate keys
+                         of input dictionaries in the result dictionary.
+    Returns
+    ----------
+        (dict) flat dictionary that contains 
+        infromation between <result> tag from bki.xml. 
+    '''
+    
+    res = {
+        **sub_dict_reader(
+            result_dict, "completecode", 
+            keys_separator = keys_separator
+        ),
+        **(
+            read_client(result_dict["client"], keys_separator = keys_separator)
+            if ("client" in result_dict.keys()) else {}
+        )
+    }
+
+    return {"result" + keys_separator + key:val for key, val in res.items()}
