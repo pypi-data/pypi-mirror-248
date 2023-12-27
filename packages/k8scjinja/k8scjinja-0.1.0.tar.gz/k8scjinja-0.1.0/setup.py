5eca6a6c3fba15db3de04e4589def1b6bbb903db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 05:03:16 +0000

    Add package k8scjinja/0.1.0/k8scjinja-0.1.0.tar.gz

---
+++ packages/k8scjinja/k8scjinja-0.1.0.tar.gz/k8scjinja-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='k8scjinja',
+    version='0.1.0',
+    author='Yury Gavrilov',
+    author_email='public@igavrilov.ru',
+    description='rendering jinja templates from any files using yaml-environment',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
