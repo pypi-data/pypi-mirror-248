360e564b210d7d634fc811785309df9b89f98a6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:22 +0000

    Add package alfred-osint/0.2.3.4/alfred_osint-0.2.3.4-py3-none-any.whl

---
+++ packages/alfred-osint/alfred_osint-0.2.3.4-py3-none-any.whl/alfred_osint/modules/crypt.py
@@ -0,0 +1,60 @@
+import platform
+from configparser import ConfigParser
+
+from cryptography.fernet import Fernet
+
+from .configcheck import *
+from .lang import *
+
+config = ConfigParser()
+
+
+language_module = language_m
+
+
+def print_encrypted_and_decrypted_text(decrypted_text):
+    print("Decrypted text:", decrypted_text)
+
+
+def saveInfo(config, syskey):
+    config.read("./alfred_osint/config/config.ini")
+    config.set("main", "privatekey", str(key))
+    config.set("main", "syscrypt", str(syskey))
+    with open("./alfred_osint/config/config.ini", "w") as f:
+        config.write(f)
+
+
+# Generate a random key
+key = Fernet.generate_key()
+print("Encryption Key:", key.decode())  # Convert the key to a string for printing
+
+# Create a Fernet object with the key
+cipher_suite = Fernet(key)
+
+def encrypt(text):
+    encrypted_text = cipher_suite.encrypt(text.encode())
+    return encrypted_text
+
+def decrypt(encrypted_text):
+    decrypted_text = cipher_suite.decrypt(encrypted_text).decode()
+    return decrypted_text
+
+
+versionToPass = configC(language_module)
+
+config.read("./alfred_osint/config/config.ini")
+browser = config.get("main", "browser")
+syskey = (
+    platform.system()
+    + platform.release()
+    + "-AlfredVer-"
+    + versionToPass
+    + "-"
+    + platform.python_version()
+    + "-"
+    + browser
+)
+
+encrypted_text = encrypt(syskey)
+decrypted_text = decrypt(encrypted_text)
+
