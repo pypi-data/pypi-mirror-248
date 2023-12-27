ca034d98a8fbc0ec349af0e5f77e210193630029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 14:03:51 +0000

    Add package draftfast/3.10.3/draftfast-3.10.3.tar.gz

---
+++ packages/draftfast/draftfast-3.10.3.tar.gz/draftfast-3.10.3/draftfast/test/test_nhl.py
@@ -0,0 +1,53 @@
+import os
+import unittest
+from draftfast.optimize import run
+from draftfast import rules
+from draftfast.csv_parse import salary_download
+from draftfast.settings import OptimizerSettings, Stack
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+salary_file = "{}/data/dk-nhl-salaries.csv".format(CURRENT_DIR)
+
+assertions = unittest.TestCase("__init__")
+
+
+def test_nhl_dk():
+    player_pool = salary_download.generate_players_from_csvs(
+        salary_file_location=salary_file,
+        game=rules.DRAFT_KINGS,
+        ruleset=rules.DK_NHL_RULE_SET,
+    )
+    roster = run(
+        rule_set=rules.DK_NHL_RULE_SET,
+        player_pool=player_pool,
+        verbose=True,
+    )
+    assertions.assertNotEqual(roster, None)
+
+
+def test_triple_stack():
+    player_pool = salary_download.generate_players_from_csvs(
+        salary_file_location=salary_file,
+        game=rules.DRAFT_KINGS,
+        ruleset=rules.DK_NHL_RULE_SET,
+    )
+    roster = run(
+        rule_set=rules.DK_NHL_RULE_SET,
+        player_pool=player_pool,
+        verbose=True,
+        optimizer_settings=OptimizerSettings(
+            stacks=[
+                Stack(team="TOR", count=3),
+                Stack(team="COL", count=3),
+                Stack(team="VAN", count=2),
+            ]
+        ),
+    )
+    players = roster.sorted_players()
+
+    phi_players = [x for x in players if x.team == "TOR"]
+    fla_players = [x for x in players if x.team == "COL"]
+    nsh_players = [x for x in players if x.team == "VAN"]
+    assertions.assertEqual(len(phi_players), 3)
+    assertions.assertEqual(len(fla_players), 3)
+    assertions.assertEqual(len(nsh_players), 2)
