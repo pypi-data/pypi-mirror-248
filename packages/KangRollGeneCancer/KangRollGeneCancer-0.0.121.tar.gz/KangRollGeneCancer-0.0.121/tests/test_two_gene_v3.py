775afd0e30d8849320582f732153d778f5caa4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 04:03:58 +0000

    Add package KangRollGeneCancer/0.0.121/KangRollGeneCancer-0.0.121.tar.gz

---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.121.tar.gz/KangRollGeneCancer-0.0.121/tests/test_two_gene_v3.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from sa004package.kangroll_basic_fn001_mln_com_release import fn010_translp
+
+# 定义TP53和BRCA1的基因功能特征谱
+v2_TP53 = np.array([0, 1, 1, 0] + [0]*12 + [0.3])  # TP53 参数
+v2_BRCA1 = np.array([0, 1, 1, 0] + [0]*12 + [0.3])  # BRCA1 参数
+
+# 初始化参数
+u_ini = 0.1  # 初始的双层网络值
+cycles = 10  # 总循环次数
+total_MLN_output = []
+
+# 模拟过程
+for cycle in range(cycles):
+    # TP53的影响
+    iterations_per_gene_TP53 = random.randint(100, 300)  # 随机选择迭代次数
+    for i in range(iterations_per_gene_TP53):
+        random.seed(42 + i)
+        H3_ini_test = np.array([random.uniform(0.0003, 0.001), 1, 0.0018, 0])
+        MLN_output_TP53, NL_final = fn010_translp(0.3, v2_TP53, u_ini, H3_ini_test, 50, 100)
+        u_ini = MLN_output_TP53[-1]  # 更新u_ini为TP53影响后的最后一个值
+        total_MLN_output.extend(MLN_output_TP53)
+
+    # BRCA1的影响
+    iterations_per_gene_BRCA1 = random.randint(100, 300)  # 随机选择迭代次数
+    for i in range(iterations_per_gene_BRCA1):
+        random.seed(42 + i + iterations_per_gene_TP53)
+        H3_ini_test = np.array([random.uniform(0.0003, 0.001), 1, 0.0018, 0])
+        MLN_output_BRCA1, NL_final = fn010_translp(0.3, v2_BRCA1, u_ini, H3_ini_test, 50, 100)
+        u_ini = MLN_output_BRCA1[-1]  # 更新u_ini为BRCA1影响后的最后一个值
+        total_MLN_output.extend(MLN_output_BRCA1)
+
+# 绘制图像
+plt.figure(figsize=(10, 6))  # 设置图像大小
+plt.plot(total_MLN_output, color='blue')  # 绘制总的MLN输出
+plt.title('Impact of TP53 and BRCA1 Gene Alterations on Tumor Dynamics: A Multi-Layer Network Analysis')
+plt.xlabel('Hours')
+plt.ylabel('Neoplastic Load Value')
+
+# 添加图例
+plt.legend(["TP53 and BRCA1 Gene Alterations"])
+
+# 添加网格
+plt.grid(True)
+
+# 设置y轴的上下限
+plt.ylim([0, 0.5])
+
+# 显示图像
+plt.show()
---
+++ packages/KangRollGeneCancer/KangRollGeneCancer-0.0.121.tar.gz/KangRollGeneCancer-0.0.121/tests/test_two_gene_v7.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+from sa004package.kangroll_basic_fn001_mln_com_release import fn010_translp
+
+# 定义TP53和BRCA1的基因功能特征谱
+v2_TP53 = np.array([0, 1, 1, 0] + [0]*12 + [0.2])  # TP53示例参数
+v2_BRCA1 = np.array([0, 1, 1, 0] + [0]*12 + [0.3])  # BRCA1示例参数
+
+# 初始化参数
+u_ini = 0.01  # 初始的双层网络值
+cycles = 10  # 总循环次数
+
+# 四组不同的损伤对
+damage_pairs = [(0.05, 0.15), (0.1, 0.2), (0.15, 0.25), (0.2, 0.3)]
+
+# 准备图形
+fig, axs = plt.subplots(4, 1, figsize=(10, 8))
+
+# 遍历每组损伤对
+for index, (v1_tp53_damage, v1_brca1_damage) in enumerate(damage_pairs):
+    total_MLN_output = []
+
+    for cycle in range(cycles):
+        # TP53的影响
+        for i in range(random.randint(10, 50)):
+            random.seed(42 + cycle * 100 + i)
+            H3_ini_test = np.array([random.uniform(0.0001, 0.0002), 1, 0.0011, 0])
+            MLN_output_TP53, _ = fn010_translp(v1_tp53_damage, v2_TP53, u_ini, H3_ini_test, 50, 100)
+            u_ini = MLN_output_TP53[-1]
+            total_MLN_output.extend(MLN_output_TP53)
+
+        # BRCA1的影响
+        for i in range(random.randint(10, 50)):
+            random.seed(43 + cycle * 100 + i)
+            H3_ini_test = np.array([random.uniform(0.0001, 0.00025), 1, 0.0008, 0])
+            MLN_output_BRCA1, _ = fn010_translp(v1_brca1_damage, v2_BRCA1, u_ini, H3_ini_test, 50, 100)
+            u_ini = MLN_output_BRCA1[-1]
+            total_MLN_output.extend(MLN_output_BRCA1)
+
+    # 绘制子图
+    axs[index].plot(total_MLN_output)
+    axs[index].set_title(f'TP53 Damage: {v1_tp53_damage}, BRCA1 Damage: {v1_brca1_damage}')
+    axs[index].set_xlabel('Hours')
+    axs[index].set_ylabel('Neoplastic Load Value')
+    axs[index].grid(True)
+    axs[index].set_ylim([0, 0.3])
+
+# 添加总标题
+plt.suptitle('Impact of TP53 and BRCA1 Gene Alterations on Tumor Dynamics - A Multi-Layer Network Analysis')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])  # 调整布局以避免重叠
+plt.show(block=False)
+plt.pause(10)
+plt.close()
