4a2af2136928ac981c58673054ea8e5dbae16587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 15:03:36 +0000

    Add package megakit/0.0.1/megakit-0.0.1-py3-none-any.whl

---
+++ packages/megakit/megakit-0.0.1-py3-none-any.whl/compress/__init__.py
@@ -0,0 +1,6 @@
+from compress.installer import *
+from compress.functions.main import *
+from compress.functions.manage import *
+from compress.functions.random import *
+from compress.functions.time import *
+from compress.functions.image import *
