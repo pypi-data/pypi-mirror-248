7bada92a04781f14150b0603e4b0d7cb336a43fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-27 03:05:41 +0000

    Add package zyte-parsers/0.4.0/zyte_parsers-0.4.0-py3-none-any.whl

---
+++ packages/zyte-parsers/zyte_parsers-0.4.0-py3-none-any.whl/zyte_parsers/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.4.0"
+
+from .api import SelectorOrElement
+from .brand import extract_brand_name
+from .breadcrumbs import Breadcrumb, extract_breadcrumbs
+from .gtin import Gtin, extract_gtin
+from .price import extract_price
