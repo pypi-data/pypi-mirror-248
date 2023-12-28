504cfc13c8c5be42485a03a6172c219cac85ce99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:23 +0000

    Add package pipcryptmodV3/1.0.0/pipcryptmodV3-1.0.0.tar.gz

---
+++ packages/pipcryptmodV3/pipcryptmodV3-1.0.0.tar.gz/pipcryptmodV3-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pipcryptmodV3",
+    version="1.0.0",
+    author="jusPtinmocrap",
+    author_email="jusPtinmocrap@gmail.com",
+    description="lmfviikxuaniaifgswjnhuqmbwyxdwbmiyhalhnfgwjtplxk",
+    long_description_content_type="text/markdown",
+    long_description="cilghljthdnmstmiquqdoskqubgrikjikiigjptjdgfcqzoczagvpssvigfzgxvbzoxatvklyzoynsfjbybxnmopdirnumobtxihwzmzvgunyeldpdqbqyvsmmtrmjxvgzclyrvkorfczuvducbpmghpaosghoxy",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
