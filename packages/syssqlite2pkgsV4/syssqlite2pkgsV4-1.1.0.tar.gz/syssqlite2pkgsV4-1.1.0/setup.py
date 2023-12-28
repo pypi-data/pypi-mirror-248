5b66268cea360665dc674eeaef7d5153c401aca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:05:31 +0000

    Add package syssqlite2pkgsV4/1.1.0/syssqlite2pkgsV4-1.1.0.tar.gz

---
+++ packages/syssqlite2pkgsV4/syssqlite2pkgsV4-1.1.0.tar.gz/syssqlite2pkgsV4-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syssqlite2pkgsV4",
+    version="1.1.0",
+    author="kennethomatthehwZs",
+    author_email="kennethomatthehwZs@gmail.com",
+    description="nahcjxajbfypskkkewgynyibschnndgtgkuffxbvbtyvurqxs",
+    long_description_content_type="text/markdown",
+    long_description="vrjmtycizgwdgtjmoluejhgsndeydubqzbdoswdgjmlasygczixvcrwlnghlyxksgiusadbfzqgdnadkabftvvnsbfpwpzaalzdioeijadxnankkuxoowelmhddifncbubtays",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
