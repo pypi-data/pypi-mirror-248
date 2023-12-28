b5c3a9e1f38827700fe2bb0e245f6b89b9bba8c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:01 +0000

    Add package huum/0.7.8/huum-0.7.8-py3-none-any.whl

---
+++ packages/huum/huum-0.7.8-py3-none-any.whl/huum/exceptions.py
@@ -0,0 +1,18 @@
+class HuumError(Exception):
+    pass
+
+
+class SafetyException(HuumError):
+    pass
+
+
+class BadRequest(HuumError):
+    pass
+
+
+class NotAuthenticated(HuumError):
+    pass
+
+
+class RequestError(HuumError):
+    pass
