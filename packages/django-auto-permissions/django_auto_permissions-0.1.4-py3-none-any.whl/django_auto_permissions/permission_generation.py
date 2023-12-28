c30c9168bca305ad20d472cd9f97a96f9f483605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:03:59 +0000

    Add package django-auto-permissions/0.1.4/django_auto_permissions-0.1.4-py3-none-any.whl

---
+++ packages/django-auto-permissions/django_auto_permissions-0.1.4-py3-none-any.whl/django_auto_permissions/permission_generation.py
@@ -0,0 +1,35 @@
+from .viewset_analysis import analyze_registered_viewsets
+
+
+def generate_permission_codename(viewset_class, method_name):
+    """
+        Generates a unique permission codename for a given viewset method.
+
+        :param viewset_class: The viewset class.
+        :param method_name: The custom method name.
+        :return: A string representing the permission codename.
+        """
+    return f"{viewset_class.__name__.lower()}_{method_name}"
+
+
+def register_permissions():
+    """
+        Registers generated permissions with Django's permission system.
+        """
+    from django.contrib.auth.models import Permission
+    from django.contrib.contenttypes.models import ContentType
+    for viewset_class, methods in analyze_registered_viewsets().items():
+        for method in methods:
+            codename = generate_permission_codename(viewset_class, method)
+            name = f"Can {method} {viewset_class.__name__}"
+
+            # Creating and saving the permission
+            content_type = ContentType.objects.get_for_model(viewset_class)
+            permission, created = Permission.objects.get_or_create(
+                codename=codename,
+                name=name,
+                content_type=content_type,
+            )
+
+            if created:
+                print(f"Created permission: {permission}")
