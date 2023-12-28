7c34beccbda205e2e92259d74582696f91cd02f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 05:04:03 +0000

    Add package nomad-media-pip/0.0.11/nomad_media_pip-0.0.11-py3-none-any.whl

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.11-py3-none-any.whl/nomad_media_pip/admin/schedule_event/add_asset_schedule_event.py
@@ -0,0 +1,46 @@
+from src.admin.schedule_event.event_types import _EVENT_TYPES
+from src.exceptions.api_exception_handler import _api_exception_handler
+
+import json, requests
+
+def _add_asset_schedule_event(AUTH_TOKEN, URL, ID, CHANNEL_ID, ASSET, 
+                              IS_LOOP, DURATION_TIME_CODE, PREVIOUS_ID, DEBUG):
+    API_URL = f"{URL}/liveChannel/{CHANNEL_ID}/liveScheduleEvent" 
+
+    # Create header for the request
+    HEADERS = {
+        'Content-Type': 'application/json',
+        "Authorization": "Bearer " + AUTH_TOKEN
+    }
+
+    # Build the payload BODY
+    BODY = {
+        "id": ID,
+        "isLoop": IS_LOOP,
+        "channelId": CHANNEL_ID,
+        "durationTimeCode": DURATION_TIME_CODE,
+        "previousId": PREVIOUS_ID,
+        "type": {
+            "id": _EVENT_TYPES["videoAsset"],
+            "description": "Video-Asset"
+        },
+        "asset": ASSET,
+        "previousId": None,
+    }
+
+    if DEBUG:
+        print(f"API URL: {API_URL}\nMETHOD: POST\nBODY: {json.dumps(BODY, indent=4)}")
+
+    try:
+        # Send the request
+        RESPONSE = requests.post(API_URL, headers= HEADERS, data= json.dumps(BODY))
+    
+        
+        if not RESPONSE.ok:
+            raise Exception()
+        
+        return RESPONSE.json()
+
+    except:
+        _api_exception_handler(RESPONSE, "Add Asset Schedule Event failed")
+
