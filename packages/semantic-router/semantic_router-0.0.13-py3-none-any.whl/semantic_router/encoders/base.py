35e6d4130a22d8123e9746380b44ebc2ea3c0525
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 16:03:51 +0000

    Add package semantic-router/0.0.13/semantic_router-0.0.13-py3-none-any.whl

---
+++ packages/semantic-router/semantic_router-0.0.13-py3-none-any.whl/semantic_router/encoders/base.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, Field
+
+
+class BaseEncoder(BaseModel):
+    name: str
+    type: str = Field(default="base")
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def __call__(self, docs: list[str]) -> list[list[float]]:
+        raise NotImplementedError("Subclasses must implement this method")
---
+++ packages/semantic-router/semantic_router-0.0.13-py3-none-any.whl/semantic_router/encoders/cohere.py
@@ -0,0 +1,35 @@
+import os
+
+import cohere
+
+from semantic_router.encoders import BaseEncoder
+
+
+class CohereEncoder(BaseEncoder):
+    client: cohere.Client | None = None
+    type: str = "cohere"
+
+    def __init__(
+        self,
+        name: str | None = None,
+        cohere_api_key: str | None = None,
+    ):
+        if name is None:
+            name = os.getenv("COHERE_MODEL_NAME", "embed-english-v3.0")
+        super().__init__(name=name)
+        cohere_api_key = cohere_api_key or os.getenv("COHERE_API_KEY")
+        if cohere_api_key is None:
+            raise ValueError("Cohere API key cannot be 'None'.")
+        try:
+            self.client = cohere.Client(cohere_api_key)
+        except Exception as e:
+            raise ValueError(f"Cohere API client failed to initialize. Error: {e}")
+
+    def __call__(self, docs: list[str]) -> list[list[float]]:
+        if self.client is None:
+            raise ValueError("Cohere client is not initialized.")
+        try:
+            embeds = self.client.embed(docs, input_type="search_query", model=self.name)
+            return embeds.embeddings
+        except Exception as e:
+            raise ValueError(f"Cohere API call failed. Error: {e}")
