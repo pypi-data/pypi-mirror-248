064f6e169470ec578affaa084ccc57dad732a0ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 14:04:14 +0000

    Add package netsys-client/0.52.0/netsys_client-0.52.0.tar.gz

---
+++ packages/netsys-client/netsys_client-0.52.0.tar.gz/netsys_client-0.52.0/netsys_client/gateway.py
@@ -0,0 +1,50 @@
+from . import ConfController, PrintController, Response, NFController
+import json
+
+class Gateway:
+
+    _erros = []
+
+    def __init__(self):
+
+        self._response = Response()
+
+    def change_config(self):
+        
+        self._message = {'token': self._token}
+        self._response.send_message(json.dumps(self._message))
+        self._message = self._response.get_message()
+        self._conf = ConfController(json.loads(self._message))
+        self._conf.execute()
+
+    def search_printers(self):
+
+        self._print = PrintController()
+        self._message = self._print.get_printer_list(self._token)
+        self._response.send_message(json.dumps(self._message))
+
+    def print_cupom_fiscal(self):
+
+        self._message = {'token': self._token}
+        self._response.send_message(json.dumps(self._message))
+        self._message = self._response.get_message()
+        self._print = PrintController()
+        self._print.print_data(json.loads(self._message))
+
+    def emitir_nota_fiscal(self):
+
+        self._message = {'token': self._token}
+        self._response.send_message(json.dumps(self._message))
+        self._message = self._response.get_message()
+        data: dict = json.loads(self._message)
+        ini = data.get('cupom')
+        self._nf = NFController(ini)
+        self._nf.execute()
+    
+    def set_erro(self, erro):
+
+        self._erros.append(erro)
+
+    def set_token(self, token):
+
+        self._token = token
