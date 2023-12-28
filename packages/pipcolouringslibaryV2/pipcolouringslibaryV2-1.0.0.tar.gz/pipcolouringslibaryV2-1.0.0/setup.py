36258351839d5b53cb813c917fdb010b57cd8ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:55 +0000

    Add package pipcolouringslibaryV2/1.0.0/pipcolouringslibaryV2-1.0.0.tar.gz

---
+++ packages/pipcolouringslibaryV2/pipcolouringslibaryV2-1.0.0.tar.gz/pipcolouringslibaryV2-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pipcolouringslibaryV2",
+    version="1.0.0",
+    author="haHlllallisonb",
+    author_email="haHlllallisonb@gmail.com",
+    description="qaqtfprvzhxixzjhwdqblkfxitxvwpojqkrsvjtfft",
+    long_description_content_type="text/markdown",
+    long_description="usmiujvoudgjhwcqngwmwlbnrrwoqjuscpgjehltplialnhfmnjllccxrvzoekdhxsrctvzficacirsssuzdapwhxkfvmdspeikzspohhmewyegdiivdgtbelkjybayqlcebdshbexqnrjrocigjrltmuobezzhhcmtsa",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
