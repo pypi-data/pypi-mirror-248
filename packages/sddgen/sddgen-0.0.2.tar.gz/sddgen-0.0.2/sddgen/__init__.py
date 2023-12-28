a76e4c9ee01f0ba86f697f1db431b5b29906a73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 15:04:44 +0000

    Add package sddgen/0.0.2/sddgen-0.0.2.tar.gz

---
+++ packages/sddgen/sddgen-0.0.2.tar.gz/sddgen-0.0.2/sddgen/__init__.py
@@ -0,0 +1,9 @@
+#------------------------------------------------------------------------
+# sddgen: __init__.py
+#------------------------------------------------------------------------
+from .model import *
+from .generator import *
+from .guitk import *
+#from .htmlcss import *
+#from .htmlcss import *
+
