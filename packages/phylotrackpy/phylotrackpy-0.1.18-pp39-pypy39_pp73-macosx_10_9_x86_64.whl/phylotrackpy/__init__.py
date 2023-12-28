5ad8b5ab238c15b3a69641be4922f848ba80c5b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 05:04:02 +0000

    Add package phylotrackpy/0.1.18/phylotrackpy-0.1.18-pp39-pypy39_pp73-macosx_10_9_x86_64.whl

---
+++ packages/phylotrackpy/phylotrackpy-0.1.18-pp39-pypy39_pp73-macosx_10_9_x86_64.whl/phylotrackpy/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for phylotrackpy."""
+
+__author__ = """Emily Dolson"""
+__email__ = 'emilydolson@gmail.com'
+__version__ = '0.1.18'
+
+from . import systematics
+
+__all__ = [
+    "systematics",
+]
