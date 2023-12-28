2d0929209e1bb8c090d356e8cd0f7f6987c5fa7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:02 +0000

    Add package ampltools/0.7.3/ampltools-0.7.3-py3-none-any.whl

---
+++ packages/ampltools/ampltools-0.7.3-py3-none-any.whl/ampltools/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+__version__ = "0.7.3"
+from .notebooks import (
+    ampl_notebook,
+)
+
+from .modules import (
+    activate as activate_license,
+    cloud_platform_name,
+)
+
+from .utils import (
+    register_magics,
+    add_to_path,
+)
+
+_SUPPORT_MESSAGE = """
+
+Please report any bugs at: https://github.com/ampl/amplpy
+
+For support/feedback go to https://discuss.ampl.com or e-mail <support@ampl.com>
+"""
+
+try:
+    from IPython import get_ipython
+
+    register_magics(ampl_object="ampl", globals_=get_ipython().user_global_ns)
+except:
+    pass
