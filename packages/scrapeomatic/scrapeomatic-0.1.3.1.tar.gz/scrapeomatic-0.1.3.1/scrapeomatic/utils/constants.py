3c8e2eaeabe18c51c75159f8587eef51fdafdd82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 05:04:04 +0000

    Add package scrapeomatic/0.1.3.1/scrapeomatic-0.1.3.1.tar.gz

---
+++ packages/scrapeomatic/scrapeomatic-0.1.3.1.tar.gz/scrapeomatic-0.1.3.1/scrapeomatic/utils/constants.py
@@ -0,0 +1,12 @@
+DEFAULT_BROWSER = "chrome"
+DEFAULT_TIMEOUT = 5
+DEFAULT_VIDEO_LIMIT = 10
+DEFAULT_USER_AGENT =  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+
+
+# Base URLs
+GITHUB_BASE_URL = "https://github.com"
+INSTAGRAM_BASE_URL =  "https://www.instagram.com"
+INSTAGRAM_PROFILE_URL = INSTAGRAM_BASE_URL + "/api/v1/users/web_profile_info/"
+TIKTOK_BASE_URL = "https://tiktok.com/@"
+YOUTUBE_BASE_URL = "https://www.youtube.com/@"
