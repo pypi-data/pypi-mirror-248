1721be107cb431ce20dcb13603d6a6246ef460bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 04:05:07 +0000

    Add package epure/0.3.5/epure-0.3.5-py3-none-any.whl

---
+++ packages/epure/epure-0.3.5-py3-none-any.whl/epure/resource/join_resource/join_resource.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from ...parser.inspect_parser.model import Model
+    # from ...resource.db.table import Table
+
+class Join:
+    model:Model
+    on_clause:str
+    join_type:str
+    alias:str
+
+    def __init__(self, model:Model, on_clause:str, join_type:str="LEFT", alias:str="") -> None:
+        self.model = model
+        self.on_clause = on_clause
+        self.join_type = join_type
+        self.alias = alias
+        
+class JoinResource:
+    model:Model
+    joins:list[Join]
+
+    def __init__(self, model:Model) -> None:
+        self.joins = []
+        self.model = model
+
+    def join(self, joined_model:Model, on_clause:str, join_type:str="LEFT", alias:str="") -> None:
+        join = Join(joined_model, on_clause, join_type, alias)
+        self.joins.append(join)
+
+    def read(self, *args, **kwargs):
+
+        # header = []
+        where_clause = ""
+
+        if len(args) >= 2:
+            header = args[0]
+            where_clause = args[1]
+
+        elif len(args) == 1:
+            where_clause = args[0]
+
+        if len(args) <= 1:
+            header = [self.model]
+            header = header + [join.model for join in self.joins]
+
+        return self.model.__table__.read(header, self.joins, where_clause, **kwargs)
