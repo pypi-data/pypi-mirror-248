5ade507231812de530a0e2e0906318d935a4b8d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 04:05:13 +0000

    Add package pkg13/0.1.0/pkg13-0.1.0-py3-none-any.whl

---
+++ packages/pkg13/pkg13-0.1.0-py3-none-any.whl/job13/__init__.py
@@ -0,0 +1,2 @@
+from job13.tebak_angka import tebak_angka
+from job13.konversi_berat import konversi_berat
---
+++ packages/pkg13/pkg13-0.1.0-py3-none-any.whl/job13/konversi_berat.py
@@ -0,0 +1,8 @@
+def konversi_berat():
+    berat = int(input("Masukkan berat anda :"))
+    satuan = input("Dalam satuan apa berat yang anda masukkan? (K untuk KG, L untuk LBS) : ")
+
+    if satuan.lower() == 'l':
+        print(f"Berat anda dikonversi menjadi kilogram adalah {round(berat * 0.453592)} kg")
+    elif satuan.lower() == 'k':
+        print(f"Berat anda dikonversi menjadi pon adalah {round(berat * 2.20462)} lbs")
