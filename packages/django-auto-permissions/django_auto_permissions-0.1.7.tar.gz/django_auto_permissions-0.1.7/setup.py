bfc9c46f1c11f2c1c84c6d7bbd931c5cebde8185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:03 +0000

    Add package django-auto-permissions/0.1.7/django_auto_permissions-0.1.7.tar.gz

---
+++ packages/django-auto-permissions/django_auto_permissions-0.1.7.tar.gz/django_auto_permissions-0.1.7/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django_auto_permissions',
+    version='0.1.7',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Automated permission generation for Django viewsets',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Jeet Padhya',
+    author_email='jeet.padhya@hotmail.com',
+    url='https://github.com/jeetkreditserve/django_auto_permissions',
+    install_requires=[
+        'Django>=3.2',  # Specify the Django version requirement
+        # Add other dependencies if needed
+    ],
+    classifiers=[
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
