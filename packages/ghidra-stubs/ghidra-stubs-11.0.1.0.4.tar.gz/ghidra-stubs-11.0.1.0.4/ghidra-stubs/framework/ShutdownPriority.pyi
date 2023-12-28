57480daadcd6cb0d1ac050bf8610b4bf9082ab3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:05:04 +0000

    Add package ghidra-stubs/11.0.1.0.4/ghidra-stubs-11.0.1.0.4.tar.gz

---
+++ packages/ghidra-stubs/ghidra-stubs-11.0.1.0.4.tar.gz/ghidra-stubs-11.0.1.0.4/ghidra-stubs/framework/ShutdownPriority.pyi
@@ -0,0 +1,42 @@
+import ghidra.framework
+import java.lang
+
+
+class ShutdownPriority(object):
+    DISPOSE_DATABASES: ghidra.framework.ShutdownPriority = ghidra.framework.ShutdownPriority@613ba291
+    DISPOSE_FILE_HANDLES: ghidra.framework.ShutdownPriority = ghidra.framework.ShutdownPriority@667759b0
+    FIRST: ghidra.framework.ShutdownPriority = ghidra.framework.ShutdownPriority@5299ca1e
+    LAST: ghidra.framework.ShutdownPriority = ghidra.framework.ShutdownPriority@e7fa324
+    SHUTDOWN_LOGGING: ghidra.framework.ShutdownPriority = ghidra.framework.ShutdownPriority@6cef0b3a
+
+
+
+
+
+
+
+    def after(self) -> ghidra.framework.ShutdownPriority: ...
+
+    def before(self) -> ghidra.framework.ShutdownPriority: ...
+
+    def equals(self, __a0: object) -> bool: ...
+
+    def getClass(self) -> java.lang.Class: ...
+
+    def hashCode(self) -> int: ...
+
+    def notify(self) -> None: ...
+
+    def notifyAll(self) -> None: ...
+
+    def toString(self) -> unicode: ...
+
+    @overload
+    def wait(self) -> None: ...
+
+    @overload
+    def wait(self, __a0: long) -> None: ...
+
+    @overload
+    def wait(self, __a0: long, __a1: int) -> None: ...
+
