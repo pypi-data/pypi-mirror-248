84527590e4be15f649d53ebc782831c6dd78486e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 04:05:16 +0000

    Add package supervisely/6.72.223/supervisely-6.72.223.tar.gz

---
+++ packages/supervisely/supervisely-6.72.223.tar.gz/supervisely-6.72.223/supervisely/app/widgets/empty/empty.py
@@ -0,0 +1,14 @@
+from supervisely.app.widgets import Widget
+
+
+class Empty(Widget):
+    def __init__(self, widget_id: str = None, style=""):
+        self._style: str = style
+
+        super().__init__(widget_id, file_path=__file__)
+
+    def get_json_data(self):
+        return None
+
+    def get_json_state(self):
+        return None
