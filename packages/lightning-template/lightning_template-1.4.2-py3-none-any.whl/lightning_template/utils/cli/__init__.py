bc2d74b75d479240c5012d2ae7d685607f590232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 16:03:39 +0000

    Add package lightning-template/1.4.2/lightning_template-1.4.2-py3-none-any.whl

---
+++ packages/lightning-template/lightning_template-1.4.2-py3-none-any.whl/lightning_template/utils/cli/__init__.py
@@ -0,0 +1,4 @@
+from .argument_parsers import *
+from .cli import LightningCLI as LightningCLI
+from .instantiate_class import recursive_instantate_class as recursive_instantate_class
+from .trainer import Trainer as Trainer
