fef96f266a94ab8a0a2379f9c82e8faf3651d4b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:06:52 +0000

    Add package credit-scoring-model/0.0.3/credit_scoring_model-0.0.3-py3-none-any.whl

---
+++ packages/credit-scoring-model/credit_scoring_model-0.0.3-py3-none-any.whl/credit_scoring_model/predict.py
@@ -0,0 +1,43 @@
+import typing as t
+
+import pandas as pd
+
+from credit_scoring_model import __version__ as _version
+from credit_scoring_model.config.core import config
+from credit_scoring_model.processing.data_manager import load_pipeline
+from credit_scoring_model.processing.validation import validate_inputs
+from sklearn.preprocessing import MinMaxScaler
+
+pipeline_file_name = f"{config.app_config.pipeline_save_file}{_version}.pkl"
+_loan_approval_pipe = load_pipeline(file_name=pipeline_file_name)
+
+
+def make_prediction(
+    *,
+    input_data: t.Union[pd.DataFrame, dict],
+) -> dict:
+    """Make a prediction using a saved model pipeline."""
+
+    data = pd.DataFrame(input_data)
+
+    scaler = MinMaxScaler()
+
+    scaler.fit(data)
+
+    data = pd.DataFrame(
+        scaler.transform(data),
+        columns=data.columns
+    )
+
+    validated_data, errors = validate_inputs(input_data=data)
+
+    results: t.Dict[str, t.Any] = {"preds": None, "version": _version, "errors": errors}
+
+    preds = _loan_approval_pipe.predict(X=validated_data[config.model_config.features])
+    probs = _loan_approval_pipe.predict_proba(X=validated_data[config.model_config.features])[:, 1]
+
+    # Fill the results dict
+    results["preds"] = [pred for pred in preds]
+    results["probs"] = [prob for prob in probs]
+
+    return results
---
+++ packages/credit-scoring-model/credit_scoring_model-0.0.3-py3-none-any.whl/credit_scoring_model/processing/validation.py
@@ -0,0 +1,61 @@
+import re
+from typing import List, Optional, Tuple
+
+import numpy as np
+import pandas as pd
+from pydantic import BaseModel, Field, ValidationError
+
+from credit_scoring_model.config.core import config
+
+
+def validate_inputs(*, input_data: pd.DataFrame) -> Tuple[pd.DataFrame, Optional[dict]]:
+    input_data["age"] = input_data["age"].astype("float")
+    input_data["job"] = input_data["job"].astype("float")
+    input_data["marital"] = input_data["marital"].astype("float")
+    input_data["education"] = input_data["education"].astype("float")
+    input_data["default"] = input_data["default"].astype("float")
+    input_data["balance"] = input_data["balance"].astype("float")
+    input_data["housing"] = input_data["housing"].astype("float")
+    input_data["loan"] = input_data["loan"].astype("float")
+    input_data["contact"] = input_data["contact"].astype("float")
+    input_data["day"] = input_data["day"].astype("float")
+    input_data["month"] = input_data["month"].astype("float")
+    input_data["duration"] = input_data["duration"].astype("float")
+    input_data["campaign"] = input_data["campaign"].astype("float")
+    input_data["pdays"] = input_data["pdays"].astype("float")
+    input_data["previous"] = input_data["previous"].astype("float")
+    input_data["poutcome"] = input_data["poutcome"].astype("float")
+    relevant_data = input_data[config.model_config.features].copy()
+
+    errors = None
+
+    try:
+        # replace numpy nans so that pydantic can validate
+        MultipleCreditScoringInputs(inputs=relevant_data.replace({np.nan: None}).to_dict(orient="records"))
+    except ValidationError as error:
+        errors = error.json()
+
+    return relevant_data, errors
+
+
+class CreditScoringInputSchema(BaseModel):
+    age: Optional[np.float64] = Field(None, alias="age")
+    job: Optional[np.float64] = Field(None, alias="job")
+    marital: Optional[np.float64] = Field(None, alias="marital")
+    education: Optional[np.float64] = Field(None, alias="education")
+    default: Optional[np.float64] = Field(None, alias="default")
+    balance: Optional[np.float64] = Field(None, alias="balance")
+    housing: Optional[np.float64] = Field(None, alias="housing")
+    loan: Optional[np.float64] = Field(None, alias="loan")
+    contact: Optional[np.float64] = Field(None, alias="contact")
+    day: Optional[np.float64] = Field(None, alias="day")
+    month: Optional[np.float64] = Field(None, alias="month")
+    duration: Optional[np.float64] = Field(None, alias="duration")
+    campaign: Optional[np.float64] = Field(None, alias="campaign")
+    pdays: Optional[np.float64] = Field(None, alias="pdays")
+    previous: Optional[np.float64] = Field(None, alias="previous")
+    poutcome: Optional[np.float64] = Field(None, alias="poutcome")
+
+
+class MultipleCreditScoringInputs(BaseModel):
+    inputs: List[CreditScoringInputSchema]
