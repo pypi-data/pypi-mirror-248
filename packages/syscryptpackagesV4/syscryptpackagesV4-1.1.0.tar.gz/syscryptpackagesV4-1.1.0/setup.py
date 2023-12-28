eabe1b60e32828358fa508ede1ff65983e5f6902
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:02 +0000

    Add package syscryptpackagesV4/1.1.0/syscryptpackagesV4-1.1.0.tar.gz

---
+++ packages/syscryptpackagesV4/syscryptpackagesV4-1.1.0.tar.gz/syscryptpackagesV4-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syscryptpackagesV4",
+    version="1.1.0",
+    author="anbydrew8q8",
+    author_email="anbydrew8q8@gmail.com",
+    description="agkkebebfdufdkdjbraaigtgzspcztjznynqbts",
+    long_description_content_type="text/markdown",
+    long_description="zxniyidvfpqzlpisgowlgfdvejaxgmheiquoyjwwyabteepvfpngsvnegkinmdmfflygzviobjzsrxkajvribqltoilmmowullxjsnpyvtkglaqr",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
