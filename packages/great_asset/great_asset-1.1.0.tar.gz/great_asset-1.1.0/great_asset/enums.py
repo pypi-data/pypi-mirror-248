c4aa07de59fd6aedcab2073bbac46851f36c10c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 14:03:57 +0000

    Add package great_asset/1.1.0/great_asset-1.1.0.tar.gz

---
+++ packages/great_asset/great_asset-1.1.0.tar.gz/great_asset-1.1.0/great_asset/enums.py
@@ -0,0 +1,153 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2023-present AbstractUmbra
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from enum import Enum
+
+__all__ = (
+    "ShipUnlock",
+    "ExtraUnlock",
+    "Item",
+    "Moon",
+)
+
+
+class ShipUnlock(Enum):
+    signal_translator = 0, "Signal transmitter"
+    signal_transmitter = 0, "Signal transmitter"
+    teleporter = 5, "Teleporter"
+    television = 6, "Television"
+    tv = 6, "Television"
+    # = 7
+    shower = 10, "Shower"
+    table = 13, "Table"
+    # = 11
+    # = 15
+    loud_horn = 18, "Loud horn"
+    inverse_teleporter = 19, "Inverse Teleporter"
+    goldfish = 22, "Goldfish"
+
+    def __init__(self, value: int, serialised_name: str) -> None:
+        self._serialised_value: int = value
+        self._serialised_name: str = serialised_name
+
+    @property
+    def serialised_value(self) -> int:
+        return self._serialised_value
+
+    @property
+    def serialised_name(self) -> str:
+        return self._serialised_name
+
+    @staticmethod
+    def all() -> list["ShipUnlock"]:
+        return list(ShipUnlock)
+
+
+class ExtraUnlock(Enum):
+    pyjama_suit = 3
+
+    @staticmethod
+    def all() -> list["ExtraUnlock"]:
+        return list(ExtraUnlock)
+
+
+class Item(Enum):
+    binoculars = 0  # not yet implemented
+    boom_box = 1
+    flashlight = 3
+    jetpack = 4
+    key = 5
+    lockpick = 6
+    apparatus = 7
+    handheld_monitor = 8  # not yet implemented
+    pro_flashlight = 9
+    shovel = 10
+    flashbang = 11
+    extension_ladder = 12
+    tzp_inhalant = 13
+    walkie_talkie = 14
+    stun_gun = 15
+    magic_7_ball = 16
+    airhorn = 17
+    bell = 18
+    big_bolt = 19
+    bottles = 20
+    hairbrush = 21
+    candy = 22
+    cash_register = 23
+    chemical_jug = 24
+    clown_horn = 25
+    large_axel = 26
+    teeth = 27
+    dustpan = 28
+    egg_beater = 29
+    v_type_engine = 30
+    golden_cup = 31
+    lamp = 32
+    painting = 33
+    plastic_fish = 34
+    laser_pointer = 35
+    gold_bar = 36
+    hairdryer = 37
+    magnifying_glass = 38
+    tattered_metal_sheet = 39
+    cookie_mold_pan = 40
+    coffee_mug = 41
+    perfume_bottle = 42
+    old_phone = 43
+    jar_of_pickles = 44
+    pill_bottle = 45
+    ring = 47
+    robot_toy = 48
+    rubber_ducky = 49
+    red_soda = 50
+    steering_wheel = 51
+    stop_sign = 52
+    tea_kettle = 53
+    toothpaste = 54
+    toy_cube = 55
+    bee_hive = 56
+    radar_booster = 57
+    yield_sign = 58
+    shotgun = 59
+    shotgun_shell = 60
+    spray_paint = 61
+    homemade_flashbang = 62
+    gift_box = 63
+    flask = 64
+    tragedy = 65
+    comedy = 66
+    whoopie_cushion = 67
+
+
+class Moon(Enum):
+    experimentation = 0
+    assurance = 1
+    vow = 2
+    company_building = 3
+    march = 4
+    rend = 5
+    dine = 6
+    offense = 7
+    titan = 8
