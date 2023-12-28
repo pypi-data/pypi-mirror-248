8f33532d599624695c24ce0e7a93bc7f284953b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 14:03:56 +0000

    Add package gen3-util/0.0.13rc9/gen3_util-0.0.13rc9.tar.gz

---
+++ packages/gen3-util/gen3_util-0.0.13rc9.tar.gz/gen3_util-0.0.13rc9/gen3_util/meta/cli.py
@@ -0,0 +1,109 @@
+import json
+import pathlib
+from json import JSONDecodeError
+
+import click
+
+from gen3_util.cli import NaturalOrderGroup, CLIOutput
+from gen3_util.config import Config
+from gen3_util.meta.delta import get as delta_get
+from gen3_util.meta.downloader import cp as cp_download
+from gen3_util.meta.importer import import_indexd
+from gen3_util.meta.lister import ls
+from gen3_util.meta.publisher import publish_meta_data
+from gen3_util.meta.uploader import cp as cp_upload
+from gen3_util.meta.validator import validate
+
+
+@click.group(name='meta', cls=NaturalOrderGroup)
+@click.pass_obj
+def meta_group(config):
+    """Manage meta data."""
+    pass
+
+
+meta_group.add_command(import_indexd)
+
+
+@meta_group.command(name="validate")
+@click.argument('directory')
+@click.pass_obj
+def meta_validate(config: Config, directory):
+    """Validate FHIR data in DIRECTORY."""
+    with CLIOutput(config) as output:
+        output.update(validate(config, directory))
+
+
+@meta_group.command(name="publish")
+@click.argument('meta_data_path')
+@click.option('--ignore_state', default=False, is_flag=True, show_default=True,
+              help="Upload file, even if already uploaded")
+@click.option('--project_id', default=None, show_default=True,
+              help="Gen3 program-project", envvar='PROJECT_ID')
+@click.pass_obj
+def meta_publish(config: Config, meta_data_path: str,  project_id: str, ignore_state: bool):
+    """Publish meta data on the portal
+
+    \b
+    meta_data_path: meta_data directory"""
+
+    _ = publish_meta_data(config, meta_data_path, ignore_state, project_id)
+    try:
+        output = json.loads(_['output'])
+
+        with CLIOutput(config=config) as console_output:
+            console_output.update(output)
+
+    except JSONDecodeError:
+        print("jobs_client.async_run_job_and_wait() (raw):", _)
+
+
+@meta_group.command(name="cp", hidden=True)
+@click.argument('from_')
+@click.argument('to_')
+@click.option('--ignore_state', default=False, is_flag=True, show_default=True,
+              help="Upload file, even if already uploaded")
+@click.option('--project_id', default=None, show_default=True,
+              help="Gen3 program-project", envvar='PROJECT_ID')
+@click.pass_obj
+def meta_cp(config: Config, from_: str, to_: str, project_id: str, ignore_state: bool):
+    """Copy meta to/from the project bucket.
+
+    \b
+    from_: meta data directory
+    to_: destination  bucket"""
+    with CLIOutput(config=config) as output:
+        if pathlib.Path(from_).is_dir():
+            assert project_id is not None, "--project_id is required for uploads"
+            output.update(cp_upload(config, from_, project_id, ignore_state))
+        else:
+            pathlib.Path(to_).parent.mkdir(parents=True, exist_ok=True)
+            output.update(cp_download(config, from_, to_))
+
+
+@meta_group.command(name="ls", hidden=True)
+@click.pass_obj
+def meta_ls(config: Config):
+    """Query buckets for submitted metadata."""
+    with CLIOutput(config=config) as output:
+        output.update(ls(config))
+
+
+# TODO
+# @meta_group.command(name="rm")
+# @click.pass_obj
+# def meta_rm(config: Config):
+#     """Remove meta from a project."""
+#     rm(config)
+
+
+@meta_group.command(name="node", hidden=True)
+@click.option('--project_id', default=None, show_default=True,
+              help="Gen3 program-project", envvar='PROJECT_ID')
+@click.option('--node_id', default=None, show_default=True,
+              help="Gen3 node id")
+@click.pass_obj
+def delta(config: Config, project_id: str, node_id: str):
+    """Retrieve simplified metadata for a node."""
+    with CLIOutput(config) as output:
+        output.update(delta_get(config, project_id, node_id))
---
+++ packages/gen3-util/gen3_util-0.0.13rc9.tar.gz/gen3_util-0.0.13rc9/tests/unit/test_cli.py
@@ -0,0 +1,98 @@
+from click.testing import CliRunner
+from gen3_util.cli.cli import cli
+
+
+def test_default_command(caplog):
+    """Ensure it prints version if no other command"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['version'])
+    assert result.exit_code == 0
+    expected = ['version']
+    for _ in expected:
+        assert _ in result.output, f"Should have printed expected={_} actual={result.output}"
+
+
+def test_any_command(caplog):
+    """Ensure it does not print version if command provided"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['config'])
+    assert result.exit_code == 0
+    un_expected_strings = ['Version']
+    for expected_string in un_expected_strings:
+        assert expected_string not in result.output, "only print version if nothing else to do"
+
+
+def test_help(caplog):
+    """Ensure it prints command groups"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['--help'])
+    assert result.exit_code == 0
+    print(result.output)
+    expected_strings = """
+        projects  Manage Gen3 projects.
+        buckets   Manage Gen3 buckets.
+        meta      Manage meta data.
+        files     Manage file transfers.
+        access    Manage access requests.
+        config    Configure this utility.
+        """.split()
+    for expected_string in expected_strings:
+        assert expected_string in result.output, f"Should have printed {expected_string}"
+
+
+def test_projects(caplog):
+    """Ensure it prints project"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['projects', '--help'])
+    assert result.exit_code == 0
+    print(result.output)
+    expected_strings = """
+      new  Creates project resource with default policies.
+      ls   List all projects user has access to.
+      rm   Remove project.
+    """.split()
+    for expected_string in expected_strings:
+        assert expected_string in result.output, f"Should have printed {expected_string}"
+
+
+def test_meta(caplog):
+    """Ensure it prints meta"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['meta', '--help'])
+    assert result.exit_code == 0
+    print(result.output)
+    expected_strings = """
+  create    Create minimal study meta, write to OUTPUT_PATH.
+  validate  Validate FHIR data in DIRECTORY.
+  publish   Publish meta data on the portal
+    """.split()
+    for expected_string in expected_strings:
+        assert expected_string in result.output, f"Should have printed {expected_string}"
+
+
+def test_file(caplog):
+    """Ensure it prints file"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['files', '--help'])
+    assert result.exit_code == 0
+    print(result.output)
+    expected_strings = """
+      ls        List files in a project.
+      manifest  Manage file transfers using a manifest.
+      rm        Remove files from a project.
+    """.split()
+    for expected_string in expected_strings:
+        assert expected_string in result.output, f"Should have printed {expected_string}"
+
+
+def test_config(caplog):
+    """Ensure it prints file"""
+    runner = CliRunner()
+    result = runner.invoke(cli, ['config', '--help'])
+    assert result.exit_code == 0
+    print(result.output)
+    expected_strings = """
+      ls  Show defaults.
+    """.split()
+    for expected_string in expected_strings:
+        assert expected_string in result.output, f"Should have printed {expected_string}"
