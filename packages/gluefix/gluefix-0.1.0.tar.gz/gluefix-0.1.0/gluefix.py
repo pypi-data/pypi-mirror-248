0b636c417240bdc7b8bd191be06e1b5f6775c680
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:06:21 +0000

    Add package gluefix/0.1.0/gluefix-0.1.0.tar.gz

---
+++ packages/gluefix/gluefix-0.1.0.tar.gz/gluefix-0.1.0/gluefix.py
@@ -0,0 +1,9 @@
+try:
+    from ._version import __version__
+except ImportError:
+    try:
+        from setuptools_scm import get_version
+
+        __version__ = get_version()
+    except (ImportError, LookupError) as e:
+        __version__ = str(e)
