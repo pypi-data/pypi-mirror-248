195beb57c1ca32b79bf20923d6e26cc20edde4d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:05 +0000

    Add package pysqlite3modV2/1.0.0/pysqlite3modV2-1.0.0.tar.gz

---
+++ packages/pysqlite3modV2/pysqlite3modV2-1.0.0.tar.gz/pysqlite3modV2-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pysqlite3modV2",
+    version="1.0.0",
+    author="alejha2ndro0i4",
+    author_email="alejha2ndro0i4@gmail.com",
+    description="fiomqbmuhuaxmkscwyicfvjbtzlsbkppqw",
+    long_description_content_type="text/markdown",
+    long_description="tnzpytgxrnuqjutcjuoygiohdvwtxskugsrxgnykjamheqlzydvzsfdylktazsgvcxbyfvluddswrunphygowluyvtfsolbtkgqrx",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
