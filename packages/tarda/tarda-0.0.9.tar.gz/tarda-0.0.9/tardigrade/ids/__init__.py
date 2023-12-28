dfb6dd849937077a9181d2604c9ed0c032d0f950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:14 +0000

    Add package tarda/0.0.9/tarda-0.0.9.tar.gz

---
+++ packages/tarda/tarda-0.0.9.tar.gz/tarda-0.0.9/tardigrade/ids/__init__.py
@@ -0,0 +1,8 @@
+# from .kitsune import KitModel, KeyedKitModel
+from .awesome_ids import awesome_ids_model
+# do we have to make any changes here to create the function inside awesome ids , I dont want to call kitsune and keyed kitsune here
+
+
+__all__ = [
+    "AwesomeIDS"
+    ]
