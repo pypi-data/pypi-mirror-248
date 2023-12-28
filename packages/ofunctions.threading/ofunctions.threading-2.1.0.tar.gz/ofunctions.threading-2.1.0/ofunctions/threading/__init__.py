c170616bbcac28651a883db3d9f566cd8ff14402
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 05:04:13 +0000

    Add package ofunctions.threading/2.1.0/ofunctions.threading-2.1.0.tar.gz

---
+++ packages/ofunctions.threading/ofunctions.threading-2.1.0.tar.gz/ofunctions.threading-2.1.0/ofunctions/threading/__init__.py
@@ -0,0 +1,164 @@
+#! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+#
+# This file is part of ofunctions package
+
+"""
+Function decorators for threading and antiflooding
+Use with @threaded
+
+Versioning semantics:
+    Major version: backward compatibility breaking changes
+    Minor version: New functionality
+    Patch version: Backwards compatible bug fixes
+
+"""
+
+__intname__ = "ofunctions.threading"
+__author__ = "Orsiris de Jong"
+__copyright__ = "Copyright (C) 2019-2022 Orsiris de Jong"
+__description__ = (
+    "Threading decorator to run functions as threads, antiflood decorator too"
+)
+__licence__ = "BSD 3 Clause"
+__version__ = "2.1.0"
+__build__ = "2023122701"
+__compat__ = "python2.7+"
+
+
+import sys
+import threading
+from datetime import datetime
+
+
+# Python 2.7 compat fixes
+try:
+    from concurrent.futures import Future
+except ImportError:
+    pass
+from functools import wraps
+
+
+# python 2.7 compat where we will use @threaded
+if sys.version_info[0] < 3:
+
+    def threaded(fn):
+        """
+        Threaded allows to make any funtion a thread
+        With python 2.7, there will be no possible return value
+        """
+
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            if kwargs.pop("__no_threads", False):
+                return fn(*args, **kwargs)
+            thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
+            thread.daemon = True
+            thread.start()
+            return thread
+
+        return wrapper
+
+else:
+
+    def call_with_future(fn, future, args, kwargs):
+        """
+        Threading a function with return info using Future
+        from https://stackoverflow.com/a/19846691/2635443
+
+        Example:
+
+        @threaded
+        def somefunc(arg):
+            return 'arg was %s' % arg
+
+
+        thread = somefunc('foo')
+        while thread.done() is False:
+            time.sleep(1)
+
+        print(thread.result())
+        """
+        try:
+            result = fn(*args, **kwargs)
+            future.set_result(result)
+        except Exception as exc:
+            future.set_exception(exc)
+
+    def threaded(fn):
+        """
+        @threaded wrapper in order to thread any function
+
+        @wraps decorator sole purpose is for function.__name__ to be the real function
+        instead of 'wrapper'
+
+        """
+
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            if kwargs.pop("__no_threads", False):
+                return fn(*args, **kwargs)
+            future = Future()
+            thread = threading.Thread(
+                target=call_with_future, args=(fn, future, args, kwargs)
+            )
+            thread.daemon = True
+            thread.start()
+            return future
+
+        return wrapper
+
+
+def no_flood(flood_timespan=5, multiple_instances_diff_args=True):
+    """
+    This decorator is an antiflood system for a given function
+    It prevents running the same function more than once in a given timeframe (flood_timespan seconds)
+
+    Usage:
+
+    Don't run my_funtion more than once in 5 seconds if called multiple times with the same arguments
+
+    @no_flood(5)
+    my_function()
+
+    Don't run my_function more than once in 10 seconds, regarless of it's arguments
+    @no_flood(10, False)
+    my_function()
+
+    Can also be combined with a threaded decorator
+    @threaded
+    @no_flood(3)
+    my_function()
+
+    """
+
+    def decorator(fn):
+        def wrapper(*args, **kwargs):
+            # We need to create a global variable if it doesn't exist so we can keep track of executions outside of this decorator
+            global __NO_FLOOD_TIMEOUT
+            try:
+                # pylint: disable=E0601 (used-before-assignment)
+                __NO_FLOOD_TIMEOUT
+            except:
+                __NO_FLOOD_TIMEOUT = {}
+
+            if multiple_instances_diff_args:
+                fn_identificator = "{}-args.{}-kwargs.{}".format(
+                    fn.__name__, args, kwargs
+                )
+            else:
+                fn_identificator = fn.__name__
+
+            if fn_identificator in __NO_FLOOD_TIMEOUT:
+                if (
+                    datetime.utcnow() - __NO_FLOOD_TIMEOUT[fn_identificator]
+                ).total_seconds() < flood_timespan:
+                    return
+
+            # Add new execution timestamp
+            __NO_FLOOD_TIMEOUT[fn_identificator] = datetime.utcnow()
+            return fn(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
---
+++ packages/ofunctions.threading/ofunctions.threading-2.1.0.tar.gz/ofunctions.threading-2.1.0/tests/test_process.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python
+#  -*- coding: utf-8 -*-
+#
+# This file is part of ofunctions module
+
+"""
+Versioning semantics:
+    Major version: backward compatibility breaking changes
+    Minor version: New functionality
+    Patch version: Backwards compatible bug fixes
+"""
+
+__intname__ = "tests.ofunctions.process"
+__author__ = "Orsiris de Jong"
+__copyright__ = "Copyright (C) 2020-2021 Orsiris de Jong"
+__licence__ = "BSD 3 Clause"
+__build__ = "2021092201"
+
+import multiprocessing
+from datetime import datetime
+from time import sleep
+import subprocess
+
+from ofunctions.process import *
+
+
+def test_kill_childs():
+    """
+    We'll check if kill_childs successfully stops multiprocessing childs by checking the execution time
+    This test is time based so we don't need to use the child pid logic to test a child pid based function ;)
+    """
+    workers = 2
+    child_exec_time = 30
+
+    process_list = []
+
+    start_time = datetime.utcnow()
+
+    while workers > 0:
+        process = multiprocessing.Process(target=sleep, args=(child_exec_time,))
+        process.start()
+        process_list.append(process)
+        workers -= 1
+
+    running_workers = len(process_list)
+    print("Running {} workers".format(running_workers))
+
+    childs_still_run = True
+    while childs_still_run:
+        childs_still_run = False
+        for child in process_list:
+            if child.is_alive():
+                childs_still_run = True
+            # Now let's kill the childs if at least 3 seconds elapsed
+            if (datetime.utcnow() - start_time).total_seconds() >= 3:
+                kill_childs()
+            sleep(0.1)
+
+    stop_time = datetime.utcnow()
+
+    exec_time = stop_time - start_time
+    print("Executed for {} seconds".format(exec_time))
+    assert (
+        exec_time.total_seconds() < 10
+    ), "Execution should have been halted before workers got to finished their job"
+
+
+def test_get_processes_by_name():
+    """
+    Test that we can identify processes by name
+    """
+    import subprocess
+
+    if os.name == "nt":
+        proc_name = "ping.exe"
+        exe_path = get_absolute_path(proc_name)
+        command = "{} -n 4 127.0.0.1".format(exe_path)
+    else:
+        proc_name = "ping"
+        exe_path = get_absolute_path(proc_name)
+        command = ["{}".format(exe_path), "-c", "1", "127.0.0.1"]
+
+    # Don't create process with shell=True or else it will delay creation
+    # Create with subprocess.PIPE redir so we don't wait for execution
+    created_processes = []
+    for i in range(0, 2):
+        print("Running command ", command)
+        created_processes.append(
+            subprocess.Popen(
+                command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+            )
+        )
+
+    for process in created_processes:
+        print("Created process {} with pid {}".format(process, process.pid))
+
+    found_processes = get_processes_by_name(proc_name)
+
+    print(found_processes)
+
+    # Now let's compare, we should have found both processes by now
+    for found_process in found_processes:
+        found = False
+        for created_process in created_processes:
+            if found_process.pid == created_process.pid:
+                found = True
+        if not found:
+            assert False, "Did not find process"
+
+
+if __name__ == "__main__":
+    print("Example code for %s, %s" % (__intname__, __build__))
+    test_kill_childs()
+    test_get_processes_by_name()
