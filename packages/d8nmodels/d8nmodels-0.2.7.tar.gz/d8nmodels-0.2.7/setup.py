6834c86363bdfeb05276db666e9a872dd12445c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:02 +0000

    Add package d8nmodels/0.2.7/d8nmodels-0.2.7.tar.gz

---
+++ packages/d8nmodels/d8nmodels-0.2.7.tar.gz/d8nmodels-0.2.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='d8nmodels',
+    version='0.2.7',
+    author='Christo',
+    author_email='christochristov@duck.com',
+    description='d8n API models python library',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
