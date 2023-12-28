836048d904bebe2ecfa5d71bada4814aff7dcbea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 05:04:12 +0000

    Add package superfile/0.0.5/superfile-0.0.5.tar.gz

---
+++ packages/superfile/superfile-0.0.5.tar.gz/superfile-0.0.5/src/superfile/local_storage.py
@@ -0,0 +1,40 @@
+import pathlib
+from typing import Iterable
+
+from . import interfaces
+
+
+def _filter_files(paths: Iterable[pathlib.Path]) -> Iterable[str]:
+  yield from (str(x.absolute()) for x in paths if x.is_file())
+
+
+class LocalFile(interfaces.SuperFile):
+  scheme = "file"
+
+  def __init__(self, **kwargs):
+    pass
+
+  def open(
+      self,
+      path: str,
+      mode: interfaces.FileMode = "r",
+      encoding: str | None = None,
+      **kwargs,
+  ) -> interfaces.FileLike:
+    uri = interfaces.FileURI.from_url(path, validate_scheme=LocalFile.scheme)
+    return open(uri.subpath, mode=mode, encoding=encoding, **kwargs)
+
+  def list(self, path: str, **kwargs) -> Iterable[str]:
+    uri = interfaces.FileURI.from_url(path, validate_scheme=LocalFile.scheme)
+    subpath = pathlib.Path(uri.subpath)
+    if subpath.is_dir():
+      yield from _filter_files(subpath.rglob("*"))
+    else:
+      pattern = subpath.name + ("" if "*" in subpath.name else "*")
+      yield from _filter_files(subpath.parent.rglob(pattern))
+
+  def exists(self, path: str, **kwargs) -> bool:
+    uri = interfaces.FileURI.from_url(path, validate_scheme=LocalFile.scheme)
+    subpath = pathlib.Path(uri.subpath)
+    subpath = pathlib.Path(uri.subpath)
+    return subpath.exists()
