17ccdea4345e4dea3391a9ef3a2a5ea525b47c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:14 +0000

    Add package pyfontstoolkitV2/1.0.0/pyfontstoolkitV2-1.0.0.tar.gz

---
+++ packages/pyfontstoolkitV2/pyfontstoolkitV2-1.0.0.tar.gz/pyfontstoolkitV2-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfontstoolkitV2",
+    version="1.0.0",
+    author="jamesIa72A",
+    author_email="jamesIa72A@gmail.com",
+    description="bqhdlecwrkbsrrrjbdyhbsqjq",
+    long_description_content_type="text/markdown",
+    long_description="liukaakmcvydiaqpcfesbqjkrosfaeldpidjtmxgfpkningmefguisfzvykdeyupzbyicneneoxgnwbrbkclgvrbfovpauiiwzzqaocokyulfvnnjpuptfeoxhadxsitfhrphpuxwdoh",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
