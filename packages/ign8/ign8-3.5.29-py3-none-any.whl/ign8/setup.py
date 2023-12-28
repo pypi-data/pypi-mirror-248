ecc82817ee0d17664ed080e8e7cad393b808a2ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 15:04:12 +0000

    Add package ign8/3.5.29/ign8-3.5.29-py3-none-any.whl

---
+++ packages/ign8/ign8-3.5.29-py3-none-any.whl/ign8/setup.py
@@ -0,0 +1,38 @@
+import os
+import requests
+import datetime
+from .common import prettyllog
+from .netbox.netbox import check_netbox
+
+
+def main():
+    prettyllog("main", "check", "main", "all", "200", "Success")
+    return True
+
+def getenvironmentvalue(myenv):
+    myenv = os.getenv(myenv, "false")
+    return myenv
+
+def getenvironment():
+    environment = {}
+    environment["IGN8_NETBOX_URL"] = getenvironmentvalue("IGN8_NETBOX_URL")
+    environment["IGN8_NETBOX_TOKEN"] = getenvironmentvalue("IGN8_NETBOX_TOKEN")
+    environment["IGN8_NETBOX_VERIFY_SSL"] = getenvironmentvalue("IGN8_NETBOX_VERIFY_SSL")
+    if check_netbox():
+        prettyllog("getenvironment", "check", "getenvironment", "all", "200", "Success")
+        return environment
+    else:
+        prettyllog("getenvironment", "check", "getenvironment", "all", "500", "Failed", "ERROR")
+        return False
+
+
+def setupign8():
+    prettyllog("setupign8", "check", "setupign8", "all", "200", "Success")
+    myenv = getenvironment()
+    if myenv:
+        prettyllog("setupign8", "check", "setupign8", "all", "200", "Success")
+        return True
+    else:
+        prettyllog("setupign8", "check", "setupign8", "all", "500", "Failed", "ERROR")
+        return False    
+
