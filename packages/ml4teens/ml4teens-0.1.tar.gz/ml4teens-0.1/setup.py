cffc3890087967d55e09770946c2c17042e4697a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 04:05:18 +0000

    Add package ml4teens/0.1/ml4teens-0.1.tar.gz

---
+++ packages/ml4teens/ml4teens-0.1.tar.gz/ml4teens-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ml4teens',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[ "ipython", 
+                       "opencv-python", 
+                       "numpy",
+                       "request", 
+                       "ultralytics", 
+                       "Pillow", 
+                     ],
+    # metadata para publicar en PyPI
+    author='Francisco Puentes',
+    author_email='fran@puentescalvo.com',
+    description='Machine Learning para Adolescentes (o más) ...',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/FranPuentes/ML4Teens',
+)
