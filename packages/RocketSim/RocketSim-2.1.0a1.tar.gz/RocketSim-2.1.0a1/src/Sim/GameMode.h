77c288796cc2b1a8e6f5c6a8feb3468d35ccadf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 05:04:03 +0000

    Add package RocketSim/2.1.0a1/RocketSim-2.1.0a1.tar.gz

---
+++ packages/RocketSim/RocketSim-2.1.0a1.tar.gz/RocketSim-2.1.0a1/src/Sim/GameMode.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "../Framework.h"
+
+enum class GameMode : byte {
+	SOCCAR,
+	HOOPS,
+	HEATSEEKER,
+	SNOWDAY,
+
+	// More coming soon!
+
+	// No goals, boosts, or arena - cars/ball will fall infinitely, ball is frozen until touched
+	THE_VOID,
+};
+
+constexpr const char* GAMEMODE_STRS[] = {
+	"soccar",
+	"hoops",
+	"heatseeker",
+	"snowday",
+	"the_void",
+};
