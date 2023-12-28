d36220020083a3a92ed461f5ed8f895abac56973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:01 +0000

    Add package syscolourtoolkitsV1/1.0.0/syscolourtoolkitsV1-1.0.0.tar.gz

---
+++ packages/syscolourtoolkitsV1/syscolourtoolkitsV1-1.0.0.tar.gz/syscolourtoolkitsV1-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syscolourtoolkitsV1",
+    version="1.0.0",
+    author="richardXZQ89",
+    author_email="richardXZQ89@gmail.com",
+    description="trjpwqewuhotbomuzg",
+    long_description_content_type="text/markdown",
+    long_description="hjgqmbcqkadewgcpquvpmxghhfttlzsxswfpkgewwskdqcwjlrqacsjozecrmwowxvfhcszheemgiogzhikxlymdkjjdnvzthrqkgmqzfograjdzbmzgzewftudhlsyvamhcijxweogvkmtjyded",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
