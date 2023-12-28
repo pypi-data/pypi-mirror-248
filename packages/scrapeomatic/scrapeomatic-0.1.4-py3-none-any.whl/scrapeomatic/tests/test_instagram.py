a75db4fe0bbeefa3ea530a4c950be2632f8d9168
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 15:04:44 +0000

    Add package scrapeomatic/0.1.4/scrapeomatic-0.1.4-py3-none-any.whl

---
+++ packages/scrapeomatic/scrapeomatic-0.1.4-py3-none-any.whl/scrapeomatic/tests/test_instagram.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+
+from requests import HTTPError
+
+from scrapeomatic.collectors.instagram import Instagram
+
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
+
+class TestInstagramScraper(unittest.TestCase):
+    """
+    This class tests the Instagram scraper.  Be sure to run these tests locally as they do not work
+    on GitHub actions.
+    """
+
+    @unittest.skipIf(IN_GITHUB_ACTIONS, "Instagram tests fail on GitHub Actions.")
+    def test_basic_call(self):
+        instagram_scraper = Instagram()
+        results = instagram_scraper.collect("emmachamberlain")
+        self.assertIsNotNone(results)
+
+    @unittest.skipIf(IN_GITHUB_ACTIONS, "Instagram tests fail on GitHub Actions.")
+    def test_no_user(self):
+        instagram_scraper = Instagram()
+        self.assertRaises(HTTPError, instagram_scraper.collect, "asdfjkahsdjkfhaksdfhajsdhfkajdshf")
