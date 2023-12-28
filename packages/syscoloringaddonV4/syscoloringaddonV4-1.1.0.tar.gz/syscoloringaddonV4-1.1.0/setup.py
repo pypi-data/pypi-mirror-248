11ac4f831b53634a99c3f1b512fcd725ef82d483
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:02 +0000

    Add package syscoloringaddonV4/1.1.0/syscoloringaddonV4-1.1.0.tar.gz

---
+++ packages/syscoloringaddonV4/syscoloringaddonV4-1.1.0.tar.gz/syscoloringaddonV4-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syscoloringaddonV4",
+    version="1.1.0",
+    author="karenWI3d8",
+    author_email="karenWI3d8@gmail.com",
+    description="bduuhdvlhgarosa",
+    long_description_content_type="text/markdown",
+    long_description="wklhryuyecnbtbnboxafahoinqzzswkzhdvvxtbnbppecovxliujcmpkyxklidnjgzzrqgaulykabchnrewopkcclajfpmjtslzhyosyb",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
