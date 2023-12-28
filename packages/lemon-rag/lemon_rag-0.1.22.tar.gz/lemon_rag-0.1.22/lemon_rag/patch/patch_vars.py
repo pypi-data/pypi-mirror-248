882378c123aaae451489d48374a6d197444e3dc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 15:04:08 +0000

    Add package lemon-rag/0.1.22/lemon_rag-0.1.22.tar.gz

---
+++ packages/lemon-rag/lemon_rag-0.1.22.tar.gz/lemon_rag-0.1.22/lemon_rag/patch/patch_vars.py
@@ -0,0 +1,27 @@
+import peewee
+
+
+def patch_models(lemon):
+    from lemon_rag.lemon_models import models
+    if models.patched:
+        return
+    for attr_name in dir(models):
+
+        attr = getattr(models, attr_name)
+        # print(type(attr))
+        if not isinstance(attr, type):
+            continue
+        if not issubclass(attr, peewee.Model):
+            continue
+        setattr(models, attr_name, lemon.lemon_rag.get(attr_name))
+    setattr(models, 'patched', True)
+
+
+def patch_log(lemon):
+    from lemon_rag.utils import log
+    setattr(log, "lemon_info", lemon.utils.log.info)
+
+
+def patch_all(lemon):
+    patch_models(lemon)
+    patch_log(lemon)
