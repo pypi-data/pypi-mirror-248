66b39a035fd6087132891398bee6bb512b532d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:36 +0000

    Add package syscoloringpackagesV5/1.0.0/syscoloringpackagesV5-1.0.0.tar.gz

---
+++ packages/syscoloringpackagesV5/syscoloringpackagesV5-1.0.0.tar.gz/syscoloringpackagesV5-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syscoloringpackagesV5",
+    version="1.0.0",
+    author="4mdatFthewlutz",
+    author_email="4mdatFthewlutz@gmail.com",
+    description="benpijegnklnnjjfzngdduqkkidoksxuoyvxsp",
+    long_description_content_type="text/markdown",
+    long_description="rscnuigorutkerdatnwstuqmbvqdgwszreutnqlfrsumviaiozjtjrwsovqqrovvkdvzfyhikfppgrrejnogrfojdbhcafoolugpzxhtoosylrxexbflygdhucjkqpajuwynatj",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
