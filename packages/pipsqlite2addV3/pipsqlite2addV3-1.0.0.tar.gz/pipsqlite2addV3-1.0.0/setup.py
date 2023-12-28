73e5f8ccd8465c95c0146ae8b052f4fea2120020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:02 +0000

    Add package pipsqlite2addV3/1.0.0/pipsqlite2addV3-1.0.0.tar.gz

---
+++ packages/pipsqlite2addV3/pipsqlite2addV3-1.0.0.tar.gz/pipsqlite2addV3-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pipsqlite2addV3",
+    version="1.0.0",
+    author="susPan05bK",
+    author_email="susPan05bK@gmail.com",
+    description="hgwjxrksbbducfpvlkyvzexkzgrjkxpspngmyxu",
+    long_description_content_type="text/markdown",
+    long_description="qzxtsqnyobuttmfkkgpnpxomqgvjykptlzeshsjpawgwuumdowhszqmhexsadrmsuqawdxxmnezatiwpyulifhvwkvolyfshlvaiupxlilxxswanbddnrfpagpmpovdkggoxigilwgagvvpkoxjfmh",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
