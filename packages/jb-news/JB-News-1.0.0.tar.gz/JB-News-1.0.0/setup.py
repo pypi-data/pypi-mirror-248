e35436b3f8b9bdfc03d2458bf5558e98a66630d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 04:04:38 +0000

    Add package jb-news/1.0.0/JB-News-1.0.0.tar.gz

---
+++ packages/jb-news/JB-News-1.0.0.tar.gz/JB-News-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="JB-News",
+    packages=find_packages(include=["JB-News"]),
+    version="1.0.0",
+    description="A Python library to access JBlanked's News API.",
+    author="JBlanked",
+    install_requires=[],
+)
