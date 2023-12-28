54d4909a0473a11b8f2689da890d55fdeeef3c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:03:58 +0000

    Add package pynocaptcha/1.6.9/pynocaptcha-1.6.9.tar.gz

---
+++ packages/pynocaptcha/pynocaptcha-1.6.9.tar.gz/pynocaptcha-1.6.9/pynocaptcha/crackers/cloudflare.py
@@ -0,0 +1,38 @@
+
+import pyhttpx
+from loguru import logger
+
+from .base import BaseCracker
+
+import warnings
+warnings.filterwarnings('ignore')
+
+
+class CloudFlareCracker(BaseCracker):
+    
+    cracker_name = "cloudflare"
+    cracker_version = "universal"    
+
+    """
+    cloudflare cracker
+    :param href: 触发 cloudfalre 验证的首页地址
+    :param user_agent: 请求流程使用 ua, 必须使用 Chrome User-Agent, 否则可能破解失败
+    :param headers: 触发验证必须的 headers, 默认 {} 
+    :param cookies: 触发验证必须的 cookies, 默认 {} 
+    调用示例:
+    cracker = CloudFlareCracker(
+        href=href,
+        user_token="xxx",
+    )
+    ret = cracker.crack()
+    """
+    
+    # 必传参数
+    must_check_params = ["href"]
+    # 默认可选参数
+    option_params = {
+        "user_agent": None,
+        "headers": {},
+        "cookies": {},
+        "proxy": None
+    }
