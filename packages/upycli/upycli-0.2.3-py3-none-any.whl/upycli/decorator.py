ee7afec33f47bb6055f644424b579ac84b861439
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 14:04:14 +0000

    Add package upycli/0.2.3/upycli-0.2.3-py3-none-any.whl

---
+++ packages/upycli/upycli-0.2.3-py3-none-any.whl/upycli/decorator.py
@@ -0,0 +1,26 @@
+from sys import argv 
+from typing import Callable
+
+from .runner import run
+
+
+def command(func: Callable):
+    """
+    Command Decorator
+    -----------------
+    
+    Use by putting this on top of any function.
+        ```python
+        @command
+        def myfunc(x, y = 56):
+            ...
+        ```
+    """
+    
+    if "--upycli.debug" in argv:
+        print(f"Debug: Arguments `{argv}`")
+        
+    if func.__name__ in argv:
+        run(func)
+        exit(0)
+    
