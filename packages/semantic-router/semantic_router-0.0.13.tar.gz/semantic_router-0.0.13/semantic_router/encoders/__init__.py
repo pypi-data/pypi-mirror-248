85db2dcb66b2ca9a014aea99e8d6916c6d658bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 16:03:51 +0000

    Add package semantic-router/0.0.13/semantic_router-0.0.13.tar.gz

---
+++ packages/semantic-router/semantic_router-0.0.13.tar.gz/semantic_router-0.0.13/semantic_router/encoders/__init__.py
@@ -0,0 +1,6 @@
+from semantic_router.encoders.base import BaseEncoder
+from semantic_router.encoders.bm25 import BM25Encoder
+from semantic_router.encoders.cohere import CohereEncoder
+from semantic_router.encoders.openai import OpenAIEncoder
+
+__all__ = ["BaseEncoder", "CohereEncoder", "OpenAIEncoder", "BM25Encoder"]
