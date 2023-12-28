f8ac8a4342d9bf2b150fd5ece57d6e0278015adc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 05:04:06 +0000

    Add package nomad-media-pip/0.0.13/nomad_media_pip-0.0.13-py3-none-any.whl

---
+++ packages/nomad-media-pip/nomad_media_pip-0.0.13-py3-none-any.whl/nomad_media_pip/config/config.py
@@ -0,0 +1,8 @@
+CONFIG = {
+    "username": "sfaludi@nomad-cms.com",
+    "password": "NewJ0bN0madPassword!!",
+    "serviceApiUrl": "https://admin-app.dev-05.demos.media/api",
+    #"serviceApiUrl": "https://app-api.dev-05.demos.media/api",
+    "apiType": "admin",
+    "debugMode": True
+}
