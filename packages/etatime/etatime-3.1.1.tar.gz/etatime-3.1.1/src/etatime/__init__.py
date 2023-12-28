77c4466b6a32608b3f26d883d33c81bd26d4e605
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:06:27 +0000

    Add package etatime/3.1.1/etatime-3.1.1.tar.gz

---
+++ packages/etatime/etatime-3.1.1.tar.gz/etatime-3.1.1/src/etatime/__init__.py
@@ -0,0 +1,17 @@
+"""A library for tracking, computing, and formatting time estimates. (Based on `tqdm`)."""
+
+__version__ = "3.1.1"
+
+from onecondition import ValidationError
+
+from .eta import EtaBar
+from .time import TimeString
+
+
+
+# TODO: Add annotations everywhere
+# TODO: Finish adding docstrings
+# TODO: Add doctest strings
+# TODO: Add comprehensive unit tests
+# TODO: Integration tests
+
