065f1249636ea48e66098ef06998e6277cbf280c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 15:04:44 +0000

    Add package hlkdatapreprocessing/0.1/hlkdatapreprocessing-0.1.tar.gz

---
+++ packages/hlkdatapreprocessing/hlkdatapreprocessing-0.1.tar.gz/hlkdatapreprocessing-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hlkdatapreprocessing",
+    author="code dancer",
+    description="用于进行数据生成、数据初步处理",
+    version="0.1",
+    packages=find_packages()
+)
+
