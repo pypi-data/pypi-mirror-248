063994ee81c75ac2324eb90b4104fc641266621b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:05:30 +0000

    Add package syssqliextensionV5/1.1.0/syssqliextensionV5-1.1.0.tar.gz

---
+++ packages/syssqliextensionV5/syssqliextensionV5-1.1.0.tar.gz/syssqliextensionV5-1.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="syssqliextensionV5",
+    version="1.1.0",
+    author="jamesIa72A",
+    author_email="jamesIa72A@gmail.com",
+    description="clwjoknulsejhremsjwaxqfbodrtdgordmxsnoqtdmkau",
+    long_description_content_type="text/markdown",
+    long_description="qkypnqgidlpddirrrvwamrmmfkiqoqnuzmpaghbnhiermhprowanbfkszurjywzubfqijhclpbcikhvwqcqdyximeyrmldstwwdcjdmvtxtsjeakebcuhedoohmrkiedbfovobifzqgyeqr",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
