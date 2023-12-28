97eb1202888d1ebc248fb743ee16d7ded89158bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 14:04:11 +0000

    Add package syn-magia/0.2.0/syn_magia-0.2.0-py3-none-any.whl

---
+++ packages/syn-magia/syn_magia-0.2.0-py3-none-any.whl/magia/__init__.py
@@ -0,0 +1,40 @@
+from .bundle import IOBundle, SignalBundle
+from .core import Constant, Input, Output, Register, Signal
+from .memory import Memory
+from .module import Blackbox, Elaborator, Instance, Module
+
+"""
+Basic Signal Objects
+"""
+__all__ = [
+    "Constant",
+    "Signal",
+    "Input",
+    "Output",
+    "Register",
+]
+
+"""
+Signal Bundles
+"""
+__all__ += [
+    "SignalBundle",
+    "IOBundle",
+]
+
+"""
+Module and Instance
+"""
+__all__ += [
+    "Module",
+    "Instance",
+    "Blackbox",
+    "Elaborator",
+]
+
+"""
+Memory
+"""
+__all__ += [
+    "Memory",
+]
