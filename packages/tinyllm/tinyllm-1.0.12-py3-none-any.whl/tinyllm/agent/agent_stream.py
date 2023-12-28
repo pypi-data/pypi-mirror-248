8709537714c5e475f88239e1679189bd953c02ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 04:05:21 +0000

    Add package tinyllm/1.0.12/tinyllm-1.0.12-py3-none-any.whl

---
+++ packages/tinyllm/tinyllm-1.0.12-py3-none-any.whl/tinyllm/agent/agent_stream.py
@@ -0,0 +1,102 @@
+import json
+from typing import Optional
+
+from smartpy.utility.log_util import getLogger
+from tinyllm.agent.agent import AgentInitValidator, AgentInputValidator
+
+from tinyllm.agent.toolkit import Toolkit
+from tinyllm.examples.example_manager import ExampleManager
+from tinyllm.function_stream import FunctionStream
+from tinyllm.llms.lite_llm import LLM_TOKEN_LIMITS, DEFAULT_LLM_MODEL, DEFAULT_CONTEXT_FALLBACK_DICT
+from tinyllm.memory.memory import BufferMemory, Memory
+from tinyllm.prompt_manager import PromptManager
+from tinyllm.tracing.langfuse_context import observation
+from tinyllm.util.helpers import get_openai_message, count_tokens
+
+logger = getLogger(__name__)
+
+
+class AgentStream(FunctionStream):
+
+    def __init__(self,
+                 system_role: str,
+                 llm: FunctionStream,
+                 memory: Memory = BufferMemory(),
+                 toolkit: Optional[Toolkit] = None,
+                 example_manager: Optional[ExampleManager] = ExampleManager(),
+                 answer_formatting_prompt: Optional[str] = None,
+                 **kwargs):
+        AgentInitValidator(system_role=system_role,
+                           llm=llm,
+                           toolkit=toolkit,
+                           memory=memory,
+                           example_manager=example_manager,
+                           answer_formatting_prompt=answer_formatting_prompt)
+        super().__init__(
+            input_validator=AgentInputValidator,
+            **kwargs)
+        self.system_role = system_role
+        self.llm = llm
+        self.toolkit = toolkit
+        self.example_manager = example_manager
+        self.prompt_manager = PromptManager(
+            system_role=system_role,
+            example_manager=example_manager,
+            memory=memory,
+            answer_formatting_prompt=answer_formatting_prompt,
+        )
+
+    async def run(self,
+                  user_input,
+                  **kwargs):
+
+        input_msg = get_openai_message(role='user',
+                                       content=user_input)
+
+        while True:
+            kwargs = await self.prompt_manager.format(message=input_msg,
+                                                          **kwargs)
+
+            async for msg in self.llm(tools=self.toolkit.as_dict_list() if self.toolkit else None,
+                                      **kwargs):
+                yield msg
+
+            await self.prompt_manager.add_memory(message=input_msg)
+
+            # Agent decides to call a tool
+            if msg['status'] == 'success':
+                msg_output = msg['output']
+                if msg_output['type'] == 'tool':
+                    # TODO When ready, implement parallel function calls
+                    api_tool_call = msg_output['delta']['tool_calls'][0]
+                    msg_output['delta'].pop('function_call')
+
+                    # Memorize tool call with arguments
+                    json_tool_call = {
+                        'name': msg_output['completion']['name'],
+                        'arguments': msg_output['completion']['arguments']
+                    }
+                    api_tool_call['function'] = json_tool_call
+                    msg_output['delta']['content'] = ''
+                    await self.prompt_manager.add_memory(message=msg_output['delta'])
+
+                    # Memorize tool result
+                    tool_results = await self.toolkit(
+                        tool_calls=[{
+                            'name': api_tool_call['function']['name'],
+                            'arguments': json.loads(api_tool_call['function']['arguments'])
+                        }],
+                        trace=self.parent_observation)
+                    tool_result = tool_results['output']['tool_results'][0]
+                    tool_call_result_msg = get_openai_message(
+                        name=tool_result['name'],
+                        role='tool',
+                        content=tool_result['content'],
+                        tool_call_id=api_tool_call['id']
+                    )
+                    input_msg = tool_call_result_msg
+
+                elif msg_output['type'] == 'assistant':
+                    break
+            else:
+                raise (Exception(msg))
---
+++ packages/tinyllm/tinyllm-1.0.12-py3-none-any.whl/tinyllm/prompt_manager.py
@@ -0,0 +1,83 @@
+from tinyllm.examples.example_manager import ExampleManager
+from tinyllm.llms.lite_llm import DEFAULT_LLM_MODEL, LLM_TOKEN_LIMITS, DEFAULT_CONTEXT_FALLBACK_DICT
+from tinyllm.memory.memory import Memory
+from tinyllm.util.helpers import get_openai_message, count_tokens, OPENAI_MODELS_CONTEXT_SIZES
+import datetime as dt
+
+
+class PromptManager:
+    """
+    This class is responsible for formatting the prompt for the LLM and managing:
+    - model (to avoid exceeding the token limit)
+    - max_tokens (based on the expected completion size)
+    """
+
+    def __init__(self,
+                 system_role: str,
+                 example_manager: ExampleManager,
+                 memory: Memory,
+                 answer_formatting_prompt: str = None,
+                 add_current_time: bool = False, ):
+        self.system_role = system_role
+        self.example_manager = example_manager
+        self.memory = memory
+        self.answer_formatting_prompt = answer_formatting_prompt
+        self.add_current_time = add_current_time
+
+    async def format_messages(self, message):
+        system_role = get_openai_message(role='system',
+                                         content=self.system_role)
+        memories = [] if self.memory is None else await self.memory.get_memories()
+        examples = []
+        examples += self.example_manager.constant_examples
+        if self.example_manager.example_selector.example_dicts and message['role'] == 'user':
+            best_examples = await self.example_manager.example_selector(input=message['content'])
+            for good_example in best_examples['output']['best_examples']:
+                usr_msg = get_openai_message(role='user', content=good_example['user'])
+                assistant_msg = get_openai_message(role='assistant', content=good_example['assistant'])
+                examples.append(usr_msg)
+                examples.append(assistant_msg)
+
+        answer_format_msg = [get_openai_message(role='user',
+                                                content=self.answer_formatting_prompt)] if self.answer_formatting_prompt is not None else []
+        if self.add_current_time:
+            system_role = f'{self.system_role} \nThe current time is:{str(dt.datetime.utcnow())}'
+        messages = [system_role] + memories + examples + answer_format_msg + [message]
+        return messages
+
+
+    async def format(self,
+                     message,
+                     **kwargs):
+
+        messages = await self.format_messages(message)
+        max_tokens, model = self.get_run_config(model=kwargs.get('model', DEFAULT_LLM_MODEL),
+                                                prompt_to_completion_multiplier=kwargs.get(
+                                                    'prompt_to_completion_multiplier', 1),
+                                                input_size=count_tokens(messages))
+        kwargs['messages'] = messages
+        kwargs['max_tokens'] = max_tokens
+        kwargs['model'] = model
+        return kwargs
+
+    async def add_memory(self,
+                         message):
+        if self.memory is not None:
+            await self.memory(message=message)
+
+    @property
+
+    async def size(self):
+        messages = await self.format(message=get_openai_message(role='user', content=''))
+        return count_tokens(messages)
+
+
+    def get_run_config(self, input_size, prompt_to_completion_multiplier, model):
+        model_token_limit = LLM_TOKEN_LIMITS[model]
+        context_size_available = model_token_limit - input_size
+
+        max_tokens = max(500, input_size * prompt_to_completion_multiplier)
+        expected_total_size = input_size + max_tokens
+        if expected_total_size > context_size_available:
+            model = DEFAULT_CONTEXT_FALLBACK_DICT[model]
+        return max_tokens, model
