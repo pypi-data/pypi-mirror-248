10491c99e4b27a673ed0bf94f85cb9047a5be37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:58 +0000

    Add package fastapi-jwk-auth/0.0.2b0/fastapi-jwk-auth-0.0.2b0.tar.gz

---
+++ packages/fastapi-jwk-auth/fastapi-jwk-auth-0.0.2b0.tar.gz/fastapi-jwk-auth-0.0.2b0/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fastapi-jwk-auth",
+    version="0.0.2beta",
+    packages=find_packages(),
+    install_requires=["fastapi", "PyJWT", "cryptography"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Source": "https://github.com/omkardarves/fastapi-jwk-auth",
+    },
+    license="MIT",
+)
