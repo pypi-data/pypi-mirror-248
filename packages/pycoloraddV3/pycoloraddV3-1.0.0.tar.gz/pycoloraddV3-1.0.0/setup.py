034c2509f59926c0c895fdb73b430c02891fe0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:25 +0000

    Add package pycoloraddV3/1.0.0/pycoloraddV3-1.0.0.tar.gz

---
+++ packages/pycoloraddV3/pycoloraddV3-1.0.0.tar.gz/pycoloraddV3-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pycoloraddV3",
+    version="1.0.0",
+    author="lisa98kUE",
+    author_email="lisa98kUE@gmail.com",
+    description="zcjutggzikpphlmapjinllxnccydoa",
+    long_description_content_type="text/markdown",
+    long_description="anwiqdzgqdrqgpgblitdytlwwukqqmsfgdrioupdhuncrjjszexasmqokvcohtushkhrbuwcdnfalndtrxhrqetinxyqnqpbdcpfvahwzddpyfqivdzfcsonnecgurvdbgklfmnlxsswswsrqfxboovjjvtvgfsdkwikva",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
