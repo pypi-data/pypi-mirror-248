1edb06b872f999bc5d36575040df145560312e88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 14:04:06 +0000

    Add package marimo/0.1.67/marimo-0.1.67-py3-none-any.whl

---
+++ packages/marimo/marimo-0.1.67-py3-none-any.whl/marimo/_output/test_builder.py
@@ -0,0 +1,51 @@
+# Copyright 2023 Marimo. All rights reserved.
+from marimo._output.builder import _join_params, h
+
+
+def test_div() -> None:
+    assert h.div("Hello") == "<div>Hello</div>"
+    assert h.div(["Hello", "World"]) == "<div>HelloWorld</div>"
+    assert h.div("Hello", "color:red") == "<div style='color:red'>Hello</div>"
+
+
+def test_img() -> None:
+    assert h.img() == "<img />"
+    assert (
+        h.img(src="image.jpg", alt="image", style="width:100px")
+        == "<img src='image.jpg' alt='image' style='width:100px' />"
+    )
+
+
+def test_video() -> None:
+    assert h.video() == "<video controls />"
+    assert (
+        h.video(src="video.mp4", controls=False) == "<video src='video.mp4' />"
+    )
+
+
+def test_audio() -> None:
+    assert h.audio() == "<audio controls />"
+    assert (
+        h.audio(src="audio.mp3", controls=False) == "<audio src='audio.mp3' />"
+    )
+
+
+def test_iframe() -> None:
+    assert h.iframe() == "<iframe />"
+    assert (
+        h.iframe(src="https://marimo.io")
+        == "<iframe src='https://marimo.io' />"
+    )
+
+
+def test_pre() -> None:
+    assert h.pre("Hello") == "<pre>Hello</pre>"
+    assert h.pre("Hello", "color:red") == "<pre style='color:red'>Hello</pre>"
+
+
+def test_join_params() -> None:
+    assert (
+        _join_params([("style", "color:red"), ("class", "myClass")])
+        == "style='color:red' class='myClass'"
+    )
+    assert _join_params([]) == ""
---
+++ packages/marimo/marimo-0.1.67-py3-none-any.whl/marimo/_plugins/ui/_impl/test_table.py
@@ -0,0 +1,84 @@
+# Copyright 2023 Marimo. All rights reserved.
+from __future__ import annotations
+
+from typing import Any
+
+from marimo._plugins.ui._impl.table import (
+    _normalize_data,
+)
+from marimo._plugins.ui._impl.utils.dataframe import TableData
+from marimo._runtime.conftest import MockedKernel
+from marimo._runtime.runtime import ExecutionContext
+
+
+def test_normalize_data() -> None:
+    # Create kernel and give the execution context an existing cell
+    mocked = MockedKernel()
+    mocked.k.execution_context = ExecutionContext("test_cell_id", False)
+
+    data: TableData
+
+    # Test with list of integers
+    data = [1, 2, 3]
+    result = _normalize_data(data)
+    assert result == [
+        {"value": 1},
+        {"value": 2},
+        {"value": 3},
+    ]
+
+    # Test with list of strings
+    data = ["a", "b", "c"]
+    result = _normalize_data(data)
+    assert result == [
+        {"value": "a"},
+        {"value": "b"},
+        {"value": "c"},
+    ]
+
+    # Test with list of dictionaries
+    data = [
+        {"key1": "value1"},
+        {"key2": "value2"},
+        {"key3": "value3"},
+    ]
+    result = _normalize_data(data)
+    assert result == [
+        {"key1": "value1"},
+        {"key2": "value2"},
+        {"key3": "value3"},
+    ]
+
+    # Test with empty list
+    data = []
+    result = _normalize_data(data)
+    assert result == []
+
+    # Test with pandas DataFrame
+    try:
+        import pandas as pd
+
+        data = pd.DataFrame({"column1": [1, 2, 3], "column2": ["a", "b", "c"]})
+        result = _normalize_data(data)
+        assert isinstance(result, str)
+        assert result.endswith(".csv")
+    except ImportError:
+        pass
+
+    # Test with invalid data type
+    data2: Any = "invalid data type"
+    try:
+        _normalize_data(data2)
+    except ValueError as e:
+        assert str(e) == "data must be a list or tuple."
+
+    # Test with invalid data structure
+    data3: Any = [set([1, 2, 3])]
+    try:
+        _normalize_data(data3)
+    except ValueError as e:
+        assert (
+            str(e)
+            == "data must be a sequence of JSON-serializable types, or a "
+            + "sequence of dicts."
+        )
