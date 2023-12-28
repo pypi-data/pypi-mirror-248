2ca4cdd3bc91eb0b8f11c62d86d1023106a89c1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 03:04:20 +0000

    Add package flickr-photos-api/1.6.0/flickr-photos-api-1.6.0.tar.gz

---
+++ packages/flickr-photos-api/flickr-photos-api-1.6.0.tar.gz/flickr-photos-api-1.6.0/src/flickr_photos_api/__init__.py
@@ -0,0 +1,54 @@
+from .api import FlickrPhotosApi
+from .exceptions import (
+    FlickrApiException,
+    InvalidApiKey,
+    ResourceNotFound,
+    LicenseNotFound,
+)
+from .types import (
+    License,
+    User,
+    TakenGranularity,
+    DateTaken,
+    LocationInfo,
+    Size,
+    SafetyLevel,
+    SinglePhoto,
+    CollectionOfPhotos,
+    PhotosFromUrl,
+    PhotosInAlbum,
+    PhotosInGallery,
+    PhotosInGroup,
+    AlbumInfo,
+    GalleryInfo,
+    GroupInfo,
+)
+
+
+__version__ = "1.6.0"
+
+
+__all__ = [
+    "FlickrPhotosApi",
+    "FlickrApiException",
+    "ResourceNotFound",
+    "InvalidApiKey",
+    "LicenseNotFound",
+    "License",
+    "LocationInfo",
+    "User",
+    "TakenGranularity",
+    "DateTaken",
+    "Size",
+    "SafetyLevel",
+    "SinglePhoto",
+    "CollectionOfPhotos",
+    "PhotosFromUrl",
+    "PhotosInAlbum",
+    "PhotosInGallery",
+    "PhotosInGroup",
+    "AlbumInfo",
+    "GalleryInfo",
+    "GroupInfo",
+    "__version__",
+]
