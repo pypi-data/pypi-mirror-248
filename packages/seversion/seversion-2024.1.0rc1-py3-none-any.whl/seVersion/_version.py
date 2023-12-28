7fb36611f22bddb35573c40f2458d5b0213993da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 15:04:43 +0000

    Add package seversion/2024.1.0rc1/seversion-2024.1.0rc1-py3-none-any.whl

---
+++ packages/seversion/seversion-2024.1.0rc1-py3-none-any.whl/seVersion/_version.py
@@ -0,0 +1,10 @@
+"""
+Provides `seVersion` version information.
+"""
+# This file is auto-generated! Do not edit!
+# Use `seVersion seVersion` to change this file.
+
+from seVersion import Version
+__version__ = Version("seVersion", 2024, 1, 0, 15, release_candidate=1)
+__setuptools__ = __version__.public()
+__all__ = ["__version__"]
