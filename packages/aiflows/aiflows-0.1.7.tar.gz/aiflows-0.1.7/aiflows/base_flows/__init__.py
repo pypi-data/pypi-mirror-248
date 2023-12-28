1d2afbe17bfe43c8adade2b215fea58d83600628
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-28 04:05:14 +0000

    Add package aiflows/0.1.7/aiflows-0.1.7.tar.gz

---
+++ packages/aiflows/aiflows-0.1.7.tar.gz/aiflows-0.1.7/aiflows/base_flows/__init__.py
@@ -0,0 +1,13 @@
+"""
+Contains basic flow classes that can be used to build more complex flows.
+
+AtomicFlow is the minimal execution unit in the Flow framework.
+CompositeFlow is a flow that contains subflows and defines how they are executed.
+"""
+
+from .abstract import Flow
+from .atomic import AtomicFlow
+from .composite import CompositeFlow
+from .circular import CircularFlow
+from .sequential import SequentialFlow
+from .branching import BranchingFlow
---
+++ packages/aiflows/aiflows-0.1.7.tar.gz/aiflows-0.1.7/aiflows/flow_launchers/__init__.py
@@ -0,0 +1,2 @@
+from .abstract import BaseLauncher, MultiThreadedAPILauncher
+from .flow_API_launcher import FlowLauncher
