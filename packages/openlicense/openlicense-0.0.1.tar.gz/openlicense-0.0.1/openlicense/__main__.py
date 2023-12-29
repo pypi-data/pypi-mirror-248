f172b68b9710e41ca7870a2a588b6aba04c241f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:04:27 +0000

    Add package openlicense/0.0.1/openlicense-0.0.1.tar.gz

---
+++ packages/openlicense/openlicense-0.0.1.tar.gz/openlicense-0.0.1/openlicense/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for openlicense."""
+
+from openlicense.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
---
+++ packages/openlicense/openlicense-0.0.1.tar.gz/openlicense-0.0.1/tests/test_base.py
@@ -0,0 +1,5 @@
+from openlicense.base import NAME
+
+
+def test_base():
+    assert NAME == "openlicense"
