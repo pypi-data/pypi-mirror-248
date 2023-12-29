bac89b28c7abfe2f4b62db7ac44e369b21738ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:04:00 +0000

    Add package procrastinate/0.33.0/procrastinate-0.33.0-py3-none-any.whl

---
+++ packages/procrastinate/procrastinate-0.33.0-py3-none-any.whl/procrastinate/__init__.py
@@ -0,0 +1,29 @@
+from procrastinate import metadata as _metadata_module
+from procrastinate.aiopg_connector import AiopgConnector
+from procrastinate.app import App
+from procrastinate.blueprints import Blueprint
+from procrastinate.connector import BaseConnector
+from procrastinate.job_context import JobContext
+from procrastinate.psycopg2_connector import Psycopg2Connector
+from procrastinate.psycopg_connector import PsycopgConnector
+from procrastinate.retry import BaseRetryStrategy, RetryStrategy
+
+__all__ = [
+    "App",
+    "Blueprint",
+    "JobContext",
+    "BaseConnector",
+    "BaseRetryStrategy",
+    "AiopgConnector",
+    "Psycopg2Connector",
+    "PsycopgConnector",
+    "RetryStrategy",
+]
+
+
+_metadata = _metadata_module.extract_metadata()
+__author__ = _metadata["author"]
+__author_email__ = _metadata["email"]
+__license__ = _metadata["license"]
+__url__ = _metadata["url"]
+__version__ = _metadata["version"]
