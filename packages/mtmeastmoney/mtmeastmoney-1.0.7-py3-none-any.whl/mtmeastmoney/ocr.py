89cfa13fdb9618472bd1d28f4ee2b5a7d0a84150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:05:59 +0000

    Add package mtmeastmoney/1.0.7/mtmeastmoney-1.0.7-py3-none-any.whl

---
+++ packages/mtmeastmoney/mtmeastmoney-1.0.7-py3-none-any.whl/mtmeastmoney/ocr.py
@@ -0,0 +1,15 @@
+import ddddocr
+import requests
+import time
+
+def ocr_code(vertify_code_url):
+    for _ in range(5):
+        ocr = ddddocr.DdddOcr(show_ad=False)
+        rsp = requests.get(vertify_code_url)
+        res = ocr.classification(rsp.content)
+        verify_code = res.replace("z", "2").replace("o", "0")
+        if len(verify_code) == 4:
+            return verify_code
+        else:
+            time.sleep(1)
+    raise Exception("无法识别出正确的验证码")
