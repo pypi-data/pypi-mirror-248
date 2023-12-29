004aba0ef8cb359871c35318575e2c5ea1b8d035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:04:14 +0000

    Add package dataframe-api-compat/0.2.6/dataframe_api_compat-0.2.6-py3-none-any.whl

---
+++ packages/dataframe-api-compat/dataframe_api_compat-0.2.6-py3-none-any.whl/dataframe_api_compat/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import contextlib
+
+with contextlib.suppress(ModuleNotFoundError):
+    from dataframe_api_compat import pandas_standard
+
+with contextlib.suppress(ModuleNotFoundError):
+    from dataframe_api_compat import polars_standard
+
+__all__ = ["pandas_standard", "polars_standard"]
+
+__version__ = "0.2.6"
