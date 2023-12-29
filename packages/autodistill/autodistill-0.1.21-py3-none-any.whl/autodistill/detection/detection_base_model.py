66b6efa1113a638c3f3616c21e25d2cfed7e68b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:05:36 +0000

    Add package autodistill/0.1.21/autodistill-0.1.21-py3-none-any.whl

---
+++ packages/autodistill/autodistill-0.1.21-py3-none-any.whl/autodistill/detection/detection_base_model.py
@@ -0,0 +1,95 @@
+import datetime
+import glob
+import os
+from abc import abstractmethod
+from dataclasses import dataclass
+
+import cv2
+import roboflow
+import supervision as sv
+from tqdm import tqdm
+
+from autodistill.core import BaseModel
+from autodistill.helpers import load_image, split_data
+
+from .detection_ontology import DetectionOntology
+
+
+@dataclass
+class DetectionBaseModel(BaseModel):
+    ontology: DetectionOntology
+
+    @abstractmethod
+    def predict(self, input: str) -> sv.Detections:
+        pass
+
+    def sahi_predict(self, input: str) -> sv.Detections:
+        slicer = sv.InferenceSlicer(callback=self.predict)
+
+        return slicer(load_image(input, return_format="cv2"))
+
+    def label(
+        self,
+        input_folder: str,
+        extension: str = ".jpg",
+        output_folder: str = None,
+        human_in_the_loop: bool = False,
+        roboflow_project: str = None,
+        roboflow_tags: str = ["autodistill"],
+        sahi: bool = False,
+    ) -> sv.DetectionDataset:
+        """
+        Label a dataset with the model.
+        """
+        if output_folder is None:
+            output_folder = input_folder + "_labeled"
+
+        os.makedirs(output_folder, exist_ok=True)
+
+        images_map = {}
+        detections_map = {}
+
+        if sahi:
+            slicer = sv.InferenceSlicer(callback=self.predict)
+
+        files = glob.glob(input_folder + "/*" + extension)
+        progress_bar = tqdm(files, desc="Labeling images")
+        # iterate through images in input_folder
+        for f_path in progress_bar:
+            progress_bar.set_description(desc=f"Labeling {f_path}", refresh=True)
+            image = cv2.imread(f_path)
+
+            f_path_short = os.path.basename(f_path)
+            images_map[f_path_short] = image.copy()
+
+            if sahi:
+                detections = slicer(f_path)
+            else:
+                detections = self.predict(f_path)
+
+            detections_map[f_path_short] = detections
+
+        dataset = sv.DetectionDataset(
+            self.ontology.classes(), images_map, detections_map
+        )
+
+        dataset.as_yolo(
+            output_folder + "/images",
+            output_folder + "/annotations",
+            min_image_area_percentage=0.01,
+            data_yaml_path=output_folder + "/data.yaml",
+        )
+
+        split_data(output_folder)
+
+        if human_in_the_loop:
+            roboflow.login()
+
+            rf = roboflow.Roboflow()
+
+            workspace = rf.workspace()
+
+            workspace.upload_dataset(output_folder, project_name=roboflow_project)
+
+        print("Labeled dataset created - ready for distillation.")
+        return dataset
