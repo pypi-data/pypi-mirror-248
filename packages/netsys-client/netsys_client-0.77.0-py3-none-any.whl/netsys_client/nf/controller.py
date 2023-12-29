0e1e9ca38d3a5cad9fe04a8da7761288c77aa1a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:25 +0000

    Add package netsys-client/0.77.0/netsys_client-0.77.0-py3-none-any.whl

---
+++ packages/netsys-client/netsys_client-0.77.0-py3-none-any.whl/netsys_client/nf/controller.py
@@ -0,0 +1,24 @@
+from . import NFMethods
+from mysql.connector import Error
+
+class NFController(NFMethods):
+    
+    def __init__(self, data, token):
+
+        self._data = data
+        self._file_path = self.map_nf_path()
+        self._token = token
+
+    def execute(self):
+
+        self._ini = self.generate_ini(self._data)
+        self.write_ini(f'{self._file_path}/NFe.ini', self._ini)
+        txt = f'NFe.CriarEnviarNFe("{self._file_path}/NFe.ini",1,1, , ,1)'
+        self.write_txt(f'{self._file_path}\Entrada\ENT.txt', txt)
+        txt = self.read_txt(f'{self._file_path}\Saida\ENT-resp.txt')
+        try:
+            self.send_to_db(txt, self._token)
+        except Error as e:
+            self.write_txt(f'{self._file_path}\log.txt', f'{e}')
+        self.write_txt(f'{self._file_path}\Saida\ENT-resp.txt', '')
+
