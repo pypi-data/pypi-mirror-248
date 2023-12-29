3bcbcc86981d0abd38817817a1826a5d35a149d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:04:45 +0000

    Add package schd/0.0.8/schd-0.0.8-py3-none-any.whl

---
+++ packages/schd/schd-0.0.8-py3-none-any.whl/schd/cmds/run.py
@@ -0,0 +1,27 @@
+import logging
+import yaml
+from schd.cmds.base import CommandBase
+from schd.scheduler import build_job, read_config
+
+
+def run_job(config_file, job_name):
+    config = read_config(config_file)
+
+    job_config = config['jobs'][job_name]
+
+    job_class_name = job_config.pop('class')
+    job_cron = job_config.pop('cron')
+    job = build_job(job_name, job_class_name, job_config)
+    job()
+
+
+class RunCommand(CommandBase):
+    def add_arguments(self, parser):
+        parser.add_argument('job')
+        parser.add_argument('--config', '-c')
+
+    def run(self, args):
+        logging.basicConfig(level=logging.INFO)
+        job_name = args.job
+        config_file = args.config
+        run_job(config_file, job_name)
