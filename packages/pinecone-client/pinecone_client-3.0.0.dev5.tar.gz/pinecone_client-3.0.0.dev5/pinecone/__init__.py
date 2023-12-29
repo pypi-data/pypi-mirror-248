eb8e0fbd5564cf54d012c3bf7bca609b939ba02b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:20 +0000

    Add package pinecone-client/3.0.0.dev5/pinecone_client-3.0.0.dev5.tar.gz

---
+++ packages/pinecone-client/pinecone_client-3.0.0.dev5.tar.gz/pinecone_client-3.0.0.dev5/pinecone/__init__.py
@@ -0,0 +1,11 @@
+"""
+.. include:: ../README.md
+"""
+from .config import *
+from .exceptions import *
+from .control.pinecone import Pinecone
+from .data.index import *
+from .models import *
+from .core.client.models import (
+    IndexModel,
+)
