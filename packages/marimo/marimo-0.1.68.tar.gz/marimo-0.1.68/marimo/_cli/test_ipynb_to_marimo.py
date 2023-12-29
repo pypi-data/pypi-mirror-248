a4a0eacae0648c1cfde8de9c3475044b304585eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 05:05:15 +0000

    Add package marimo/0.1.68/marimo-0.1.68.tar.gz

---
+++ packages/marimo/marimo-0.1.68.tar.gz/marimo-0.1.68/marimo/_cli/test_ipynb_to_marimo.py
@@ -0,0 +1,99 @@
+# Copyright 2023 Marimo. All rights reserved.
+from __future__ import annotations
+
+import os
+import tempfile
+import textwrap
+from collections.abc import Sequence
+
+from marimo._ast import codegen
+from marimo._cli.ipynb_to_marimo import convert_from_path
+
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
+
+
+def get_codes(ipynb_name: str) -> tuple[Sequence[str], Sequence[str]]:
+    contents = convert_from_path(
+        DIR_PATH + f"/../_test_utils/ipynb_data/{ipynb_name}.ipynb.txt"
+    )
+
+    tempfile_name = ""
+    try:
+        # in windows, can't re-open an open named temporary file, hence
+        # delete=False and manual clean up
+        with tempfile.NamedTemporaryFile(
+            mode="w", suffix=".py", delete=False
+        ) as f:
+            tempfile_name = f.name
+            f.write(contents)
+            f.seek(0)
+        app = codegen.get_app(f.name)
+        assert app is not None
+        return list(app._codes()), list(app._names())
+    finally:
+        os.remove(tempfile_name)
+
+
+def test_markdown() -> None:
+    codes, names = get_codes("markdown")
+
+    assert len(codes) == 3
+    assert (
+        codes[0]
+        == textwrap.dedent(
+            """
+            mo.md(
+                r\"\"\"
+                # Hello, markdown
+
+                \\"\\"\\"
+                'hello"
+                '''
+                \\"\\"\\"
+                \"\"\"
+            )
+            """
+        ).strip()
+    )
+    assert (
+        codes[1]
+        == textwrap.dedent(
+            """
+            mo.md(
+                r\"\"\"
+                Here is some math
+
+                $x \\approx 0$
+                \"\"\"
+            )
+            """
+        ).strip()
+    )
+    assert codes[2] == "import marimo as mo"
+    assert [name == "_" for name in names]
+
+
+def test_arithmetic() -> None:
+    codes, names = get_codes("arithmetic")
+
+    assert len(codes) == 2
+    assert codes[0] == "x = 0\nx"
+    assert codes[1] == "x + 1"
+    assert names == ["_", "_"]
+
+
+def test_blank() -> None:
+    codes, names = get_codes("blank")
+
+    assert len(codes) == 1
+    assert codes[0] == ""
+    assert names == ["_"]
+
+
+def test_unparsable() -> None:
+    codes, names = get_codes("unparsable")
+
+    assert len(codes) == 2
+    assert codes[0] == "!echo hello, world\n\nx = 0"
+    assert codes[1] == "x"
+    assert names == ["_", "_"]
---
+++ packages/marimo/marimo-0.1.68.tar.gz/marimo-0.1.68/marimo/_config/test_config.py
@@ -0,0 +1,29 @@
+# Copyright 2023 Marimo. All rights reserved.
+from marimo._config.config import DEFAULT_CONFIG, MarimoConfig, configure
+
+
+def assert_config(override: MarimoConfig) -> None:
+    user_config = configure(override)
+    assert user_config == {**DEFAULT_CONFIG, **override}
+
+
+def test_configure_partial_keymap() -> None:
+    assert_config(MarimoConfig(keymap={"preset": "vim"}))
+
+
+def test_configure_full() -> None:
+    assert_config(
+        MarimoConfig(
+            completion={"activate_on_typing": False, "copilot": False},
+            save={
+                "autosave": "after_delay",
+                "autosave_delay": 2,
+                "format_on_save": False,
+            },
+            keymap={"preset": "vim"},
+        )
+    )
+
+
+def test_configure_unknown() -> None:
+    assert_config({"super cool future config key": {"secret": "value"}})  # type: ignore[typeddict-unknown-key] # noqa: E501
