c57351a95f836faf3b27237ce4ed0c1558114211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:21 +0000

    Add package subfork/0.9.8/subfork-0.9.8.tar.gz

---
+++ packages/subfork/subfork-0.9.8.tar.gz/subfork-0.9.8/lib/subfork/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2022-2023 Subfork. All rights reserved.
+#
+
+from subfork.client import Subfork
+from subfork.util import get_client
---
+++ packages/subfork/subfork-0.9.8.tar.gz/subfork-0.9.8/lib/subfork/api/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2022-2023 Subfork. All rights reserved.
+#
+
+from .data import Datatype
+from .page import Page, Route
+from .site import Site, Version
+from .task import Queue, Task
+from .user import User
