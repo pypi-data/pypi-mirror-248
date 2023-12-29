35156b44ca9fe08ecc152a1b1437c358e22cb866
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:38 +0000

    Add package quantcrypt/0.0.4/quantcrypt-0.0.4-py3-none-any.whl

---
+++ packages/quantcrypt/quantcrypt-0.0.4-py3-none-any.whl/quantcrypt/typedefs.py
@@ -0,0 +1,20 @@
+#
+#   MIT License
+#
+#   Copyright (c) 2024, Mattias Aabmets
+#
+#   The contents of this file are subject to the terms and conditions defined in the License.
+#   You may not use, modify, or distribute this file except in compliance with the License.
+#
+#   SPDX-License-Identifier: MIT
+#
+from quantcrypt.internal.crypto.common import Variant
+from quantcrypt.internal.crypto.kem import KemParamSizes
+from quantcrypt.internal.crypto.dss import DssParamSizes
+
+
+__all__ = [
+	"Variant",
+	"KemParamSizes",
+	"DssParamSizes"
+]
