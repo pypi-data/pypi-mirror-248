fe0654db08f188eb0d909f5020338b234d51fe0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 05:05:12 +0000

    Add package custom-xorbits-dg/0.1.0/custom-xorbits-dg-0.1.0.tar.gz

---
+++ packages/custom-xorbits-dg/custom-xorbits-dg-0.1.0.tar.gz/custom-xorbits-dg-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='custom-xorbits-dg',
+    version='0.1.0',
+    author='antonvls',
+    description='A custom version of xorbits library',
+    packages=find_packages(),
+    install_requires=[
+   	 'numpy',
+   	 'pandas',
+    ],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
+
