e60516c249072086126f89f4fc01744b82dabce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:05:47 +0000

    Add package panther/3.3.2/panther-3.3.2-py3-none-any.whl

---
+++ packages/panther/panther-3.3.2-py3-none-any.whl/panther/middlewares/base.py
@@ -0,0 +1,10 @@
+from panther.request import Request
+from panther.response import Response
+
+
+class BaseMiddleware:
+    async def before(self, request: Request):
+        return request
+
+    async def after(self, response: Response):
+        return response
