c55a97cdbd8d3e9eefbfadef11a6f874848be615
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:24 +0000

    Add package swarms/3.1.1/swarms-3.1.1-py3-none-any.whl

---
+++ packages/swarms/swarms-3.1.1-py3-none-any.whl/swarms/structs/__init__.py
@@ -0,0 +1,23 @@
+from swarms.structs.agent import Agent
+from swarms.structs.sequential_workflow import SequentialWorkflow
+from swarms.structs.autoscaler import AutoScaler
+from swarms.structs.conversation import Conversation
+from swarms.structs.schemas import (
+    TaskInput,
+    Artifact,
+    ArtifactUpload,
+    StepInput,
+)
+from swarms.structs.swarm_net import SwarmNetwork
+
+__all__ = [
+    "Agent",
+    "SequentialWorkflow",
+    "AutoScaler",
+    "Conversation",
+    "TaskInput",
+    "Artifact",
+    "ArtifactUpload",
+    "StepInput",
+    "SwarmNetwork",
+]
