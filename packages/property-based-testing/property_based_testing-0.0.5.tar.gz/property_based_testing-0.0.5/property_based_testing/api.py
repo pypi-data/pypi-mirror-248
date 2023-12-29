00c9e5d81f3052be6f81f43190bd1c74508049e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:03:33 +0000

    Add package property-based-testing/0.0.5/property_based_testing-0.0.5.tar.gz

---
+++ packages/property-based-testing/property_based_testing-0.0.5.tar.gz/property_based_testing-0.0.5/property_based_testing/api.py
@@ -0,0 +1,15 @@
+from functools import wraps
+from typing import Callable
+
+from property_based_testing.dependency import Dependency, Dependencies
+
+
+def inject(
+    dependency_generator: Callable[[], Dependency],
+) -> Callable[[Callable[[Dependency, *Dependencies], None]], Callable[[*Dependencies], None]]:
+    def decorator(test: Callable[[Dependency, *Dependencies], None]) -> Callable[[*Dependencies], None]:
+        @wraps(test)
+        def curried_test(*remaining_dependencies: *Dependencies) -> None:
+            test(dependency_generator(), *remaining_dependencies)
+        return curried_test
+    return decorator
