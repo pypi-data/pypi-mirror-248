d56f4cab792e604329ddfd9339b9da657324481c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:04:44 +0000

    Add package pepperize.cdk-terraform-state-backend/0.1.887/pepperize.cdk-terraform-state-backend-0.1.887.tar.gz

---
+++ packages/pepperize.cdk-terraform-state-backend/pepperize.cdk-terraform-state-backend-0.1.887.tar.gz/pepperize.cdk-terraform-state-backend-0.1.887/src/pepperize_cdk_terraform_state_backend/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import constructs._jsii
+import pepperize_cdk_private_bucket._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/cdk-terraform-state-backend",
+    "0.1.887",
+    __name__[0:-6],
+    "cdk-terraform-state-backend@0.1.887.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
