b77a91be44a830191bc0b3c9ce5238415889e536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:03:49 +0000

    Add package KeyDnn/0.2.0/KeyDnn-0.2.0-py3-none-any.whl

---
+++ packages/KeyDnn/KeyDnn-0.2.0-py3-none-any.whl/keydnn/__init__.py
@@ -0,0 +1,6 @@
+from keydnn.activations import *
+from keydnn.utilities import *
+from keydnn.layers import *
+from keydnn.losses import *
+from keydnn.models import *
+from keydnn.conv_utils import *
