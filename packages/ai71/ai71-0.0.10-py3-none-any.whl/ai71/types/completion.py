73edf8fd77cf5765b30c80d7a903151a1d0d2f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:05:43 +0000

    Add package ai71/0.0.10/ai71-0.0.10-py3-none-any.whl

---
+++ packages/ai71/ai71-0.0.10-py3-none-any.whl/ai71/types/completion.py
@@ -0,0 +1,37 @@
+from typing import Literal, Optional, Sequence, Union
+
+import pydantic
+from typing_extensions import Annotated
+
+from ..types.completion_usage import CompletionUsage
+from ..types.finish_reason import FinishReason
+from ..types.model import Model
+
+
+class CreateCompletion(pydantic.BaseModel):
+    prompt: str
+    model: Model
+    stream: bool = False
+    max_tokens: Union[int, None] = None
+    stop: Annotated[Sequence[str], pydantic.Field(str, max_length=8)] = []
+    temperature: pydantic.NonNegativeFloat = 0.0
+    frequency_penalty: float = 0.0
+    presence_penalty: float = 0.0
+    top_p: Annotated[float, pydantic.Field(float, gt=0.0, le=1.0)] = 1.0
+    top_k: Optional[int] = None
+
+
+class Choice(pydantic.BaseModel):
+    finish_reason: FinishReason
+    index: int
+    text: str
+
+
+class Completion(pydantic.BaseModel):
+    id: str
+    choices: Sequence[Choice]
+    created: int
+    model: str
+    object: Literal["completion"]
+    system_fingerprint: Optional[str] = None
+    usage: Optional[CompletionUsage] = None
---
+++ packages/ai71/ai71-0.0.10-py3-none-any.whl/ai71/types/model.py
@@ -0,0 +1,5 @@
+import enum
+
+
+class Model(str, enum.Enum):
+    FALCON_180B = "tiiuae/falcon-180b-chat"
