203a180ebbb02f58e85ec4f19d30e194788fb4de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:21 +0000

    Add package deepgram-sdk/3.0.1/deepgram-sdk-3.0.1.tar.gz

---
+++ packages/deepgram-sdk/deepgram-sdk-3.0.1.tar.gz/deepgram-sdk-3.0.1/setup.py
@@ -0,0 +1,41 @@
+# Copyright 2023 Deepgram SDK contributors. All Rights Reserved.
+# Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+# SPDX-License-Identifier: MIT
+
+from setuptools import setup, find_packages
+import os.path
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+DESCRIPTION = (
+    "The official Python SDK for the Deepgram automated speech recognition platform."
+)
+
+setup(
+    name="deepgram-sdk",
+    author="Deepgram",
+    author_email="devrel@deepgram.com",
+    url="https://github.com/deepgram/deepgram-python-sdk",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "httpx",
+        "websockets",
+        "dataclasses-json",
+        "typing_extensions",
+        "python-dotenv",
+        "asyncio",
+        "aiohttp",
+        "verboselogs",
+    ],
+    keywords=["deepgram", "deepgram speech-to-text"],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
