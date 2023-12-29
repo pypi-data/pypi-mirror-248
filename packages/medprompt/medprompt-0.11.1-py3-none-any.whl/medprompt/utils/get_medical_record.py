ffcb481cd02c8dd09aab1644803f8dfcf6441728
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:05:22 +0000

    Add package medprompt/0.11.1/medprompt-0.11.1-py3-none-any.whl

---
+++ packages/medprompt/medprompt-0.11.1-py3-none-any.whl/medprompt/utils/get_medical_record.py
@@ -0,0 +1,81 @@
+"""
+ Copyright 2023 Bell Eapen
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+"""
+
+from typing import Any, Optional, Type
+from kink import di, inject
+from langchain.callbacks.manager import (AsyncCallbackManagerForToolRun,
+                                         CallbackManagerForToolRun)
+from langchain.pydantic_v1 import BaseModel, Field
+import logging
+_logger = logging.getLogger(__name__)
+from ..utils.fhir_server import FhirServer
+
+class SearchInput(BaseModel):
+    patient_id: str = Field()
+
+@inject # Constructor injection will happen here
+class GetMedicalRecordUtil:
+
+    def __init__(self, fhir_server: FhirServer):
+        self.fhir_server = fhir_server
+
+    def run(
+            self,
+            patient_id: str = None,
+            run_manager: Optional[CallbackManagerForToolRun] = None
+            ) -> Any:
+        query = self._format_query(patient_id)
+        try:
+            _response = self.fhir_server.call_fhir_server(query, params=None)
+        except:
+            return "Sorry I cannot find the answer as the FHIR server is not responding."
+        if _response["total"] >100:
+            _logger.info("Patient record too large")
+            return "Sorry, the patient's record is too large to be loaded."
+        elif _response["total"] < 1:
+            _logger.info("Patient record not found")
+            return "This patient does not have a record."
+        else:
+            return _response
+
+    async def arun(
+            self,
+            patient_id: str = None,
+            run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+            ) -> Any:
+        query = self._format_query(patient_id)
+        try:
+            _response = await self.fhir_server.async_call_fhir_server(query, params=None)
+        except:
+            return "Sorry I cannot find the answer as the FHIR server is not responding."
+        if _response["total"] >100:
+            _logger.info("Patient record too large")
+            return "Sorry, the patient's record is too large to be loaded."
+        elif _response["total"] < 1:
+            _logger.info("Patient record not found")
+            return "This patient does not have a record."
+        else:
+            return _response
+
+    def _format_query(self, patient_id):
+        query = "Patient?"
+        if patient_id:
+            query += "_id="+patient_id
+            query += "&_revinclude=Observation:subject"
+            query += "&_revinclude=Condition:subject"
+            query += "&_revinclude=Procedure:subject"
+            query += "&_revinclude=MedicationRequest:subject"
+        return query
