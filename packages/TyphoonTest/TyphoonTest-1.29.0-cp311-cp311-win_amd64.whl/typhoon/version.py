2d0c598e840053b9855632a85edff1e27a40bf34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:04:50 +0000

    Add package TyphoonTest/1.29.0/TyphoonTest-1.29.0-cp311-cp311-win_amd64.whl

---
+++ packages/TyphoonTest/TyphoonTest-1.29.0-cp311-cp311-win_amd64.whl/typhoon/version.py
@@ -0,0 +1,8 @@
+
+def get_typhoontest_version():
+    """Typhoontest official version is registered in this function"""
+    return "1.29.0"
+
+
+if __name__ == "__main__":
+    print(get_typhoontest_version())
