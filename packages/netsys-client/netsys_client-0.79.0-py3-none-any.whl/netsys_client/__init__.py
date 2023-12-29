721323ba27de5277d7dbba892c75c35fef984e43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:27 +0000

    Add package netsys-client/0.79.0/netsys_client-0.79.0-py3-none-any.whl

---
+++ packages/netsys-client/netsys_client-0.79.0-py3-none-any.whl/netsys_client/__init__.py
@@ -0,0 +1,5 @@
+from .conf.controller import ConfController
+from .print.controller import PrintController
+from .response import Response
+from .nf.controller import NFController
+from .gateway import Gateway
