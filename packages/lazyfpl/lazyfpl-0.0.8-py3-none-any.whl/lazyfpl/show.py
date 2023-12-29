ddf2c0550014b9b0f2cc9d8d81509339572375ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:24 +0000

    Add package lazyfpl/0.0.8/lazyfpl-0.0.8-py3-none-any.whl

---
+++ packages/lazyfpl/lazyfpl-0.0.8-py3-none-any.whl/lazyfpl/show.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+import argparse
+import itertools
+
+from lazyfpl import fetch, structures
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--top",
+        "-t",
+        default=None,
+        help="Top N players per position. (default: %(default)s)",
+        type=int,
+    )
+    args = parser.parse_args()
+
+    players = list(
+        itertools.chain(
+            *[
+                list(p)[: args.top] if args.top else list(p)
+                for _, p in itertools.groupby(
+                    sorted(
+                        fetch.players(),
+                        key=lambda x: (x.position, x.xP or 0),
+                        reverse=True,
+                    ),
+                    key=lambda x: x.position,
+                )
+            ]
+        )
+    )
+
+    print(structures.Squad(players))
