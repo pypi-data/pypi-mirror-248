44548674061e211daf1382cf7ffe322182d263db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:04:51 +0000

    Add package SRgenerator/0.1.0/SRgenerator-0.1.0.tar.gz

---
+++ packages/SRgenerator/SRgenerator-0.1.0.tar.gz/SRgenerator-0.1.0/README.txt
@@ -0,0 +1,5 @@
+SR generator
+Operating, generating, and processing chemical structures based on RDkit package.
+Version 0.1.0
+1. add selfies representation of chemicals for application on transformer
+2. add random split function and fix the missoutputed result in reorder smiles function
---
+++ packages/SRgenerator/SRgenerator-0.1.0.tar.gz/SRgenerator-0.1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: MacOS',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: Microsoft',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 3',
+]
+
+with open("README.txt", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name                = 'SRgenerator',
+    version             = '0.1.0',
+    description         = "Stereo operation for chemicals based on RDkit package",
+    long_description    = long_description,
+    author              = 'Mola Lin',
+    author_email        = 'acps91012@gmail.com',
+    license             = 'BSD',
+    packages            = find_packages(),
+    install_requires    = ['selfies >= 2.0.0', 'rdkit >= 2023.3.1'],
+    classifiers         = classifiers,
+    package_data={
+        '':['*.csv'],
+        'bandwidth_reporter':['*.csv']
+               },
+    python_requires='>=3.6',
+)
