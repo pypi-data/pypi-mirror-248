552c5d4e97b983be2d10e68c91d8c6cde671eb28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:05:15 +0000

    Add package avilla-core/1.0.0a20/avilla_core-1.0.0a20.tar.gz

---
+++ packages/avilla-core/avilla_core-1.0.0a20.tar.gz/avilla_core-1.0.0a20/avilla/standard/core/profile/__init__.py
@@ -0,0 +1,4 @@
+from .capability import NickCapability as NickCapability
+from .capability import SummaryCapability as SummaryCapability
+from .metadata import Nick as Nick
+from .metadata import Summary as Summary
