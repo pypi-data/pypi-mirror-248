54026d0f3b0dbd51afd3faa331959852bf9a75f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:04:42 +0000

    Add package piops/0.0.14/piops-0.0.14.tar.gz

---
+++ packages/piops/piops-0.0.14.tar.gz/piops-0.0.14/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages 
+setup(
+    name="piops", 
+    version="0.0.14", 
+    description="Python package to support Process Intelligence related tasks", 
+    url="https://github.com/jcppc/piops", 
+    author="João Caldeira", 
+    author_email="jcppc@iscte-iul.pt", 
+    license="BSD 2-clause", 
+    install_requires=["fitter","numpy","pandas","seaborn"],
+    packages=find_packages()
+)
+
+
+
