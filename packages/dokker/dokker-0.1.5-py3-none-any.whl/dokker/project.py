c2622bf96397f0cbc7f162ab01ba8acbbe62d771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 14:04:01 +0000

    Add package dokker/0.1.5/dokker-0.1.5-py3-none-any.whl

---
+++ packages/dokker/dokker-0.1.5-py3-none-any.whl/dokker/project.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional, Dict, Protocol, runtime_checkable, Any
+from pathlib import Path
+from .compose_spec import ComposeSpecification
+
+
+@runtime_checkable
+class Project(Protocol):
+    async def aget_client_params(self) -> Dict[str, Any]:
+        ...
+
+    async def abefore_pull(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_up(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_enter(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_down(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
+
+    async def abefore_stop(self) -> None:
+        """A setup method for the project.
+
+        Returns:
+            Optional[List[str]]: A list of logs from the setup process.
+        """
+        ...
