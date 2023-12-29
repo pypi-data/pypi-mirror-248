fcb207931e29030d85c0009c587e919254818463
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:28 +0000

    Add package general-tamsat-alert/0.0.1a2/general_tamsat_alert-0.0.1a2.tar.gz

---
+++ packages/general-tamsat-alert/general_tamsat_alert-0.0.1a2.tar.gz/general_tamsat_alert-0.0.1a2/tests/test_ensembles.py
@@ -0,0 +1,11 @@
+import unittest
+import src.general_tamsat_alert.ensembles as ensembles
+
+
+class TestEnsembles(unittest.TestCase):
+    def test_get_ensembles(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ packages/general-tamsat-alert/general_tamsat_alert-0.0.1a2.tar.gz/general_tamsat_alert-0.0.1a2/tests/test_periodicity.py
@@ -0,0 +1,10 @@
+import unittest
+import src.general_tamsat_alert.periodicity as periodicity
+
+class TestPeriodicity(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+
+if __name__ == "__main__":
+    unittest.main()
