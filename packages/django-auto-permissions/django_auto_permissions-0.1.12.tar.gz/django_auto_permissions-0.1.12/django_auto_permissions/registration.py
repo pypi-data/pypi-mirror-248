51ceabe69dcf827db975580b32e687756f34a334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:05:47 +0000

    Add package django-auto-permissions/0.1.12/django_auto_permissions-0.1.12.tar.gz

---
+++ packages/django-auto-permissions/django_auto_permissions-0.1.12.tar.gz/django_auto_permissions-0.1.12/django_auto_permissions/registration.py
@@ -0,0 +1,45 @@
+class ViewsetRegistrar:
+    registered_viewsets = []
+
+    @classmethod
+    def register(cls, viewset, model):
+        print("Registering viewset:", viewset, model)
+        cls.registered_viewsets.append((viewset, model))
+        print("Registered viewsets post registration:", cls.registered_viewsets)
+        cls.register_permissions(viewset, model)
+
+    @classmethod
+    def get_custom_methods(cls, viewset):
+        standard_methods = {'list', 'create', 'retrieve', 'update', 'partial_update', 'destroy'}
+
+        # Get methods defined directly in the viewset class, not inherited
+        custom_methods = []
+        for attr_name in dir(viewset):
+            if attr_name in standard_methods:
+                continue  # Skip standard DRF methods
+            attr = getattr(viewset, attr_name)
+            if callable(attr) and attr.__qualname__.startswith(viewset.__name__ + '.'):
+                custom_methods.append(attr_name)
+
+        return custom_methods
+
+    @classmethod
+    def register_permissions(cls, viewset, model):
+        from django.contrib.auth.models import Permission
+        from django.contrib.contenttypes.models import ContentType
+        print("Registering permissions for:", viewset, model)
+        content_type = ContentType.objects.get_for_model(model)
+        custom_methods = cls.get_custom_methods(viewset)
+        print("Custom methods:", custom_methods)
+        for method in custom_methods:
+            codename = f"{model._meta.model_name}_{method}"
+            name = f"Can {method} {model._meta.verbose_name}"
+            Permission.objects.get_or_create(
+                codename=codename, name=name, content_type=content_type
+            )
+            print(f"Registered permission: {codename}")
+
+    @classmethod
+    def process_registrations(cls):
+        for viewset, model in cls.registered_viewsets:
+            cls.register_permissions(viewset, model)
