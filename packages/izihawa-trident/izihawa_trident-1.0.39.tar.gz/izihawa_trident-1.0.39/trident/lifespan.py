700abb5876f886d95204a4fbe8cf11ce88311beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 15:05:19 +0000

    Add package izihawa-trident/1.0.39/izihawa_trident-1.0.39.tar.gz

---
+++ packages/izihawa-trident/izihawa_trident-1.0.39.tar.gz/izihawa_trident-1.0.39/trident/lifespan.py
@@ -0,0 +1,29 @@
+import dataclasses
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from izihawa_loglib import configure_logging
+from starlette.requests import Request
+
+from .configs import config
+from .storage import Storage
+
+
+@dataclasses.dataclass
+class State:
+    storage: Storage
+
+
+class TridentFastAPI(FastAPI):
+    state: State
+
+
+class TridentFastAPIRequest(Request):
+    app: TridentFastAPI
+
+
+@asynccontextmanager
+async def lifespan(app: TridentFastAPI):
+    configure_logging(config.get(dict))
+    app.state = State(storage=Storage(config))
+    yield
