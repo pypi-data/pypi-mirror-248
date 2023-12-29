60192b0f4d9e13bf0d1ac6e4a932ee6a63ff1c4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:04:06 +0000

    Add package taskingai/0.0.6/taskingai-0.0.6.tar.gz

---
+++ packages/taskingai/taskingai-0.0.6.tar.gz/taskingai-0.0.6/setup.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+"""
+    TaskingAI
+
+    OpenAPI spec version: 0.1.0
+"""
+
+from setuptools import setup, find_packages  # noqa: H301
+import taskingai
+
+NAME = "taskingai"
+VERSION = taskingai.__version__
+# To install the library, run the following
+#
+# python setup.py install
+#
+# prerequisite: setuptools
+# http://pypi.python.org/pypi/setuptools
+
+REQUIRES = ["httpx >= 0.23.0", "six >= 1.10", "certifi", "python-dateutil"]
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description="TaskingAI",
+    author_email="support@tasking.ai",
+    url="http://www.tasking.ai",
+    keywords=["TaskingAI", "LLM", "AI"],
+    install_requires=REQUIRES,
+    packages=find_packages(),
+    include_package_data=True,
+    long_description="""\
+    No description provided 
+    """
+)
