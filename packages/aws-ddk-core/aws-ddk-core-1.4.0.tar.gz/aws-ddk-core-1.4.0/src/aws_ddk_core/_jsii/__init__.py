165dca86aaaa5fa1848848f2e0e4f372dbb1f75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 14:04:04 +0000

    Add package aws-ddk-core/1.4.0/aws-ddk-core-1.4.0.tar.gz

---
+++ packages/aws-ddk-core/aws-ddk-core-1.4.0.tar.gz/aws-ddk-core-1.4.0/src/aws_ddk_core/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from typeguard import check_type
+
+import aws_cdk._jsii
+import aws_cdk.aws_glue_alpha._jsii
+import aws_cdk.aws_kinesisfirehose_alpha._jsii
+import aws_cdk.aws_kinesisfirehose_destinations_alpha._jsii
+import aws_cdk.integ_tests_alpha._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "aws-ddk-core", "1.4.0", __name__[0:-6], "aws-ddk-core@1.4.0.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
