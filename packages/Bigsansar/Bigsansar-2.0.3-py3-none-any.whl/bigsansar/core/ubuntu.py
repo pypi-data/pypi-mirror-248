2163882e0340b62b2a0c884037189722b4903106
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 03:03:37 +0000

    Add package Bigsansar/2.0.3/Bigsansar-2.0.3-py3-none-any.whl

---
+++ packages/Bigsansar/Bigsansar-2.0.3-py3-none-any.whl/bigsansar/core/ubuntu.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+def deploy():
+
+    cmd1 = "sudo apt update  && sudo apt upgrade -y"
+    os.system(cmd1)
+
+
+    
+    print('we are installing some ubuntu package for configurations....')
+    cmd2 = "sudo apt install openssh-server ufw apache2 libapache2-mod-wsgi-py3 postgresql postgresql-contrib libpq-dev -y"
+    os.system(cmd2)
+
+    print(' RE-Configuring internal setiings now.................................................................')
+
+    # Specify the path of the file you want to copy
+    file_to_copy = '/usr/local/lib/*/dist-packages/bigsansar/etc/serve-cgi-bin.conf'
+
+    # Specify the path of the destination directory you want to copy to
+    destination_directory = '/etc/apache2/conf-available/'
+
+    # Use the shutil.copy() method to copy the file to the destination directory
+    shutil.copy(file_to_copy, destination_directory)
+
+    print('secure on firewall...................................................')
+    ufw = "sudo ufw allow ssh && sudo ufw allow openssh"
+
+    print('restarting all service ...........................................')
+    restart = "sudo service ssh restart"
+    os.system(restart)
+
+    print('checking the service status........................................................ ')
+
+    check = "sudo service ssh status"
+    os.system(check)
+    
