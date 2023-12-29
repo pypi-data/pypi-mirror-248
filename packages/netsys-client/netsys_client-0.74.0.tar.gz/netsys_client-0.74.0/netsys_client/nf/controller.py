9c81efb78c13843378e08e27f1546f9979d93d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-29 04:04:16 +0000

    Add package netsys-client/0.74.0/netsys_client-0.74.0.tar.gz

---
+++ packages/netsys-client/netsys_client-0.74.0.tar.gz/netsys_client-0.74.0/netsys_client/nf/controller.py
@@ -0,0 +1,20 @@
+from . import NFMethods
+
+class NFController(NFMethods):
+    
+    def __init__(self, data, token):
+
+        self._data = data
+        self._file_path = self.map_nf_path()
+        self._token = token
+
+    def execute(self):
+
+        self._ini = self.generate_ini(self._data)
+        self.write_ini(f'{self._file_path}/NFe.ini', self._ini)
+        txt = f'NFe.CriarEnviarNFe("{self._file_path}/NFe.ini",1,1, , ,1)'
+        self.write_txt(f'{self._file_path}\Entrada\ENT.txt', txt)
+        txt = self.read_txt(f'{self._file_path}\Saida\ENT-resp.txt')
+        self.send_to_db(txt, self._token)
+        self.write_txt(f'{self._file_path}\Saida\ENT-resp.txt', '')
+
