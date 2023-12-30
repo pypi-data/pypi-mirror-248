59a3f4915de204a1eb5d797f04f7471c1215b2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 04:04:34 +0000

    Add package pocbot/0.1.3/pocbot-0.1.3.tar.gz

---
+++ packages/pocbot/pocbot-0.1.3.tar.gz/pocbot-0.1.3/pocbot/chain_template.py
@@ -0,0 +1,21 @@
+"""
+Chain interface template
+"""
+from typing import List, Any
+from abc import ABC, abstractmethod
+
+
+class ChatBotChain(ABC):
+    """
+    This is the interface for a chatbot chain model
+    The chain must implement an `invoke` method in order to interact with the chatbot. 
+    If not implemented, it will raise an error.
+    """
+
+    @abstractmethod
+    def invoke(self, input: str, chat_history: List[Any]) -> str:
+        """given a question, return an answer.
+        - input: input to the chain. This is the query to be processed by the LLM
+        - chat_history: Steamlit messages saved to session_sate."""
+        raise NotImplementedError("invoke not implemented")
+    
