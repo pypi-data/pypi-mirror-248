2eba0171a25fbb014184f82c4a3ce5ef5375fc50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 15:04:01 +0000

    Add package ast-torch/0.0.3/ast_torch-0.0.3.tar.gz

---
+++ packages/ast-torch/ast_torch-0.0.3.tar.gz/ast_torch-0.0.3/README.md
@@ -0,0 +1,19 @@
+[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)
+
+# AST
+Implementation of AST from the paper: "AST: Audio Spectrogram Transformer' in PyTorch and Zeta.
+
+## Install
+`pip3 install ast-torch`
+
+## Usage
+```
+
+```
+
+
+# License
+MIT
+
+
+
---
+++ packages/ast-torch/ast_torch-0.0.3.tar.gz/ast_torch-0.0.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ast_torch']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['einops', 'swarms', 'torch', 'zetascale']
+
+setup_kwargs = {
+    'name': 'ast-torch',
+    'version': '0.0.3',
+    'description': 'ast - Pytorch',
+    'long_description': '[![Multi-Modality](agorabanner.png)](https://discord.gg/qUtxnK2NMf)\n\n# AST\nImplementation of AST from the paper: "AST: Audio Spectrogram Transformer\' in PyTorch and Zeta.\n\n## Install\n`pip3 install ast-torch`\n\n## Usage\n```\n\n```\n\n\n# License\nMIT\n\n\n\n',
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kyegomez/AST',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
