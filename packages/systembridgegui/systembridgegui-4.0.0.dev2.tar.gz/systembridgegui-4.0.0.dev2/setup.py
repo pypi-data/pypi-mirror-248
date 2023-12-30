1b1d467291f65875c2dd9f3d7fd008438be0aa3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:04:58 +0000

    Add package systembridgegui/4.0.0.dev2/systembridgegui-4.0.0.dev2.tar.gz

---
+++ packages/systembridgegui/systembridgegui-4.0.0.dev2.tar.gz/systembridgegui-4.0.0.dev2/setup.py
@@ -0,0 +1,27 @@
+"""Setup."""
+from setuptools import find_packages, setup
+
+# Get setup packages from requirements.txt
+with open("requirements_setup.txt", encoding="utf-8") as f:
+    requirements_setup = f.read().splitlines()
+
+# Get packages from requirements.txt
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+package_data = ["icon.png"]
+
+setup(
+    name="systembridgegui",
+    description="System Bridge GUI",
+    keywords="system-bridge",
+    author="Aidan Timson (Timmo)",
+    author_email="aidan@timmo.dev",
+    license="Apache-2.0",
+    url="https://github.com/timmo001/system-bridge-gui",
+    packages=find_packages(exclude=["tests", "generator"]),
+    package_data={"": package_data},
+    install_requires=requirements,
+    setup_requires=requirements_setup,
+    use_incremental=True,
+)
