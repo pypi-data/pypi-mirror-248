b4c03e923065c726ab3fae846e3044b8b145d389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 04:04:10 +0000

    Add package grapycal/0.10.0/grapycal-0.10.0-py3-none-any.whl

---
+++ packages/grapycal/grapycal-0.10.0-py3-none-any.whl/grapycal/sobjects/controls/textControl.py
@@ -0,0 +1,24 @@
+from grapycal.sobjects.controls.control import Control
+from objectsync import StringTopic, IntTopic
+
+
+class TextControl(Control):
+    '''
+    To add a text control to a node, use the following code in the node:
+    ```python
+    self.add_control(TextControl, text='', label='', editable=True)
+    ```
+    '''
+    frontend_type = 'TextControl'
+    def build(self, text:str='', label:str='',readonly=False, editable:bool=True):
+        if readonly:
+            editable = False
+        self.text = self.add_attribute('text', StringTopic, text, is_stateful= not readonly, order_strict= not readonly)
+        self.label = self.add_attribute('label', StringTopic, label)
+        self.editable = self.add_attribute('editable', IntTopic, 1 if editable else 0)
+    
+    def set(self, text:str):
+        self.text.set(text)
+
+    def get(self):
+        return self.text.get()
