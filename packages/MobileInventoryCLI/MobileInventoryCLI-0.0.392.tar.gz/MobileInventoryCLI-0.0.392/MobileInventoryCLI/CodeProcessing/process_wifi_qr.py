53f3b71f875234204cae492f6db6590f95ef6b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 15:04:17 +0000

    Add package MobileInventoryCLI/0.0.392/MobileInventoryCLI-0.0.392.tar.gz

---
+++ packages/MobileInventoryCLI/MobileInventoryCLI-0.0.392.tar.gz/MobileInventoryCLI-0.0.392/MobileInventoryCLI/CodeProcessing/process_wifi_qr.py
@@ -0,0 +1,41 @@
+from colored import Fore, Back, Style
+
+class Parser:
+    code=None
+    def __init__(self,code=None):
+        if code != None:
+            self.code=code
+
+        else:
+            self.code=input("wifi code: ")
+        self.processCode()
+        self.printCode()
+
+    def processCode(self):
+        self.header="WIFI:"
+        self.code.replace(self.header,'')
+        self.code_split_fields=self.code.split(";")
+        self.code_as_dict={}
+
+        for line in self.code_split_fields:
+            l=line.split(":")
+            if len(l) > 1:
+                #print(l)
+                if l[0] == 'WIFI':
+                    self.code_as_dict[l[0]]=l[-1]
+                else:
+                    #print(l)
+                    if l[0] == 'T':
+                        l[0]='Type'
+                    elif l[0] == 'P':
+                        l[0] = "Password"
+                    elif l[0] == 'H':
+                        l[0]="Hidden"
+                    elif l[0]=="WIFI":
+                        l[0]="WirelessNameString"
+                    self.code_as_dict[l[0]]=l[1]
+    def printCode(self):
+        for k in self.code_as_dict.keys():
+            print(Fore.red+k+Style.reset,":",Fore.green+self.code_as_dict[k]+Style.reset)
+        
+Parser()
