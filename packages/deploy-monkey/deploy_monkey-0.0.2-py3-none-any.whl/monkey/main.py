c51cb5520f521a0aab7f0dc0a52754595b061b1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:03:57 +0000

    Add package deploy-monkey/0.0.2/deploy_monkey-0.0.2-py3-none-any.whl

---
+++ packages/deploy-monkey/deploy_monkey-0.0.2-py3-none-any.whl/monkey/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("You installed the wrong monkey package.")
+    print(
+        "This one comes from the public PyPi. It means you didn't have "
+        "access to our private PyPi whilst installing."
+    )
