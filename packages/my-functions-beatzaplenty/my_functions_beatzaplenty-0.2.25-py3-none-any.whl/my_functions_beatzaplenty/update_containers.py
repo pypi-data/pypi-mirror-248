8a17031bbab8447f923e8af76d0a11433d9054ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 15:03:59 +0000

    Add package my-functions-beatzaplenty/0.2.25/my_functions_beatzaplenty-0.2.25-py3-none-any.whl

---
+++ packages/my-functions-beatzaplenty/my_functions_beatzaplenty-0.2.25-py3-none-any.whl/my_functions_beatzaplenty/update_containers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import my_functions_beatzaplenty.general_purpose as general_purpose
+
+def main(services):
+    """
+    Update Docker Containers.
+
+    :param services: An array of service names to be updated
+    """
+    for service in services:
+        try:
+            path = f"/docker/{service}/docker-compose.yml"
+            pull_command = ["docker-compose", "--file", path, "pull"]
+            up_command = ["docker-compose", "--file", path, "up", "-d"]
+            if not general_purpose.run_command(pull_command):
+                continue
+            
+            if not general_purpose.run_command(up_command):
+                continue
+                
+        except Exception as e:
+            print("Error: {}".format(e))
+
+if __name__ == "__main__":
+    # Pass the list of services to the main function
+    main(["service1", "service2"])  # Replace with your list of services
