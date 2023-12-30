c727b331535c78c1aafddd8ce5a9f5475ab2b920
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 15:04:38 +0000

    Add package krptn/0.8.6/krptn-0.8.6-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/krptn/krptn-0.8.6-cp310-cp310-macosx_11_0_arm64.whl/krypton/_load.py
@@ -0,0 +1,340 @@
+"""
+Load up databases, and configure Krptn.
+"""
+# pylint: disable=cyclic-import
+# pylint: disable=invalid-name
+
+import os
+import pathlib
+import importlib.metadata
+from sqlalchemy import (
+    DateTime,
+    Index,
+    Text,
+    UniqueConstraint,
+    create_engine,
+    Column,
+    Integer,
+    LargeBinary,
+    select,
+    Boolean,
+    ForeignKey,
+)
+from sqlalchemy.orm import declarative_base, Session, sessionmaker
+
+__version__ = importlib.metadata.version("krptn")
+
+USER_DIR = pathlib.Path.home()
+
+MAX_USER_NAME_LEN = 450
+
+KR_DATA = pathlib.Path(USER_DIR, ".krptn-data/")
+if not KR_DATA.exists():
+    os.mkdir(KR_DATA.as_posix())
+
+Base = declarative_base()
+
+USER_TABLE_NAME = "users"
+
+
+class DBschemas:  # pylint: disable=too-few-public-methods
+    """Database Schema"""
+
+    class CryptoTable(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        - id: int
+        - ctext: bytes
+        - salt: bytes
+        - saltIter: int"""
+
+        __tablename__ = "crypto"
+        id = Column(Integer, primary_key=True)
+        ctext = Column(LargeBinary)
+        salt = Column(LargeBinary)
+        saltIter = Column(Integer)
+
+    class KeysTable(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        id: int
+        name: str
+        key: bytes
+        salt: bytes
+        saltIter: int
+        year: int"""
+
+        __tablename__ = "keys"
+        id = Column(Integer, primary_key=True)
+        name = Column(Text(MAX_USER_NAME_LEN), index=True, unique=True)
+        key = Column(LargeBinary)
+        salt = Column(LargeBinary)
+        saltIter = Column(Integer)
+        year = Column(Integer)
+
+    class UserTable(Base):  # pylint: disable=too-few-public-methods
+        """Database
+        id: int
+        name: str
+        pwdAuthToken: bytes,
+        salt: bytes,
+        mfa: bytes,
+        fidoPub: bytes,
+        fidoID: bytes"""
+
+        __tablename__ = "users"
+        id = Column(Integer, primary_key=True)
+        name = Column(Text(MAX_USER_NAME_LEN), index=True, unique=True)
+        pwdAuthToken = Column(Text)
+        mfa = Column(LargeBinary, default=b"*")
+        fidoPub = Column(LargeBinary, default=b"*")
+        fidoID = Column(LargeBinary, default=b"*")
+        fidoChallenge = Column(LargeBinary, default=b"*")
+
+    class PubKeyTable(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        Uid: int
+        key: str
+        krVersion: str"""
+
+        __tablename__ = "pubKeys"
+        id = Column(Integer, primary_key=True)
+        Uid = Column(
+            Integer,
+            ForeignKey(f"{USER_TABLE_NAME}.id"),
+            index=True,
+        )
+        krVersion = Column(Text, default=__version__)
+        key = Column(LargeBinary)
+
+    class SessionKeys(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        id: int
+        Uid: int
+        key: str
+        exp: DateTime
+        iss: DateTime"""
+
+        __tablename__ = "sessions"
+        id = Column(Integer, primary_key=True)
+        Uid = Column(Integer, ForeignKey(f"{USER_TABLE_NAME}.id"), index=True)
+        key = Column(LargeBinary)
+        exp = Column(DateTime, index=True)
+        iss = Column(DateTime)
+
+    class UserData(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        Uid: int
+        name: str
+        value: bytes
+        shared: int, default=0"""
+
+        __tablename__ = "userData"
+        id = Column(Integer, primary_key=True)
+        Uid = Column(Integer, ForeignKey(f"{USER_TABLE_NAME}.id"), index=True)
+        name = Column(Text(MAX_USER_NAME_LEN), index=True)
+        value = Column(LargeBinary)
+        __table_args__ = (
+            Index("_name_Uid_index", "Uid", "name"),
+            UniqueConstraint("name", "Uid", name="uc_name_uid"),
+        )
+
+    class UserShareTable(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        sender: int
+        name: bytes
+        salt: bytes
+        value: bytes
+        shareUid: int"""
+
+        __tablename__ = "userShareData"
+        id = Column(Integer, primary_key=True)
+        sender = Column(Integer, ForeignKey(f"{USER_TABLE_NAME}.id"), index=True)
+        name = Column(Text(MAX_USER_NAME_LEN), index=True)
+        value = Column(LargeBinary)
+        shareUid = Column(Integer, ForeignKey(f"{USER_TABLE_NAME}.id"), index=True)
+        __table_args__ = (
+            Index("_name_suid_index", "sender", "name", "shareUid"),
+            UniqueConstraint("sender", "name", "shareUid", name="uc_sender_name_suid"),
+        )
+
+    class UnsafeShare(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        sender: int
+        name: string
+        value: bytes"""
+
+        __tablename__ = "unsafeShare"
+        id = Column(Integer, primary_key=True)
+        sender = Column(Integer, ForeignKey(f"{USER_TABLE_NAME}.id"), index=True)
+        name = Column(Text(MAX_USER_NAME_LEN), index=True, unique=True)
+        value = Column(LargeBinary)
+
+    class PWDReset(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        Uid: int,
+        key: bytes,
+        iter: int,
+        salt: bytes
+        """
+
+        __tablename__ = "pwdReset"
+        id = Column(Integer, primary_key=True)
+        Uid = Column(Integer, ForeignKey(f"{USER_TABLE_NAME}.id"), index=True)
+        key = Column(LargeBinary)
+        iter = Column(Integer)
+        salt = Column(LargeBinary)
+
+    class Logs(Base):  # pylint: disable=too-few-public-methods
+        """Database Schema
+        logId: int,
+        time: DaeTime,
+        exp: DateTime,
+        success: bool
+        userId: int
+        """
+
+        __tablename__ = "logs"
+        logId = Column(Integer, primary_key=True)
+        time = Column(DateTime)
+        exp = Column(DateTime)
+        success = Column(Boolean)
+        userId = Column(Integer, ForeignKey(f"{USER_TABLE_NAME}.id"), index=True)
+
+
+class ConfigTemp:
+    """Configuration templates"""
+
+    APP_NAME = "KryptonApp"
+    HOST_NAME = ""
+    ORIGIN = ""
+    # The below options for Argon2 are based on RFC-9106
+    # They are higher then the recomended values
+    defaultArgonOps = 3
+    _memLimitArgon = 268435456
+    defaultPasswordResetArgonOps = 4
+    defaultCryptoperiod = 2
+    defaultSessionPeriod = 15  # Minutes
+    defaultLogRetentionPeriod = 43200  # Minutes
+    _saltLen = 16
+    _totpSecretLen = 20
+    _aesKeyLen = 32
+    _cryptoDB: sessionmaker = None
+    _cryptoDbEngine = None
+    _altKeyDB: sessionmaker = None
+    _altKeyDbEngine = None
+    _userDB: sessionmaker = None
+    _userDbEngine = None
+
+    @property
+    def SQLDefaultCryptoDBpath(self) -> sessionmaker:
+        """
+        Connection to the default database used with Crypto Class
+        """
+        return self._cryptoDB
+
+    @SQLDefaultCryptoDBpath.setter
+    def SQLDefaultCryptoDBpath(self, path: str) -> None:
+        """
+        Connection to the default database used with Crypto Class
+        """
+        engine = create_engine(path, echo=False, future=True)
+        c = Session(engine)
+        Base.metadata.create_all(engine)
+        error = False
+        stmt = select(DBschemas.CryptoTable).where(DBschemas.CryptoTable.id == 1)
+        test = None
+        try:
+            test = c.scalar(stmt)
+        except Exception:
+            error = True
+        if test is None or error:
+            stmt = DBschemas.CryptoTable(
+                id=1,
+                ctext=b"Position Reserved",
+                salt=b"Position Reserved",
+                saltIter=0,
+            )
+            c.add(stmt)
+        c.autoflush = True
+        c.flush()
+        c.commit()
+        c.close()
+        self._cryptoDbEngine = engine
+        self._cryptoDB = sessionmaker(engine, autoflush=True)
+
+    @property
+    def SQLDefaultKeyDBpath(self) -> sessionmaker:
+        """
+        Connection to the default database used by the KMS
+        """
+        return self._altKeyDB
+
+    @SQLDefaultKeyDBpath.setter
+    def SQLDefaultKeyDBpath(self, path: str):
+        """
+        Connection to the default database used by the KMS
+        """
+        engine = create_engine(path, echo=False, future=True)
+        c = Session(engine)
+        Base.metadata.create_all(engine)
+        c.autoflush = True
+        c.commit()
+        c.flush()
+        c.close()
+        self._altKeyDbEngine = engine
+        self._altKeyDB = sessionmaker(engine, autoflush=True)
+
+    @property
+    def SQLDefaultUserDBpath(self) -> sessionmaker:
+        """
+        Connection to the default database used to store User Data.
+        """
+        return self._userDB
+
+    @SQLDefaultUserDBpath.setter
+    def SQLDefaultUserDBpath(self, path: str):
+        """
+        Connection to the default database used to store User Data.
+        """
+        engine = create_engine(path, echo=False, future=True)
+        c = Session(engine)
+        Base.metadata.create_all(engine)
+        error = False
+
+        stmt = select(DBschemas.UserTable).where(
+            DBschemas.UserTable.id == 1
+        )  # Has to be one because of shared
+        test = None
+        try:
+            test = c.scalar(stmt)
+        except Exception:
+            error = True
+        if test is None or error:
+            stmt = DBschemas.UserTable(
+                id=1, name="Position Reserved", pwdAuthToken=b"Position Reserved"
+            )
+            c.add(stmt)
+        c.autoflush = True
+        c.flush()
+        c.commit()
+        c.close()
+        self._userDbEngine = engine
+        self._userDB = sessionmaker(engine, autoflush=True)
+
+
+configs = ConfigTemp()
+
+
+def setupDatabase():
+    """This function setups the SQL Database to Krptn's default SQLite files"""
+    configs.SQLDefaultCryptoDBpath = "sqlite+pysqlite:///" + os.path.join(
+        USER_DIR, ".krptn-data/crypto.db"
+    )
+    configs.SQLDefaultKeyDBpath = "sqlite+pysqlite:///" + os.path.join(
+        USER_DIR, ".krptn-data/altKMS.db"
+    )
+    configs.SQLDefaultUserDBpath = "sqlite+pysqlite:///" + os.path.join(
+        USER_DIR, ".krptn-data/users.db"
+    )
+
+
+setupDatabase()
---
+++ packages/krptn/krptn-0.8.6-cp310-cp310-macosx_11_0_arm64.whl/krypton/basic.py
@@ -0,0 +1,296 @@
+"""
+Basic security related classes.
+"""
+import os
+from datetime import datetime
+from typing import ByteString
+from sqlalchemy import select, func
+from sqlalchemy.orm import Session, scoped_session
+from .base import seal, unSeal, zeromem, passwordHash
+from . import configs, base, DBschemas
+
+SQLDefaultCryptoDBpath: Session = configs.SQLDefaultCryptoDBpath
+SQLDefaultKeyDBpath: Session = configs.SQLDefaultKeyDBpath
+
+
+class KeyManagementError(RuntimeError):
+    """Error in Key Management System
+
+    For example, compliance issues
+
+    Arguments:
+        Exception -- Inherits base Exception class
+    """
+
+    def __init__(self, *args: object) -> None:
+        self.message = args[0]
+        super().__init__()
+
+    def __str__(self) -> str:
+        return self.message
+
+
+class KMS:
+    """
+    They Key Management System
+    """
+
+    def _encipher(
+        self, text: ByteString, pwd: ByteString, salt: bytes, iterations: int
+    ):
+        """Encrypt a string
+
+        Arguments:
+            text -- Plain text
+
+            pwd -- Password
+
+            salt -- Salt for hashing
+
+            iterations -- Iterations for hashing
+
+        Returns:
+            Cipher text
+        """
+        key = passwordHash(pwd, salt, iterations) if iterations > 0 else pwd
+        r = seal(text, key)
+        zeromem(key)
+        return r
+
+    def _decipher(
+        self, ctext: ByteString, pwd: ByteString, salt: bytes, iterations: int
+    ):
+        """Decrypt a string
+
+        Arguments:
+            ctext -- Cipher text
+
+            pwd -- Password
+
+            salt -- Salt for Hashing (16 bytes)
+
+            iterations -- Iterations for hashing
+
+        Returns:
+            Plaintext
+        """
+        key = passwordHash(pwd, salt, iterations) if iterations > 0 else pwd
+        r = unSeal(ctext, key)
+        zeromem(key)
+        return r
+
+    def __init__(self, keyDB: Session = scoped_session(SQLDefaultKeyDBpath)) -> None:
+        """The title says it all"""
+        self.c: Session = keyDB
+
+    def getKey(self, name: str, pwd: ByteString = None, force: bool = False) -> bytes:
+        """Get a Key
+
+        Arguments:
+            name -- Name of the key to get
+
+        Keyword Arguments:
+            pwd -- Password (default: {None})
+
+            force -- Override Cryptoperiod Compliance errors (default: {False})
+
+        Raises:
+            ValueError: If the key does not exist
+
+            KeyManagementError: If the key has expired - set force=True to override
+
+            ValueError: If an unsupported cipher is used
+
+            ValueError: Wrong passwords were provided or the key was tampered with
+
+        Returns:
+            The key as python bytes
+        """
+        stmt = (
+            select(DBschemas.KeysTable).where(DBschemas.KeysTable.name == name).limit(1)
+        )
+        key: DBschemas.KeysTable = self.c.scalar(stmt)
+        if key is None:
+            raise ValueError("Such key does not exist")
+        if datetime.now().year - key.year >= configs.defaultCryptoperiod and not force:
+            raise KeyManagementError(
+                "This key has expired. Please add force to the argument to retrieve it anyway."
+            )
+        r = self._decipher(key.key, pwd, key.salt, key.saltIter)
+        splited = r.split(b"$")
+        if splited[1] != name.encode() or splited[2] != str(key.year).encode():
+            raise ValueError(
+                "Wrong passwords have been provided or the database has been tampered with."
+            )
+        result = base.base64decode(splited[0])
+        zeromem(r)
+        zeromem(splited[0])
+        return result
+
+    def createNewKey(self, name: str, pwd: ByteString = None) -> str:
+        """Create a new key and store it
+
+        Arguments:
+            name -- Name of the Key
+
+        Keyword Arguments:
+            pwd -- Password (default: {None})
+
+        Raises:
+            KeyError: If key with same name already exists
+
+        Returns:
+            The key as python bytes
+        """
+        year = datetime.today().year
+        stmt = select(DBschemas.KeysTable).where(DBschemas.KeysTable.name == "name")
+        a = True
+        try:
+            self.c.scalars(stmt).one()
+        except Exception:
+            a = False
+        finally:
+            if a:
+                raise KeyError("Such a name already exists")
+        k = os.urandom(configs._aesKeyLen)
+        s = os.urandom(configs._saltLen)
+        rebased = base.base64encode(k)
+        editedRebased = rebased + f"${name}${year}"
+        ek = self._encipher(editedRebased, pwd, s, configs.defaultArgonOps)
+        zeromem(rebased)
+        zeromem(editedRebased)
+        key = DBschemas.KeysTable(
+            name=name,
+            key=ek,
+            salt=s,
+            saltIter=configs.defaultArgonOps,
+            year=year,
+        )
+        self.c.add(key)
+        self.c.flush()
+        self.c.commit()
+        return k
+
+    def removeKey(self, name: str, pwd: ByteString = None) -> None:
+        """Delete a Key
+
+        Arguments:
+            name -- Name of the Key
+
+        Keyword Arguments:
+            pwd -- Password (default: {None})
+        """
+        zeromem(self.getKey(name, pwd, True))
+        stmt = (
+            select(DBschemas.KeysTable).where(DBschemas.KeysTable.name == name).limit(1)
+        )
+        key: DBschemas.KeysTable = self.c.scalar(stmt)
+        self.c.delete(key)
+        self.c.flush()
+        self.c.commit()
+
+    def __del__(self):
+        self.c.close()
+
+
+class Crypto(KMS):
+    """
+    Crypto Class (see Documentation)
+    """
+
+    def __init__(self, keyDB: Session = scoped_session(SQLDefaultCryptoDBpath)):
+        """The title says it all"""
+        self.c: scoped_session = keyDB
+        stmt = select(func.max(DBschemas.CryptoTable.id))
+        self.num = self.c.scalar(stmt)
+        super().__init__(self.c)
+
+    def secureCreate(self, data: ByteString, pwd: ByteString = None, _num: int = None):
+        """Store Encrypted Data
+
+        Arguments:
+            data -- Plaintext data
+
+        Keyword Arguments:
+            pwd -- Password To Decrypt (default: {None})
+
+            _num -- Unless you know what this is, not good idea to set! Id to store in DB (default: {None})
+
+        Returns:
+            Integer to be passed to secureRead to return data
+        """
+        if _num is None:
+            self.num += 1
+            _num = self.num
+        key = self.createNewKey(str(_num), pwd)
+        salt = os.urandom(configs._saltLen)
+        keyOb = DBschemas.CryptoTable(
+            id=_num,
+            ctext=self._encipher(data, key, salt, 0),
+            salt=salt,
+            saltIter=configs.defaultArgonOps,
+        )
+        self.c.add(keyOb)
+        zeromem(key)
+        self.c.flush()
+        self.c.commit()
+        return _num
+
+    def secureRead(self, num: int, pwd: ByteString):
+        """Read data from secureCreate
+
+        Arguments:
+            num -- Integer returned from secureCreate
+
+            pwd -- Password set in secureCreate
+
+        Returns:
+            Plaintext data
+        """
+        stmt = (
+            select(DBschemas.CryptoTable)
+            .where(DBschemas.CryptoTable.id == num)
+            .limit(1)
+        )
+        ctext = self.c.scalar(stmt)
+        reset = False
+        # Reset indicates whether cryptographic keys need rotating
+        try:
+            key = self.getKey(str(num), pwd)
+        except KeyManagementError:
+            reset = True
+            key = self.getKey(str(num), pwd, True)
+        text = self._decipher(ctext.ctext, key, ctext.salt, 0)
+        if reset:
+            self.secureUpdate(num, text, pwd)
+        zeromem(key)
+        return text
+
+    def secureUpdate(self, num: int, new: ByteString, pwd: ByteString):
+        """Update Entry Set by secureCreate
+
+        Arguments:
+            num -- Integer id of entry
+
+            new -- New data to set
+
+            pwd -- Password
+        """
+        self.secureDelete(num, pwd)
+        self.secureCreate(new, pwd, num)
+
+    def secureDelete(self, num: int, pwd: ByteString = None) -> None:
+        """Delete Data set by secureCreate
+
+        Arguments:
+            num -- Integer id of entry
+
+        Keyword Arguments:
+            pwd -- Password (default: {None})
+        """
+        zeromem(self.getKey(str(num), pwd, True))
+        stmt = select(DBschemas.CryptoTable).where(DBschemas.CryptoTable.id == num)
+        key: DBschemas.CryptoTable = self.c.scalar(stmt)
+        self.c.delete(key)
+        self.c.flush()
+        self.removeKey(str(num), pwd)
