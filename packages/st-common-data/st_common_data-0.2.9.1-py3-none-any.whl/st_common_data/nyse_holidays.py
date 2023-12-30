38fcab36b4673da68a53d3fd893ef5a637e4b63f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:05:28 +0000

    Add package st-common-data/0.2.9.1/st_common_data-0.2.9.1-py3-none-any.whl

---
+++ packages/st-common-data/st_common_data-0.2.9.1-py3-none-any.whl/st_common_data/nyse_holidays.py
@@ -0,0 +1,272 @@
+NYSE_HOLIDAYS = [
+    # ---------- 2019 --------------------
+    {
+        'date': '01.01.2019',
+        'name': 'New Year\'s Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '21.01.2019',
+        'name': 'Martin Luther King Jr. Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '18.02.2019',
+        'name': 'Washington\'s Birthday',
+        'status': 'Closed',
+    },
+    {
+        'date': '19.04.2019',
+        'name': 'Good Friday',
+        'status': 'Closed',
+    },
+    {
+        'date': '27.05.2019',
+        'name': 'Memorial Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '04.07.2019',
+        'name': 'Independence Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '02.09.2019',
+        'name': 'Labor Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '28.11.2019',
+        'name': 'Thanksgiving Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '29.11.2019',
+        'name': 'Thanksgiving Day',
+        'status': '9:30 AM - 1:00 PM',
+    },
+    {
+        'date': '24.12.2019',
+        'name': 'Christmas',
+        'status': '9:30 AM - 1:00 PM',
+    },
+    {
+        'date': '25.12.2019',
+        'name': 'Christmas',
+        'status': 'Closed',
+    },
+    # ---------- 2020 --------------------
+    {
+        'date': '01.01.2020',
+        'name': 'New Year\'s Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '20.01.2020',
+        'name': 'Martin Luther King Jr. Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '17.02.2020',
+        'name': 'Washington\'s Birthday',
+        'status': 'Closed',
+    },
+    {
+        'date': '10.04.2020',
+        'name': 'Good Friday',
+        'status': 'Closed',
+    },
+    {
+        'date': '25.05.2020',
+        'name': 'Memorial Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '03.07.2020',
+        'name': 'Independence Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '07.09.2020',
+        'name': 'Labor Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '26.11.2020',
+        'name': 'Thanksgiving Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '27.11.2020',
+        'name': 'Thanksgiving Day',
+        'status': '9:30 AM - 1:00 PM',
+    },
+    {
+        'date': '24.12.2020',
+        'name': 'Christmas',
+        'status': '9:30 AM - 1:00 PM',
+    },
+    {
+        'date': '25.12.2020',
+        'name': 'Christmas',
+        'status': 'Closed',
+    },
+    # ---------- 2021 --------------------
+    {
+        'date': '01.01.2021',
+        'name': 'New Year\'s Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '18.01.2021',
+        'name': 'Martin Luther King Jr. Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '15.02.2021',
+        'name': 'Washington\'s Birthday',
+        'status': 'Closed',
+    },
+    {
+        'date': '02.04.2021',
+        'name': 'Good Friday',
+        'status': 'Closed',
+    },
+    {
+        'date': '31.05.2021',
+        'name': 'Memorial Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '05.07.2021',
+        'name': 'Independence Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '06.09.2021',
+        'name': 'Labor Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '25.11.2021',
+        'name': 'Thanksgiving Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '26.11.2021',
+        'name': 'Thanksgiving Day',
+        'status': '9:30 AM - 1:00 PM',
+    },
+    {
+        'date': '24.12.2021',
+        'name': 'Christmas',
+        'status': 'Closed',
+    },
+    # ---------- 2022 -------------------
+    {
+        'date': '17.01.2022',
+        'name': 'Martin Luther King Jr. Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '21.02.2022',
+        'name': 'Washington\'s Birthday',
+        'status': 'Closed',
+    },
+    {
+        'date': '15.04.2022',
+        'name': 'Good Friday',
+        'status': 'Closed',
+    },
+    {
+        'date': '30.05.2022',
+        'name': 'Memorial Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '20.06.2022',
+        'name': 'Juneteenth N.I. Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '04.07.2022',
+        'name': 'Independence Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '05.09.2022',
+        'name': 'Labor Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '24.11.2022',
+        'name': 'Thanksgiving Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '25.11.2022',
+        'name': 'Thanksgiving Day',
+        'status': '9:30 AM - 1:00 PM',
+    },
+    {
+        'date': '26.12.2022',
+        'name': 'Christmas Day',
+        'status': 'Closed',
+    },
+    # ---------- 2023 -------------------
+    {
+        'date': '02.01.2023',
+        'name': 'New Years Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '16.01.2023',
+        'name': 'Martin Luther King Jr. Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '20.02.2023',
+        'name': 'Washington\'s Birthday',
+        'status': 'Closed',
+    },
+    {
+        'date': '07.04.2023',
+        'name': 'Good Friday',
+        'status': 'Closed',
+    },
+    {
+        'date': '29.05.2023',
+        'name': 'Memorial Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '19.06.2023',
+        'name': 'Juneteenth N.I. Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '04.07.2023',
+        'name': 'Independence Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '04.09.2023',
+        'name': 'Labor Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '23.11.2023',
+        'name': 'Thanksgiving Day',
+        'status': 'Closed',
+    },
+    {
+        'date': '24.11.2023',
+        'name': 'Thanksgiving Day',
+        'status': '9:30 AM - 1:00 PM',
+    },
+    {
+        'date': '25.12.2023',
+        'name': 'Christmas Day',
+        'status': 'Closed',
+    },
+]
