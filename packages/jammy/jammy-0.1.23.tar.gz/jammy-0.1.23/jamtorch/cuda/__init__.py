9beaa5d2ef3d5408bf7eef1b6274c776937823b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 04:04:04 +0000

    Add package jammy/0.1.23/jammy-0.1.23.tar.gz

---
+++ packages/jammy/jammy-0.1.23.tar.gz/jammy-0.1.23/jamtorch/cuda/__init__.py
@@ -0,0 +1,22 @@
+from .deep_to import *
+from .device import *
+from .to import *
+
+
+def get_torch_info():
+    torch_flag_list = [
+        "torch.backends.cuda.is_built",
+        "torch.backends.cuda.matmul.allow_tf32",
+        "torch.backends.cuda.matmul.allow_fp16_reduced_precision_reduction",
+        "torch.backends.cuda.matmul.allow_bf16_reduced_precision_reduction",
+        "torch.backends.cudnn.allow_tf32",
+        "torch.backends.cudnn.benchmark",
+    ]
+    torch_info = {}
+    for flag in torch_flag_list:
+        value = eval(flag)  # pylint: disable=eval-used
+        if callable(value):
+            torch_info[flag] = value()
+        else:
+            torch_info[flag] = value
+    return torch_info
