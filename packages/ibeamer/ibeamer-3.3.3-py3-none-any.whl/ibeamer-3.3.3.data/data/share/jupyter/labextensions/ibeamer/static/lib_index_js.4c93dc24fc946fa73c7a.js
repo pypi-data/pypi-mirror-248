dc8ee045e9c1901b52bc6c0416484b994013e1ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:05:32 +0000

    Add package ibeamer/3.3.3/ibeamer-3.3.3-py3-none-any.whl

---
+++ packages/ibeamer/ibeamer-3.3.3-py3-none-any.whl/ibeamer-3.3.3.data/data/share/jupyter/labextensions/ibeamer/static/lib_index_js.4c93dc24fc946fa73c7a.js
@@ -0,0 +1,43 @@
+"use strict";
+(self["webpackChunkibeamer"] = self["webpackChunkibeamer"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _lumino_widgets__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @lumino/widgets */ "webpack/sharing/consume/default/@lumino/widgets");
+/* harmony import */ var _lumino_widgets__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_lumino_widgets__WEBPACK_IMPORTED_MODULE_1__);
+
+
+/**
+ * Initialization data for the ibeamer extension.
+ */
+const plugin = {
+    id: 'ibeamer:plugin',
+    description: 'A simple .css LaTeX Beamer Environment Extension for Jupyter Lab/Notebooks',
+    autoStart: true,
+    activate: (app) => {
+        // Widget & DOMUtils Stuff goes here ...
+        console.log('JupyterLab extension ibeamer is activated!');
+        const node = document.createElement('div');
+        node.innerHTML = '<a href="https://www.lambda.joburg">link - click here</a>';
+        const widget = new _lumino_widgets__WEBPACK_IMPORTED_MODULE_1__.Widget({ node });
+        widget.id = _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.DOMUtils.createDomID();
+        app.shell.add(widget, 'top', { rank: 1000 });
+    }
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.4c93dc24fc946fa73c7a.js.map
