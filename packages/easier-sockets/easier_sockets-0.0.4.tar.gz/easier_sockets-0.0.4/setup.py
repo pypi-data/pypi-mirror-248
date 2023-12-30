dc72fa573d9ea94e56afb188049eb8992ae7d354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:03:55 +0000

    Add package easier-sockets/0.0.4/easier_sockets-0.0.4.tar.gz

---
+++ packages/easier-sockets/easier_sockets-0.0.4.tar.gz/easier_sockets-0.0.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.4'
+
+setup(
+    name="easier_sockets",
+    version=VERSION,
+    author="OverclockedD2",
+    author_email="<overclockedd2@gmail.com>",
+    packages=find_packages(),
+    install_requires=['rsa'],
+    keywords=['python', 'sockets', 'encrypted', 'rsa', 'easy', 'simple'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+    ]
+)
