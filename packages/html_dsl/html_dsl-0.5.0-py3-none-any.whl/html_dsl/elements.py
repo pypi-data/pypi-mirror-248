03648bd58430a6b4271278e3f3514bb7f4c899ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 15:03:58 +0000

    Add package html_dsl/0.5.0/html_dsl-0.5.0-py3-none-any.whl

---
+++ packages/html_dsl/html_dsl-0.5.0-py3-none-any.whl/html_dsl/elements.py
@@ -0,0 +1,63 @@
+from collections.abc import Iterable
+from dataclasses import dataclass, field
+from typing import Any, Optional
+
+
+def flatten(source: Any):
+    if isinstance(source, str) or not isinstance(source, Iterable):
+        yield source
+        return
+    for element in source:
+        yield from flatten(element)
+
+
+@dataclass
+class BaseHtmlElement:
+    name: str
+    single: bool = False
+    no_content: bool = False
+    parent: Optional["BaseHtmlElement"] = None
+    attrs: dict = field(default_factory=dict)
+    children: list = field(default_factory=list)
+
+    def __call__(self, **attrs) -> "BaseHtmlElement":
+        if "_class" in attrs:
+            attrs["class"] = attrs.pop("_class")
+        return BaseHtmlElement(self.name, self.single, self.no_content, self.parent, attrs)
+
+    def __getitem__(self, children) -> "BaseHtmlElement":
+        element = BaseHtmlElement(self.name, self.single, self.no_content, self.parent, self.attrs)
+        true_children = list(flatten(children))
+        for one in true_children:
+            if isinstance(one, BaseHtmlElement):
+                one.parent = element
+        element.children = true_children
+        return element
+
+    def level(self) -> int:
+        return self.parent.level() + 1 if self.parent else 0
+
+    def __repr__(self):
+        blank = "  " * self.level()
+        attrs = (
+            "({})".format(";".join(f"{key}={repr(value)}" for key, value in self.attrs.items())) if self.attrs else ""
+        )
+        children = "\n".join(
+            repr(child) if isinstance(child, BaseHtmlElement) else blank + repr(child) for child in self.children
+        )
+        if self.single or self.no_content:
+            return "{blank}{name}{attrs}".format(blank=blank, name=self.name, attrs=attrs)
+        return "{blank}{name}{attrs}[\n{children}]".format(blank=blank, name=self.name, attrs=attrs, children=children)
+
+    def __str__(self):
+        attrs = (
+            " {}".format(" ".join(f'{key.replace("_", "-")}="{str(value)}"' for key, value in self.attrs.items()))
+            if self.attrs
+            else ""
+        )
+        children = "".join(str(child) if isinstance(child, BaseHtmlElement) else str(child) for child in self.children)
+        if self.single:
+            return "<{name}{attrs}>".format(name=self.name, attrs=attrs)
+        if self.no_content:
+            return "<{name}{attrs}/>".format(name=self.name, attrs=attrs)
+        return "<{name}{attrs}>{children}</{name}>".format(name=self.name, attrs=attrs, children=children)
