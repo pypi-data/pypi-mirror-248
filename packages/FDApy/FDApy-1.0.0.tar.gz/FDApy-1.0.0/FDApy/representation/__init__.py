724bfcce7457a1fb9ed60aeffa565f8ea514c417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:04:59 +0000

    Add package FDApy/1.0.0/FDApy-1.0.0.tar.gz

---
+++ packages/FDApy/FDApy-1.0.0.tar.gz/FDApy-1.0.0/FDApy/representation/__init__.py
@@ -0,0 +1,7 @@
+"""
+Representation module.
+
+This module is used to manage the different representation of functional data.
+Thus, DenseFunctionalData and IrregularFunctionalData are the basis classes
+from which the others are build upon.
+"""
