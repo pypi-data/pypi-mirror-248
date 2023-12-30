99eb46fd7b2af3bf980153b19d6d574dc4e47b66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 15:03:58 +0000

    Add package avro.py/2023.12.30/avro.py-2023.12.30.tar.gz

---
+++ packages/avro.py/avro.py-2023.12.30.tar.gz/avro.py-2023.12.30/avro/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-License-Identifier: MIT
+
+
+# Import local modules.
+from .main import *
+
+# Set version information.
+__version_info__ = ('2023', '12', '30')  # YYYY / MM / DD
+__version__ = '.'.join(__version_info__)
