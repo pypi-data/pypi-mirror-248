b49d3b62898a2c99dacd7519e0c874ccf32ebc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 04:04:03 +0000

    Add package gapmap/1.0.2/gapmap-1.0.2.tar.gz

---
+++ packages/gapmap/gapmap-1.0.2.tar.gz/gapmap-1.0.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+VERSION = "1.0.2"
+AUTHOR = "Paul Gardiner"
+AUTHOR_EMAIL = "franticstone@gmail.com"
+APP_NAME = "gapmap"
+DESCRIPTION = "Maptools"
+
+setup(
+    name=APP_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author_email=AUTHOR_EMAIL,
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["radius", "point", "mathlib"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
