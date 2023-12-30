15120c62a6778e66d5a3d3ec1183969b343ac187
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 14:04:24 +0000

    Add package seversion/2024.1.0rc3/seversion-2024.1.0rc3.tar.gz

---
+++ packages/seversion/seversion-2024.1.0rc3.tar.gz/seversion-2024.1.0rc3/seVersion/_version.py
@@ -0,0 +1,10 @@
+"""
+Provides `seVersion` version information.
+"""
+# This file is auto-generated! Do not edit!
+# Use `seVersion seVersion` to change this file.
+
+from seVersion import Version
+__version__ = Version("seVersion", 2024, 1, 0, 29, release_candidate=3)
+__setuptools__ = __version__.public()
+__all__ = ["__version__", "__setuptools__"]
