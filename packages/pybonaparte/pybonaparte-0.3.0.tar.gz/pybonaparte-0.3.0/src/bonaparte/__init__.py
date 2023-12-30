3b0bb723e1547093b4f9a47bfe36b1e95b6a74ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:05:08 +0000

    Add package pybonaparte/0.3.0/pybonaparte-0.3.0.tar.gz

---
+++ packages/pybonaparte/pybonaparte-0.3.0.tar.gz/pybonaparte-0.3.0/src/bonaparte/__init__.py
@@ -0,0 +1,8 @@
+"""The Bonaparte Library."""
+from __future__ import annotations
+
+__version__ = "0.3.0"
+
+from .fireplace import Fireplace, FireplaceFeatures, FireplaceState
+
+__all__ = ["Fireplace", "FireplaceFeatures", "FireplaceState"]
