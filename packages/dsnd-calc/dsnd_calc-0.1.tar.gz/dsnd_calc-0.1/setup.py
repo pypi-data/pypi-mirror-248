e5d307d63d8a951eee11eb60405d09facb8d124a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:04:45 +0000

    Add package dsnd-calc/0.1/dsnd_calc-0.1.tar.gz

---
+++ packages/dsnd-calc/dsnd_calc-0.1.tar.gz/dsnd_calc-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='dsnd_calc',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_calc'],
+      author_email = 'farouk.sallak@outlook.com',
+      zip_safe=False)
