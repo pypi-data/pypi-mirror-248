8ba256e880ed3183ce0eab9a80ece3cfc8d3f468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:03:57 +0000

    Add package dfpwm/0.1.5/dfpwm-0.1.5-cp311-cp311-manylinux_2_38_x86_64.whl

---
+++ packages/dfpwm/dfpwm-0.1.5-cp311-cp311-manylinux_2_38_x86_64.whl/dfpwm/resample.py
@@ -0,0 +1,17 @@
+from typing import BinaryIO
+
+import numpy as np
+
+SAMPLE_RATE = 48000
+
+
+def resample_from_file(io: "BinaryIO") -> tuple["np.ndarray", float]:
+    import warnings
+    import librosa
+    warnings.warn("resample_from_file is deprecated, use dfpwm.resample instead")
+    return librosa.load(io, dtype='float32', sr=SAMPLE_RATE)
+
+
+def resample(data: np.ndarray, origin_samplerate: float, target_sample_rate=SAMPLE_RATE):
+    import librosa
+    return librosa.resample(data, orig_sr=origin_samplerate, target_sr=target_sample_rate)
