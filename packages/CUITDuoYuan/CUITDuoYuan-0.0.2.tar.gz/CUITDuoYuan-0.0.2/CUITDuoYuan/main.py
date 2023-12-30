bf93277dbbb70e7bdd2844479e0b3634f55dbbde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 05:04:20 +0000

    Add package CUITDuoYuan/0.0.2/CUITDuoYuan-0.0.2.tar.gz

---
+++ packages/CUITDuoYuan/CUITDuoYuan-0.0.2.tar.gz/CUITDuoYuan-0.0.2/CUITDuoYuan/main.py
@@ -0,0 +1,17 @@
+from .question import question
+
+
+class CUIT:
+    all_question = question
+
+    @classmethod
+    def get_answer(cls, question):
+        ret = []
+        for i in cls.all_question:
+            if question in i.question_text:
+                ret.append(i.answer)
+
+        if len(ret) == 0:
+            return '没有题目'
+        else:
+            return ret
---
+++ packages/CUITDuoYuan/CUITDuoYuan-0.0.2.tar.gz/CUITDuoYuan-0.0.2/CUITDuoYuan/question.py
@@ -0,0 +1,12 @@
+class Question:
+    def __init__(self,question,answer):
+        self.question_text=question
+        self.answer=answer
+
+
+
+
+question=[
+    Question('1+1','2'),
+    Question('2+2','4'),
+]
