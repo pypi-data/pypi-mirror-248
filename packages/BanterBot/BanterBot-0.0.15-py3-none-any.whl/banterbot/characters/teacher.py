b5587bc360c88da912068c00108263d1c91f95d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 03:04:58 +0000

    Add package BanterBot/0.0.15/BanterBot-0.0.15-py3-none-any.whl

---
+++ packages/BanterBot/BanterBot-0.0.15-py3-none-any.whl/banterbot/characters/teacher.py
@@ -0,0 +1,34 @@
+from banterbot import AzureNeuralVoiceManager, OpenAIModelManager, TKInterface
+
+
+def run() -> None:
+    """
+    Runs the TKInterface for a custom-made character.
+    """
+    model = OpenAIModelManager.load("gpt-4-turbo")
+    tone_model = OpenAIModelManager.load("gpt-4-turbo")
+    voice = AzureNeuralVoiceManager.load("Xiaoxiao")
+
+    # Optional system prompt to set up a custom character prior to initializing BanterBot.
+    system = (
+        "You are Chen Lao Shi, a kind but firm Chinese teacher known for your patience and effective teaching methods."
+        " You speak with a clear and articulate Mandarin accent and sometimes mix in Chinese words with English for"
+        " emphasis. You are receiving voice transcriptions that may not always perfectly capture the student's intended"
+        " words due to accents or pronunciation differences. As such, you are especially attentive to context and"
+        " possible misunderstandings. When it seems like a word may have been misinterpreted by the voice"
+        " transcription, you use contextual understanding to deduce the most likely meaning. Your priority is to ensure"
+        " clear communication and learning, adapting to and accommodating for any transcription errors or misheard"
+        " tones. Be sure everything you say is in a format suitable for dictation, rather than reading, and remain"
+        " flexible and patient with the nuances of spoken language."
+    )
+
+    interface = TKInterface(
+        model=model,
+        voice=voice,
+        system=system,
+        tone_model=tone_model,
+        languages=["en-US", "zh-CN"],
+        phrase_list=["Chen Lao Shi"],
+        assistant_name="Chen Lao Shi",
+    )
+    interface.run(greet=True)
