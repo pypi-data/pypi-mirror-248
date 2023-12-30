c548764e5ddb7225a25e2db3ac0ea1c20416c9b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 14:04:15 +0000

    Add package historical-geocoder/0.1.8/historical_geocoder-0.1.8.tar.gz

---
+++ packages/historical-geocoder/historical_geocoder-0.1.8.tar.gz/historical_geocoder-0.1.8/historical_geocoder/detector.py
@@ -0,0 +1,19 @@
+import json
+from .utils.detection import direction_detection_ch as direction_detector_ch
+
+
+def detect_direction(data, lang):
+    if lang == "ch":
+        with open("./data/direction.json", "r", encoding="utf-8-sig") as f:
+            direction_dictionary = json.load(f)
+        directions = direction_dictionary["ch"]["directions"]
+        direction_postfixes = direction_dictionary["ch"]["postfixes"]
+
+        direction_with_postfixes = directions.copy()
+        for direction in directions:
+            for direction_postfix in direction_postfixes:
+                direction_with_postfixes.append(direction + direction_postfix)
+
+    direction_detector_ch.detect_direction(data, direction_with_postfixes, direction_postfixes)
+    
+    return data
