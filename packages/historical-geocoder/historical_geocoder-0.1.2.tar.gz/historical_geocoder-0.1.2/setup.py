1317aff47283ad8d073e58a1bd7e20098f6283f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 04:04:21 +0000

    Add package historical-geocoder/0.1.2/historical_geocoder-0.1.2.tar.gz

---
+++ packages/historical-geocoder/historical_geocoder-0.1.2.tar.gz/historical_geocoder-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+VERSION = '0.1.2'
+
+setup(
+    name='historical_geocoder',  # package name
+    version=VERSION,  # package version
+    author='Yuqi Chen',
+    author_email='cyq0722@pku.edu.cn',
+    install_requires=[
+        'geopandas',
+        'geopy',
+        'shapely',
+        'openai',
+        'keplergl',
+        'setuptools',
+    ],
+    description='A library to extract historical toponyms and locations from texts, geocode the locations, and visualize the results.',
+    packages=find_packages(),
+    zip_safe=False,
+)
