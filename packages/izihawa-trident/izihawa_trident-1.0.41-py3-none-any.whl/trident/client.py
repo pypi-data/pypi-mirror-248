66ad40c60862e890e2a465ba07f79dcdb63becd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-30 04:04:12 +0000

    Add package izihawa-trident/1.0.41/izihawa_trident-1.0.41-py3-none-any.whl

---
+++ packages/izihawa-trident/izihawa_trident-1.0.41-py3-none-any.whl/trident/client.py
@@ -0,0 +1,51 @@
+from typing import AsyncGenerator
+
+from aiobaseclient import BaseClient
+from aiobaseclient.exceptions import ExternalServiceError
+
+
+class TridentClient(BaseClient):
+    async def response_processor(self, response):
+        if response.status == 404:
+            return None
+        elif response.status != 200:
+            data = await response.read()
+            if hasattr(response, "request"):
+                raise ExternalServiceError(response.request.url, response.status, data)
+            else:
+                raise ExternalServiceError(None, response.status, data)
+        return response
+
+    async def store(self, key: str, data: bytes, timeout: float = None) -> dict:
+        url = f"/kv/{key}"
+        response = await self.put(url, data=data, timeout=timeout)
+        return await response.json()
+
+    async def broadcast(self, doc_name: str, doc_key: str, key: str) -> dict:
+        url = f"/broadcast/{doc_name}/{doc_key}/"
+        response = await self.post(url, params={'key': key})
+        return await response.json()
+
+    async def delete_key(self, key: str) -> dict:
+        url = f"/kv/{key}"
+        response = await self.delete(url)
+        return await response.json()
+
+    async def read(self, key: str, timeout: float = None) -> bytes:
+        url = f"/kv/{key}"
+        response = await self.get(url, timeout=timeout)
+        return await response.read()
+
+    async def read_chunks(self, key: str, timeout: float = None) -> AsyncGenerator[bytes, None]:
+        url = f"/kv/{key}"
+        response = await self.get(url, timeout=timeout)
+        async for data, _ in response.content.iter_chunks():
+            yield data
+
+    async def exists(self, key: str) -> bool:
+        url = f"/kv/{key}/exists"
+        response = await self.get(url)
+        if response is None:
+            return False
+        response = await response.json()
+        return response["exists"]
