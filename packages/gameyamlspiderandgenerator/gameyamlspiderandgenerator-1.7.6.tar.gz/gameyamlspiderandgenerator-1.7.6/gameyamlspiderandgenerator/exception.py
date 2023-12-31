224dd2710768bc995f50bd5a10f85c0a47050e2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:04:30 +0000

    Add package gameyamlspiderandgenerator/1.7.6/gameyamlspiderandgenerator-1.7.6.tar.gz

---
+++ packages/gameyamlspiderandgenerator/gameyamlspiderandgenerator-1.7.6.tar.gz/gameyamlspiderandgenerator-1.7.6/gameyamlspiderandgenerator/exception.py
@@ -0,0 +1,18 @@
+class ReadOrWriteConfigFailed(Exception):
+    def __init__(self):
+        super().__init__("Failed to read or write config")
+
+
+class InvalidTargetResourceError(Exception):
+    def __init__(self, code: int):
+        super().__init__(f"The target resource is no longer valid.status code: {code}")
+
+
+class ResponseNotInitialized(Exception):
+    def __init__(self, url: str):
+        super().__init__(f"Response not initialized, url: {url}")
+
+
+class InvalidResponse(Exception):
+    def __init__(self, url: str):
+        super().__init__(f"Invalid response, url: {url}")
