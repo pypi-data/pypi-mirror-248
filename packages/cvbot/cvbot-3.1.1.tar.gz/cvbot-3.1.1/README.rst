73415d97824d31b13631355d8e409eaea0e62ce4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:04:17 +0000

    Add package cvbot/3.1.1/cvbot-3.1.1.tar.gz

---
+++ packages/cvbot/cvbot-3.1.1.tar.gz/cvbot-3.1.1/README.rst
@@ -0,0 +1,6 @@
+CVBot
+======
+
+
+**CVBot** is an image recognition Python package for visual based automation with mouse and keyboard control support.
+
---
+++ packages/cvbot/cvbot-3.1.1.tar.gz/cvbot-3.1.1/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+setup(
+    name='cvbot',
+    package=find_packages(','),
+    version='3.1.1',
+    license='LICENSE',
+    description='Computer vision toolbox for automating tasks, mainly desktop apps, games and others',
+    author='Hisham Moe',
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'mss',
+        'pynput',
+        'pywin32',
+        'easyocr',
+        'thefuzz[speedup]',
+        'onnx',
+        'onnxruntime'
+        ]
+    )
