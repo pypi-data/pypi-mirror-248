6f8369ca2837e8292a71ebd3c679b4b9a4e788b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:03:57 +0000

    Add package objectory/0.1.2/objectory-0.1.2-py3-none-any.whl

---
+++ packages/objectory/objectory-0.1.2-py3-none-any.whl/objectory/__init__.py
@@ -0,0 +1,6 @@
+__all__ = ["AbstractFactory", "OBJECT_INIT", "OBJECT_TARGET", "Registry", "factory"]
+
+from objectory.abstract_factory import AbstractFactory
+from objectory.constants import OBJECT_INIT, OBJECT_TARGET
+from objectory.registry import Registry
+from objectory.universal import factory
---
+++ packages/objectory/objectory-0.1.2-py3-none-any.whl/objectory/constants.py
@@ -0,0 +1,6 @@
+r"""This module defines some constants."""
+
+__all__ = ["OBJECT_INIT", "OBJECT_TARGET"]
+
+OBJECT_INIT = "_init_"
+OBJECT_TARGET = "_target_"
