10eb4f7cb4695e0b377e48a89ec66271db4e4e45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:04:28 +0000

    Add package PyMusify/1.0.0/PyMusify-1.0.0.tar.gz

---
+++ packages/PyMusify/PyMusify-1.0.0.tar.gz/PyMusify-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyMusify',
+    version='1.0.0',
+    packages=find_packages(),
+    install_requires=['numpy'],
+    author='KVI Kontent',
+    author_email='kviappsgames@Gmail.com',
+    description='A simple music generator library',
+    url='https://github.com/your_username/music-generator',
+)
