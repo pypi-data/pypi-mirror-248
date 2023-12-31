104ed68a44e134212526a2cbe3825f5e0e6c3f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:04:14 +0000

    Add package xiaopydesktop/1.0.8/xiaopydesktop-1.0.8-py3-none-any.whl

---
+++ packages/xiaopydesktop/xiaopydesktop-1.0.8-py3-none-any.whl/xiaopydesktop/__init__.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+# -*-coding:utf-8 -*-
+
+"""
+# File       : __init__.py.py
+# Time       ：2023.8.11 22:37
+# Author     ：Lex
+# HomePage   : xiaopy.cn
+# Email      : 2983997560@qq.com
+# Description：
+"""
+from .xiaopydesktop import DmObject, gl_data, td_data, Thread
