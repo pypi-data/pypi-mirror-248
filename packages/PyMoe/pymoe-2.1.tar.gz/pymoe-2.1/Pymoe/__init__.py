1a682b007afe82ef0a82bbf341cd2ddd76b00af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:23 +0000

    Add package PyMoe/2.1/pymoe-2.1.tar.gz

---
+++ packages/PyMoe/pymoe-2.1.tar.gz/pymoe-2.1/Pymoe/__init__.py
@@ -0,0 +1,7 @@
+from . import anime
+from . import ln
+from . import manga
+
+__doc__ = "This is Pymoe. The only python library you'll need if you need information on Anime, Manga, Live Novels, or Visual Novels."
+
+__version__ = "2.01"
