99e4c7b13328865eef4b766501cd396be7163559
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:04:27 +0000

    Add package domainlab/0.2.9/domainlab-0.2.9-py3-none-any.whl

---
+++ packages/domainlab/domainlab-0.2.9-py3-none-any.whl/domainlab/compos/vae/compos/encoder_dirichlet.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+from torch.distributions import Dirichlet
+
+
+class EncoderH2Dirichlet(nn.Module):
+    """
+    hidden representation to Dirichlet Distribution
+    """
+
+    def __init__(self, dim_topic, device):
+        """
+        """
+        super().__init__()
+        self.layer_bn = nn.BatchNorm1d(dim_topic)
+        self.device = device
+
+    def forward(self, hidden):
+        """
+        :param hidden:
+        """
+        feat_bnorm = self.layer_bn(hidden)
+        alphas_batch = feat_bnorm.exp()
+        q_topic = Dirichlet(alphas_batch)
+        topic_q = q_topic.rsample().to(self.device)
+        return q_topic, topic_q
