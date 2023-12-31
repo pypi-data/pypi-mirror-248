63826d6c6e0117783f021a08130ba022c835bc7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:03:45 +0000

    Add package django-react-templatetags/8.0.0/django_react_templatetags-8.0.0-py3-none-any.whl

---
+++ packages/django-react-templatetags/django_react_templatetags-8.0.0-py3-none-any.whl/django_react_templatetags/tests/test_representation_mixin.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+
+from django_react_templatetags.mixins import RepresentationMixin
+
+
+class RepresentationMixinTest(SimpleTestCase):
+    def test_implementation_error_raised_if_not_fully_implemented(self):
+        class MyObj(RepresentationMixin, object):
+            pass
+
+        instance = MyObj()
+
+        with self.assertRaises(NotImplementedError) as err:
+            instance.to_react_representation()
+
+        self.assertEqual(
+            str(err.exception), "Missing property to_react_representation in class"
+        )
