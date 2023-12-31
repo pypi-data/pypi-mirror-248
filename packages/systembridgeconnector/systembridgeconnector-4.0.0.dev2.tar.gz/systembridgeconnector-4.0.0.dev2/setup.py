e1b6d71adb3f20f37db65d6616fa37f73893a416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:03:54 +0000

    Add package systembridgeconnector/4.0.0.dev2/systembridgeconnector-4.0.0.dev2.tar.gz

---
+++ packages/systembridgeconnector/systembridgeconnector-4.0.0.dev2.tar.gz/systembridgeconnector-4.0.0.dev2/setup.py
@@ -0,0 +1,24 @@
+"""Setup."""
+from setuptools import find_packages, setup
+
+# Get setup packages from requirements.txt
+with open("requirements_setup.txt", encoding="utf-8") as f:
+    requirements_setup = f.read().splitlines()
+
+# Get packages from requirements.txt
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="systembridgeconnector",
+    description="System Bridge Connector",
+    keywords="system-bridge",
+    author="Aidan Timson (Timmo)",
+    author_email="aidan@timmo.dev",
+    license="Apache-2.0",
+    url="https://github.com/timmo001/system-bridge-connector",
+    packages=find_packages(exclude=["tests", "generator"]),
+    install_requires=requirements,
+    setup_requires=requirements_setup,
+    use_incremental=True,
+)
