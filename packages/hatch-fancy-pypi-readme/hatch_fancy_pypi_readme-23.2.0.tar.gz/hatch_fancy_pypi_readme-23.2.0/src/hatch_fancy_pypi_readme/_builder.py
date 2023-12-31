135b0ab7ad3d5bb11854d9b7eec7c00c02ee09c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:28 +0000

    Add package hatch-fancy-pypi-readme/23.2.0/hatch_fancy_pypi_readme-23.2.0.tar.gz

---
+++ packages/hatch-fancy-pypi-readme/hatch_fancy_pypi_readme-23.2.0.tar.gz/hatch_fancy_pypi_readme-23.2.0/src/hatch_fancy_pypi_readme/_builder.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2022 Hynek Schlawack <hs@ox.cx>
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from ._fragments import Fragment
+    from ._substitutions import Substituter
+
+
+def build_text(
+    fragments: list[Fragment],
+    substitutions: list[Substituter],
+    version: str,
+) -> str:
+    text = "".join(f.render() for f in fragments)
+
+    for sub in substitutions:
+        text = sub.substitute(text)
+
+    return text.replace("$HFPR_VERSION", version)
