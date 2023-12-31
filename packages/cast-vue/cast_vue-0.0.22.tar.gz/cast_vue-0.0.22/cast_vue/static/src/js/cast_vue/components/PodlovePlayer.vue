f5f3b2dcd05754790c9a8e50583b66818d98f536
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:48 +0000

    Add package cast-vue/0.0.22/cast_vue-0.0.22.tar.gz

---
+++ packages/cast-vue/cast_vue-0.0.22.tar.gz/cast_vue-0.0.22/cast_vue/static/src/js/cast_vue/components/PodlovePlayer.vue
@@ -0,0 +1,39 @@
+<template>
+    <div ref="player"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted, onBeforeUnmount, ref } from 'vue';
+declare const podlovePlayer: any;
+
+
+export default defineComponent({
+    name: 'PodlovePlayer',
+    props: {
+        elementId: {
+            type: String,
+            required: true,
+        },
+        apiUrl: {
+            type: String,
+            required: true,
+        },
+    },
+    setup(props) {
+        const player = ref(null);
+
+        onMounted(() => {
+            // Ensure that the function is available globally
+            if (typeof podlovePlayer === 'function') {
+                podlovePlayer(props.elementId, props.apiUrl);
+            }
+        });
+
+        onBeforeUnmount(() => {
+            // Perform any necessary cleanup here
+        });
+
+        return { player };
+    },
+});
+</script>
