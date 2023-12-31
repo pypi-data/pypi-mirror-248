9d5ac5d58eeac977a95f7b1722013fbc378f10d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:20 +0000

    Add package timefmt/0.2.0/timefmt-0.2.0.tar.gz

---
+++ packages/timefmt/timefmt-0.2.0.tar.gz/timefmt-0.2.0/src/timefmt/__init__.py
@@ -0,0 +1,7 @@
+"""Provides methods to manipulate and format various time-based objects into human-readable strings."""
+
+__version__ = "0.2.0"
+
+from timefmt._helpers import SplitTime, split_seconds, day_of_month_suffix, day_of_month_string, timezone_name
+from timefmt import dt, td
+from timefmt._auto import auto
