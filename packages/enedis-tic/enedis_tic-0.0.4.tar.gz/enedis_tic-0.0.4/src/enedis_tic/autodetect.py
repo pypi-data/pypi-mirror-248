607ae654fac9b6a9bda663c96dd2b44dabf5a355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:04:22 +0000

    Add package enedis-tic/0.0.4/enedis_tic-0.0.4.tar.gz

---
+++ packages/enedis-tic/enedis_tic-0.0.4.tar.gz/enedis_tic-0.0.4/src/enedis_tic/autodetect.py
@@ -0,0 +1,33 @@
+import asyncio
+
+from . import physical_layer
+from serial.tools.list_ports import comports
+
+
+async def autodetect():
+    tasks = []
+    for port in comports():
+        tasks.append(asyncio.create_task(detect(port.device)))
+    await asyncio.gather(*tasks)
+    return [t.result() for t in tasks if t.result()]
+
+
+async def detect(port):
+    for mode in ('H', 'S'):
+        try:
+            res = await asyncio.wait_for(_detect_port(port, mode), 3)
+        except Exception:
+            continue
+        else:
+            return res
+    return None
+
+
+async def _detect_port(port, mode):
+    reader = await physical_layer.serial_reader(port, mode)
+    async for raw_frame in physical_layer.frame_iterator(reader):
+        return {
+            'port': port,
+            'mode': mode,
+            'sep': '\t' if '\t' in raw_frame else ' '
+        }
