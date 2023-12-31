f5bb829c1a1da39bbafe4d1c59560c14282a1a08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:09 +0000

    Add package counterweight/0.0.3/counterweight-0.0.3.tar.gz

---
+++ packages/counterweight/counterweight-0.0.3.tar.gz/counterweight-0.0.3/counterweight/events.py
@@ -0,0 +1,51 @@
+from time import monotonic_ns
+from typing import Union
+
+from pydantic import Field
+
+from counterweight.geometry import Position
+from counterweight.types import FrozenForbidExtras
+
+
+class _Event(FrozenForbidExtras):
+    timestamp_ns: int = Field(default_factory=monotonic_ns)
+
+
+class TerminalResized(_Event):
+    pass
+
+
+class KeyPressed(_Event):
+    key: str
+
+
+class MouseMoved(_Event):
+    position: Position
+
+
+class MouseDown(_Event):
+    position: Position
+    button: int
+
+
+class MouseUp(_Event):
+    position: Position
+
+
+class StateSet(_Event):
+    pass
+
+
+class Dummy(_Event):
+    pass
+
+
+AnyEvent = Union[
+    TerminalResized,
+    KeyPressed,
+    MouseMoved,
+    MouseDown,
+    MouseUp,
+    StateSet,
+    Dummy,
+]
