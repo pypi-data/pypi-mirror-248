73dfc0033cfbc77071f86172da6b7116709c9a67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:26 +0000

    Add package shmessy/0.0.6/shmessy-0.0.6.tar.gz

---
+++ packages/shmessy/shmessy-0.0.6.tar.gz/shmessy-0.0.6/src/shmessy/validators/ipv4_address.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+from numpy import ndarray
+from pandas import Series
+from pydantic import BaseModel
+from pydantic.networks import IPv4Address  # noqa
+
+from ..schema import InferredField, ValidatorTypes
+from .base import BaseValidator
+
+
+class Model(BaseModel):
+    ip: IPv4Address
+
+
+class Validator(BaseValidator):
+    validator_type = ValidatorTypes.STRING
+
+    def validate(self, data: ndarray) -> Optional[InferredField]:
+        if not self.is_validator_type_valid(dtype=data.dtype):
+            return None
+
+        for value in data:
+            try:
+                Model(ip=value)
+            except ValueError:
+                return None
+        return InferredField(inferred_type=str, inferred_virtual_type=IPv4Address)
+
+    def fix(self, column: Series, sample_size: int) -> Series:
+        sample_data = column[:sample_size]
+        inferred = self.validate(sample_data)
+        if inferred:
+            return column
