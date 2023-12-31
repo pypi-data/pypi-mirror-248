fcb34aa5bd3a1f130edab20837473a845310965e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:03:59 +0000

    Add package netk/0.1.1/netk-0.1.1.tar.gz

---
+++ packages/netk/netk-0.1.1.tar.gz/netk-0.1.1/netk/base.py
@@ -0,0 +1,21 @@
+from .attribute import NAttribute
+from .object import NObject
+
+
+class NBase(NObject):
+
+    _type = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self._ = self._type(*args, **kwargs)
+
+    def bind(self, name, func):
+        _ = getattr(self._, name)
+        _ += func
+        setattr(self._, name, func)
+
+    def unbind(self, name, func):
+        _ = getattr(self._, name)
+        _ -= func
+        setattr(self._, name, func)
