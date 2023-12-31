d2aed8fa39079fd6c63490e2920868a05b22d640
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:03:57 +0000

    Add package pybloxlink/0.2.0/pybloxlink-0.2.0.tar.gz

---
+++ packages/pybloxlink/pybloxlink-0.2.0.tar.gz/pybloxlink-0.2.0/bloxlink/__init__.py
@@ -0,0 +1,9 @@
+__title__ = "pybloxlink",
+__author__ = "acatiadroid",
+__license__ = "MIT"
+__copyright__ = "Copyright 2023-present acatiadroid"
+__version__ = "0.2.0"
+
+from .client import Bloxlink
+from .objects import *
+from .exception import *
