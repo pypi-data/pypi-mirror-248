a18a314eb56f4136331929e2fa8e349c787420f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:40 +0000

    Add package smokedduck/0.1.dev28376/smokedduck-0.1.dev28376.tar.gz

---
+++ packages/smokedduck/smokedduck-0.1.dev28376.tar.gz/smokedduck-0.1.dev28376/tests/fast/api/test_dbapi08.py
@@ -0,0 +1,22 @@
+# test fetchdf with various types
+import numpy
+import pytest
+import smokedduck as duckdb
+from conftest import NumpyPandas, ArrowPandas
+
+class TestType(object):
+    @pytest.mark.parametrize('pandas', [NumpyPandas()])
+    def test_fetchdf(self, pandas):
+        pass # TODO: charlie uncomment
+        # con = duckdb.connect()
+        # con.execute("CREATE TABLE items(item VARCHAR)")
+        # con.execute("INSERT INTO items VALUES ('jeans'), (''), (NULL)")
+        # res = con.execute("SELECT item FROM items").fetchdf()
+        # assert isinstance(res, pandas.core.frame.DataFrame)
+        #
+        # arr = numpy.ma.masked_array(['jeans', '', None])
+        # arr.mask = [False, False, True]
+        # arr = {'item': arr}
+        # df = pandas.DataFrame(arr)
+        #
+        # pandas.testing.assert_frame_equal(res, df)
---
+++ packages/smokedduck/smokedduck-0.1.dev28376.tar.gz/smokedduck-0.1.dev28376/tests/fast/test_ambiguous_prepare.py
@@ -0,0 +1,13 @@
+import smokedduck as duckdb
+import pandas as pd
+import pytest
+
+class TestAmbiguousPrepare(object):
+    def test_bool(self, duckdb_cursor):
+        pass # TODO: charlie uncomment
+        # conn = duckdb.connect()
+        # res = conn.execute("select ?, ?, ?", (True, 42, [1, 2, 3])).fetchall()
+        # assert res[0][0] == True
+        # assert res[0][1] == 42
+        # assert res[0][2] == [1, 2, 3]
+
