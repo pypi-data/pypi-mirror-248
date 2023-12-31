abe855c4ae9cc62c4ee7d22b0051d4de38bfbad5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 03:04:04 +0000

    Add package odoo14-addon-ssi-company-currency-mixin/14.0.1.1.0/odoo14_addon_ssi_company_currency_mixin-14.0.1.1.0-py3-none-any.whl

---
+++ packages/odoo14-addon-ssi-company-currency-mixin/odoo14_addon_ssi_company_currency_mixin-14.0.1.1.0-py3-none-any.whl/odoo/addons/ssi_company_currency_mixin/models/mixin_company_currency.py
@@ -0,0 +1,28 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from odoo import api, fields, models
+
+
+class MixinCompanyCurrency(models.AbstractModel):
+    _name = "mixin.company_currency"
+    _description = "Company Currency Mixin"
+
+    @api.model
+    def _default_company_id(self):
+        return self.env.user.company_id.id
+
+    company_id = fields.Many2one(
+        string="Company",
+        comodel_name="res.company",
+        required=True,
+        default=lambda self: self._default_company_id(),
+        copy=True,
+    )
+    company_currency_id = fields.Many2one(
+        string="Company Currency",
+        comodel_name="res.currency",
+        related="company_id.currency_id",
+        store=True,
+    )
