c6d9278ec21b6774efe57d76f942cefe18e21096
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:04:03 +0000

    Add package leads/0.1.13/leads-0.1.13.tar.gz

---
+++ packages/leads/leads-0.1.13.tar.gz/leads-0.1.13/leads_vec/_bootloader/systemd.py
@@ -0,0 +1,18 @@
+from os import chmod as _chmod
+from os.path import abspath as _abspath
+
+
+def create_service() -> None:
+    _chmod(script := _abspath(__file__)[:-10] + "leads.service.sh", 777)
+    with open("/etc/systemd/system/leads.service", "w") as f:
+        f.write(f"""
+        [Unit]
+        Description=LEADS
+        After=default.target
+        
+        [Service]
+        ExecStart=/bin/bash {script}
+        
+        [Install]
+        WantedBy=default.target
+        """)
