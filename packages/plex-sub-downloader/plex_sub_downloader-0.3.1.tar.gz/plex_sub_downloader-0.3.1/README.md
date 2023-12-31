280dc3dfef4675c679485924cdd0440c8d1999ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-12-31 14:03:57 +0000

    Add package plex-sub-downloader/0.3.1/plex_sub_downloader-0.3.1.tar.gz

---
+++ packages/plex-sub-downloader/plex_sub_downloader-0.3.1.tar.gz/plex_sub_downloader-0.3.1/README.md
@@ -0,0 +1,193 @@
+Plex Sub Downloader
+===================
+
+
+## What is it?
+
+This is a command-line tool designed to automate the downloading of subtitles for media on your [Plex Media Server](https://www.plex.tv/). It makes use of [Flask](https://flask.palletsprojects.com/en/2.1.x/) and [Python-PlexAPI](https://github.com/pkkid/python-plexapi) to listen for newly-added media, and [Subliminal](https://github.com/Diaoul/subliminal) to search your favorite subtitle providers.
+
+## Okay, Cool, but Why?
+
+Plex has built-in Agents for downloading subtitles from OpenSubtitles.org, but it doesn't search for subtitles automatically, and, more importantly, doesn't support VIP accounts (which means you're stuck reading ads _in your subtitles!_).
+
+Plex Plugins like [Sub-Zero](https://github.com/pannal/Sub-Zero.bundle) are getting increasingly complicated to install and use, as Plex has been threatening to completely phase out plugins since 2018.
+
+And there's other tools like [Bazarr](https://github.com/bazarr/), which works best if you've already bought into the [Sonarr](https://sonarr.tv/)/[Radarr](https://radarr.video/) ecosystem. But, honestly, while these tools are great, I find them to be over-built for what I want to do.
+
+I just wanted something that tries to download subtitles for new media added to my Plex server, and that's it.
+
+# Features
+
+- Automatically download subtitles for one or more languages when new media is added
+- Ignore existing subtitles in favor of preferred formats (ie replace .ssa with .srt)
+- Automatically download and enable/disable subtitles for the next episode of the show that you and other users are watching
+
+
+# Requirements
+- Requires python >=3.8
+- You'll need to purchase [Plex Pass](https://www.plex.tv/plex-pass/) to enable [push notifications](https://support.plex.tv/articles/push-notifications/) and [webhooks](https://support.plex.tv/articles/115002267687-webhooks/) 
+
+----------------
+<br />
+
+# Setup
+
+### NOTE: This project is still VERY MUCH a work in progress. The setup process will hopefully be easier in a future release. ###
+
+<br />
+
+First, install plex_sub_downloader:
+```
+pip3 install plex_sub_downloader
+```
+Then, find an auth token for your Plex account:
+  https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+
+
+Then, create a config.json file somewhere:
+
+```
+{
+    "plex_base_url": "<url to your plex server, ie http://localhost:32400>",
+    "plex_auth_token": "<your auth token here>",
+    "languages": [
+        "eng"
+    ],
+    "webhook_host": "0.0.0.0",
+    "webhook_port": <some port number, default = 5000>
+}
+```
+
+- Next, run `configtest` on this config file to make sure it's formatted correctly
+```
+plex_sub_downloader --config path/to/config.json configtest
+```
+
+You should get a result like:
+```
+2022-07-16 21:08:38:plex_sub_downloader:INFO - Testing config file '/path/to/config.json'
+2022-07-16 21:08:38:plex_sub_downloader:INFO - config file is valid.
+```
+
+<br />
+
+# Running the Webhook Listener Thing
+
+To start plex_sub_downloader in webhook mode, run:
+```
+plex_sub_downloader --config path/to/config.json start-webhook
+``` 
+
+Assuming it starts and runs correctly, you should see something like the following:
+```
+ 2023-04-01 15:51:01:PlexSubDownloader:INFO - Configuring PlexSubDownloader
+2023-04-01 15:51:01:plex_sub_downloader:INFO - plex-sub-downloader starting up
+2023-04-01 15:51:01:PlexSubDownloader:INFO - Checking if webhook url http://192.168.1.248:5000/webhook has been added to Plex...
+2023-04-01 15:51:02:PlexSubDownloader:INFO - webhook url http://192.168.1.248:5000/webhook has been added to Plex
+ ```
+If it says `webhook url {webhookUrl} has been added to Plex`, then congrats, things worked right.
+
+If instead it says `Could not add the webhook url {webhookUrl} to Plex. You may need to manually add this through the web dashboard.`, then try the following:
+
+The url you'll need to add to Plex will be `http://<ip address>:<port>/webhook`. 
+
+- Open Plex, navigate to Settings, and select Webhooks from the left-hand menu. 
+- Click 'Add Webhook' and add your webhook url.
+
+<br />
+
+# Verifying that the Webhook Works
+
+To verify that Plex can call your webhook, re-run the above startup command, and add the `--debug` flag, then start playing a video on Plex. You'll get a big dump of data, starting with:
+```
+2022-07-16 21:28:14:PlexSubDownloader:DEBUG - handleWebhookEvent
+2022-07-16 21:28:14:PlexSubDownloader:DEBUG - Event type: media.play
+```
+
+# Verifying that Subtitles Can Get Downloaded
+
+To verify that subtitles can be downloaded, add something new to your library. Within about 10-20 seconds, you should see output like:
+```
+2022-07-19 14:14:30:PlexSubDownloader:INFO - Handling library.new event
+2022-07-19 14:14:30:PlexSubDownloader:INFO - Title: Wild Wild West, type: movie, section: Movies
+2022-07-19 14:14:30:PlexSubDownloader:INFO - Found 1 videos missing subtitles
+2022-07-19 14:14:30:PlexSubDownloader:INFO - ['Wild Wild West, /library/metadata/45525']
+2022-07-19 14:14:30:PlexSubDownloader:INFO - Downloading subtitles for 1 videos
+2022-07-19 14:14:30:PlexSubDownloader:INFO - ['Wild Wild West']
+2022-07-19 14:14:32:PlexSubDownloader:INFO - Saving subtitles to Plex metadata
+2022-07-19 14:14:32:PlexSubDownloader:INFO - found 1 for video /path/to/movies/Wild.Wild.West.1999/Wild.Wild.West.1999.mp4
+```
+
+Congrats! It's probably working?
+
+<br />
+
+# Manually Running for a Specific Video
+
+plex_sub_downloader also has a command for manually checking a Movie, Episode, Season, or Show for missing subtitles.
+Simply run plex_sub_downloader with the `check-video` command option, and pass it the item's metadata key:
+
+```
+plex_sub_downloader --config path/to/config.json check-video /library/metadata/42069
+```
+
+# Command-line Arguments
+
+| Argument | Description |
+| -------- | ----------- |
+| -h, --help | Show this help message and exits |
+| -v, --version | Prints version info and exits |
+| -c CONFIG, --config CONFIG | Config File |
+| -d, --debug | Enable debug logging |
+| configtest | Run validation on config file |
+| start-webhook | Run http webhook server |
+| check-video {video key} | Manually check the given video for missing subtitles. |
+
+<br />
+
+# Configuration
+
+| Parameter | Required? | Description |
+| --------- | --------- | ----------- |
+| plex_base_url | Required |Base url to reach your Plex Media Server (ie `"http://127.0.0.1:32400"`) |
+| plex_auth_token | Required |Authentication token, needed to send requests to your server. |
+| subtitle_providers | Required | List of subtitle providers to search. Currently, this really is only guaranteed to work with `"opensubtitles"` and `"opensubtitlesvip"`. Subliminal supports `"legendastv", "opensubtitles", "opensubtitlesvip", "podnapisi", "shooter", "thesubdb", "tvsubtitles"`, so you're welcome to try any of those if you want. |
+|subtitle_provider_configs | Required | Dictionary of configuration parameters for your chosen subtitle providers. Each provider may support different config parameters. See [Subliminal's documentation](https://subliminal.readthedocs.io/en/latest/api/providers.html) for more details. |
+| webhook_host | Optional, default `"127.0.0.1"` | The hostname to listen on. By default, the server will only be accessible from the computer running it. Set this to `"0.0.0.0"` to make it publicly available on your network.|
+| webhook_port | Optional, default `5000` | the port to listen on. |
+| subtitle_destination | Optional, default `"with_media"` | Either `"with_media"` or `"metadata"`. `"with_media"` will save subtitle files alongside the media files. `"metadata"` will upload the subtitles to Plex, which stores the subtitles as part of the media's metadata. If Plex and PlexSubDownloader don't run on the same server, you'll need to set this to `"metadata"`.
+| languages | Optional, default `["eng"]` | Array of [ISO 639-3 language tags](https://en.wikipedia.org/wiki/List_of_ISO_639-3_codes) to download subtitles for.|
+| format_priority | Optional, default `None` | Array of subtitle formats (file extensions, without the ".") that should be prioritized. PlexSubDownloader will ignore any existing subtitles with formats not listed and will try to find subtitles in one of the formats listed. [Plex fully supports](https://support.plex.tv/articles/200471133-adding-local-subtitles-to-your-media/) `"srt", "smi", "ssa", "ass"`, and `"vtt"` formats. |
+| set_next_episode_subtitles | Optional, default `false` | Boolean value, when set to `true`, will try to set/unset subtitles for the next episode of a tv show when you start watching an episode. 
+| log_level | Optional, default `INFO` | The log level to set [Python's logging](https://docs.python.org/3/howto/logging.html). Expects a string value, one of `"DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"`. |
+
+
+### Example configuration:
+
+```
+{
+    "plex_base_url": "http://127.0.0.1:32400",
+    "plex_auth_token": "<token-goes-here>"
+    "webhook_host": "0.0.0.0",
+    "webhook_port": 6669,
+    "subtitle_destination": "metadata",
+    "languages": [
+        "eng"
+    ],
+    "format_priority": [
+        "srt", 
+        "smi"
+    ],
+    "set_next_episode_subtitles": true,
+    "subtitle_providers": [
+        "opensubtitlesvip"
+    ],
+    "subtitle_provider_configs": {
+        "opensubtitlesvip": {
+            "username": "<username here>",
+            "password": "<password here>"
+        }
+    },
+    "log_level": "DEBUG"
+}
+```
---
+++ packages/plex-sub-downloader/plex_sub_downloader-0.3.1.tar.gz/plex_sub_downloader-0.3.1/src/plex_sub_downloader/PlexSubDownloader.py
@@ -0,0 +1,258 @@
+import os
+import tempfile
+import socket
+from .subliminalHelper import SubliminalHelper
+from subliminal.video import Video as SubVideo
+from subliminal.subtitle import Subtitle
+from .PlexWebhookEvent import PlexWebhookEvent
+import logging
+import plexapi
+from plexapi.video import Video, EpisodeSession
+from plexapi.library import LibrarySection
+from plexapi.media import SubtitleStream
+from .plexHelper import PlexHelper
+
+log = logging.getLogger('plex-sub-downloader')
+
+class PlexSubDownloader:
+
+    def __init__(self):
+        self.config = None
+        self.sub = None
+        self.plexHelper = None
+
+    def configure(self, config):
+        """initializes and configures the needed classes for PlexSubDownloader to work.
+        :param object config: config json. See config.schema.json for structure.
+        :return: True if everything initializes correctly, otherwise False.
+        """
+
+        log.info("Configuring PlexSubDownloader")
+        self.config = config
+        self.subtitle_destination = config.get('subtitle_destination', 'with_media')
+        self.format_priority = config.get('format_priority', None)
+        if self.format_priority is not None and len(self.format_priority) == 0:
+            self.format_priority = None
+
+        self.sub = SubliminalHelper(
+            providers= config.get('subtitle_providers', None),
+            provider_configs=config.get('subtitle_provider_configs', None),
+            format_priority=self.format_priority
+            )
+        
+        self.plexHelper = PlexHelper(baseurl=config['plex_base_url'], 
+                                     token=config['plex_auth_token'], 
+                                     host=config.get('webhook_host', '127.0.0.1'), 
+                                     port=config.get('webhook_port', 5000))
+        
+        if config['subtitle_destination'] == 'with_media' and self.plexHelper.check_library_permissions() == False:
+            log.error("One or more of the Plex libraries are not readable/writable by the current user.")
+            return False
+        return True
+        
+
+    def handle_webhook_event(self, event):
+        """Handles the given webhook event. 
+        :param PlexWebhookEvent event:
+        """
+        log.debug("handleWebhookEvent")
+        log.debug("Event type: " + event.event)
+        if self.config.get("save_plex_webhook_events", False):
+            save_dir = self.config.get("save_plex_webhook_events_dir", None)
+            if save_dir is not None:
+                self.save_webhook_event(event, save_dir)
+        
+        if event.event == "library.new":
+            self.handle_library_new_event(event)
+        elif event.event == "media.play" or event.event == "media.resume":
+            self.handle_video_play_event(event)
+
+    def handle_library_new_event(self, event):
+        """Handles webhook events of type library.new.
+        Retrieves the relevent item from Plex and searches for subtitles.
+        :param PlexWebhookEvent event:
+        """
+        log.info("Handling library.new event")
+        log.info(f'Title: {event.Metadata.title}, type: {event.Metadata.type}, section: {event.Metadata.librarySectionTitle}')
+        
+        video = self.plexHelper.get_video_item_from_event(event)
+        if video is None:
+            log.info("Video referenced in event could not be retrieved.")
+            return
+        
+        self.handle_downloading_video_subtitles(video)
+
+    def handle_video_play_event(self, event):
+        """Handles webhook events of type media.play and media.resume.
+            If `set_next_episode_subtitles` is set to True in config, attempts to set subtitles 
+            for the next episode in the series (assuming that the event is for an Episode).
+            :param PlexWebhookEvent event:
+        """
+        if self.config.get('set_next_episode_subtitles', False) == False or event.Metadata.type != "episode":
+            return
+        
+        log.info(f"Handling {event.event} event")
+        log.info(f'Title: {event.Metadata.title}, type: {event.Metadata.type}, section: {event.Metadata.librarySectionTitle}')
+
+        session = self.plexHelper.get_session_for_play_event(event)
+        if session is None or type(session) is not EpisodeSession:
+            log.debug("No session found for this event. Skipping")
+            return
+        
+        next_episode = self.plexHelper.get_next_episode(session.key)
+        if next_episode is None:
+            log.debug("No next episode for this session. Skipping")
+            return
+        
+        subtitle_stream = self.plexHelper.get_selected_subtitles_for_play_session(session)
+        if subtitle_stream is None:
+            log.debug("No subtitles set for this session. Setting next episode to show no subtitles.")
+            self.plexHelper.unset_video_subtitles_for_user(video=next_episode, user=session.user)
+            return
+                
+        self.manually_check_video_subtitles(next_episode.key)
+        next_episode.reload()
+        self.plexHelper.select_video_subtitles_for_user(video=next_episode, user=session.user, subtitle_to_match=subtitle_stream)
+
+
+    def manually_check_video_subtitles(self, video_key):
+        """Manually check video for missing subtitles, and try to download missing subs.
+        """
+
+        video = self.plexHelper.get_video_item(video_key)
+        if video is None:
+            log.info(f"Video with key {video_key} could not be retrieved.")
+            return 
+        self.handle_downloading_video_subtitles(video)
+
+    def handle_downloading_video_subtitles(self, video):
+        missingVideos = self.get_videos_missing_subtitles([video])
+        log.info("Found " + str(len(missingVideos)) + " videos missing subtitles")
+        log.info([f'{video.title}, {video.key}' for video in missingVideos])
+        if len(missingVideos) > 0:
+            subtitles = self.download_subtitles_for_videos(missingVideos)
+
+            if self.subtitle_destination == "metadata":
+                self.upload_subtitles_to_metadata(missingVideos, subtitles)
+            else:
+                self.sub.save_subtitles(subtitles)
+        else:
+            log.info("No subtitles to download, doing nothing!")
+        
+    def get_videos_missing_subtitles(self,videos):
+        """Search the given list of videos for ones that don't already have subtitles.
+        For videos of type 'season' or 'show', this will search through all of the episodes
+        as well.
+        :param list videos: list of plexapi.video.Video objects.
+        :return: list of Video objects that don't have any subtitles.
+        """
+
+        vidsMissingSubs = []
+        for v in videos:
+            if v.type == 'movie' or v.type == 'episode':
+                if self.is_video_missing_subtitles(v):
+                    vidsMissingSubs.append(v)
+                
+            elif v.type == 'season' or v.type == 'show':
+                eps = v.episodes()
+                for e in eps:
+                    e.reload()
+                    if self.is_video_missing_subtitles(e):
+                        vidsMissingSubs.append(e)
+
+        return vidsMissingSubs
+
+    def is_video_missing_subtitles(self, video):
+        """Checks the given video to see if it's missing subtitles for any of the languages defined in config['languages'].
+        :param video: plexapi.video.Video object
+        :return: boolean, False if the video has subtitles for every requested language, True otherwise
+        """
+
+        missingSubtitles = self.get_missing_subtitle_languages(video)
+        return len(missingSubtitles) > 0
+    
+    def get_missing_subtitle_languages(self, video):
+        """Compares the existing subtitle languages on the video to the languages requested based on config['languages'],
+        and returns requested languages that aren't already present.
+        :param video: plexapi.video.Video object
+        :return: array of language codes
+        """
+        requestedLanguages = self.config['languages'].copy()
+
+        subtitles = video.subtitleStreams()
+        
+        for subtitle in subtitles:
+            if self.format_priority is not None and subtitle.format not in self.format_priority:
+                continue
+            if subtitle.languageCode in requestedLanguages:
+                requestedLanguages.remove(subtitle.languageCode)
+
+        log.info(f'Video {video.title} {video.key} is missing {len(requestedLanguages)} subtitle languages:')
+        log.info(f'{requestedLanguages}')
+
+        return requestedLanguages
+
+    def download_subtitles_for_videos(self, videos):
+        """Attempts to download subtitles for the given list of videos.
+        :param list videos: list of plexapi.video.Video objects.
+        :return: dict[subliminal.video.Video, list[subliminal.subtitle.Subtitle]]
+        """
+
+        log.info(f"Downloading subtitles for {len(videos)} videos:")
+        log.info([video.title for video in videos])
+        missing_languages = [self.get_missing_subtitle_languages(video) for video in videos]
+        subtitles = self.sub.search_videos(videos, missing_languages)
+        return subtitles
+
+    def upload_subtitles_to_metadata(self, plexVideos, subtitleDict):
+        """Saves the subtitles to Plex.
+        :param list plexVideos: list of plexapi.video.Video objects.
+        :param dict subtitles: dict of dict[subliminal.video.Video, list[subliminal.subtitle.Subtitle]]
+        """
+
+        log.info("Saving subtitles to Plex metadata")
+        tempdir = tempfile.gettempdir()
+        for video in plexVideos:
+            mediaPart = video.media[0].parts[0]
+            filepath = video.media[0].parts[0].file
+            for subVideo, subtitles in subtitleDict.items():
+                if subVideo.name == filepath:
+                    log.debug(f'found {len(subtitles)} subtitles for video {subVideo.name}')
+                    if len(subtitles) == 0:
+                        continue
+                    savedSubtitlePaths = self.sub.save_subtitle(subVideo, subtitles, destination=tempdir)
+                    for subtitlePath in savedSubtitlePaths:
+                        log.debug(f'Uploading subtitles \'{subtitlePath}\' to video {video.title} {video.key}')
+                        originalDefault = None 
+                        existingSubs = video.subtitleStreams();
+                        for sub in existingSubs:
+                            if sub.default:
+                                originalDefault = sub
+                                break
+
+                        video.uploadSubtitles(subtitlePath)
+                        try:
+                            if originalDefault is not None:
+                                mediaPart.setSelectedSubtitleStream(originalDefault)
+                            else:
+                                mediaPart.resetSelectedSubtitleStream()
+                        except Exception as e:
+                            log.debug('Error when trying to set default subtitle stream. This probably isn\'t a big deal?')
+                            log.debug(e)
+                            
+    def check_webhook_registration(self):
+        return self.plexHelper.check_webhook_registration()
+    
+    def add_webhook_to_plex(self):
+        return self.plexHelper.add_webhook_to_plex()
+        
+    def save_webhook_event(self, event, dir):
+        import json
+        import time
+        
+        webhook_filename = f'event_{int(time.time())}_{event.event}.json'
+        filepath = os.path.join(dir, webhook_filename)
+        with open(filepath, 'w') as fp:
+            log.debug(f'Saving webhook event to {filepath}')
+            json.dump(event._data, fp, indent=4)
+
