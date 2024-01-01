6536d6c3acd4effc46fd88f3a31b609c57acdc4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:05:30 +0000

    Add package nandboxbotsapi/1.2.0/nandboxbotsapi-1.2.0.tar.gz

---
+++ packages/nandboxbotsapi/nandboxbotsapi-1.2.0.tar.gz/nandboxbotsapi-1.2.0/nandboxbots/outmessages/GetChatMemberOutMessage.py
@@ -0,0 +1,23 @@
+import json
+
+from nandboxbots.outmessages.OutMessage import OutMessage
+
+
+class GetChatMemberOutMessage(OutMessage):
+    __KEY_USER_ID = "user_id"
+
+    user_id = None
+
+    def __init__(self):
+        self.method = "getChatMember"
+
+    def to_json_obj(self):
+        _, dictionary = super(GetChatMemberOutMessage, self).to_json_obj()
+
+        if self.chat_id is not None:
+            dictionary[self.__KEY_CHAT_ID] = self.chat_id
+        if self.user_id is not None:
+            dictionary[self.__KEY_USER_ID] = self.user_id
+
+        return json.dumps(dictionary), dictionary
+    
