3eb256f5661e4aa22447dd097b444585bf8f0ebb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:04:01 +0000

    Add package unitxt/1.4.1/unitxt-1.4.1-py3-none-any.whl

---
+++ packages/unitxt/unitxt-1.4.1-py3-none-any.whl/unitxt/card.py
@@ -0,0 +1,28 @@
+from typing import List, Union
+
+from .artifact import Artifact
+from .collections import Collection
+from .dataclass import OptionalField
+from .loaders import Loader
+from .operator import StreamingOperator
+from .splitters import RandomSampler, Sampler
+from .task import FormTask
+
+
+class TaskCard(Artifact):
+    loader: Loader
+    task: FormTask
+    preprocess_steps: List[StreamingOperator] = None
+    templates: Collection = None
+    instructions: Collection = None
+    sampler: Sampler = OptionalField(default_factory=RandomSampler)
+
+
+class ICLCard(Artifact):
+    demos_pool_name: str = "demos_pool"
+    demos_pool_size: int = None
+    demos_field: str = "demos"
+    num_demos: int = None
+    sampler_type: str = "random"
+    instruction_item: Union[str, int] = None
+    template_card_index: Union[str, int] = None
