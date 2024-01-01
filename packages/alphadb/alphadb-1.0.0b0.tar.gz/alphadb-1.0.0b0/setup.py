7cb9c853a01c7c43719590f5ce5e03f38e69aae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:05:13 +0000

    Add package alphadb/1.0.0b0/alphadb-1.0.0b0.tar.gz

---
+++ packages/alphadb/alphadb-1.0.0b0.tar.gz/alphadb-1.0.0b0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="alphadb",
+    version="1.0.0b0",
+    author="Wibo Kuipers",
+    author_email="wibokuip@gmail.com",
+    description="MySQL Database versioning toolset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/w-kuipers/alphadb",
+    project_urls={
+        "Bug Tracker": "https://github.com/w-kuipers/alphadb/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=["alphadb", "alphadb.utils", "alphadb.utils.query", "alphadb.utils.query.column", "alphadb.utils.query.table", "alphadb.verification", "alphadb.utils.concatenate"],
+    python_requires=">=3.6",
+)
