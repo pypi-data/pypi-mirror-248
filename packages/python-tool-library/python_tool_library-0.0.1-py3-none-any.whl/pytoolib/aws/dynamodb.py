300919f6b88fcf99f3ce57aee3f679dcb1e4164d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:51 +0000

    Add package python-tool-library/0.0.1/python_tool_library-0.0.1-py3-none-any.whl

---
+++ packages/python-tool-library/python_tool_library-0.0.1-py3-none-any.whl/pytoolib/aws/dynamodb.py
@@ -0,0 +1,17 @@
+"""Convenient functions for DynamoDB."""
+
+
+def ddb_json_to_json(ddb_json: str) -> str:
+    """Convert DynamoDB JSON to normal JSON.
+
+    Parameters
+    ----------
+    ddb_json: str
+        DynamoDB format JSON string.
+
+    Returns
+    -------
+    str
+        Normal JSON string.
+    """
+    raise NotImplementedError("The function is not implemented")
---
+++ packages/python-tool-library/python_tool_library-0.0.1-py3-none-any.whl/pytoolib/primary.py
@@ -0,0 +1,19 @@
+"""Basic convenient functions."""
+
+
+def list_diff(list1: list, list2: list) -> list:
+    """Return the difference between two lists.
+
+    Parameters
+    ----------
+    list1: list
+        The first list to be compared.
+    list2: list
+        The second list to be compared.
+
+    Returns
+    -------
+    list
+        The difference between two lists as a list.
+    """
+    raise NotImplementedError("The function has not implemented yet")
