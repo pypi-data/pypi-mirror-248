82a6d92ab452ae4c9d5c193f4012086f0ddf2f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:04 +0000

    Add package testghacookiecutter/0.0.100/testghacookiecutter-0.0.100-cp39-cp39-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/testghacookiecutter/testghacookiecutter-0.0.100-cp39-cp39-manylinux_2_17_i686.manylinux2014_i686.whl/testghacookiecutter/include/test-gha-cookiecutter/test-gha-cookiecutter.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+namespace testghacookiecutter {
+
+/** @brief A very interesting function!
+ *
+ * This function is of course just a self-explanatory placeholder,
+ * but surprisingly often, things aren't this easy. You should
+ * therefore *really* document your C++ code with Doxygen!
+ *
+ * @param x The number to increase
+ * @returns the successor of x
+ */
+inline int
+add_one(int x)
+{
+  return x + 1;
+}
+
+} // namespace testghacookiecutter
