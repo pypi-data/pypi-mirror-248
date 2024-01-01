8acae9b69fee77a66805522ff8f328d03df23973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:05 +0000

    Add package ms-swift/1.5.0/ms_swift-1.5.0-py3-none-any.whl

---
+++ packages/ms-swift/ms_swift-1.5.0-py3-none-any.whl/swift/trainers/arguments.py
@@ -0,0 +1,41 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from transformers.training_args import TrainingArguments as HfTrainingArguments
+from transformers.training_args_seq2seq import \
+    Seq2SeqTrainingArguments as HfSeq2SeqTrainingArguments
+from trl import DPOTrainer
+
+
+@dataclass
+class SwiftArgumentsMixin:
+    # ckpt only save model
+    only_save_model: bool = False
+    train_sampler_random: bool = True
+    push_hub_strategy: str = field(
+        default='push_best',
+        metadata={
+            'choices':
+            {'end', 'push_best', 'push_last', 'checkpoint', 'all_checkpoints'}
+        })
+    acc_strategy: str = field(
+        default='token', metadata={'choices': ['token', 'sentence']})
+    additional_saved_files: Optional[List[str]] = None
+
+    def __post_init__(self):
+        if self.additional_saved_files is None:
+            self.additional_saved_files = []
+        super().__post_init__()
+
+
+@dataclass
+class TrainingArguments(SwiftArgumentsMixin, HfTrainingArguments):
+    pass
+
+
+@dataclass
+class Seq2SeqTrainingArguments(SwiftArgumentsMixin,
+                               HfSeq2SeqTrainingArguments):
+    pass
