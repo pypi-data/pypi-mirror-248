c7745582ed524275fc54dd5309202465dd8aa839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:04 +0000

    Add package testghacookiecutter/0.0.100/testghacookiecutter-0.0.100-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/testghacookiecutter/testghacookiecutter-0.0.100-cp38-cp38-macosx_10_9_x86_64.whl/testghacookiecutter/__init__.py
@@ -0,0 +1,11 @@
+from testghacookiecutter._testghacookiecutter import add_one
+
+# Export the version given in project metadata
+from importlib import metadata
+
+__version__ = metadata.version(__package__)
+del metadata
+
+
+def one_plus_one():
+    return add_one(1)
