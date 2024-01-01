6fe7156dd5b9e82495ec29df39c6ba455329edde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:05:30 +0000

    Add package bestErrors/0.7/bestErrors-0.7.tar.gz

---
+++ packages/bestErrors/bestErrors-0.7.tar.gz/bestErrors-0.7/bestErrors/main.py
@@ -0,0 +1,17 @@
+from erodecor import Error
+
+
+@Error
+class MathError: ...
+
+@Error
+class AuthenticationError: ...
+
+@Error
+class PermissionDeniedError: ...
+
+@Error
+class OutOfRangeError: ...
+
+@Error
+class InvalidTypeError: ...
