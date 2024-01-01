31559583be826b8aae8ebc4528c61974fba5319a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 04:04:18 +0000

    Add package klldFN/1.0.9/klldFN-1.0.9.tar.gz

---
+++ packages/klldFN/klldFN-1.0.9.tar.gz/klldFN-1.0.9/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
+setuptools.setup(
+    name="klldFN",
+    version="1.0.9",
+    author="klld",
+    description="klldFN",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://klldfn.xyz",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+          'crayons',
+          'PirxcyPinger==5.0.0',
+          'fortnitepy-edit',
+          'BenBotAsync==3.0.1',
+          'FortniteAPIAsync==0.1.6',
+          'sanic==20.6.3',
+          'aiohttp',
+          'uvloop',
+          'requests'  
+      ],
+)
