37df44495be508e6ac86778d75d647420ecf58c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 04:04:21 +0000

    Add package vampytest/0.0.16/vampytest-0.0.16.tar.gz

---
+++ packages/vampytest/vampytest-0.0.16.tar.gz/vampytest-0.0.16/vampytest/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.0.16'
+
+try:
+    import scarletio
+except ImportError as err:
+    raise ImportError(
+        'Couldn\'t import scarletio. \n'
+        'Are you sure it\'s installed and available on your PYTHONPATH environment variable?\n'
+        'Did you forget to activate a virtual environment?\n'
+    ) from err
+
+del scarletio
+
+
+from .core import *
+
+__all__ = core.__all__
