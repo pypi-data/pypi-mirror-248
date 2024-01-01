f25f144f5d5fabb60daa691b44ca43febbbe007b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:33 +0000

    Add package llmdataset/0.1.4/llmdataset-0.1.4-py3-none-any.whl

---
+++ packages/llmdataset/llmdataset-0.1.4-py3-none-any.whl/llmdataset/dataset/multiarith.py
@@ -0,0 +1,30 @@
+from datasets import load_dataset, concatenate_datasets
+
+def multiarith_dataset():
+    try:
+        dataset = load_dataset("ChilleD/MultiArith")
+
+        # データセットのサイズをカウント
+        train_data_count = len(dataset['train'])
+        test_data_count = len(dataset['test'])
+        data_types = ['train','test']
+        data_count = [train_data_count, test_data_count]
+        return data_types, data_count, dataset
+
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
+
+def multiarith_split(dataset, data_type):
+    if data_type == 'train':
+        all_dataset = dataset['train']
+    elif data_type == 'test':
+        all_dataset = dataset['test']
+    else:
+        train_dataset = dataset['train']
+        test_dataset = dataset['test']
+        all_dataset = concatenate_datasets([train_dataset, test_dataset])
+
+    data_list = [[example['question'], example['final_ans']] for example in all_dataset]
+
+    return data_list
---
+++ packages/llmdataset/llmdataset-0.1.4-py3-none-any.whl/llmdataset/main.py
@@ -0,0 +1,53 @@
+from .dataset.gsm8k import gsm8k_dataset, gsm8k_split
+from .dataset.multiarith import multiarith_dataset
+import random
+
+
+class LLMdataset:
+    def __init__(
+        self,
+        dataset_name = None
+        ):
+        self.dataset_name = dataset_name
+        if self.dataset_name:
+            self.data_types, self.num_data, self.dataset = self._select_dataset()
+            print(f"Data types:{self.data_types}")
+            print(f"Number of data:{self.num_data}")
+
+    def _select_dataset(self):
+        if self.dataset_name == 'gsm8k':
+            data_types, data_count, dataset = gsm8k_dataset()
+            return data_types, data_count, dataset
+        elif self.dataset_name == 'multiarith':
+            data_types, data_count, dataset = multiarith_dataset()
+            return data_types, data_count, dataset
+
+
+    def _split_dataset(self, data_type):
+        if self.dataset_name == 'gsm8k':
+            data_list = gsm8k_split(self.dataset, data_type)
+            return data_list
+        elif self.dataset_name == 'multiarith':
+            return data_list
+
+
+    def dataloader(self, data_type = None, batch_size=1, seed=None, max_data=5):
+        self.data_list = self._split_dataset(data_type)
+
+        if seed is not None:
+            random.seed(seed)
+
+        # 現在のデータセットのサイズと max_data を比較
+        if max_data is not None and max_data > self.num_data:
+            raise ValueError(f"max_data ({max_data}) cannot be greater than the size of the dataset ({self.num_data}).")
+
+        if max_data is not None:
+            data_list = random.sample(self.data_list, max_data)
+        else:
+            data_list = self.dataset[:max_data]
+
+        # バッチサイズでデータセットを分割し、ジェネレータとして提供
+        for i in range(0, len(data_list), batch_size):
+            yield data_list[i:i + batch_size]
+
+
