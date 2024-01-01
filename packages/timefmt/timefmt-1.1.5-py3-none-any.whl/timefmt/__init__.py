22fbb1336e666a638ae10ac3c626067885f6c4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:04:58 +0000

    Add package timefmt/1.1.5/timefmt-1.1.5-py3-none-any.whl

---
+++ packages/timefmt/timefmt-1.1.5-py3-none-any.whl/timefmt/__init__.py
@@ -0,0 +1,26 @@
+"""Provides methods to manipulate and format various time-based objects into human-readable strings.
+
+:Example:
+    >>> import timefmt
+    >>> import datetime
+    >>> test_datetime = datetime.datetime(2023, 12, 31, 12, 23, 31, 379292)
+    >>> test_timedelta = datetime.timedelta(hours=1000, seconds=9999)
+
+    >>> timefmt.auto(test_datetime)
+    '12:23:31 PM'
+    >>> timefmt.auto(test_timedelta, long=True)
+    '5 weeks, 6 days, 18 hours, 46 minutes, and 39 seconds'
+"""
+
+__version__ = "1.1.5"
+
+__all__ = [
+    "dt", "td",
+    "SplitTime", "split_seconds", "day_of_month_suffix", "day_of_month_string", "timezone_name",
+    "auto",
+]
+
+# TODO: Make timezone name universal across operating systems (may need tp literally make my own module, maybe pytz)
+
+from timefmt._helpers import SplitTime, split_seconds, day_of_month_suffix, day_of_month_string, timezone_name
+from timefmt._auto import auto
