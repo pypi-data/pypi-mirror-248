9ecf035aeb6f8c40c475957c4ae4d0a53256a16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 04:04:20 +0000

    Add package demisto-sdk/1.25.1/demisto_sdk-1.25.1-py3-none-any.whl

---
+++ packages/demisto-sdk/demisto_sdk-1.25.1-py3-none-any.whl/demisto_sdk/commands/common/content_constant_paths.py
@@ -0,0 +1,40 @@
+import logging
+from pathlib import Path
+
+from demisto_sdk.commands.common.constants import NATIVE_IMAGE_FILE_NAME, TESTS_DIR
+from demisto_sdk.commands.common.tools import get_content_path
+
+logger = logging.getLogger("demisto-sdk")
+
+CONTENT_PATH = Path(get_content_path())  # type: ignore
+
+ALL_PACKS_DEPENDENCIES_DEFAULT_PATH = CONTENT_PATH / "all_packs_dependencies.json"
+
+CONF_PATH = CONTENT_PATH / TESTS_DIR / "conf.json"
+
+DEFAULT_ID_SET_PATH = CONTENT_PATH / TESTS_DIR / "id_set.json"
+MP_V2_ID_SET_PATH = CONTENT_PATH / TESTS_DIR / "id_set_mp_v2.json"
+XPANSE_ID_SET_PATH = CONTENT_PATH / TESTS_DIR / "id_set_xpanse.json"
+LANDING_PAGE_SECTIONS_PATH = (
+    CONTENT_PATH / TESTS_DIR / "Marketplace" / "landingPage_sections.json"
+)
+NATIVE_IMAGE_PATH = CONTENT_PATH / "Tests" / NATIVE_IMAGE_FILE_NAME
+
+
+PYTHONPATH = [
+    Path(CONTENT_PATH).absolute(),
+    Path(CONTENT_PATH / "Packs" / "Base" / "Scripts" / "CommonServerPython").absolute(),
+    Path(CONTENT_PATH / TESTS_DIR / "demistomock").absolute(),
+    Path(__file__).parent.parent / "lint" / "resources" / "pylint_plugins",
+]
+try:
+    PYTHONPATH.extend(
+        (
+            path.absolute()
+            for path in Path(
+                CONTENT_PATH / "Packs" / "ApiModules" / "Scripts"
+            ).iterdir()
+        )
+    )
+except FileNotFoundError:
+    logger.info("ApiModules not found, skipping adding to PYTHONPATH")
