fbcec76a021403d906148e7fa570a215243ec604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:05:23 +0000

    Add package waypaper/2.0.3/waypaper-2.0.3-py3-none-any.whl

---
+++ packages/waypaper/waypaper-2.0.3-py3-none-any.whl/waypaper/changer.py
@@ -0,0 +1,86 @@
+"""Module that runs the system processes to change the wallpaper"""
+
+import subprocess
+import time
+
+
+def change_wallpaper(image_path, fill_option, color, backend, monitor, transition, txt):
+    """Run a system command to change the wallpaper depending on the backend"""
+
+    try:
+        # swaybg backend:
+        if backend == "swaybg":
+            fill = fill_option.lower()
+            try:
+                subprocess.Popen(["killall", "swaybg"])
+                time.sleep(0.005)
+            except Exception as e:
+                print(f"{ERR_KILL} {e}")
+            command = ["swaybg"]
+            # if monitor != "All":
+                # command.extend(["-o", monitor])
+            command.extend(["-i", image_path])
+            command.extend(["-m", fill, "-c", color])
+            subprocess.Popen(command)
+            print(f"{txt.msg_setwith} {backend}")
+
+        # swww backend:
+        elif backend == "swww":
+            fill_types = {
+                    "fill": "crop",
+                    "fit": "fit",
+                    "center": "no",
+                    "stretch": "crop",
+                    "tile": "no",
+                    }
+            fill = fill_types[fill_option.lower()]
+            try:
+                subprocess.Popen(["killall", "swaybg"])
+                time.sleep(0.005)
+            except Exception as e:
+                print(f"{ERR_KILL} {e}")
+            subprocess.Popen(["swww", "init"])
+            command = ["swww", "img", image_path]
+            command.extend(["--resize", fill])
+            command.extend(["--fill-color", color])
+            command.extend(["--transition-type", transition])
+            # command.extend(["--transition-step", str(30)])
+            if monitor != "All":
+                command.extend(["--outputs", monitor])
+            subprocess.Popen(command)
+            print(f"{txt.msg_setwith} {backend}")
+
+        # feh backend:
+        elif backend == "feh":
+            fill_types = {
+                    "fill": "--bg-fill",
+                    "fit": "--bg-max",
+                    "center": "--bg-center",
+                    "stretch": "--bg-scale",
+                    "tile": "--bg-tile",
+                    }
+            fill = fill_types[fill_option.lower()]
+            command = ["feh", fill, "--image-bg", color]
+            command.extend([image_path])
+            subprocess.Popen(command)
+            print(f"{txt.msg_setwith} {backend}")
+
+        # wallutils backend:
+        elif backend == "wallutils":
+            fill_types = {
+                    "fill": "scale",
+                    "fit": "scale",
+                    "center": "center",
+                    "stretch": "stretch",
+                    "tile": "tile",
+                    }
+            fill = fill_types[fill_option.lower()]
+
+            subprocess.Popen(["setwallpaper", "--mode", fill, image_path])
+            print(f"{txt.msg_setwith} {backend}")
+
+        else:
+            print(f"{txt.err_notsup} {backend}")
+
+    except Exception as e:
+        print(f"{txt.err_wall} {e}")
