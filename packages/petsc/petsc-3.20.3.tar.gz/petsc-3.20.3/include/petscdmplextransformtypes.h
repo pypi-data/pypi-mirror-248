57ea7501f90398a8f70d53d335d306dd0378132d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 04:04:16 +0000

    Add package petsc/3.20.3/petsc-3.20.3.tar.gz

---
+++ packages/petsc/petsc-3.20.3.tar.gz/petsc-3.20.3/include/petscdmplextransformtypes.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* SUBMANSEC = DMPlex */
+
+/*S
+  DMPlexTransform - An object encapsulating a mesh transformation
+
+  Level: intermediate
+
+.seealso: `DMPlexTransformCreate()`, `DMPlexTransformType`
+S*/
+typedef struct _p_DMPlexTransform *DMPlexTransform;
