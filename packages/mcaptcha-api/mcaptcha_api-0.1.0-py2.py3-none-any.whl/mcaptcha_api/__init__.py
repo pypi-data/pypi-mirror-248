2caa3c497819c4b544989b1e71bda043a1aac7ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:04:57 +0000

    Add package mcaptcha-api/0.1.0/mcaptcha_api-0.1.0-py2.py3-none-any.whl

---
+++ packages/mcaptcha-api/mcaptcha_api-0.1.0-py2.py3-none-any.whl/mcaptcha_api/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023 Aravinth Manivannan <realaravinth@batsense.net>
+#
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-License-Identifier: MIT
+
+from .client import MCaptcha
