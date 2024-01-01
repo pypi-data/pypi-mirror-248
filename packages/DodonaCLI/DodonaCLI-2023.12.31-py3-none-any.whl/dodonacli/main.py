ca1d8d6003bbaed89e033b8258aacd1fa3f957a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:04:03 +0000

    Add package DodonaCLI/2023.12.31/DodonaCLI-2023.12.31-py3-none-any.whl

---
+++ packages/DodonaCLI/DodonaCLI-2023.12.31-py3-none-any.whl/dodonacli/main.py
@@ -0,0 +1,30 @@
+import click
+
+from dodonacli.commands import select, status, up, post
+from dodonacli.commands import tutorial, submission, display
+
+
+@click.group(help="A 3rd party Command Line Interface for Dodona. "
+                  "Finally you have no need to exit your terminal anymore!\n"
+                  "Use --help for more info about flags, or read the README on discord.")
+def main():
+    """
+    A Command Line Interface for Dodona.
+    Finally, you have no need to exit your terminal any more!
+    Use --help for more info about flags, or read the README on discord.
+    """
+    pass
+
+
+main.add_command(display.display)
+main.add_command(select.select)
+main.add_command(status.status)
+main.add_command(up.up)
+main.add_command(post.post)
+main.add_command(tutorial.tutorial)
+main.add_command(submission.sub)
+
+
+if __name__ == "__main__":
+    # Main entry-point
+    main()
