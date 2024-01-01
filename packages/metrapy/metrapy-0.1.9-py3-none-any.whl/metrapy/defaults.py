7a791bca081d8df29d73b21eaf742ec84dd25a8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:52 +0000

    Add package metrapy/0.1.9/metrapy-0.1.9-py3-none-any.whl

---
+++ packages/metrapy/metrapy-0.1.9-py3-none-any.whl/metrapy/defaults.py
@@ -0,0 +1,8 @@
+# this file contains defaults used at various other files 
+import pytz
+
+class Defaults:
+    mt5_path = "C:/Program Files/MetaTrader 5/terminal64.exe"
+    timezone = pytz.utc
+
+defaults = Defaults()
