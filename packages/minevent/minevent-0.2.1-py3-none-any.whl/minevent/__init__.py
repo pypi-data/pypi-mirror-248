622108173e32d5d74a423a093dd815a6566fabd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:12 +0000

    Add package minevent/0.2.1/minevent-0.2.1-py3-none-any.whl

---
+++ packages/minevent/minevent-0.2.1-py3-none-any.whl/minevent/__init__.py
@@ -0,0 +1,25 @@
+r"""Contain the main features of the ``minevent`` package."""
+
+from __future__ import annotations
+
+__all__ = [
+    "BaseCondition",
+    "BaseEventHandler",
+    "BaseEventHandlerWithArguments",
+    "ConditionEqualityOperator",
+    "ConditionalEventHandler",
+    "EventHandler",
+    "EventHandlerEqualityOperator",
+    "EventManager",
+    "PeriodicCondition",
+]
+
+from minevent.comparators import ConditionEqualityOperator, EventHandlerEqualityOperator
+from minevent.conditions import BaseCondition, PeriodicCondition
+from minevent.handlers import (
+    BaseEventHandler,
+    BaseEventHandlerWithArguments,
+    ConditionalEventHandler,
+    EventHandler,
+)
+from minevent.manager import EventManager
