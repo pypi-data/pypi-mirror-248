46d86b3750ebc3d14b3a53685a53474e0e7537a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:32 +0000

    Add package remote-graphcast/0.0.5/remote-graphcast-0.0.5.tar.gz

---
+++ packages/remote-graphcast/remote-graphcast-0.0.5.tar.gz/remote-graphcast-0.0.5/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="remote-graphcast",
+    version="0.0.5",
+    author="Louka Ewington-Pitsos",
+    author_email="lewingtonpitsos@gmail.com",
+    description="Allows people without massive GPUs to easily run graphcast on remote runpod servers",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Lewington-pitsos/remote-graphcast",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'boto3', 
+        'cdsapi',
+        'runpod'
+	],
+    python_requires='>=3.8',
+)
