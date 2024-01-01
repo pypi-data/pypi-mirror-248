c5658c5d55ef0a3bc94d60d53535f006b2d06951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:23 +0000

    Add package indiatools/0.1.0/indiatools-0.1.0.tar.gz

---
+++ packages/indiatools/indiatools-0.1.0.tar.gz/indiatools-0.1.0/setup.py
@@ -0,0 +1,22 @@
+﻿import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="indiatools",
+    version="0.1.0",
+    author="Nikhil Swami",
+    author_email="nikhil@swaimx.com",
+    description="A Python package generated Swamix SoftEngine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nikhil-Software-Cartel/IndiaTools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
