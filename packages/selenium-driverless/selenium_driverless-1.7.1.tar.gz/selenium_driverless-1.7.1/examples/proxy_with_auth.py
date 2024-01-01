35c432f75f0880f1ff31d7735fbbb9c6fe01662b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:05:20 +0000

    Add package selenium-driverless/1.7.1/selenium_driverless-1.7.1.tar.gz

---
+++ packages/selenium-driverless/selenium_driverless-1.7.1.tar.gz/selenium_driverless-1.7.1/examples/proxy_with_auth.py
@@ -0,0 +1,21 @@
+from selenium_driverless import webdriver
+import asyncio
+
+
+async def main():
+    proxy = "http://user1:passwrd1@example.proxy.com:5001/"
+
+    options = webdriver.ChromeOptions()
+    # options.single_proxy = proxy
+
+    async with webdriver.Chrome(options=options) as driver:
+
+        # this will overwrite the proxy for ALL CONTEXTS
+        await driver.set_single_proxy(proxy)
+
+        await driver.get("https://browserleaks.com/webrtc")
+        await driver.clear_proxy()  # clear proxy
+        print()
+
+
+asyncio.run(main())
