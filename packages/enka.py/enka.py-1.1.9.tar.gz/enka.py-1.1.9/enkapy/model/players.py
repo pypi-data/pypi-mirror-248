69c795431b1228e8c699ec67707d2d9c10183bf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:05 +0000

    Add package enka.py/1.1.9/enka.py-1.1.9.tar.gz

---
+++ packages/enka.py/enka.py-1.1.9.tar.gz/enka.py-1.1.9/enkapy/model/players.py
@@ -0,0 +1,42 @@
+from typing import List, Optional
+
+from pydantic import BaseModel
+
+
+class ProfilePicture(BaseModel):
+    id: Optional[int]
+
+
+class ShowAvatar(BaseModel):
+    avatarId: int
+    """Character id"""
+    level: int
+    """Character level"""
+
+
+class PlayerInfo(BaseModel):
+    """
+    Player info class
+    """
+    finishAchievementNum: Optional[int]
+    """Total achievement"""
+    level: int
+    """Player level"""
+    nameCardId: int
+    """Player name card background id"""
+    nickname: Optional[str]
+    """Player nickname"""
+    signature: Optional[str] = ''
+    """Player signature"""
+    worldLevel: Optional[int]
+    """Player world level"""
+    profilePicture: ProfilePicture
+    """Player profile picture"""
+
+    showAvatarInfoList: Optional[List[ShowAvatar]] = []
+    """Player characters shown"""
+
+    towerFloorIndex: Optional[int]
+    """Player abyss floor"""
+    towerLevelIndex: Optional[int]
+    """Player abyss level"""
