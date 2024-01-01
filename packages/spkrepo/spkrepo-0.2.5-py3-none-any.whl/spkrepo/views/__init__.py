067548f92d592423b6de7f9f2c77bb951860acf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:10 +0000

    Add package spkrepo/0.2.5/spkrepo-0.2.5-py3-none-any.whl

---
+++ packages/spkrepo/spkrepo-0.2.5-py3-none-any.whl/spkrepo/views/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from .admin import (
+    ArchitectureView,
+    BuildView,
+    FirmwareView,
+    IndexView,
+    PackageView,
+    ScreenshotView,
+    UserView,
+    VersionView,
+)
+from .api import api
+from .frontend import SpkrepoConfirmRegisterForm, frontend
+from .nas import nas
