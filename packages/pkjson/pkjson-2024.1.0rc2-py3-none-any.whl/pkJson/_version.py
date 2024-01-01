0e3d01a91b9b5041bc04e25ca295dc2f45fbec08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:05:21 +0000

    Add package pkjson/2024.1.0rc2/pkjson-2024.1.0rc2-py3-none-any.whl

---
+++ packages/pkjson/pkjson-2024.1.0rc2-py3-none-any.whl/pkJson/_version.py
@@ -0,0 +1,10 @@
+"""
+Provides `pkJson` version information.
+"""
+# This file is auto-generated! Do not edit!
+# Use `seVersion pkJson` to change this file.
+
+from seVersion import Version
+__version__ = Version("pkJson", 2024, 1, 0, 0, release_candidate=2)
+__setuptools__ = __version__.public()
+__all__ = ["__version__", "__setuptools__"]
