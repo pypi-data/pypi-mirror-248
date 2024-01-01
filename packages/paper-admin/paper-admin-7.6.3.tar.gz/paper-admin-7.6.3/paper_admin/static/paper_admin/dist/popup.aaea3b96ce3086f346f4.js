26b7e8e443f5e4ff991d387195b3934bbfd36058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:05:38 +0000

    Add package paper-admin/7.6.3/paper-admin-7.6.3.tar.gz

---
+++ packages/paper-admin/paper-admin-7.6.3.tar.gz/paper-admin-7.6.3/paper_admin/static/paper_admin/dist/popup.aaea3b96ce3086f346f4.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpaper_admin=self.webpackChunkpaper_admin||[]).push([[42],{7106:(e,t,n)=>{if(n.r(t),/(add|change)\/?$/.test(window.location.pathname)){const e=document.querySelector(".paper-form");if(e){const t=e.querySelector("input:not([type=hidden]), select, textarea");t&&t.focus()}}document.addEventListener("keydown",(e=>{"Escape"===e.code&&window.close()}))}}]);
+//# sourceMappingURL=popup.aaea3b96ce3086f346f4.js.map
