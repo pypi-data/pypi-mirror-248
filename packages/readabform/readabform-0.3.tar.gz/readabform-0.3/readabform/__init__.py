5fe1e669ba98a4a2ddae7fae7394b658a7f59a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 03:04:55 +0000

    Add package readabform/0.3/readabform-0.3.tar.gz

---
+++ packages/readabform/readabform-0.3.tar.gz/readabform-0.3/readabform/__init__.py
@@ -0,0 +1,7 @@
+from .main import ReadabForm
+from .main import abbreviations
+from .main import abbrs
+from .main import readabformat
+from .main import unreadabform
+from .main import form
+from .main import unform
