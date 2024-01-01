50363e9e073936f46b5a0d9e8cddd36f7689d4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-01 14:04:59 +0000

    Add package django-better-auth-oidc/0.2.0/django_better_auth_oidc-0.2.0-py2.py3-none-any.whl

---
+++ packages/django-better-auth-oidc/django_better_auth_oidc-0.2.0-py2.py3-none-any.whl/django_better_auth_oidc/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'django_better_auth_oidc'
+urlpatterns = [
+	path('', views.login, name='login'),
+	path('done/', views.callback, name='login-done'),
+	path('logout/', views.logout, name='logout'),
+]
