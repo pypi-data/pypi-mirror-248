4cb63207ed0aeaa07a99ce42f1472db25b3a7c07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 03:07:03 +0000

    Add package djream/0.1a3/djream-0.1a3.tar.gz

---
+++ packages/djream/djream-0.1a3.tar.gz/djream-0.1a3/djream/decorators.py
@@ -0,0 +1,81 @@
+import json
+from functools import wraps
+from pathlib import Path
+
+from django.conf import settings
+from django.shortcuts import render
+
+from .response import (BaseDjreamResponse, DjreamLoadItResponse,
+                       DjreamRedirectResponse)
+
+
+def djream_view(fn):
+    """
+    Wraps a view to make it load with Djream
+    """
+
+    @wraps(fn)
+    def wrapper(request, *args, **kwargs):
+        response = fn(request, *args, **kwargs)
+
+        if response.status_code == 301:
+            return response
+
+        # If the request was made by Djream
+        # (using `fetch()`, rather than a regular browser request)
+        if request.META.get("HTTP_X_REQUESTED_WITH") == "Djream":
+            if isinstance(response, BaseDjreamResponse):
+                return response
+
+            elif response.status_code == 302:
+                return DjreamRedirectResponse(response["Location"])
+
+            else:
+                # Response couldn't be converted into a Djream response. Reload the page
+                return DjreamLoadItResponse()
+
+        # Regular browser request
+        # If the response is a Djream response, wrap it in our bootstrap template
+        # to load the React SPA and render the response data.
+        if isinstance(response, BaseDjreamResponse):
+            if settings.DJREAM_VITE_SERVER_ORIGIN:
+                # Development - Fetch JS/CSS from Vite server
+                js = [
+                    settings.DJREAM_VITE_SERVER_ORIGIN + "/@vite/client",
+                    settings.DJREAM_VITE_SERVER_ORIGIN + "/static/src/main.tsx",
+                ]
+                css = []
+                vite_react_refresh_runtime = (
+                    settings.DJREAM_VITE_SERVER_ORIGIN + "/@react-refresh"
+                )
+            else:
+                # Production - Use asset manifest to find URLs to bundled JS/CSS
+                asset_manifest = json.loads(Path("/client/manifest.json").read_text())
+
+                js = [
+                    "/static/" + asset_manifest["src/main.tsx"]["file"],
+                ]
+                css = asset_manifest["src/main.tsx"]["css"]
+                vite_react_refresh_runtime = None
+
+            # Wrap the response with our bootstrap template
+            new_response = render(
+                request,
+                "djream/bootstrap.html",
+                {
+                    "data": response.content.decode("utf-8"),
+                    "js": js,
+                    "css": css,
+                    "vite_react_refresh_runtime": vite_react_refresh_runtime,
+                },
+            )
+
+            # Copy status_code and cookies from the original response
+            new_response.status_code = response.status_code
+            new_response.cookies = response.cookies
+
+            return new_response
+
+        return response
+
+    return wrapper
