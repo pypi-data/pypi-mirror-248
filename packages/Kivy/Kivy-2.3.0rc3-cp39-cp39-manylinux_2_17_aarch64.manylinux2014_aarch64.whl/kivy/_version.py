050de85ecc3e96fb7f2e50fe05f5f50e02c79a6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 03:06:25 +0000

    Add package Kivy/2.3.0rc3/Kivy-2.3.0rc3-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/Kivy/Kivy-2.3.0rc3-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/kivy/_version.py
@@ -0,0 +1,17 @@
+# This file is imported from __init__.py and exec'd from setup.py
+
+MAJOR = 2
+MINOR = 3
+MICRO = 0
+RELEASE = False
+
+__version__ = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+if not RELEASE:
+    # if it's a rcx release, it's not proceeded by a period. If it is a
+    # devx release, it must start with a period
+    __version__ += 'rc3'
+
+
+_kivy_git_hash = 'c1d89471bb60a3b19fa017e2df9fe64ad0bd67a0'
+_kivy_build_date = '20240101'
