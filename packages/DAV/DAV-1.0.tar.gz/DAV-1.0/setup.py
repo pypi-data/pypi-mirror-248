35240f5a87e27376bf70f7fbd5b648de1d72c4c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-01-02 03:06:28 +0000

    Add package DAV/1.0/DAV-1.0.tar.gz

---
+++ packages/DAV/DAV-1.0.tar.gz/DAV-1.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+
+'''
+    install_requires=[
+        'DataVisualization.exe',
+        'glewinfo.exe',
+        'visualinfo.exe'
+        'DataVisualization.exp',
+        'DataVisualization.lib',
+        'DataVisualization.pdb',
+        'freeglut.dll',
+        'glew32.dll',
+        'glfw3.dll',
+        'imgui.ini',
+        'MANIFEST.in',
+        'openal32.dll',
+        'BackgroundFragment.glsl',
+        'SpheresFragment.glsl',
+        'SpheresVertex.glsl',
+        'UserPrefs.txt'
+    ],
+'''
+
+
+setup(
+    name='DAV',
+    version='1.0',
+    include_package_data=True,
+    packages=find_packages(),
+    
+)
+
+
